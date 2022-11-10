ca0b724ba76f94dade5edc7e166e67fa8199ebc9
KevinX <Xzb04720638k>
2022-11-09 20:42:23 -0500

    update code

---
+++ src/main/java/com/leetcode/Test15_Math.java
@@ -0,0 +1,10 @@
+package com.leetcode;
+
+public class Test15_Math {
+
+    public static void main(String[] args) {
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+}
