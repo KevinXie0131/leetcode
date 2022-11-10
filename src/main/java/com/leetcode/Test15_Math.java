e5df0408956a70866f67c7851ac09047c50e501c
KevinX <Xzb04720638k>
2022-11-10 13:39:39 -0500

    update code

--- src/main/java/com/leetcode/Test15_Math.java
+++ src/main/java/com/leetcode/Test15_Math.java
@@ -6,5 +6,25 @@ public class Test15_Math {
 
         System.out.println(Integer.MAX_VALUE);
         System.out.println(Integer.MIN_VALUE);
+
+        System.out.println(Math.pow(2, 5));
+
+        System.out.println(Math.max(2, 3));
+        System.out.println(Math.min(2, 3));
+
+        System.out.println(Math.ceil(4.3));
+        System.out.println(Math.floor(5.4));
+
+        System.out.println(Math.round(7.6));
+        System.out.println(Math.round(7.3));
+
+        int a = 8;
+        int b = 12;
+        System.out.println("a & b -> " + (a & b));
+        System.out.println("a | b -> " + (a | b));
+        System.out.println("~a -> " + (~a));
+        System.out.println("a >> 1 -> " + (a >> 1));
+        System.out.println("a % 3 -> " + (a % 3));
+
     }
 }
