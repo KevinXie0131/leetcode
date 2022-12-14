8448df094acbf15ac975d2f67d279e0898cbb9fd
KevinX <Xzb04720638k>
2022-12-14 11:26:16 -0500

    update code

---
+++ src/main/java/com/leetcode/Test17_Character.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+public class Test17_Character {
+
+    public static void main(String[] args) {
+
+        System.out.println(Character.isDigit('a'));
+        System.out.println(Character.isDigit('1'));
+
+        System.out.println(Character.isLetter('a'));
+        System.out.println(Character.isLetter('1'));
+    }
+}
