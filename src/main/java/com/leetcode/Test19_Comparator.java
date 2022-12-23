9e8ad2d3bce7693ab9db017b5e0d3127393eb91f
KevinX <Xzb04720638k>
2022-12-22 22:35:00 -0500

    update code

--- src/main/java/com/leetcode/Test19_Comparator.java
+++ src/main/java/com/leetcode/Test19_Comparator.java
@@ -3,7 +3,7 @@ package com.leetcode;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
-public class Comparator {
+public class Test19_Comparator {
 
     public static void main(String[] args) {
 
