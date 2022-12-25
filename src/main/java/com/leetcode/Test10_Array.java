ca63632f28eee9f2040d36492205b5c153a5b81d
KevinX <Xzb04720638k>
2022-12-25 17:58:28 -0500

    update code

--- src/main/java/com/leetcode/Test10_Array.java
+++ src/main/java/com/leetcode/Test10_Array.java
@@ -8,6 +8,9 @@ public class Test10_Array {
 
     public static void main(String[] args) {
 
+        int[] array0 = new int[] {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(array0));
+
         int[] array1 = new int[5];
         Arrays.fill(array1, -1);
         for (int arr : array1) {
