b4db434fb0472123bbb21ad75e32cdb3002fa6e2
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2022-12-31 15:10:31 -0500

    update code

--- src/main/java/com/leetcode/Test21_StringBuilder.java
+++ src/main/java/com/leetcode/Test21_StringBuilder.java
@@ -14,6 +14,29 @@ public class Test21_StringBuilder {
         StringBuilder str = new StringBuilder("saa");
         str.setLength(10);
         System.out.print(str);
-        System.out.print(str);
+        System.out.println(str);
+
+        StringBuilder str1 = new StringBuilder("abcdefg");
+        str1.setCharAt(2, 'C');
+        System.out.println(str1);
+
+        str1.insert(3, 'X');
+        System.out.println(str1);
+
+        str1.deleteCharAt(3);
+        System.out.println(str1);
+
+        str1.replace(4, 6, "E");
+        System.out.println(str1);
+
+        str1.append('G');
+        str1.append("HI");
+        System.out.println(str1);
+
+        str1.reverse();
+        System.out.println(str1);
+
+        System.out.println(str1.length());
+        System.out.println(str1.toString().toCharArray());
     }
 }
