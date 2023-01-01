6e17d301c848b33e40319b5744c03c4d667f0291
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2022-12-31 22:06:23 -0500

    update code

---
+++ src/main/java/com/template/Template4_Monotone_queue.java
@@ -0,0 +1,36 @@
+package com.template;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Template4_Monotone_queue {
+    public static void main(String[] args) {
+
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+        int n = nums.length;
+
+        Deque<Integer> deque = new LinkedList<Integer>();
+
+        for (int i = 0; i < n; ++i) {
+            while (!deque.isEmpty() && nums[i] >= deque.peekLast()) {
+                deque.pollLast();
+                System.out.println(deque);
+            }
+            deque.offerLast(nums[i]);
+            System.out.println(deque);
+        }
+
+/*
+        for (int i = k; i < n; ++i) {
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            while (deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+        }*/
+
+    }
+}
