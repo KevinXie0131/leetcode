a4168190cc50ce11c51bac3ec5b4cf004f543214
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2022-12-31 20:09:47 -0500

    update code

--- src/main/java/com/leetcode/Test2_reverseList.java
+++ src/main/java/com/leetcode/Test2_reverseList.java
@@ -7,10 +7,12 @@ public class Test2_reverseList {
         ListNode node1 = new ListNode(1);
         ListNode node2 = new ListNode(2);
         ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
 
         node1.next = node2;
         node2.next = node3;
-        node3.next = null;
+        node3.next = node4;
+        node4.next = null;
 
         ListNode current = node1;
        while (current != null)
@@ -21,7 +23,8 @@ public class Test2_reverseList {
 
      //   ListNode current1 = reverseList(node1);
      //   ListNode current1 = reverseListIterate(node1);
-        ListNode current1 = reverseListNew(node1, null);
+    //    ListNode current1 = reverseListNew(node1, null);
+        ListNode current1 = swapPairs(node1);
         while (current1 != null)
         {
             System.out.println(current1.value);
@@ -58,5 +61,24 @@ public class Test2_reverseList {
         ListNode next = cur.next;
         cur.next = pre;
         return reverseListNew(next, cur);
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+
+        // If the list has no node or has only one node left.
+        if ((head == null) || (head.next == null)) {
+            return head;
+        }
+
+        // Nodes to be swapped
+        ListNode firstNode = head;
+        ListNode secondNode = head.next;
+
+        // Swapping
+        firstNode.next  = swapPairs(secondNode.next);
+        secondNode.next = firstNode;
+
+        // Now the head is the second node
+        return secondNode;
     }
 }
