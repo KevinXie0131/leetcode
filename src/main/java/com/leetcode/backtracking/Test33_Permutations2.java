fcc3b549f75ec0acbf8a41dcc3f8449405eeac37
KevinX <Xzb04720638k>
2023-01-11 22:29:05 -0500

    update code

---
+++ src/main/java/com/leetcode/backtracking/Test33_Permutations2.java
@@ -0,0 +1,41 @@
+package com.leetcode.backtracking;
+
+import java.util.*;
+
+public class Test33_Permutations2 {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+
+        List<List<Integer>> result = permute(nums);
+        System.out.println(result);
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        List<Integer> output = new ArrayList<Integer>();
+        for (int num : nums) {
+            output.add(num);
+        }
+
+        int n = nums.length;
+        backtrack(n, output, res, 0);
+        return res;
+    }
+
+    public static void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+        // 所有数都填完了
+        if (first == n) {
+            res.add(new ArrayList<Integer>(output));
+        }
+        for (int i = first; i < n; i++) {
+            // 动态维护数组
+            Collections.swap(output, first, i);
+            // 继续递归填下一个数
+            backtrack(n, output, res, first + 1);
+            // 撤销操作
+            Collections.swap(output, first, i);
+        }
+    }
+}
