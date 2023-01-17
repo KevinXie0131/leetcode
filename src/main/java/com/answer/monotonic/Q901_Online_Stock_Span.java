90e8b0103b857bad229004f32eb969e34aaae4be
KevinX <Xzb04720638k>
2023-01-16 23:23:00 -0500

    update code

--- src/main/java/com/answer/monotonic/Q901_Online_Stock_Span.java
+++ src/main/java/com/answer/monotonic/Q901_Online_Stock_Span.java
@@ -15,6 +15,17 @@ public class Q901_Online_Stock_Span {
         System.out.println(stockSpanner.next(75));  // return 4, because the last 4 prices (including today's price of 75) were less than or equal to today's price.
         System.out.println(stockSpanner.next(85));  // return 6
         System.out.println(stockSpanner.next(110)); // return 8
+
+        StockSpanner_1 stockSpanner_1 = new StockSpanner_1();
+
+        System.out.println(stockSpanner_1.next(100)); // return 1
+        System.out.println(stockSpanner_1.next(80));  // return 1
+        System.out.println(stockSpanner_1.next(60));  // return 1
+        System.out.println(stockSpanner_1.next(70));  // return 2
+        System.out.println(stockSpanner_1.next(60));  // return 1
+        System.out.println(stockSpanner_1.next(75));  // return 4, because the last 4 prices (including today's price of 75) were less than or equal to today's price.
+        System.out.println(stockSpanner_1.next(85));  // return 6
+        System.out.println(stockSpanner_1.next(110)); // return 8
     }
 }
 
@@ -40,4 +51,30 @@ class StockSpanner{
 
         return result;
     }
+}
+
+class StockSpanner_1{
+        Deque<int[]> stack = new ArrayDeque<>();
+        int cur = 0;
+
+        public StockSpanner_1() {
+        }
+
+        public int next(int price) {
+            while (!stack.isEmpty() && stack.peek()[1] <= price) {
+                stack.pop();
+            }
+            int prev;
+            int ans = 0;
+            if(stack.isEmpty()){
+                prev = -1;
+            }else{
+                prev = stack.peek()[0];
+            }
+            ans = cur - prev;
+
+            stack.push(new int[]{cur++, price});
+
+            return ans;
+        }
 }
