2802816846ae0106a306a8a8bb89403ca5bd3114
KevinX <Xzb04720638k>
2023-01-18 15:18:05 -0500

    update code

--- src/main/java/com/answer/string/Q344_Reverse_String.java
+++ src/main/java/com/answer/string/Q344_Reverse_String.java
@@ -1,4 +1,39 @@
 package com.answer.string;
 
 public class Q344_Reverse_String {
+
+    /**
+     * Approach 1: Recursion, In-Place, O(N) Space
+     */
+    public void reverseString(char[] s) {
+        int right = s.length - 1;
+        int left = 0;
+
+        recursion(s, left, right);
+    }
+
+    public void recursion(char[] s, int left, int right){
+        if(left >= right)return;
+
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        recursion(s, left+1, right-1);
+    }
+
+    /**
+     * Approach 2: Two Pointers, Iteration, O(1) Space
+     */
+    public void reverseString_1(char[] s) {
+        int right = s.length - 1;
+        int left = 0;
+        while(left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
 }
