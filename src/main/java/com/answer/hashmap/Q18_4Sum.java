1e20cd7ad3240a3cfb8d8e1a4fd43d3ecf2d30f7
KevinX <Xzb04720638k>
2023-01-17 22:49:07 -0500

    update code

--- src/main/java/com/answer/hashmap/Q18_4Sum.java
+++ src/main/java/com/answer/hashmap/Q18_4Sum.java
@@ -1,4 +1,54 @@
 package com.answer.hashmap;
 
+import java.util.*;
+
 public class Q18_4Sum {
+
+    public static void main(String[] args) {
+        int[] nums = {1,0,-1,0,-2,2};
+        int target = 0;
+        List<List<Integer>>  res = fourSum(nums, target);
+        System.out.println(res);
+    }
+
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        int n = nums.length;
+        if(n<4)return result;
+
+        Arrays.sort(nums);
+        for(int i = 0; i< n;i++){
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
+
+            for(int j = i+1; j< n;j++){
+                if(j > i && nums[j] == nums[j-1]){
+                    continue;
+                }
+                int left = j + 1;
+                int right = n-1;
+                while(left < right){
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if(sum < target){
+                        left++;
+                    } else if(sum > target){
+                        right--;
+                    } else {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left] , nums[right]));
+                        while(left<right && nums[left]==nums[left+1]){
+                            left++;
+                        }
+                        while(left<right && nums[right]==nums[right-1]){
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
 }
---
+++ src/main/java/com/answer/hashmap/Q349_Intersection_of_Two_Arrays.java
@@ -0,0 +1,4 @@
+package com.answer.hashmap;
+
+public class Q349_Intersection_of_Two_Arrays {
+}
