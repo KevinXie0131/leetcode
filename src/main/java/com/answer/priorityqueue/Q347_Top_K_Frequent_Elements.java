74fc8cf1c3c41af630d7b4ba058ba6e436ad4730
KevinX <Xzb04720638k>
2023-01-20 18:38:32 -0500

    update code

--- src/main/java/com/answer/priorityqueue/Q347_Top_K_Frequent_Elements.java
+++ src/main/java/com/answer/priorityqueue/Q347_Top_K_Frequent_Elements.java
@@ -8,10 +8,13 @@ public class Q347_Top_K_Frequent_Elements {
         int[] nums = {1,1,1,2,2,3};
         int k = 2;
 
-        int[] result = topKFrequent(nums, k);
+        int[] result = topKFrequent_1(nums, k);
         System.out.println(Arrays.toString(result));
     }
 
+    /**
+     * 大顶堆 max heap
+     */
     public static int[] topKFrequent(int[] nums, int k) {
         int[] result = new int[k];
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -29,6 +32,42 @@ public class Q347_Top_K_Frequent_Elements {
 
         for(Map.Entry<Integer, Integer> entry : entries){
             queue.offer(entry);
+        }
+
+        for(int i = k-1; i >= 0; i--){
+            result[i] = queue.poll().getKey();
+        }
+
+        return result;
+    }
+    /**
+     * 小顶堆 min heap
+     */
+    public static int[] topKFrequent_1(int[] nums, int k) {
+        int[] result = new int[k];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
+
+        /**
+         * 根据map的value值，构建于一个大顶堆（o1 - o2: 小顶堆， o2 - o1 : 大顶堆）
+         */
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((o1, o2) -> o1.getValue() - o2.getValue());
+
+        for(Map.Entry<Integer, Integer> entry : entries){
+            if(queue.size() == k){
+                if(queue.peek().getValue() < entry.getValue()){
+                    queue.poll();
+                    queue.offer(entry);
+                }
+            } else {
+                queue.offer(entry);
+            }
+
         }
 
         for(int i = k-1; i >= 0; i--){
