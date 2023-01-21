7965c9db39224ea92b578c2f400e6d960da36890
KevinX <Xzb04720638k>
2023-01-20 23:44:30 -0500

    update code

---
+++ src/main/java/com/answer/priorityqueue/Q215_Kth_Largest_Element_in_an_Array.java
@@ -0,0 +1,44 @@
+package com.answer.priorityqueue;
+
+import java.util.PriorityQueue;
+
+public class Q215_Kth_Largest_Element_in_an_Array {
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        int k = 2;
+
+        System.out.println(findKthLargest_1(nums, k));
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue();
+
+        for(int i = 0; i < nums.length; i++){
+            queue.offer(nums[i]);
+        }
+
+        for(int i = 0; i < nums.length - k; i++){
+            queue.poll();
+        }
+        return queue.poll();
+    }
+    /**
+     *
+     */
+    public static int findKthLargest_1(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue();
+
+        for(int i = 0; i < k; i++){
+            queue.offer(nums[i]);
+        }
+
+        for(int i = k; i < nums.length; i++){
+            if(nums[i] > queue.peek()){
+                queue.poll();
+                queue.offer(nums[i]);
+            }
+        }
+        return queue.peek();
+    }
+}
