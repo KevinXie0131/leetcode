4386c3f3b5caf71010f742e95c640fbf259ff4af
KevinX <Xzb04720638k>
2023-01-20 23:11:51 -0500

    update code

--- src/main/java/com/answer/priorityqueue/Offer_40_Least_K_Numbers.java
+++ src/main/java/com/answer/priorityqueue/Offer_40_Least_K_Numbers.java
@@ -3,14 +3,15 @@ package com.answer.priorityqueue;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.*;
 
 public class Offer_40_Least_K_Numbers {
 
     public static void main(String[] args) {
-        int[] arr = {3,2,1};
+        int[] arr = {3,2, 3, 2, 2 ,1 ,1, 4, 4, 5};
         int k = 2;
 
-        int[] result = getLeastNumbers(arr, k);
+        int[] result = getLeastNumbers_1(arr, k);
         System.out.println(Arrays.toString(result));
     }
 
@@ -36,5 +37,50 @@ public class Offer_40_Least_K_Numbers {
         }
 
         return result;
+    }
+
+    /**
+     * Treemap
+     */
+    public static int[] getLeastNumbers_1(int[] arr, int k) {
+        if (k == 0 || arr.length == 0) {
+            return new int[0];
+        }
+        // TreeMap的key是数字, value是该数字的个数。
+        // cnt表示当前map总共存了多少个数字。
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int cnt = 0;
+        for (int num: arr) {
+            // 1. 遍历数组，若当前map中的数字个数小于k，则map中当前数字对应个数+1
+            if (cnt < k) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+                cnt++;
+                continue;
+            }
+            // 2. 否则，取出map中最大的Key（即最大的数字), 判断当前数字与map中最大数字的大小关系：
+            //    若当前数字比map中最大的数字还大，就直接忽略；
+            //    若当前数字比map中最大的数字小，则将当前数字加入map中，并将map中的最大数字的个数-1。
+            Map.Entry<Integer, Integer> entry = map.lastEntry();
+            if (entry.getKey() > num) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+                if (entry.getValue() == 1) {
+                    map.pollLastEntry();
+                } else {
+                    map.put(entry.getKey(), entry.getValue() - 1);
+                }
+            }
+
+        }
+
+        // 最后返回map中的元素
+        int[] res = new int[k];
+        int idx = 0;
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            int freq = entry.getValue();
+            while (freq-- > 0) {
+                res[idx++] = entry.getKey();
+            }
+        }
+        return res;
     }
 }
