a29b5ad26f7236693bb84a70ef55066eba12d546
KevinX <Xzb04720638k>
2023-01-21 22:19:22 -0500

    update code

---
+++ src/main/java/com/answer/array/Q153_Find_Minimum_in_Rotated_Sorted_Array.java
@@ -0,0 +1,36 @@
+package com.answer.array;
+
+public class Q153_Find_Minimum_in_Rotated_Sorted_Array {
+
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < nums[high]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return nums[low];
+
+    }
+    /**
+     *
+     */
+    public int findMin_1(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
