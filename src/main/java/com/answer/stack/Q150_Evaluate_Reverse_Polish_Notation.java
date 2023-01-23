23e697e1748651709296c32bd984662f49ec73ea
KevinX <Xzb04720638k>
2023-01-22 22:37:05 -0500

    update code

--- src/main/java/com/answer/stack/Q150_Evaluate_Reverse_Polish_Notation.java
+++ src/main/java/com/answer/stack/Q150_Evaluate_Reverse_Polish_Notation.java
@@ -24,4 +24,32 @@ public class Q150_Evaluate_Reverse_Polish_Notation {
 
         return stack.pop();
     }
+
+    public int evalRPN_1(String[] tokens) {
+        int n = tokens.length;
+        int[] stack = new int[(n + 1) / 2];
+        int index = -1;
+        for(String s : tokens){
+            if(s.equals("+")){
+                stack[index-1] = stack[index-1] + stack[index];
+                index--;
+            }else if(s.equals("-")){
+                stack[index-1] = stack[index-1] - stack[index];
+                index--;
+            }else if(s.equals("*")){
+                stack[index-1] = stack[index-1] * stack[index];
+                index--;
+            }else if(s.equals("/")){
+                int temp1 = stack[index];
+                int temp2 = stack[index-1];
+                stack[index-1] = temp2 / temp1;
+                index--;
+            } else{
+                index++;
+                stack[index] = Integer.valueOf(s);
+            }
+        }
+
+        return stack[index];
+    }
 }
