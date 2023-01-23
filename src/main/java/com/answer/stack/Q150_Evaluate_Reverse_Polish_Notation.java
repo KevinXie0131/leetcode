7d0fc02cfb18ed82307157f21562cc6525f63156
KevinX <Xzb04720638k>
2023-01-22 22:31:39 -0500

    update code

--- src/main/java/com/answer/stack/Q150_Evaluate_Reverse_Polish_Notation.java
+++ src/main/java/com/answer/stack/Q150_Evaluate_Reverse_Polish_Notation.java
@@ -1,4 +1,27 @@
 package com.answer.stack;
 
+import java.util.*;
+
 public class Q150_Evaluate_Reverse_Polish_Notation {
+
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(String s : tokens){
+            if(s.equals("+")){
+                stack.push(stack.pop() + stack.pop());
+            }else if(s.equals("-")){
+                stack.push(-stack.pop() + stack.pop());
+            }else if(s.equals("*")){
+                stack.push(stack.pop() * stack.pop());
+            }else if(s.equals("/")){
+                int temp1 = stack.pop();
+                int temp2 = stack.pop();
+                stack.push(temp2 / temp1);
+            } else{
+                stack.push(Integer.valueOf(s));
+            }
+        }
+
+        return stack.pop();
+    }
 }
