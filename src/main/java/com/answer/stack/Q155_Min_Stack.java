c77125e5ee76e212399ce82e9b7b428371cf6c04
KevinX <Xzb04720638k>
2023-01-22 23:15:05 -0500

    update code

--- src/main/java/com/answer/stack/Q155_Min_Stack.java
+++ src/main/java/com/answer/stack/Q155_Min_Stack.java
@@ -1,4 +1,36 @@
 package com.answer.stack;
 
+import java.util.*;
+
 public class Q155_Min_Stack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public Q155_Min_Stack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int val) {
+        stack.push(val);
+
+        int top = minStack.peek();
+        minStack.push(Math.min(top, val));
+
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
 }
