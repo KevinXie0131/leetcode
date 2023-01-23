a59775691c2bf7bc6bed40cb78b651b15bd160d2
KevinX <Xzb04720638k>
2023-01-22 23:55:36 -0500

    update code

--- src/main/java/com/answer/stack/Q232_Implement_Queue_using_Stacks.java
+++ src/main/java/com/answer/stack/Q232_Implement_Queue_using_Stacks.java
@@ -1,4 +1,40 @@
 package com.answer.stack;
 
+import java.util.*;
+
 public class Q232_Implement_Queue_using_Stacks {
+
+    Deque<Integer> stackIn;
+    Deque<Integer> stackOut;
+
+    public Q232_Implement_Queue_using_Stacks() {
+        stackIn = new ArrayDeque<>();
+        stackOut = new ArrayDeque<>();
+    }
+
+    public void push(int x) {
+        stackIn.push(x);
+    }
+
+    public int pop() {
+        if(stackOut.isEmpty()){
+            while(!stackIn.isEmpty()){
+                stackOut.push(stackIn.pop());
+            }
+        }
+        return stackOut.pop();
+    }
+
+    public int peek() {
+        if(stackOut.isEmpty()){
+            while(!stackIn.isEmpty()){
+                stackOut.push(stackIn.pop());
+            }
+        }
+        return stackOut.peek();
+    }
+
+    public boolean empty() {
+        return stackOut.isEmpty() && stackIn.isEmpty();
+    }
 }
