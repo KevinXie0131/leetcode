0a8e310b8fac485e82112b94e1ddd8b9f54111a8
KevinX <Xzb04720638k>
2023-01-24 21:18:00 -0500

    update code

--- src/main/java/com/answer/linkedlist/Q876_Middle_of_the_Linked_List.java
+++ src/main/java/com/answer/linkedlist/Q876_Middle_of_the_Linked_List.java
@@ -1,4 +1,18 @@
 package com.answer.linkedlist;
 
 public class Q876_Middle_of_the_Linked_List {
+
+    /**
+     * fast-slow pointers
+     */
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
 }
