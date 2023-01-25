b87463d09dd2d3b86c12164549cf6843a3459b89
KevinX <Xzb04720638k>
2023-01-24 22:32:47 -0500

    update code

--- src/main/java/com/answer/linkedlist/Q142_Linked_List_Cycle_II.java
+++ src/main/java/com/answer/linkedlist/Q142_Linked_List_Cycle_II.java
@@ -1,4 +1,28 @@
 package com.answer.linkedlist;
 
 public class Q142_Linked_List_Cycle_II {
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1, null);
+        ListNode result =  detectCycle(head);
+        System.out.println(result);
+    }
+
+    public static ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (true) {
+            if (fast == null || fast.next == null) return null;
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) break;
+        }
+        fast = head;
+        while(fast != slow){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return fast;
+    }
 }
