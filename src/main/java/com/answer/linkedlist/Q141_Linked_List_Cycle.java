342e679d09a2e78128ddbf770805fdff08793b53
KevinX <Xzb04720638k>
2023-01-24 22:16:26 -0500

    update code

--- src/main/java/com/answer/linkedlist/Q141_Linked_List_Cycle.java
+++ src/main/java/com/answer/linkedlist/Q141_Linked_List_Cycle.java
@@ -1,4 +1,19 @@
 package com.answer.linkedlist;
 
 public class Q141_Linked_List_Cycle {
+
+    /**
+     * fast-slow pointers
+     */
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
 }
