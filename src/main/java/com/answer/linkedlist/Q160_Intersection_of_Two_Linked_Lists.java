1a0da1d67d6aa3203e4915aaaf3790c545c35bcf
KevinX <Xzb04720638k>
2023-01-24 22:51:13 -0500

    update code

--- src/main/java/com/answer/linkedlist/Q160_Intersection_of_Two_Linked_Lists.java
+++ src/main/java/com/answer/linkedlist/Q160_Intersection_of_Two_Linked_Lists.java
@@ -1,4 +1,27 @@
 package com.answer.linkedlist;
 
 public class Q160_Intersection_of_Two_Linked_Lists {
+
+    /**
+     * two pointers
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        while(curA != curB){
+            if(curA != null){
+                curA = curA.next;
+            } else {
+                curA = headB;
+            }
+
+            if(curB != null){
+                curB = curB.next;
+            } else {
+                curB = headA;
+            }
+        }
+
+        return curA;
+    }
 }
