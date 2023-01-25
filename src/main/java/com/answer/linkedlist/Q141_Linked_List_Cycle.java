3cb0745f4151f8d95da3e851152f68c8b85393b4
KevinX <Xzb04720638k>
2023-01-24 22:19:47 -0500

    update code

--- src/main/java/com/answer/linkedlist/Q141_Linked_List_Cycle.java
+++ src/main/java/com/answer/linkedlist/Q141_Linked_List_Cycle.java
@@ -1,5 +1,8 @@
 package com.answer.linkedlist;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Q141_Linked_List_Cycle {
 
     /**
@@ -13,6 +16,19 @@ public class Q141_Linked_List_Cycle {
             if(fast == slow){
                 return true;
             }
+        }
+        return false;
+    }
+    /**
+     * Use set
+     */
+    public boolean hasCycle_1(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while(head != null){
+            if(!set.add(head)){
+                return true;
+            }
+            head = head.next;
         }
         return false;
     }
