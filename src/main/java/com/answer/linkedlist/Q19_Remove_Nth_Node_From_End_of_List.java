defdbc1f4c1fd938e114cea71bfc665d0583ee65
KevinX <Xzb04720638k>
2023-01-24 21:45:03 -0500

    update code

--- src/main/java/com/answer/linkedlist/Q19_Remove_Nth_Node_From_End_of_List.java
+++ src/main/java/com/answer/linkedlist/Q19_Remove_Nth_Node_From_End_of_List.java
@@ -1,4 +1,23 @@
 package com.answer.linkedlist;
 
 public class Q19_Remove_Nth_Node_From_End_of_List {
+
+    /**
+     * fast-slow pointers
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode (-1, head);
+        ListNode slow = dummy;
+        ListNode fast = head;
+        for(int i = 0; i < n; i++){
+            fast = fast.next;
+        }
+        while(fast != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
 }
