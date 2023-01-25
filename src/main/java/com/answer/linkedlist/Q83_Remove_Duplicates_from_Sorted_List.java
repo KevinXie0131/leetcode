4f02303b263d6bdeeb0625808cca3d0665510e09
KevinX <Xzb04720638k>
2023-01-25 13:11:13 -0500

    update code

--- src/main/java/com/answer/linkedlist/Q83_Remove_Duplicates_from_Sorted_List.java
+++ src/main/java/com/answer/linkedlist/Q83_Remove_Duplicates_from_Sorted_List.java
@@ -1,4 +1,21 @@
 package com.answer.linkedlist;
 
 public class Q83_Remove_Duplicates_from_Sorted_List {
+
+    /**
+     * Iterative
+     * Approach 1: Straight-Forward Approach
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur = head;
+        while(cur != null && cur.next != null){
+            if(cur.val == cur.next.val){
+                cur.next = cur.next.next;
+            }else{
+                cur = cur.next;
+            }
+        }
+
+        return head;
+    }
 }
