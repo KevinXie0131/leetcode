909c1bf505a02b678eb66df4d19d102d483a023d
KevinX <Xzb04720638k>
2023-01-25 21:48:09 -0500

    update code

--- src/main/java/com/answer/linkedlist/Q2_Add_Two_Numbers.java
+++ src/main/java/com/answer/linkedlist/Q2_Add_Two_Numbers.java
@@ -1,4 +1,31 @@
 package com.answer.linkedlist;
 
 public class Q2_Add_Two_Numbers {
+
+    /**
+     * Iterative
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1, null);
+        ListNode cur = dummy;
+        int carry = 0;
+        while(l1 != null || l2 != null){
+            int v1 = l1 == null ? 0 : l1.val;
+            int v2 = l2 == null ? 0 : l2.val;
+
+            int sum = v1 + v2 + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+
+            cur.next = new ListNode(sum, null);
+            cur = cur.next;
+
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }
+        if(carry > 0){
+            cur.next = new ListNode(carry, null);
+        }
+        return dummy.next;
+    }
 }
