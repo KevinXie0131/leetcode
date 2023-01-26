5a41af9e6c40f3697881af58a4738e77791b7632
KevinX <Xzb04720638k>
2023-01-25 22:40:24 -0500

    update code

--- src/main/java/com/answer/linkedlist/Q148_Sort_List.java
+++ src/main/java/com/answer/linkedlist/Q148_Sort_List.java
@@ -1,4 +1,38 @@
 package com.answer.linkedlist;
 
+import java.util.*;
+
 public class Q148_Sort_List {
+
+    public static void main(String[] args) {
+        //head = [1,2,3,4,5]
+      //  ListNode node5 = new ListNode(5, null);
+        ListNode node4 = new ListNode(4, null);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1,node2);
+
+        reorderList(node1);
+        node1.print();
+    }
+
+    public static void reorderList(ListNode head) {
+        List<ListNode> list = new ArrayList<>();
+        ListNode cur = head;
+        while(cur != null){
+            list.add(cur);
+            cur = cur.next;
+        }
+        int i = 0, j = list.size() - 1;
+        while(i < j){
+            list.get(i).next = list.get(j);
+            i++;
+            if(i == j){
+                break;
+            }
+            list.get(j).next = list.get(i);
+            j--;
+        }
+        list.get(i).next = null;
+    }
 }
