6c5104a4bebc77d943fceebfbda73f6eae8989e8
KevinX <Xzb04720638k>
2023-01-25 22:10:48 -0500

    update code

--- src/main/java/com/answer/linkedlist/Q328_Odd_Even_Linked_List.java
+++ src/main/java/com/answer/linkedlist/Q328_Odd_Even_Linked_List.java
@@ -1,4 +1,22 @@
 package com.answer.linkedlist;
 
 public class Q328_Odd_Even_Linked_List {
+
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode odd = head;
+        ListNode evenHead = head.next;
+        ListNode even = evenHead;
+
+        while(even != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
 }
