1f18fe1d59bbae8fc46ca7d275b33c42a69228c4
KevinX <Xzb04720638k>
2023-01-25 20:59:53 -0500

    update code

--- src/main/java/com/answer/linkedlist/Q21_Merge_Two_Sorted_Lists.java
+++ src/main/java/com/answer/linkedlist/Q21_Merge_Two_Sorted_Lists.java
@@ -34,4 +34,26 @@ public class Q21_Merge_Two_Sorted_Lists {
             return list2;
         }
     }
+    /**
+     * Iterative
+     */
+    public ListNode mergeTwoLists_1(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1, null);
+        ListNode cur = dummy;
+
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+
+        cur.next = list1 == null ? list2 : list1;
+
+        return dummy.next;
+    }
 }
