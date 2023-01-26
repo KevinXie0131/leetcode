85c7af204fa444485c113a56a1f4624ab77319bf
KevinX <Xzb04720638k>
2023-01-25 21:57:15 -0500

    update code

--- src/main/java/com/answer/linkedlist/Q2_Add_Two_Numbers.java
+++ src/main/java/com/answer/linkedlist/Q2_Add_Two_Numbers.java
@@ -1,7 +1,19 @@
 package com.answer.linkedlist;
 
 public class Q2_Add_Two_Numbers {
+    public static void main(String[] args) {
+        // l1 = [2,4,3], l2 = [5,6,4]
+        ListNode node3 = new ListNode(4, null);
+        ListNode node2 = new ListNode(6, node3);
+        ListNode node1= new ListNode(5,node2);
 
+        ListNode node3a = new ListNode(3, null);
+        ListNode node2a = new ListNode(4, node3a);
+        ListNode node1a = new ListNode(2,node2a);
+
+        ListNode result = addTwoNumbers_1(node1, node1a);
+        result.print();
+    }
     /**
      * Iterative
      */
@@ -27,5 +39,24 @@ public class Q2_Add_Two_Numbers {
             cur.next = new ListNode(carry, null);
         }
         return dummy.next;
+    }
+    /**
+     * Recursive
+     */
+    public static ListNode addTwoNumbers_1(ListNode l1, ListNode l2) {
+        if(l1 == null) {
+            return l2;
+        }
+        if(l2 == null) {
+            return l1;
+        }
+        int sum = l1.val + l2.val;
+        ListNode head = new ListNode(sum % 10);
+        head.next = addTwoNumbers_1(l1.next, l2.next);
+
+        if(sum > 9) {
+            head.next = addTwoNumbers_1(head.next, new ListNode(1));
+        }
+        return head;
     }
 }
