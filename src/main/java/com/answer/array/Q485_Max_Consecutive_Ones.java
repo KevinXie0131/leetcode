04597eaddfe6024ca064a6f1cb2306d2a0e4d293
KevinX <Xzb04720638k>
2023-01-27 10:56:13 -0500

    update code

--- src/main/java/com/answer/array/Q485_Max_Consecutive_Ones.java
+++ src/main/java/com/answer/array/Q485_Max_Consecutive_Ones.java
@@ -2,6 +2,14 @@ package com.answer.array;
 
 public class Q485_Max_Consecutive_Ones {
 
+    public static void main(String[] args) {
+        int[] nums = {1,1,0,1,1,1};
+        int res = findMaxConsecutiveOnes_1(nums);
+        System.out.println(res);
+    }
+    /**
+     * Brute force
+     */
     public int findMaxConsecutiveOnes(int[] nums) {
         int maxCount = 0, count = 0;
 
@@ -14,6 +22,25 @@ public class Q485_Max_Consecutive_Ones {
             }
         }
         maxCount = Math.max(count, maxCount);
+        return maxCount;
+    }
+    /**
+     * Sliding window
+     */
+    public static int findMaxConsecutiveOnes_1(int[] nums) {
+        int maxCount = 0, left = 0, right = 0;
+        int length = nums.length;
+
+        while(right < length){
+            if(nums[right] == 0){
+                maxCount = Math.max(maxCount, right - left);
+                right++;
+                left = right;
+            } else {
+                right++;
+            }
+        }
+        maxCount = Math.max(maxCount, right - left);
         return maxCount;
     }
 }
