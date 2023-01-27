d38b31bf36d636b35cd75959f3fe8ba98799348d
KevinX <Xzb04720638k>
2023-01-26 19:18:24 -0500

    update code

--- src/main/java/com/answer/linkedlist/Q148_Sort_List.java
+++ src/main/java/com/answer/linkedlist/Q148_Sort_List.java
@@ -4,6 +4,89 @@ import java.util.*;
 
 public class Q148_Sort_List {
 
-
+    /**
+     * Merge sorting - from top to bottom
+     */
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode fast = head.next, slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode temp = slow.next;
+        slow.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(temp);
+        ListNode h = new ListNode(0);
+        ListNode result = h;
+        while(left != null && right != null){
+            if(left.val < right.val){
+                h.next = left;
+                left = left.next;
+            }else{
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left == null ? right : left;
+        return result.next;
+    }
+    /**
+     * Merge sorting - from bottom to top
+     */
+    public ListNode sortList_1(ListNode head) {
+        ListNode h, h1, h2, pre, res;
+        h = head;
+        int length = 0, intv = 1;
+        while (h != null) {
+            h = h.next;
+            length++;
+        }
+        res = new ListNode(0);
+        res.next = head;
+        while (intv < length) {
+            pre = res;
+            h = res.next;
+            while (h != null) {
+                int i = intv;
+                h1 = h;
+                while (i > 0 && h != null) {
+                    h = h.next;
+                    i--;
+                }
+                if (i > 0) break;
+                i = intv;
+                h2 = h;
+                while (i > 0 && h != null) {
+                    h = h.next;
+                    i--;
+                }
+                int c1 = intv, c2 = intv - i;
+                while (c1 > 0 && c2 > 0) {
+                    if (h1.val < h2.val) {
+                        pre.next = h1;
+                        h1 = h1.next;
+                        c1--;
+                    } else {
+                        pre.next = h2;
+                        h2 = h2.next;
+                        c2--;
+                    }
+                    pre = pre.next;
+                }
+                pre.next = c1 == 0 ? h2 : h1;
+                while (c1 > 0 || c2 > 0) {
+                    pre = pre.next;
+                    c1--;
+                    c2--;
+                }
+                pre.next = h;
+            }
+            intv *= 2;
+        }
+        return res.next;
+    }
 
 }
