eb9688bdc7a22326b91acbe41ee8a6cb8ec09372
KevinX <Xzb04720638k>
2023-01-27 15:05:38 -0500

    update code

---
+++ src/main/java/com/answer/array/Q67_Add_Binary.java
@@ -0,0 +1,26 @@
+package com.answer.array;
+
+public class Q67_Add_Binary {
+
+    public String addBinary(String a, String b) {
+        StringBuffer sb = new StringBuffer();
+        int n = a.length();
+        int m = b.length();
+        int i = n - 1, j = m - 1;
+        int carry = 0;
+        while(i >= 0 || j >= 0){
+            int x = i >= 0 ? a.charAt(i) - '0' : 0;
+            int y = j >= 0 ? b.charAt(j) - '0' : 0;
+            int sum = x + y + carry;
+            sb.insert(0, sum % 2); // Change to 2 since is it binary
+            carry = sum / 2;
+
+            i--;
+            j--;
+        }
+        if(carry > 0){
+            sb.insert(0, carry);
+        }
+        return sb.toString();
+    }
+}
