e92601153aadaebea6b2bf1c9f80e40c040102d7
KevinX <Xzb04720638k>
2023-01-27 22:52:56 -0500

    update code

---
+++ src/main/java/com/answer/hashmap/Q268_Missing_Number.java
@@ -0,0 +1,76 @@
+package com.answer.hashmap;
+
+import java.util.*;
+
+public class Q268_Missing_Number {
+
+    /**
+     * HashSet
+     */
+    public int missingNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int len = nums.length;
+        for(int i = 0; i <= len; i++){
+            set.add(i);
+        }
+        for(int n : nums){
+            set.remove(n);
+        }
+        return set.iterator().next();
+    }
+    /**
+     *
+     */
+    public int missingNumber_1(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int len = nums.length;
+
+        for(int n : nums){
+            set.add(n);
+        }
+        for(int i = 0; i <= len; i++){
+            if(!set.contains(i)){
+                return i;
+            }
+        }
+        return -1;
+    }
+    /**
+     * Sorting
+     */
+    public int missingNumber_2(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return n;
+    }
+    /**
+     * XOR
+     */
+    public int missingNumber_3(int[] nums) {
+        int x = 0;
+        for (int n : nums) {
+            x ^= n;
+        }
+        for (int i = 0; i <= nums.length; i++) {
+            x ^= i;
+        }
+        return x;
+    }
+    /**
+     * Sum
+     */
+    public int missingNumber_4(int[] nums) {
+        int n = nums.length;
+        int total = n * (n + 1) / 2;
+        int arrSum = 0;
+        for (int i = 0; i < n; i++) {
+            arrSum += nums[i];
+        }
+        return total - arrSum;
+    }
+}
