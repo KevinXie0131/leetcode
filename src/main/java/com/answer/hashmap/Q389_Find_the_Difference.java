5fc729b3b47ce369c30feed4650737050ef52d6f
KevinX <Xzb04720638k>
2023-01-28 23:14:05 -0500

    update code

---
+++ src/main/java/com/answer/hashmap/Q389_Find_the_Difference.java
@@ -0,0 +1,83 @@
+package com.answer.hashmap;
+
+import java.util.*;
+
+public class Q389_Find_the_Difference {
+    public static void main(String[] args) {
+        String s = "abcd";
+        String t = "abcde";
+        char c = findTheDifference_1(s, t);
+        System.out.println(c);
+    }
+    /**
+     * doesn't work for "a"& "aa"
+     */
+    public char findTheDifference(String s, String t) {
+        Set<Character> set = new HashSet<>();
+
+        char[] c1 = t.toCharArray();
+        for(char c : c1){
+            set.add(c);
+        }
+        char[] c2 = s.toCharArray();
+        for(char c : c2){
+            set.remove(c);
+        }
+        return set.iterator().next();
+    }
+    /**
+     *
+     */
+    public static char findTheDifference_1(String s, String t) {
+        int[] count = new int[26];
+        char[] c1 = t.toCharArray();
+        char[] c2 = s.toCharArray();
+
+        for(char c : c1){
+            count[c -'a']++;
+        }
+        for(char c : c2){
+            count[c -'a']--;
+        }
+        for(int i = 0; i < count.length; i++){
+            if(count[i] > 0){
+                return (char)('a' + i);
+            }
+        }
+        return 'a';
+    }
+    /**
+     *
+     */
+    public char findTheDifference_2(String s, String t) {
+        int sum = 0;
+        char[] c1 = t.toCharArray();
+        char[] c2 = s.toCharArray();
+
+        for(char c : c1){
+            sum += c -'a';
+        }
+        for(char c : c2){
+            sum -= c -'a';
+        }
+
+        return (char)('a' + sum);
+    }
+    /**
+     *
+     */
+    public char findTheDifference_3(String s, String t) {
+        int xor = 0;
+        char[] c1 = t.toCharArray();
+        char[] c2 = s.toCharArray();
+
+        for(char c : c1){
+            xor ^= c;
+        }
+        for(char c : c2){
+            xor ^= c;
+        }
+
+        return (char)xor;
+    }
+}
