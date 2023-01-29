551a58d93c9d140e964beb8192da7c39337ee971
KevinX <Xzb04720638k>
2023-01-28 23:32:04 -0500

    update code

--- src/main/java/com/answer/hashmap/Q389_Find_the_Difference.java
+++ src/main/java/com/answer/hashmap/Q389_Find_the_Difference.java
@@ -10,7 +10,7 @@ public class Q389_Find_the_Difference {
         System.out.println(c);
     }
     /**
-     * doesn't work for "a"& "aa"
+     * HashSet - doesn't work for "a"& "aa"
      */
     public char findTheDifference(String s, String t) {
         Set<Character> set = new HashSet<>();
@@ -26,7 +26,7 @@ public class Q389_Find_the_Difference {
         return set.iterator().next();
     }
     /**
-     * this method works
+     * HashSet - this method works
      */
     public char findTheDifference_0(String s, String t) {
         Set<Character> set = new HashSet<>();
@@ -41,7 +41,37 @@ public class Q389_Find_the_Difference {
         return (char)set.toArray()[0];
     }
     /**
-     *
+     * HashMap
+     */
+    public char findTheDifference_0a(String s, String t) {
+        char extraChar = '\0';
+
+        // Prepare a counter for string s.
+        // This hash map holds the characters as keys and respective frequency as value.
+        HashMap <Character,Integer> counterS = new HashMap <>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            counterS.put(ch, counterS.getOrDefault(ch, 0) + 1);
+        }
+
+        // Iterate through string t and find the character which is not in s.
+        for (int i = 0; i < t.length(); i += 1) {
+            char ch = t.charAt(i);
+            if (!counterS.containsKey(ch)) {
+                extraChar = ch;
+                break;
+            } else {
+                int countOfChar = counterS.get(ch);
+                if(countOfChar == 0) return ch;
+                // Once a match is found we reduce frequency left.
+                // This eliminates the possibility of a false match later.
+                counterS.put(ch, countOfChar - 1);
+            }
+        }
+        return extraChar;
+    }
+    /**
+     * Use counter
      */
     public static char findTheDifference_1(String s, String t) {
         int[] count = new int[26];
@@ -62,7 +92,7 @@ public class Q389_Find_the_Difference {
         return 'a';
     }
     /**
-     *
+     * Find sum
      */
     public char findTheDifference_2(String s, String t) {
         int sum = 0;
@@ -79,7 +109,7 @@ public class Q389_Find_the_Difference {
         return (char)('a' + sum);
     }
     /**
-     *
+     * XOR
      */
     public char findTheDifference_3(String s, String t) {
         int xor = 0;
@@ -96,7 +126,7 @@ public class Q389_Find_the_Difference {
         return (char)xor;
     }
     /**
-     *
+     * XOR - concat string
      */
     public char findTheDifference_4(String s, String t) {
         char exor = 0;
