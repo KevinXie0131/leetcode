bf750ee7ad32559c9d8fbf05ca0c3681bc1b55d4
KevinX <Xzb04720638k>
2023-01-28 23:38:58 -0500

    update code

---
+++ src/main/java/com/answer/priorityqueue/Q263_Ugly_Number.java
@@ -0,0 +1,27 @@
+package com.answer.priorityqueue;
+
+public class Q263_Ugly_Number {
+
+    public boolean isUgly(int n) {
+        if(n <= 0) return false;
+        int[] factors = {2, 3, 5};
+        for(int factor : factors){
+            while(n % factor == 0){
+                n = n / factor;
+            }
+        }
+        return n == 1;
+    }
+    /**
+     *
+     */
+    public boolean isUgly_1(int n) {
+        if (n <= 0) return false;
+
+        while(n % 2 == 0) n = n / 2;
+        while(n % 3 == 0) n = n / 3;
+        while(n % 5 == 0) n = n / 5;
+
+        return n == 1;
+    }
+}
