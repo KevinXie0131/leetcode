1228b1d231a86be9226fd8e738288b18a04ab087
KevinX <Xzb04720638k>
2023-01-30 21:34:35 -0500

    update code

--- src/main/java/com/answer/array/Q74_Search_a_2D_Matrix.java
+++ src/main/java/com/answer/array/Q74_Search_a_2D_Matrix.java
@@ -2,9 +2,9 @@ package com.answer.array;
 
 public class Q74_Search_a_2D_Matrix {
     public static void main(String[] args) {
-        int[][] matrix = {{1}};
-        int target = 2;
-        System.out.println(searchMatrix_1(matrix ,target));
+        int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        int target = 13;
+        System.out.println(searchMatrix_2(matrix ,target));
     }
     /**
      * Brute force
@@ -17,6 +17,27 @@ public class Q74_Search_a_2D_Matrix {
                 if(matrix[i][j] == target){
                     return true;
                 }
+                if(matrix[i][j] > target){
+                    break;
+                }
+            }
+        }
+        return false;
+    }
+    /**
+     * Approach 4: Search Space Reduction
+     * Optimized Brute force
+     */
+    public boolean searchMatrix_0(int[][] matrix, int target) {
+        int rows = matrix.length - 1, columns = 0;
+        while (rows >= 0 && columns < matrix[0].length) {
+            int num = matrix[rows][columns];
+            if (num == target) {
+                return true;
+            } else if (num > target) {
+                rows--;
+            } else {
+                columns++;
             }
         }
         return false;
@@ -40,6 +61,47 @@ public class Q74_Search_a_2D_Matrix {
                 left = mid + 1;
             }else{
                 right = mid - 1;
+            }
+        }
+        return false;
+    }
+    /**
+     * Binary Search
+     */
+    public static boolean searchMatrix_2(int[][] matrix, int target) {
+        int rowIndex = binarySearchFirstColumn(matrix, target);
+        if (rowIndex < 0) {
+            return false;
+        }
+        return binarySearchRow(matrix[rowIndex], target);
+    }
+
+    public static int binarySearchFirstColumn(int[][] matrix, int target) {
+        int low = 0, high = matrix.length - 1;
+        while(low <= high){
+            int mid = (low + high) >>> 1;
+            int value = matrix[mid][0];
+            if(value == target){
+                return mid;
+            }else if(value > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return low - 1;
+    }
+
+    public static boolean binarySearchRow(int[] row, int target) {
+        int low = 0, high = row.length - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            if (row[mid] == target) {
+                return true;
+            } else if (row[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return false;
