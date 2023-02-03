f86c067103cfb1bc4383d9d652052b0d897c0980
KevinX <Xzb04720638k>
2023-02-02 23:15:21 -0500

    update code

--- src/main/java/com/answer/array/Q1288_Remove_Covered_Intervals.java
+++ src/main/java/com/answer/array/Q1288_Remove_Covered_Intervals.java
@@ -23,4 +23,31 @@ public class Q1288_Remove_Covered_Intervals {
 
         return count;
     }
+    /**
+     * Approach 1: Greedy Algorithm
+     */
+    public int removeCoveredIntervals_1(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                // Sort by start point.
+                // If two intervals share the same start point,
+                // put the longer one to be the first.
+                return o1[0] == o2[0] ? o2[1] - o1[1] : o1[0] - o2[0];
+            }
+        });
+
+        int count = 0;
+        int end, prev_end = 0;
+        for (int[] curr : intervals) {
+            end = curr[1];
+            // if current interval is not covered
+            // by the previous one
+            if (prev_end < end) {
+                ++count;
+                prev_end = end;
+            }
+        }
+        return count;
+    }
 }
