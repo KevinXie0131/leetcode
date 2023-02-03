ad781982c2b723c5a28179466350a1485d6310ea
KevinX <Xzb04720638k>
2023-02-02 23:13:14 -0500

    update code

--- src/main/java/com/answer/array/Q1288_Remove_Covered_Intervals.java
+++ src/main/java/com/answer/array/Q1288_Remove_Covered_Intervals.java
@@ -8,7 +8,7 @@ public class Q1288_Remove_Covered_Intervals {
         System.out.println(removeCoveredIntervals(intervals));
     }
     public static int removeCoveredIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? a[1]-b[1]:a[0]-b[0]);
+        Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? b[1]-a[1]:a[0]-b[0]);
         int len = intervals.length;
         int count = len;
         int right = intervals[0][1];
