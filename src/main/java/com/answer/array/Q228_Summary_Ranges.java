a2e07343d3ff9055a60b4befc403ec324434e7de
KevinX <Xzb04720638k>
2023-02-02 22:10:57 -0500

    update code

--- src/main/java/com/answer/array/Q228_Summary_Ranges.java
+++ src/main/java/com/answer/array/Q228_Summary_Ranges.java
@@ -26,4 +26,22 @@ public class Q228_Summary_Ranges {
         }
         return list;
     }
+    /**
+     * Two pointers
+     */
+    public List<String> summaryRanges_1(int[] nums) {
+        List<String> summary = new ArrayList<>();
+        for (int i = 0, j = 0; j < nums.length; ++j) {
+            // check if j + 1 extends the range [nums[i], nums[j]]
+            if (j + 1 < nums.length && nums[j + 1] == nums[j] + 1)
+                continue;
+            // put the range [nums[i], nums[j]] into the list
+            if (i == j)
+                summary.add(nums[i] + "");
+            else
+                summary.add(nums[i] + "->" + nums[j]);
+            i = j + 1;
+        }
+        return summary;
+    }
 }
