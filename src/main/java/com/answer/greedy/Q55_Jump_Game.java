6a83fb89ae7cabdacb488fa6203fd6650a6f1322
KevinX <Xzb04720638k>
2023-02-04 18:58:37 -0500

    update code

--- src/main/java/com/answer/greedy/Q55_Jump_Game.java
+++ src/main/java/com/answer/greedy/Q55_Jump_Game.java
@@ -24,4 +24,42 @@ public class Q55_Jump_Game {
 
         return true;
     }
+    /**
+     * Official answer
+     */
+    public boolean canJump_1(int[] nums) {
+        int n = nums.length;
+        int rightmost = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightmost) {
+                rightmost = Math.max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    /**
+     * Approach 1: Backtracking
+     * Time Limit Exceeded
+     */
+    public boolean canJump_2(int[] nums) {
+        return backtracking(0, nums);
+    }
+    public boolean backtracking(int position, int[] nums) {
+        if (position == nums.length - 1) {
+            return true;
+        }
+        int furthestJump = Math.min(position + nums[position], nums.length - 1);
+
+        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+            if (backtracking(nextPosition, nums)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
