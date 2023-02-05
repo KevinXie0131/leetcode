afadb2d46421f32671fa1147449f8d0d383e3591
KevinX <Xzb04720638k>
2023-02-04 20:19:45 -0500

    update code

--- src/main/java/com/answer/greedy/Q122_Best_Time_to_Buy_and_Sell_Stock_II.java
+++ src/main/java/com/answer/greedy/Q122_Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -4,13 +4,14 @@ public class Q122_Best_Time_to_Buy_and_Sell_Stock_II {
 
     /**
      * Greedy
+     * Approach 3: Simple One Pass
      */
     public int maxProfit(int[] prices) {
         int profit = 0;
 
             for(int i = 1; i < prices.length; i++){
             if(prices[i] - prices[i - 1] > 0){
-                profit += prices[i] - prices[i - 1];
+                profit += prices[i] - prices[i - 1]; // Math.max(prices[i + 1] - prices[i], 0);
             }
         }
         return profit;
