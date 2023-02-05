5376366fd5acf926cb45a831039229c53267ea7d
KevinX <Xzb04720638k>
2023-02-05 17:18:23 -0500

    update code

---
+++ src/main/java/com/answer/math/Q338_Counting_Bits.java
@@ -0,0 +1,36 @@
+package com.answer.math;
+
+public class Q338_Counting_Bits {
+    /**
+     * Approach 1: Pop Count
+     * Time complexity: O(nlogn)
+     * Space complexity: O(1)
+     */
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        for(int i = 0; i <= n; i++){
+            res[i] = count(i);
+        }
+        return res;
+    }
+    /**
+     * 101 & 100 = 100
+     * 100 & 011 = 0
+     *
+     * 110 & 101 = 100
+     * 100 & 011 = 0
+     *
+     * 1011 & 1010 = 1010
+     * 1010 & 1001 = 1000
+     * 1000 & 0111 = 0
+     */
+    public int count(int x){
+        int count = 0;
+        while(x > 0){
+            x = x & (x - 1); // zeroing out the least significant nonzero bit
+            count++;
+        }
+
+        return count;
+    }
+}
