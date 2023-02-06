abaf2faff2d4fc9122e00229ab9c282f899b622e
KevinX <Xzb04720638k>
2023-02-05 20:39:34 -0500

    update code

--- src/main/java/com/answer/math/Q786_Kth_Smallest_Prime_Fraction.java
+++ src/main/java/com/answer/math/Q786_Kth_Smallest_Prime_Fraction.java
@@ -10,7 +10,7 @@ public class Q786_Kth_Smallest_Prime_Fraction {
     }
 
     /**
-     * Approach #2: Heap
+     * Approach #2: Heap - 多路归并
      * Time: O(klogn)
      * Space: O(n)
      */
@@ -19,7 +19,9 @@ public class Q786_Kth_Smallest_Prime_Fraction {
         int m = arr.length;
 
         PriorityQueue<int[]> heap = new PriorityQueue<int[]>((x, y) -> arr[x[0]] * arr[y[1]] - arr[y[0]] * arr[x[1]]);
-
+        /**
+         * PriorityQueue<int[]> q = new PriorityQueue<>((a,b)->Double.compare(b[0]*1.0/b[1],a[0]*1.0/a[1]));
+         */
         for(int i = 1; i < m; i++){
             heap.offer(new int[]{0, i});
         }
@@ -48,5 +50,43 @@ public class Q786_Kth_Smallest_Prime_Fraction {
         }
         Collections.sort(frac, (x,y)->x[0]*y[1] -x[1]*y[0]);
         return frac.get(k-1);
+    }
+    /**
+     * Approach #1: Binary Search
+     */
+    double eps = 1e-8;
+
+    public int[] kthSmallestPrimeFraction_2(int[] arr, int k) {
+        int n = arr.length;
+        double left = 0;
+        double right = 1;
+        int[] ans = new int[2];
+        while (right - left > eps) {
+            double mid = (left + right) / 2;
+            if (check(arr, mid, ans) >= k) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        return ans;
+    }
+
+    int check(int[] arr, double val, int[] ans) {
+        int count = 0;
+        int n = arr.length;
+        for (int i = 0, j = 1; j < n; j++) {
+            while (arr[i + 1] * 1.0 / arr[j] <= val) {
+                i++;
+            }
+            if (arr[i] * 1.0 / arr[j] <= val) {
+                count += i + 1;
+            }
+            if (Math.abs(arr[i] * 1.0 / arr[j] - val) < eps) {
+                ans[0] = arr[i];
+                ans[1] = arr[j];
+            }
+        }
+        return count;
     }
 }
