406d68b1c1e9e6e5c52e0ebe15ea1fd0c15b41e4
KevinX <Xzb04720638k>
2023-02-08 17:33:37 -0500

    update code

--- src/main/java/com/answer/two_pointers/Q643_Maximum_Average_Subarray_I.java
+++ src/main/java/com/answer/two_pointers/Q643_Maximum_Average_Subarray_I.java
@@ -1,7 +1,11 @@
 package com.answer.two_pointers;
 
 public class Q643_Maximum_Average_Subarray_I {
-
+    public static void main(String[] args) {
+        int[] nums = {1,12,-5,-6,50,3};
+        int k = 4;
+        System.out.println(findMaxAverage_2(nums, k));
+    }
     /**
      * Approach #2 Sliding Window
      */
@@ -44,5 +48,24 @@ public class Q643_Maximum_Average_Subarray_I {
         }
 
         return max * 1.0 / k;
+    }
+    /**
+     * Approach #1 Cumulative Sum
+     */
+    public static double findMaxAverage_2(int[] nums, int k) {
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++){
+            sum[i] = sum[i - 1] + nums[i];
+        }
+        // 1,12,-5,-6,50,3
+        // 1,13,8,2,52,55
+        double res = sum[k - 1] * 1.0 / k;
+
+        for (int i = k; i < nums.length; i++) {
+            res = Math.max(res, (sum[i] - sum[i - k]) * 1.0 / k);
+        }
+        return res;
     }
 }
