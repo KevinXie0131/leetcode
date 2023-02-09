79dfaed831458c26689eeaca2499e5ef51708e2e
KevinX <Xzb04720638k>
2023-02-08 20:25:29 -0500

    update code

---
+++ src/main/java/com/answer/string/Q58_Length_of_Last_Word.java
@@ -0,0 +1,50 @@
+package com.answer.string;
+
+public class Q58_Length_of_Last_Word {
+
+    /**
+     * Approach 1: String Index Manipulation
+     */
+    public int lengthOfLastWord(String s) {
+        char[] ch = s.toCharArray();
+        int len = ch.length;
+        int right = len - 1;
+
+        while(right >= 0){
+            if(ch[right] == ' '){
+                right--;
+            }else{
+                break;
+            }
+        }
+
+        int left = right - 1;
+        while(left >= 0){
+            if(ch[left] == ' '){
+                break;
+            }else{
+                left--;
+            }
+        }
+
+        return right - left;
+    }
+    /**
+     * Official answer
+     */
+    public int lengthOfLastWord_1(String s) {
+        // trim the trailing spaces
+        int p = s.length() - 1;
+        while (p >= 0 && s.charAt(p) == ' ') {
+            p--;
+        }
+
+        // compute the length of last word
+        int length = 0;
+        while (p >= 0 && s.charAt(p) != ' ') {
+            p--;
+            length++;
+        }
+        return length;
+    }
+}
