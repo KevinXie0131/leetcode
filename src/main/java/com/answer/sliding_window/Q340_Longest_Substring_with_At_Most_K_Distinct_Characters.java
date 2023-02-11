126fe4f8a0f0285d54ae2a329726cc5b7282aeb0
KevinX <Xzb04720638k>
2023-02-10 21:00:13 -0500

    update code

--- src/main/java/com/answer/sliding_window/Q340_Longest_Substring_with_At_Most_K_Distinct_Characters.java
+++ src/main/java/com/answer/sliding_window/Q340_Longest_Substring_with_At_Most_K_Distinct_Characters.java
@@ -4,8 +4,11 @@ import java.util.*;
 
 public class Q340_Longest_Substring_with_At_Most_K_Distinct_Characters {
     public static void main(String[] args) {
-        String s = "eceba";
-        System.out.println(lengthOfLongestSubstringKDistinct_1(s, 2 ));
+    //    String s = "eceba";
+    //    int k = 2;
+        String s = "aa";
+        int k = 1;
+        System.out.println(lengthOfLongestSubstringKDistinct_0(s, k ));
     }
     /**
      * Similar with 159. Longest Substring with At Most Two Distinct Characters
@@ -27,6 +30,41 @@ public class Q340_Longest_Substring_with_At_Most_K_Distinct_Characters {
             }
             max = Math.max(max, i - left + 1);
         }
+        return max;
+    }
+    /**
+     * Approach 2: Sliding Window + Ordered Dictionary
+     */
+    public static int lengthOfLongestSubstringKDistinct_0(String s, int k) {
+        if (k == 0) {
+            return 0;
+        }
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        int left = 0;
+        int right = 0;
+        int max = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            Character ch = s.charAt(i);
+            // if character is already in the hashmap -
+            // delete it, so that after insert it becomes
+            // the rightmost element in the hashmap
+            if(map.containsKey(ch)){
+                map.remove(ch);
+            }
+            map.put(ch, right);
+            right++;
+
+            if(map.size() > k){
+                // delete the leftmost character
+                Map.Entry<Character, Integer> leftmost = map.entrySet().iterator().next();
+                map.remove(leftmost.getKey());
+                // move left pointer of the slidewindow
+                left = leftmost.getValue() + 1;
+            }
+            max = Math.max(max, right - left);
+        }
+
         return max;
     }
     /**
