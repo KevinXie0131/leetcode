66b084cb94daf5c4383ee22f34b5c1291fc67c88
KevinX <Xzb04720638k>
2023-02-10 22:07:35 -0500

    update code

---
+++ src/main/java/com/answer/sliding_window/Q438_Find_All_Anagrams_in_a_String.java
@@ -0,0 +1,80 @@
+package com.answer.sliding_window;
+
+import java.util.*;
+
+public class Q438_Find_All_Anagrams_in_a_String {
+    public static void main(String[] args) {
+/*        String s1 = "cbaebabacd";
+        String s2 = "abc";*/
+/*        String s1 = "acdcaeccde";
+        String s2 ="c";*/
+        String s1 = "aa";
+        String s2 ="bb";
+
+        System.out.println(findAnagrams_1(s1, s2));
+    }
+
+    /**
+     * Brute force
+     */
+    public static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0, j = p.length() - 1; i <= j && j < s.length(); i++, j++){
+            if(checkAnagram(s.substring(i, j + 1), p)){
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    public static boolean checkAnagram(String str, String pstr){
+        int[] counts = new int[26];
+        int length = str.length();
+
+        for (int i = 0; i < length; i++) {
+            counts[str.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < pstr.length(); i++) {
+            counts[pstr.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if(counts[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * Approach 2: Sliding Window with Array
+     */
+    public static List<Integer> findAnagrams_1(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        int slen = s.length();
+        int plen = p.length();
+        if (slen < plen) {
+            return new ArrayList<Integer>();
+        }
+        int[] sCount = new int[26];
+        int[] pCount = new int[26];
+        for(int i = 0; i < plen; i++){
+            sCount[s.charAt(i) - 'a']++;
+            pCount[p.charAt(i) - 'a']++;
+        }
+        if(Arrays.equals(sCount, pCount)){
+            res.add(0);
+        }
+
+        for(int i = 0; i < slen - plen; i++){
+            sCount[s.charAt(i) - 'a']--;
+            sCount[s.charAt(i + plen) - 'a']++;
+
+            if(Arrays.equals(sCount, pCount)){
+                res.add(i + 1);
+            }
+        }
+        return res;
+    }
+    /**
+     * Approach 1: Sliding Window with HashMap
+     */
+}
