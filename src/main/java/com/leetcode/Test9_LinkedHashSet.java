2ec5bfa50e9d06f0c7371596fe2ce59cc757a040
KevinX <Xzb04720638k>
2023-02-10 20:43:37 -0500

    update code

--- src/main/java/com/leetcode/Test9_LinkedHashSet.java
+++ src/main/java/com/leetcode/Test9_LinkedHashSet.java
@@ -1,7 +1,7 @@
 package com.leetcode;
 
 import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Test9_LinkedHashSet {
 
@@ -16,6 +16,16 @@ public class Test9_LinkedHashSet {
 
         for (Integer value : set) {
             System.out.println(value);
+        }
+
+        LinkedHashMap<String, String> map = new LinkedHashMap<>();
+        map.put("b", "b2");
+        map.put("e", "e5");
+        map.put("a", "a1");
+        map.put("c", "c3");
+        map.put("d", "d4");
+        for(Map.Entry<String, String> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
     }
 }
