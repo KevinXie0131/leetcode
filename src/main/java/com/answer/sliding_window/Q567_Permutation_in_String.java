3734e21cf7f49ef1d17f3695737c6b71723a4df7
KevinX <Xzb04720638k>
2023-02-12 15:30:30 -0500

    update code

--- src/main/java/com/answer/sliding_window/Q567_Permutation_in_String.java
+++ src/main/java/com/answer/sliding_window/Q567_Permutation_in_String.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 public class Q567_Permutation_in_String {
     public static void main(String[] args) {
-        String s1 = "ab", s2 = "eidbaooo";
-        System.out.println(checkInclusion(s1, s2));
+        String s1 = "ab", s2 = "eidbfaooab";
+        System.out.println(checkInclusion_3(s1, s2));
     }
 
     /**
@@ -58,5 +58,33 @@ public class Q567_Permutation_in_String {
         char[] t = s.toCharArray();
         Arrays.sort(t);
         return new String(t);
+    }
+    /**
+     * Two pointers
+     * Sliding window的思路: 我们在保证区间长度为n的情况下，去考察是否存在一个区间使得cnt的值全为0
+     * 反过来，还可以在保证cnt的值不为正的情况下，去考察是否存在一个区间，其长度恰好为n
+     */
+    public static boolean checkInclusion_3(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        if (n > m) {
+            return false;
+        }
+        int[] cnt = new int[26];
+        for (int i = 0; i < n; ++i) {
+            --cnt[s1.charAt(i) - 'a'];
+        }
+        int left = 0;
+        for (int right = 0; right < m; ++right) {
+            int x = s2.charAt(right) - 'a';
+            ++cnt[x];
+            while (cnt[x] > 0) {
+                --cnt[s2.charAt(left) - 'a'];
+                ++left;
+            }
+            if (right - left + 1 == n) {
+                return true;
+            }
+        }
+        return false;
     }
 }
