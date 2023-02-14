4938f5ce1d313b53d3a388073ddeabfc5dbd0f19
KevinX <Xzb04720638k>
2023-02-13 21:50:12 -0500

    unionfind

--- src/main/java/com/answer/union_find/Q721_Accounts_Merge.java
+++ src/main/java/com/answer/union_find/Q721_Accounts_Merge.java
@@ -5,6 +5,14 @@ import java.util.*;
 import java.util.List;
 
 public class Q721_Accounts_Merge {
+    public static void main(String[] args) {
+        List<List<String>> accounts = new ArrayList<>();
+        accounts.add(Arrays.asList("John","johnsmith@mail.com","john_newyork@mail.com"));
+        accounts.add(Arrays.asList("John","johnsmith@mail.com","john00@mail.com"));
+        accounts.add(Arrays.asList("Mary","mary@mail.com"));
+        accounts.add(Arrays.asList("John","johnnybravo@mail.com"));
+        System.out.println(accountsMerge_1(accounts));
+    }
     /**
      * 方法一：哈希表 + 并查集
      * Union Find
@@ -67,6 +75,73 @@ public class Q721_Accounts_Merge {
         }
         return parent[index];
     }
+    /**
+     * Approach 1: Depth First Search (DFS)
+     */
+    static HashSet<String> visited = new HashSet<>();
+    static Map<String, List<String>> adjacent = new HashMap<String, List<String>>();
+
+    public static List<List<String>> accountsMerge_1(List<List<String>> accountList) {
+        int accountListSize = accountList.size();
+
+        for (List<String> account : accountList) {
+            int accountSize = account.size();
+            // Building adjacency list
+            // Adding edge between first email to all other emails in the account
+            String accountFirstEmail = account.get(1);
+            for (int j = 2; j < accountSize; j++) {
+                String accountEmail = account.get(j);
+
+                if (!adjacent.containsKey(accountFirstEmail)) {
+                    adjacent.put(accountFirstEmail, new ArrayList<String>());
+                }
+                adjacent.get(accountFirstEmail).add(accountEmail);
+
+                if (!adjacent.containsKey(accountEmail)) {
+                    adjacent.put(accountEmail, new ArrayList<String>());
+                }
+                adjacent.get(accountEmail).add(accountFirstEmail);
+            }
+        }
+
+        // Traverse over all th accounts to store components
+        List<List<String>> mergedAccounts = new ArrayList<>();
+        for (List<String> account : accountList) {
+            String accountName = account.get(0);
+            String accountFirstEmail = account.get(1);
+
+            // If email is visited, then it's a part of different component
+            // Hence perform DFS only if email is not visited yet
+            if (!visited.contains(accountFirstEmail)) {
+                List<String> mergedAccount = new ArrayList<>();
+                // Adding account name at the 0th index
+                mergedAccount.add(accountName);
+
+                DFS(mergedAccount, accountFirstEmail);
+                Collections.sort(mergedAccount.subList(1, mergedAccount.size()));
+                mergedAccounts.add(mergedAccount);
+            }
+        }
+        return mergedAccounts;
+    }
+    static private void DFS(List<String> mergedAccount, String email) {
+        visited.add(email);
+        // Add the email vector that contains the current component's emails
+        mergedAccount.add(email);
+
+        if (!adjacent.containsKey(email)) {
+            return;
+        }
+
+        for (String neighbor : adjacent.get(email)) {
+            if (!visited.contains(neighbor)) {
+                DFS(mergedAccount, neighbor);
+            }
+        }
+    }
+    /**
+     * Approach 2: Breadth First Search (BFS)
+     */
 }
 
 class UnionFind {
