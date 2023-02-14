91818f238b61ff6cf3f7ec00660fb65555d110ae
KevinX <Xzb04720638k>
2023-02-13 22:57:16 -0500

    unionfind

--- src/main/java/com/answer/union_find/Q684_Redundant_Connection.java
+++ src/main/java/com/answer/union_find/Q684_Redundant_Connection.java
@@ -1,6 +1,7 @@
 package com.answer.union_find;
 
 import java.util.Arrays;
+import java.util.*;
 
 public class Q684_Redundant_Connection {
     public static void main(String[] args) {
@@ -9,6 +10,9 @@ public class Q684_Redundant_Connection {
         System.out.println(Arrays.toString(findRedundantConnection(edges)));
     }
 
+    /**
+     * Approach #2: Union-Find
+     */
     public static int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
         int[] parent = new int[n + 1];
@@ -36,5 +40,38 @@ public class Q684_Redundant_Connection {
             parent[index] = find(parent, parent[index]);
         }
         return parent[index];
+    }
+    /**
+     * Approach #1: DFS
+     */
+    Set<Integer> seen = new HashSet();
+    int MAX_EDGE_VAL = 1000;
+
+    public int[] findRedundantConnection_1(int[][] edges) {
+        ArrayList<Integer>[] graph = new ArrayList[MAX_EDGE_VAL + 1];
+        for (int i = 0; i <= MAX_EDGE_VAL; i++) {
+            graph[i] = new ArrayList();
+        }
+
+        for (int[] edge: edges) {
+            seen.clear();
+            if (!graph[edge[0]].isEmpty() && !graph[edge[1]].isEmpty() &&
+                    dfs(graph, edge[0], edge[1])) {
+                return edge;
+            }
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+        throw new AssertionError();
+    }
+    public boolean dfs(ArrayList<Integer>[] graph, int source, int target) {
+        if (!seen.contains(source)) {
+            seen.add(source);
+            if (source == target) return true;
+            for (int nei: graph[source]) {
+                if (dfs(graph, nei, target)) return true;
+            }
+        }
+        return false;
     }
 }
