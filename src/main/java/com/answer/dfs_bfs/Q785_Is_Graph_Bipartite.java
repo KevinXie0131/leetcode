a9d0f6b3739bf37caba081c8622a1e24c2686e45
KevinX <Xzb04720638k>
2023-02-18 22:39:48 -0500

    BFS/DFS

--- src/main/java/com/answer/dfs_bfs/Q785_Is_Graph_Bipartite.java
+++ src/main/java/com/answer/dfs_bfs/Q785_Is_Graph_Bipartite.java
@@ -8,20 +8,21 @@ public class Q785_Is_Graph_Bipartite {
     /**
      * Approach #1: Coloring by Depth-First Search - Recursion
      */
+    //涂颜色使用的颜色，分别是：无色、红色和绿色
     private static final int UNCOLORED = 0;
     private static final int RED = 1;
     private static final int GREEN = 2;
-    private int[] color;
-    private boolean valid;
+    private int[] color; //用于记录节点颜色
+    private boolean valid;  //记录是否二分图
 
     public boolean isBipartite(int[][] graph) {
-        int n = graph.length;
+        int n = graph.length; //图节点数量
         valid = true;
         color = new int[n];
         Arrays.fill(color, UNCOLORED);
 
         for (int i = 0; i < n && valid; ++i) {
-            if (color[i] == UNCOLORED) {
+            if (color[i] == UNCOLORED) { //题中没有说明一定是连通图，所以需要每个没有涂色的节点作为起始节点涂颜色
                 dfs(i, RED, graph);
             }
         }
@@ -29,20 +30,21 @@ public class Q785_Is_Graph_Bipartite {
     }
     public void dfs(int node, int col, int[][] graph) {
         color[node] = col;
-        int cNei;
+        int cNei; //这次遍历中当前节点相邻节点应该涂的颜色
         if(col == RED){
             cNei = GREEN;
         }else{
             cNei = RED;
         }
-
+        //遍历当前节点相邻所有节点
         for (int neighbor : graph[node]) {
-            if (color[neighbor] == UNCOLORED) {
+            if (color[neighbor] == UNCOLORED) { //如果相邻接点还没涂色，则继续深度优先遍历涂色
                 dfs(neighbor, cNei, graph);
+                //判断上面遍历已经确定图不是二分图
                 if (!valid) {
                     return;
                 }
-            } else if (color[neighbor] != cNei) {
+            } else if (color[neighbor] != cNei) { //如果相邻节点已经有颜色，并且跟这次遍历本该涂的颜色不一致，说明不是二分图
                 valid = false;
                 return;
             }
