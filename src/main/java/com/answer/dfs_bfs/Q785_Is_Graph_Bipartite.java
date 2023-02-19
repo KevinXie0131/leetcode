7cde12dc50ed7589ed716785b019c521accd5958
KevinX <Xzb04720638k>
2023-02-18 22:35:14 -0500

    BFS/DFS

---
+++ src/main/java/com/answer/dfs_bfs/Q785_Is_Graph_Bipartite.java
@@ -0,0 +1,81 @@
+package com.answer.dfs_bfs;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Q785_Is_Graph_Bipartite {
+
+    /**
+     * Approach #1: Coloring by Depth-First Search - Recursion
+     */
+    private static final int UNCOLORED = 0;
+    private static final int RED = 1;
+    private static final int GREEN = 2;
+    private int[] color;
+    private boolean valid;
+
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        valid = true;
+        color = new int[n];
+        Arrays.fill(color, UNCOLORED);
+
+        for (int i = 0; i < n && valid; ++i) {
+            if (color[i] == UNCOLORED) {
+                dfs(i, RED, graph);
+            }
+        }
+        return valid;
+    }
+    public void dfs(int node, int col, int[][] graph) {
+        color[node] = col;
+        int cNei;
+        if(col == RED){
+            cNei = GREEN;
+        }else{
+            cNei = RED;
+        }
+
+        for (int neighbor : graph[node]) {
+            if (color[neighbor] == UNCOLORED) {
+                dfs(neighbor, cNei, graph);
+                if (!valid) {
+                    return;
+                }
+            } else if (color[neighbor] != cNei) {
+                valid = false;
+                return;
+            }
+        }
+    }
+    /**
+     * Approach #1: Coloring by Depth-First Search - Use stack
+     */
+    public boolean isBipartite_1(int[][] graph) {
+        int n = graph.length;
+        int[] color = new int[n];
+        Arrays.fill(color, -1);
+
+        for (int start = 0; start < n; ++start) {
+            if (color[start] == -1) {
+                Stack<Integer> stack = new Stack();
+                stack.push(start);
+                color[start] = 0;
+
+                while (!stack.empty()) {
+                    Integer node = stack.pop();
+                    for (int nei: graph[node]) {
+                        if (color[nei] == -1) {
+                            stack.push(nei);
+                            color[nei] = color[node] ^ 1;
+                        } else if (color[nei] == color[node]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
