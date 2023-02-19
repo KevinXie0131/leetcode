99c9a7b755417b16b1c8b10fdc2491eebb26bb28
KevinX <Xzb04720638k>
2023-02-18 23:10:44 -0500

    BFS/DFS

--- src/main/java/com/answer/dfs_bfs/Q797_All_Paths_From_Source_to_Target.java
+++ src/main/java/com/answer/dfs_bfs/Q797_All_Paths_From_Source_to_Target.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Q797_All_Paths_From_Source_to_Target {
     /**
      * 本题中的图为有向无环图DAG, 搜索过程中不会反复遍历同一个点, 因此我们无需判断当前点是否遍历过
-     * Approach 1: Backtracking
+     * Approach 1: Depth-First Search (DFS)  - Backtracking
      */
     List<List<Integer>> res= new ArrayList<>();
     Deque<Integer> stack = new ArrayDeque<>();
@@ -18,7 +18,7 @@ public class Q797_All_Paths_From_Source_to_Target {
 
     public void dfs(int[][] graph, int x, int n) {
         if(x == n){
-            List<Integer> list = new ArrayList<>(stack);
+            List<Integer> list = new ArrayList<>(stack); // Note: one should make a deep copy of the path
             Collections.reverse(list);
             res.add(list);
             return;
