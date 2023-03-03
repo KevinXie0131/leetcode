d4dadf89fd030cacdb05e6ef5f47156f04a97e2e
KevinX <Xzb04720638k>
2023-03-02 19:46:49 -0500

    update code

---
+++ src/main/java/com/learn/template/unionfind/UnionFind_PathCompression_Example.java
@@ -0,0 +1,49 @@
+package com.learn.template.unionfind;
+
+public class UnionFind_PathCompression_Example {
+
+    public static void main(String[] args) throws Exception {
+        UnionFind_PathCompression_Example uf = new UnionFind_PathCompression_Example(10);
+        // 1-2-5-6-7 3-8-9 4
+        uf.union(1, 2);
+        uf.union(2, 5);
+        uf.union(5, 6);
+        uf.union(6, 7);
+        uf.union(3, 8);
+        uf.union(8, 9);
+        System.out.println(uf.connected(1, 5)); // true
+        System.out.println(uf.connected(5, 7)); // true
+        System.out.println(uf.connected(4, 9)); // false
+        // 1-2-5-6-7 3-8-9-4
+        uf.union(9, 4);
+        System.out.println(uf.connected(4, 9)); // true
+    }
+
+    private int[] root;
+
+    public UnionFind_PathCompression_Example(int size) {
+        root = new int[size];
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            root[rootY] = rootX;
+        }
+    }
+
+    public boolean connected(int x, int y) {
+        return find(x) == find(y);
+    }
+}
