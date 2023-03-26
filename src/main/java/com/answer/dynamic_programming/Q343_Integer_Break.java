0a58761021a0f313c2ded84645fb3e930e241444
KevinX <Xzb04720638k>
2023-03-26 18:36:14 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q343_Integer_Break.java
+++ src/main/java/com/answer/dynamic_programming/Q343_Integer_Break.java
@@ -19,4 +19,19 @@ public class Q343_Integer_Break {
 
         return dp[n];
     }
+
+    public int integerBreak_1(int n) {
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+
+        for(int i = 3; i < n + 1; i++){
+            int max = 0;
+            for(int j = 1; j < i - 1; j++){
+                max = Math.max(max, Math.max(dp[i-j]*j, (i-j)*j));
+            }
+            dp[i] = max;
+        }
+
+        return dp[n];
+    }
 }
