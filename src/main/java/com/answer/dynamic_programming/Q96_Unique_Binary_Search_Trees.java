45635041c434465e81a798657ace9c80429b8922
KevinX <Xzb04720638k>
2023-03-26 18:41:47 -0400

    update code

---
+++ src/main/java/com/answer/dynamic_programming/Q96_Unique_Binary_Search_Trees.java
@@ -0,0 +1,20 @@
+package com.answer.dynamic_programming;
+
+public class Q96_Unique_Binary_Search_Trees {
+
+    /**
+     * DP
+     */
+    public int numTrees(int n) {
+        int[] dp = new int[n+1];
+
+        dp[0] = 1;
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j <= i; j++){
+                dp[i] += dp[i-j] * dp[j-1];
+            }
+        }
+
+        return dp[n];
+    }
+}
