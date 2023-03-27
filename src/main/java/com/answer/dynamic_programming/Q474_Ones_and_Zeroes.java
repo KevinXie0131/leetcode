9666a6f6b2280049eb35140660823c1b8d8bb773
KevinX <Xzb04720638k>
2023-03-27 19:49:09 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q474_Ones_and_Zeroes.java
+++ src/main/java/com/answer/dynamic_programming/Q474_Ones_and_Zeroes.java
@@ -3,6 +3,49 @@ package com.answer.dynamic_programming;
 public class Q474_Ones_and_Zeroes {
 
     /**
+     * Approach #3 Using Recursion [Time Limit Exceeded]
+     */
+    public int findMaxForm_1(String[] strs, int m, int n) {
+        return calculate(strs, 0, m, n);
+    }
+    public int calculate(String[] strs, int i, int zeroes, int ones) {
+        if (i == strs.length)
+            return 0;
+        int[] count = countzeroesones(strs[i]);
+        int taken = -1;
+        if (zeroes - count[0] >= 0 && ones - count[1] >= 0)
+            taken = calculate(strs, i + 1, zeroes - count[0], ones - count[1]) + 1;
+        int not_taken = calculate(strs, i + 1, zeroes, ones);
+        return Math.max(taken, not_taken);
+    }
+    public int[] countzeroesones(String s) {
+        int[] c = new int[2];
+        for (int i = 0; i < s.length(); i++) {
+            c[s.charAt(i)-'0']++;
+        }
+        return c;
+    }
+    /**
+     * Approach #4 Using Memoization [Accepted]
+     */
+    public int findMaxForm_2(String[] strs, int m, int n) {
+        int[][][] memo = new int[strs.length][m + 1][n + 1];
+        return calculate_2(strs, 0, m, n, memo);
+    }
+    public int calculate_2(String[] strs, int i, int zeroes, int ones, int[][][] memo) {
+        if (i == strs.length)
+            return 0;
+        if (memo[i][zeroes][ones] != 0)
+            return memo[i][zeroes][ones];
+        int[] count = countzeroesones(strs[i]);
+        int taken = -1;
+        if (zeroes - count[0] >= 0 && ones - count[1] >= 0)
+            taken = calculate_2(strs, i + 1, zeroes - count[0], ones - count[1], memo) + 1;
+        int not_taken = calculate_2(strs, i + 1, zeroes, ones, memo);
+        memo[i][zeroes][ones] = Math.max(taken, not_taken);
+        return memo[i][zeroes][ones];
+    }
+    /**
      * Approach #5 Dynamic Programming [Accepted]
      */
     public int findMaxForm(String[] strs, int m, int n) {
