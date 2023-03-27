0bc7c7509c33f03e906d016c575e830c5183753e
KevinX <Xzb04720638k>
2023-03-27 19:44:30 -0400

    update code

---
+++ src/main/java/com/answer/dynamic_programming/Q474_Ones_and_Zeroes.java
@@ -0,0 +1,30 @@
+package com.answer.dynamic_programming;
+
+public class Q474_Ones_and_Zeroes {
+
+    /**
+     * Approach #5 Dynamic Programming [Accepted]
+     */
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+
+        for(String s : strs){
+            int one = 0, zero = 0;
+            for(char c : s.toCharArray()){
+                if(c == '0'){
+                    zero++;
+                }else if(c == '1'){
+                    one++;
+                }
+            }
+
+            for(int i = m; i >= zero; i--){
+                for(int j = n; j >= one; j--){
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zero][j - one] + 1);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
