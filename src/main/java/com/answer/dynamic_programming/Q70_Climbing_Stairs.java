8200f8a47475e70dfbb88306546b1ca41b063af1
KevinX <Xzb04720638k>
2023-03-28 16:27:48 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q70_Climbing_Stairs.java
+++ src/main/java/com/answer/dynamic_programming/Q70_Climbing_Stairs.java
@@ -27,7 +27,7 @@ public class Q70_Climbing_Stairs {
     /**
      * Approach 3: Dynamic Programming
      */
-    public int climbStairs(int n) {
+    public static int climbStairs(int n) {
         if(n <= 2){
             return n;
         }
@@ -42,5 +42,26 @@ public class Q70_Climbing_Stairs {
         }
 
         return stairs[n];
+    }
+    /**
+     * Knapsack 完全背包
+     */
+    public static void main(String[] args) {
+        System.out.println( climbStairs(10));
+        System.out.println( climbStairs_2(10));
+    }
+    public static int climbStairs_2(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        int m = 2;
+
+        for (int i = 1; i <= n; i++) { // 遍历背包
+            for (int j = 1; j <= m; j++) { // 遍历物品
+                if (i - j >= 0) {
+                    dp[i] += dp[i - j];
+                }
+            }
+        }
+        return dp[n];
     }
 }
