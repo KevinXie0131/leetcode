0f9b0dcc438729004a3c3498cbd99270433726a3
KevinX <Xzb04720638k>
2023-03-27 20:45:19 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q518_Coin_Change_II.java
+++ src/main/java/com/answer/dynamic_programming/Q518_Coin_Change_II.java
@@ -12,6 +12,9 @@ public class Q518_Coin_Change_II {
 
     /**
      * Approach 1: Dynamic Programming
+     *
+     * 如果求组合数就是外层for循环遍历物品，内层for遍历背包。
+     * 如果求排列数就是外层for遍历背包，内层for循环遍历物品。
      */
     public static int change(int amount, int[] coins) {
         int[] dp = new int[amount + 1];
@@ -23,7 +26,13 @@ public class Q518_Coin_Change_II {
             }
             System.out.println(Arrays.toString(dp));
         }
-
+        /**
+         *  for (int coin : coins) {
+         *       for (int x = coin; x < amount + 1; ++x) {
+         *         dp[x] += dp[x - coin];
+         *       }
+         *     }
+         */
         return dp[amount];
     }
 }
