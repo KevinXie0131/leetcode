b900c17a65cbcdb58f1d36528a461515badb2517
KevinX <Xzb04720638k>
2023-03-28 18:09:27 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q322_Coin_Change.java
+++ src/main/java/com/answer/dynamic_programming/Q322_Coin_Change.java
@@ -29,4 +29,66 @@ public class Q322_Coin_Change {
         System.out.println(Arrays.toString(dp));
         return dp[amount];
     }
+    /**
+     * Backtracking
+     */
+    int res = Integer.MAX_VALUE;
+    public int coinChange_1(int[] coins, int amount) {
+        if(coins.length == 0){
+            return -1;
+        }
+        findWay(coins,amount,0);
+        // 如果没有任何一种硬币组合能组成总金额，返回 -1。
+        if(res == Integer.MAX_VALUE){
+            return -1;
+        }
+        return res;
+    }
+
+    public void findWay(int[] coins,int amount,int count){
+        if(amount < 0){
+            return;
+        }
+        if(amount == 0){
+            res = Math.min(res, count);
+        }
+        for(int i = 0;i < coins.length;i++){
+            findWay(coins,amount - coins[i],count + 1);
+        }
+    }
+    /**
+     * Memorization 记忆化搜索
+     */
+    int[] memo;
+    public int coinChange_2(int[] coins, int amount) {
+        if(coins.length == 0){
+            return -1;
+        }
+        memo = new int[amount];
+        return findWay(coins,amount);
+    }
+    // memo[n] 表示钱币n可以被换取的最少的硬币数，不能换取就为-1
+    // findWay函数的目的是为了找到 amount数量的零钱可以兑换的最少硬币数量，返回其值int
+    public int findWay(int[] coins,int amount){
+        if(amount < 0){
+            return -1;
+        }
+        if(amount == 0){
+            return 0;
+        }
+        // 记忆化的处理，memo[n]用赋予了值，就不用继续下面的循环
+        // 直接的返回memo[n] 的最优值
+        if(memo[amount-1] != 0){
+            return memo[amount-1];
+        }
+        int min = Integer.MAX_VALUE;
+        for(int i = 0;i < coins.length;i++){
+            int res = findWay(coins,amount-coins[i]);
+            if(res >= 0 && res < min){
+                min = res + 1; // 加1，是为了加上得到res结果的那个步骤中，兑换的一个硬币
+            }
+        }
+        memo[amount-1] = (min == Integer.MAX_VALUE ? -1 : min);
+        return memo[amount-1];
+    }
 }
