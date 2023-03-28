14b007f56b31cbd1512ed834c35443941976e7d4
KevinX <Xzb04720638k>
2023-03-27 20:52:37 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q518_Coin_Change_II.java
+++ src/main/java/com/answer/dynamic_programming/Q518_Coin_Change_II.java
@@ -8,6 +8,7 @@ public class Q518_Coin_Change_II {
         int amount = 5;
         int[] coins = {1,2,5};
         System.out.println(change(amount, coins));
+        System.out.println(change_1(amount, coins));
     }
 
     /**
@@ -33,6 +34,23 @@ public class Q518_Coin_Change_II {
          *       }
          *     }
          */
+        return dp[amount];
+    }
+    /**
+     * 究其原因，该代码计算的结果是 排列数，而不是 组合数
+     * the rsult is not correct
+     */
+    public static int change_1(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for (int j = 1; j <= amount; j++){ //枚举金额
+            for (int i = 0; i < coins.length; i++){
+                int coin = coins[i]; //枚举硬币
+                if (j < coin) continue; // coin不能大于amount
+                dp[j] += dp[j-coin];
+            }
+        }
         return dp[amount];
     }
 }
