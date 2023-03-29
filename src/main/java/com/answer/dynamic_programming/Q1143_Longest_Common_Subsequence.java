407c51b2eb6f622f81a2ec6165283590a7e95d74
KevinX <Xzb04720638k>
2023-03-28 22:54:28 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q1143_Longest_Common_Subsequence.java
+++ src/main/java/com/answer/dynamic_programming/Q1143_Longest_Common_Subsequence.java
@@ -1,4 +1,23 @@
 package com.answer.dynamic_programming;
 
 public class Q1143_Longest_Common_Subsequence {
+    /**
+     * Approach 3: Dynamic Programming
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        int M = text1.length();
+        int N = text2.length();
+        int[][] dp = new int[M + 1][N + 1];
+
+        for(int i = 1; i <= M; i++){
+            for(int j = 1; j <= N; j++){
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[M][N];
+    }
 }
