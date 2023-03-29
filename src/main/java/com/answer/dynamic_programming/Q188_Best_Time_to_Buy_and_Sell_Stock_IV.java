0b1ce8f297276d303e2467f51ee6a9f486975658
KevinX <Xzb04720638k>
2023-03-28 20:55:18 -0400

    dynamic

---
+++ src/main/java/com/answer/dynamic_programming/Q188_Best_Time_to_Buy_and_Sell_Stock_IV.java
@@ -0,0 +1,23 @@
+package com.answer.dynamic_programming;
+
+public class Q188_Best_Time_to_Buy_and_Sell_Stock_IV {
+    /**
+     * Dynamic Programming - Hard
+     */
+    public int maxProfit(int k, int[] prices) {
+        if (prices.length == 0) return 0;
+        int[][] dp = new int[prices.length][2 * k + 1];
+
+        for (int j = 1; j < 2 * k; j += 2) {
+            dp[0][j] = -prices[0];
+        }
+
+        for (int i = 1; i < prices.length; i++) {
+            for (int j = 0; j < 2 * k - 1; j += 2) {
+                dp[i][j + 1] = Math.max(dp[i - 1][j + 1], dp[i - 1][j] - prices[i]);
+                dp[i][j + 2] = Math.max(dp[i - 1][j + 2], dp[i - 1][j + 1] + prices[i]);
+            }
+        }
+        return dp[prices.length - 1][2 * k];
+    }
+}
