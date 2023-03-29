aee0b2911baa77cc2dec72506efb76d526bf1cb1
KevinX <Xzb04720638k>
2023-03-28 21:26:21 -0400

    update code

---
+++ src/main/java/com/answer/dynamic_programming/Q309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
@@ -0,0 +1,22 @@
+package com.answer.dynamic_programming;
+
+public class Q309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown {
+    /**
+     * Approach 1: Dynamic Programming
+     */
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (n == 0) return 0;
+
+        int[][] dp = new int[n][4];
+        dp[0][0] -= prices[0]; // 持股票
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], Math.max(dp[i - 1][3], dp[i - 1][1]) - prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][3]);
+            dp[i][2] = dp[i - 1][0] + prices[i];
+            dp[i][3] = dp[i - 1][2];
+        }
+        return Math. max(dp[n - 1][3], Math.max(dp[n - 1][1], dp[n - 1][2]));
+    }
+}
