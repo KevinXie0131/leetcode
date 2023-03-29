f264845cbb2b05285a94e05bb6b6f3ac59894d7d
KevinX <Xzb04720638k>
2023-03-29 18:14:22 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q674_Longest_Continuous_Increasing_Subsequence.java
+++ src/main/java/com/answer/dynamic_programming/Q674_Longest_Continuous_Increasing_Subsequence.java
@@ -25,7 +25,6 @@ public class Q674_Longest_Continuous_Increasing_Subsequence {
     }
     /**
      * Approach #1: Sliding Window [Accepted]
-     * Greedy
      */
     public int findLengthOfLCIS_1(int[] nums) {
         int max = 0;
@@ -39,5 +38,24 @@ public class Q674_Longest_Continuous_Increasing_Subsequence {
         }
 
         return max;
+    }
+    /**
+     * Greedy
+     */
+    public int findLengthOfLCIS_2(int[] nums) {
+        int result = 1; // 连续⼦序列最少也是1
+        int count = 1;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i + 1] > nums[i]) { // 连续记录
+                count++;
+            } else { // 不连续，count从头开始
+                count = 1;
+            }
+            if (count > result) {
+                result = count;
+            }
+        }
+        return result;
     }
 }
