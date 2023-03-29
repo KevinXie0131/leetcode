60e165fd5698fe5861edc68955d4b11f7d3d7f59
KevinX <Xzb04720638k>
2023-03-28 22:33:55 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q740_Delete_and_Earn.java
+++ src/main/java/com/answer/dynamic_programming/Q740_Delete_and_Earn.java
@@ -22,8 +22,8 @@ public class Q740_Delete_and_Earn {
         // f[i][0] 代表「不选」数值 i；f[i][1] 代表「选择」数值 i
         int[][] f = new int[max + 1][2];
         for (int i = 1; i <= max; i++) {
-            f[i][1] = f[i - 1][0] + i * cnts[i];
-            f[i][0] = Math.max(f[i - 1][1], f[i - 1][0]);
+            f[i][1] = f[i - 1][0] + i * cnts[i]; //当数值 i 被选，那么前一个数只能「不选」，同时为了总和最大数值 i 要选就全部选完
+            f[i][0] = Math.max(f[i - 1][1], f[i - 1][0]); // 当数值 i 不被选择，那么前一个数「可选/可不选」，在两者中取 max 即可
         }
         for(int[] arr : f){
             System.out.println(Arrays.toString(arr));
