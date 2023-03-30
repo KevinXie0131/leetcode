bdcaa0bcae62133d6bda1c1a8422da34ca6089e0
KevinX <Xzb04720638k>
2023-03-29 21:01:44 -0400

    update code

--- src/main/java/com/answer/two_pointers/Q392_Is_Subsequence.java
+++ src/main/java/com/answer/two_pointers/Q392_Is_Subsequence.java
@@ -16,14 +16,37 @@ public class Q392_Is_Subsequence {
         int i = 0 ;
         int j = 0 ;
         while(i< source.length && j < target.length){
-            if( source[i] == target[j]) {
+            if(source[i] == target[j]) {
                 i++;
 
-                if(i == source.length) return true;
+                if(i == source.length){
+                    return true;
+                }
             }
             j++;
         }
 
         return i == source.length;
+    }
+    /**
+     * Dynamic Programming
+     */
+    public boolean isSubsequence_2(String s, String t) {
+        int M = s.length();
+        int N = t.length();
+
+        int[][] dp = new int[M + 1][N + 1];
+
+        for(int i = 1; i <= M; i++){
+            for(int j = 1; j <= N; j++){
+                if(s.charAt(i - 1) == t.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }else{
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[M][N] == M;
     }
 }
