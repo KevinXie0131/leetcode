7e6c2c41b54358c8830efbca3983809a721c8f5f
KevinX <Xzb04720638k>
2023-03-29 21:44:10 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q583_Delete_Operation_for_Two_Strings.java
+++ src/main/java/com/answer/dynamic_programming/Q583_Delete_Operation_for_Two_Strings.java
@@ -20,6 +20,14 @@ public class Q583_Delete_Operation_for_Two_Strings {
                             Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1));
                 }
             }
+            /**
+             * 当word1[i - 1] 与 word2[j - 1]相同的时候，dp[i][j] = dp[i - 1][j - 1];
+             * 当word1[i - 1] 与 word2[j - 1]不相同的时候，有三种情况：
+             *     情况一：删word1[i - 1]，最少操作次数为dp[i - 1][j] + 1
+             *     情况二：删word2[j - 1]，最少操作次数为dp[i][j - 1] + 1
+             *     情况三：同时删word1[i - 1]和word2[j - 1]，操作的最少次数为dp[i - 1][j - 1] + 2
+             *     那最后当然是取最小值，所以当word1[i - 1] 与 word2[j - 1]不相同的时候，递推公式：dp[i][j] = min({dp[i - 1][j - 1] + 2, dp[i - 1][j] + 1, dp[i][j - 1] + 1});
+             */
         }
 
         return dp[word1.length()][word2.length()];
