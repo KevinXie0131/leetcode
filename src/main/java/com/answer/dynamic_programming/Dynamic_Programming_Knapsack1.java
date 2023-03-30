e70bdffb9bc3194c9937956d43e864fd73ab1b96
KevinX <Xzb04720638k>
2023-03-30 19:34:25 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Dynamic_Programming_Knapsack1.java
+++ src/main/java/com/answer/dynamic_programming/Dynamic_Programming_Knapsack1.java
@@ -44,4 +44,33 @@ public class Dynamic_Programming_Knapsack1 {
         //  System.out.println(Arrays.toString(dp));
         return dp[W];
     }
+    /**
+     * 完全背包的物品是可以添加多次的，所以要从⼩到⼤去遍历
+     * 先遍历物品，再遍历背包
+     */
+    static int knapsack_4(int bagWeight, int[] weight, int[] value){
+        int[] dp = new int[bagWeight + 1];
+        // 先遍历物品，再遍历背包
+        for(int i = 0; i < weight.length; i++) { // 遍历物品
+            for(int j = weight[i]; j <= bagWeight ; j++) { // 遍历背包容量
+                dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i]);
+            }
+        }
+        return dp[bagWeight];
+    }
+    /**
+     * 完全背包
+     * 先遍历被背包, 再遍历物品
+     */
+    static int knapsack_5(int bagWeight, int[] weight, int[] value){
+        int[] dp = new int[bagWeight + 1];
+        // 先遍历背包，再遍历物品
+        for(int j = 0; j <= bagWeight; j++) { // 遍历背包容量
+            for(int i = 0; i < weight.length; i++) { // 遍历物品
+                if (j - weight[i] >= 0)
+                    dp[j] = Math. max(dp[j], dp[j - weight[i]] + value[i]);
+            }
+        }
+        return dp[bagWeight];
+    }
 }
