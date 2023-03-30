d8774541fc81472ebc71fc639c2625ec76f2b332
KevinX <Xzb04720638k>
2023-03-30 14:45:02 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q416_Partition_Equal_Subset_Sum.java
+++ src/main/java/com/answer/dynamic_programming/Q416_Partition_Equal_Subset_Sum.java
@@ -30,6 +30,9 @@ public class Q416_Partition_Equal_Subset_Sum {
     }
     /**
      * Approach 4: Optimised Dynamic Programming - Using 1D Array
+     *
+     * 在动态规划：关于01背包问题，你该了解这些！（滚动数组）中就已经说明：
+     * 如果使⽤⼀维dp数组，物品遍历的for循环放在外层，遍历背包的for循环放在内层，且内层for循环倒叙遍历
      */
     public boolean canPartition(int[] nums) {
         int sum = 0;
@@ -39,14 +42,16 @@ public class Q416_Partition_Equal_Subset_Sum {
         if(sum % 2 == 1) return false;
         int target = sum / 2;
 
+        // 每个数组中的元素不会超过 100，数组的⼤⼩不会超过 200总和不会⼤于20000，背包最⼤只需要其中⼀半，所以10001⼤⼩就可以了
         int[] dp = new int[20001];
-
+        // 01背包
         for(int i = 0; i < nums.length; i++){
             for(int j= target; j >= nums[i]; j--){
+                // 每⼀个元素⼀定是不可重复放⼊，所以从⼤到⼩遍历
                 dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
             }
         }
-
+        // 集合中的元素正好可以凑成总和target
         return dp[target] == target;
     }
 }
