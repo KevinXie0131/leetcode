a2fc6aeb8c9c714d69c6742c0e26b2599955de5d
KevinX <Xzb04720638k>
2023-03-31 21:10:08 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q91_Decode_Ways.java
+++ src/main/java/com/answer/dynamic_programming/Q91_Decode_Ways.java
@@ -37,13 +37,16 @@ public class Q91_Decode_Ways {
      */
     public int numDecodings_1(String s) {
         int n = s.length();
+        // DP array to store the subproblem results
         int[] dp = new int[n + 1];
         dp[0] = 1;
 
         for (int i = 1; i <= n; ++i) {
+            // Check if successful single digit decode is possible.
             if (s.charAt(i - 1) != '0') {
                 dp[i] += dp[i - 1];
             }
+            // Check if successful two digit decode is possible.
             if (i > 1 && s.charAt(i - 2) != '0'
                     && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
                 dp[i] += dp[i - 2];
