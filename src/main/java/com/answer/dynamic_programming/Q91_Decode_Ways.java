ea95186d9095187b7f2cc2d7f5b7839d661fa6d4
KevinX <Xzb04720638k>
2023-03-31 21:06:43 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q91_Decode_Ways.java
+++ src/main/java/com/answer/dynamic_programming/Q91_Decode_Ways.java
@@ -37,18 +37,18 @@ public class Q91_Decode_Ways {
      */
     public int numDecodings_1(String s) {
         int n = s.length();
-        int[] f = new int[n + 1];
-        f[0] = 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
 
         for (int i = 1; i <= n; ++i) {
             if (s.charAt(i - 1) != '0') {
-                f[i] += f[i - 1];
+                dp[i] += dp[i - 1];
             }
             if (i > 1 && s.charAt(i - 2) != '0'
                     && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
-                f[i] += f[i - 2];
+                dp[i] += dp[i - 2];
             }
         }
-        return f[n];
+        return dp[n];
     }
 }
