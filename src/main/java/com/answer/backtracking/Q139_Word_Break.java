4537e06f330d624e4a650f358de2690cc56b4455
KevinX <Xzb04720638k>
2023-03-31 20:00:02 -0400

    update code

--- src/main/java/com/answer/backtracking/Q139_Word_Break.java
+++ src/main/java/com/answer/backtracking/Q139_Word_Break.java
@@ -115,6 +115,9 @@ public class Q139_Word_Break {
     /**
      * Approach 4: Using Dynamic Programming
      * Time complexity : O(n3)
+     *
+     * dp(i) = true if s.substring(i - word.length + 1, i + 1) == word and dp[i - word.length] == true
+     * for any word in wordDict, otherwise false
      */
     public static boolean wordBreak_5(String s, List<String> wordDict) {
         boolean[] dp = new boolean[s.length() + 1];
@@ -132,5 +135,23 @@ public class Q139_Word_Break {
         System.out.println(Arrays.toString(dp));
         return dp[s.length()];
     }
+    /**
+     * Bottom-up Implementation
+     */
+    public boolean wordBreak_6(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            for (String word : wordDict) {
+                // Make sure to stay in bounds while checking criteria
+                if (i >= word.length() - 1 && (i == word.length() - 1 || dp[i - word.length()])) {
+                    if (s.substring(i - word.length() + 1, i + 1).equals(word)) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
 
+        return dp[s.length() - 1];
+    }
 }
