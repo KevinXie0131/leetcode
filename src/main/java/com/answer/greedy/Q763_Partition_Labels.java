605f50f55240f55e9a9ba3e2a9b05aa89c2048d5
KevinX <Xzb04720638k>
2023-04-02 21:34:02 -0400

    update code

--- src/main/java/com/answer/greedy/Q763_Partition_Labels.java
+++ src/main/java/com/answer/greedy/Q763_Partition_Labels.java
@@ -11,6 +11,11 @@ public class Q763_Partition_Labels {
     /**
      * Approach 1: Greedy
      * For each letter encountered, process the last occurrence of that letter, extending the current partition [anchor, j] appropriately.
+     *
+     * 由于同一个字母只能出现在同一个片段，显然同一个字母的第一次出现的下标位置和最后一次出现的下标位置必须出现在同一个片段。
+     *
+     * 遍历一遍找到每个字符的最远位置
+     * 若找到之前遍历过的字母的最远边界，则找到分割点
      */
     public static List<Integer> partitionLabels(String s) {
         List<Integer> res = new ArrayList<>();
