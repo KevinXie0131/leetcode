00457e01e8fb72568e65e0225227a6100f631067
KevinX <Xzb04720638k>
2023-04-04 17:59:56 -0400

    update code

--- src/main/java/com/answer/backtracking/Q17_Letter_Combinations_of_a_Phone_Number.java
+++ src/main/java/com/answer/backtracking/Q17_Letter_Combinations_of_a_Phone_Number.java
@@ -3,8 +3,11 @@ package com.answer.backtracking;
 import java.util.*;
 
 public class Q17_Letter_Combinations_of_a_Phone_Number {
+    public static void main(String[] args) {
+        System.out.println(letterCombinations("23"));
+    }
 
-    public List<String> letterCombinations(String digits) {
+    public static List<String> letterCombinations(String digits) {
         Map<Character, String> map = new HashMap<>();
         map.put('2', "abc");
         map.put('3', "def");
@@ -22,7 +25,7 @@ public class Q17_Letter_Combinations_of_a_Phone_Number {
         return result;
     }
 
-    public void backtracking(String digits, List<String> result, int startIndex, StringBuffer path,  Map<Character, String> map ){
+    public static void backtracking(String digits, List<String> result, int startIndex, StringBuffer path,  Map<Character, String> map ){
         if(path.length() == digits.length()){
             result.add(path.toString());
             return;
