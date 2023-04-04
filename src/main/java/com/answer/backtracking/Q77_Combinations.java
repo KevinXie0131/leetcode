a6046cf84ac0ffacfd60d6dd0fd564b6916a4a19
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2023-04-04 16:07:55 -0400

    update code

--- src/main/java/com/answer/backtracking/Q77_Combinations.java
+++ src/main/java/com/answer/backtracking/Q77_Combinations.java
@@ -3,17 +3,20 @@ package com.answer.backtracking;
 import java.util.*;
 
 public class Q77_Combinations {
+    public static void main(String[] args) {
+        System.out.println(combine(4, 2));
+    }
 
-    List<List<Integer>> result = new ArrayList<List<Integer>>();
-    List<Integer> path = new ArrayList<>();
+    static List<List<Integer>> result = new ArrayList<List<Integer>>();
+    static List<Integer> path = new ArrayList<>();
 
-    public List<List<Integer>> combine(int n, int k) {
+    public static List<List<Integer>> combine(int n, int k) {
         backtracking(n, k, 1);
 
         return result;
     }
 
-    public void backtracking(int n, int k, int startIndex){
+    public static void backtracking(int n, int k, int startIndex){
         if (path.size() == k) {
             result.add(new ArrayList<Integer>(path));
         }
