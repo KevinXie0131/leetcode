8d8cf40761f7d099143f024450f8fbac97d1b623
KevinX <Xzb04720638k>
2023-04-05 14:40:55 -0400

    update code

--- src/main/java/com/answer/backtracking/Q784_Letter_Case_Permutation.java
+++ src/main/java/com/answer/backtracking/Q784_Letter_Case_Permutation.java
@@ -7,7 +7,9 @@ public class Q784_Letter_Case_Permutation {
     public static void main(String[] args) {
         System.out.println(letterCasePermutation("a1b2"));
     }
-
+    /**
+     * Backtracking
+     */
     static List<String> result = new ArrayList<String>();
     static public List<String> letterCasePermutation(String s) {
         char[] arr = s.toCharArray();
@@ -30,5 +32,32 @@ public class Q784_Letter_Case_Permutation {
     }
     static public char changeLetter(char c) {
         return (c >= 'a' && c <= 'z') ? (char) (c - 32) : (char) (c + 32);
+    }
+    /**
+     * Approach #1: Recursion [Accepted]
+     */
+    public List<String> letterCasePermutation_1(String S) {
+        List<StringBuilder> ans = new ArrayList();
+        ans.add(new StringBuilder());
+
+        for (char c: S.toCharArray()) {
+            int n = ans.size();
+            if (Character.isLetter(c)) {
+                for (int i = 0; i < n; ++i) {
+                    ans.add(new StringBuilder(ans.get(i)));
+                    ans.get(i).append(Character.toLowerCase(c));
+                    ans.get(n+i).append(Character.toUpperCase(c));
+                }
+            } else {
+                for (int i = 0; i < n; ++i)
+                    ans.get(i).append(c);
+            }
+        }
+
+        List<String> finalans = new ArrayList();
+        for (StringBuilder sb: ans) {
+            finalans.add(sb.toString());
+        }
+        return finalans;
     }
 }
