d1012d1173722c25f0fcb5cadebb5eb10d4e7074
KevinX <Xzb04720638k>
2023-04-04 21:32:22 -0400

    update code

--- src/main/java/com/answer/backtracking/Q491_Non_decreasing_Subsequences.java
+++ src/main/java/com/answer/backtracking/Q491_Non_decreasing_Subsequences.java
@@ -20,14 +20,15 @@ public class Q491_Non_decreasing_Subsequences {
     public void backtracking(int[] nums, int startIndex) {
         if(path.size() >= 2){
             result.add(new ArrayList(path));
-            //return;
+            //return;  // 注意这⾥不要加return，要取树上的节点
         }
         HashSet<Integer> uset = new HashSet<>();
         for (int i = startIndex; i < nums.length; i++) {
             if (!path.isEmpty() && nums[i] < path.getLast()){
                 continue;
             }
-            if(!uset.add(nums[i])) {
+            // 记录这个元素在本层⽤过了，本层后⾯不能再⽤了
+            if(!uset.add(nums[i])) { // 使⽤set来对本层元素进⾏去重 同⼀⽗节点下的同层上使⽤过的元素就不能在使⽤了
                 continue;
             }
             path.add(nums[i]);
