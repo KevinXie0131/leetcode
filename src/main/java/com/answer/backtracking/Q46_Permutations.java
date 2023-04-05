bd2ab148cb8d34a4bfb2b993b5dabbc21c9eb93c
KevinX <Xzb04720638k>
2023-04-04 21:45:17 -0400

    update code

--- src/main/java/com/answer/backtracking/Q46_Permutations.java
+++ src/main/java/com/answer/backtracking/Q46_Permutations.java
@@ -3,7 +3,11 @@ package com.answer.backtracking;
 import java.util.*;
 
 public class Q46_Permutations {
-
+    /**
+     * 排列问题的不同:
+     *    每层都是从0开始搜索⽽不是startIndex
+     *    需要used数组记录path⾥都放了哪些元素了
+     */
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         Deque<Integer> path = new ArrayDeque<>();
@@ -14,17 +18,17 @@ public class Q46_Permutations {
     }
 
     public void backtracking(int[] nums, int[] used, List<List<Integer>> result, Deque<Integer> path){
-        if (path.size() == nums.length) {
+        if (path.size() == nums.length) {  // 此时说明找到了⼀组
             result.add(new ArrayList<Integer>(path));
             return;
         }
         for(int i = 0; i < nums.length; i++){
             if( used[i] == 1){
-                continue;
+                continue; // path⾥已经收录的元素，直接跳过
             }
             path.addLast(nums[i]);
             used[i] = 1;
-            backtracking(nums, used, result, path);
+            backtracking(nums, used, result, path); // 所以处理排列问题就不⽤使⽤startIndex了
             path.removeLast();
             used[i] = 0;
         }
