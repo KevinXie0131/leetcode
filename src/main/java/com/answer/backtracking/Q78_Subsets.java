e1a849ed16e526de56d6ede2d4b9b56142aadaa0
KevinX <Xzb04720638k>
2023-04-04 20:28:21 -0400

    update code

--- src/main/java/com/answer/backtracking/Q78_Subsets.java
+++ src/main/java/com/answer/backtracking/Q78_Subsets.java
@@ -3,6 +3,9 @@ package com.answer.backtracking;
 import java.util.*;
 
 public class Q78_Subsets {
+    /**
+     * 求取⼦集问题，不需要任何剪枝！因为⼦集就是要遍历整棵树。
+     */
     List<List<Integer>> result = new ArrayList<List<Integer>>();
     Deque<Integer> path = new LinkedList<>();
 
@@ -16,7 +19,7 @@ public class Q78_Subsets {
     }
 
     public void backtracking(int[] nums, int startIndex) {
-        result.add(new ArrayList(path));
+        result.add(new ArrayList(path)); // 收集⼦集，要放在终⽌添加的上⾯，否则会漏掉⾃⼰
         /**
          * if(startIndex > nums.length - 1){
          */
@@ -26,7 +29,7 @@ public class Q78_Subsets {
 
         for(int i = startIndex; i < nums.length; i++){
             path.add(nums[i]);
-            backtracking(nums, i + 1);
+            backtracking(nums, i + 1); // 注意从i+1开始，元素不重复取
             path.removeLast();
         }
 
