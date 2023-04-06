86c8a5ae2c4b345df31c320b6f63d27d4ce20c4d
KevinX <Xzb04720638k>
2023-04-06 19:20:49 -0400

    update code

--- src/main/java/com/answer/tree/Q104_Maximum_Depth_of_Binary_Tree.java
+++ src/main/java/com/answer/tree/Q104_Maximum_Depth_of_Binary_Tree.java
@@ -5,15 +5,20 @@ import com.template.TreeNode;
 import java.util.*;
 
 public class Q104_Maximum_Depth_of_Binary_Tree {
+    /**
+     * ⼆叉树的深度为根节点到最远叶⼦节点的最长路径上的节点数
+     * 说明: 叶⼦节点是指没有⼦节点的节点
+     */
     public int maxDepth(TreeNode root) {
         if (root == null) {
-            return 0;
+            return 0; // 确定终⽌条件：如果为空节点的话，就返回0，表⽰⾼度为0
         }
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        return Math.max(left, right) + 1;
+        // 确定单层递归的逻辑：先求它的左⼦树的深度，再求的右⼦树的深度，最后取左右
+        //                    深度最⼤的数值 再+1 （加1是因为算上当前中间节点）就是⽬前节点为根节点的树的深度
+        int left = maxDepth(root.left);    // 左
+        int right = maxDepth(root.right);  // 右
+        return Math.max(left, right) + 1;  // 中
     }
-
     /**
      *
      */
