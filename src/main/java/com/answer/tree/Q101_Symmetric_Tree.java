72a564ad0c65f48a6fa67367dfc812fb39dc685e
KevinX <Xzb04720638k>
2023-04-06 19:14:37 -0400

    update code

--- src/main/java/com/answer/tree/Q101_Symmetric_Tree.java
+++ src/main/java/com/answer/tree/Q101_Symmetric_Tree.java
@@ -11,22 +11,30 @@ public class Q101_Symmetric_Tree {
 
         return dfs(root.left, root.right);
     }
-
+    /**
+     * 本题遍历只能是“后序遍历”，因为我们要通过递归函数的返回值来判断两个⼦树的内侧节点
+     * 和外侧节点是否相等。
+     */
     public boolean dfs(TreeNode left, TreeNode right) {
         if (left == null && right == null) {
             return true;
         }
-        if (left == null || right == null) {
+        if (left == null || right == null) {  // if (left == NULL && right != NULL) return false;
+            return false;                     // else if (left != NULL && right == NULL) return false;
+        }
+        if (left.value != right.value) { // 排除了空节点，再排除数值不相同的情况
             return false;
         }
-        if (left.value != right.value) {
-            return false;
-        }
+        // ⽐较⼆叉树外侧是否对称：传⼊的是左节点的左孩⼦，右节点的右孩⼦。
+        // ⽐较内测是否对称，传⼊左节点的右孩⼦，右节点的左孩⼦。
+        // 如果左右都对称就返回true ，有⼀侧不对称就返回false 。
 
-        boolean isLeft = dfs(left.left, right.right);
-        boolean isRight = dfs(left.right, right.left);
+        // 此时就是：左右节点都不为空，且数值相同的情况
+        // 此时才做递归，做下⼀层的判断
+        boolean isLeft = dfs(left.left, right.right); // 左⼦树：左、 右⼦树：右
+        boolean isRight = dfs(left.right, right.left); // 左⼦树：右、 右⼦树：左
 
-        return isLeft && isRight;
+        return isLeft && isRight; // 左⼦树：中、 右⼦树：中（逻辑处理）
     }
 
     /**
