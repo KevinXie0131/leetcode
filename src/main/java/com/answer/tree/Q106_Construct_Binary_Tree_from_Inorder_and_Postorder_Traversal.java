1b51d97d5a053e8810c3674b1545bcc26d453dc1
KevinX <Xzb04720638k>
2023-04-07 19:16:48 -0400

    tree

--- src/main/java/com/answer/tree/Q106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.java
+++ src/main/java/com/answer/tree/Q106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.java
@@ -5,26 +5,33 @@ import com.template.TreeNode;
 import java.util.*;
 
 public class Q106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal {
+    /**
+     * 根据⼀棵树的中序遍历与后序遍历构造⼆叉树
+     * 以后序数组的最后⼀个元素为切割点，先切中序数组，根据中序数组，反过来在切后序数组。⼀层⼀层切下去，每次后序数组最后⼀个元素就是节点元素
+     *
+     * 递归
+     */
     public TreeNode buildTree(int[] inorder, int[] postorder) {
 
-        if(inorder.length == 0){
+        if(inorder.length == 0){ // 第⼀步
             return null;
         }
-        int inorderRoot = -1;
+        int inorderRoot = -1; // 第三步：找切割点
         for (int i = 0; i < inorder.length; i++) {
             if (postorder[postorder.length-1] == inorder[i]) {
                 inorderRoot = i;
                 break;
             }
         }
-        TreeNode root = new TreeNode(postorder[postorder.length-1]);
-
+        TreeNode root = new TreeNode(postorder[postorder.length-1]); // 第⼆步：后序遍历数组最后⼀个元素，就是当前的中间节点
+        // 第四步：切割中序数组，得到 中序左数组和中序右数组
         root.right = buildTree(Arrays.copyOfRange(inorder, inorderRoot + 1, inorder.length),
                 Arrays.copyOfRange(postorder, inorderRoot, postorder.length -1));
-
+        // 第五步：切割后序数组，得到 后序左数组和后序右数组
         root.left = buildTree(Arrays.copyOfRange(inorder, 0, inorderRoot),
                 Arrays.copyOfRange(postorder, 0, inorderRoot));
-
+        // ⾸先后序数组的最后⼀个元素指定不能要了，这是切割点 也是 当前⼆叉树中间节点的元素，已经⽤了
+        // 中序数组⼤⼩⼀定是和后序数组的⼤⼩相同的（这是必然）
         return root;
     }
     /**
