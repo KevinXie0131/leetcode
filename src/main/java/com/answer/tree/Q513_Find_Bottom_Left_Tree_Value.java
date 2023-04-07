d400b54dd4e3207564f91e806866039b8bfda14a
KevinX <Xzb04720638k>
2023-04-07 18:13:43 -0400

    tree

--- src/main/java/com/answer/tree/Q513_Find_Bottom_Left_Tree_Value.java
+++ src/main/java/com/answer/tree/Q513_Find_Bottom_Left_Tree_Value.java
@@ -8,23 +8,44 @@ import java.util.Deque;
 import java.util.List;
 
 public class Q513_Find_Bottom_Left_Tree_Value {
+    /**
+     * 本地要找出树的最后⼀⾏找到最左边的值。此时⼤家应该想起⽤层序遍历是⾮常简单的了，反⽽⽤递归的话会⽐较难⼀点
+     *
+     * 如果需要遍历整颗树，递归函数就不能有返回值。如果需要遍历某⼀条固定路线，递归函数就⼀定要有返回值
+     */
     int result, maxHeight;
     public int findBottomLeftValue(TreeNode root) {
-        result = 0;
-        maxHeight = -1;
+        result = 0; // 全局变量 最⼤深度最左节点的数值
+        maxHeight = -1; // 全局变量 记录最⼤深度
         dfs(root, 0);
         return result;
     }
-
-    private void dfs(TreeNode node, int level) {
+    /**
+     * 递归
+     * ⾸先要是最后⼀⾏，然后是最左边的值, 所以要找深度最⼤的叶⼦节点
+     * 可以使⽤前序遍历，这样才先优先左边搜索，然后记录深度最⼤的叶⼦节点，此时就是树的最后⼀⾏最左边的值
+     */
+    private void dfs(TreeNode node, int level) { // 是要遍历整个树找到最深的叶⼦节点，需要遍历整颗树，所以递归函数没有返回值
         if (node == null) return;
         if (level > maxHeight) {
-            maxHeight = level;
-            result = node.value;
+            maxHeight = level;   // 更新最⼤深度
+            result = node.value; // 最⼤深度最左⾯的数值
         }
-        dfs(node.left, level + 1);
-        dfs(node.right, level + 1);
-
+        dfs(node.left, level + 1);  // 隐藏着回溯
+        dfs(node.right, level + 1); // 隐藏着回溯
+        /**
+         * 回溯
+         * if (root->left) { // 左
+         *      leftLen++; // 深度加⼀
+         *      traversal(root->left, leftLen);
+         *      leftLen--; // 回溯，深度减⼀
+         * }
+         * if (root->right) { // 右
+         *      leftLen++; // 深度加⼀
+         *      traversal(root->right, leftLen);
+         *      leftLen--; // 回溯，深度减⼀
+         * }
+         */
     }
     /**
      *
@@ -50,5 +71,4 @@ public class Q513_Find_Bottom_Left_Tree_Value {
         }
         return value;
     }
-
 }
