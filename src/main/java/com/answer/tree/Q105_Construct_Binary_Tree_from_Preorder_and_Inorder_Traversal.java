6674a37f2464c3f30689cd380e0aa963363769ac
KevinX <Xzb04720638k>
2023-04-07 19:33:01 -0400

    tree

--- src/main/java/com/answer/tree/Q105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
+++ src/main/java/com/answer/tree/Q105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
@@ -6,6 +6,20 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class Q105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal {
+    public static void main(String[] args) {
+        int[] preorder = {3,9,20,15,7};
+        int[] inorder = {9,3,15,20,7};
+        TreeNode root = buildTree_1(preorder, inorder);
+        System.out.println(root);
+    }
+    /**
+     * 根据⼀棵树的前序遍历与中序遍历构造⼆叉树
+     *
+     * 前序和中序可以唯⼀确定⼀颗⼆叉树。
+     * 后序和中序可以唯⼀确定⼀颗⼆叉树。
+     * 那么前序和后序可不可以唯⼀确定⼀颗⼆叉树呢？
+     *    前序和后序不能唯⼀确定⼀颗⼆叉树！，因为没有中序遍历⽆法确定左右部分，也就是⽆法分割
+     */
     int pre_idx;
     int[] preorder;
     int[] inorder;
@@ -45,5 +59,28 @@ public class Q105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal {
         for (Integer val : inorder)
             idx_map.put(val, idx++);
         return helper(0, inorder.length - 1);
+    }
+
+    public static TreeNode buildTree_1(int[] preorder, int[] inorder) {
+
+        if(inorder.length == 0){ // 第⼀步
+            return null;
+        }
+        int inorderRoot = -1; // 第三步：找切割点
+        for (int i = 0; i < inorder.length; i++) {
+            if (preorder[0] == inorder[i]) {
+                inorderRoot = i;
+                break;
+            }
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+
+        root.right = buildTree_1(Arrays.copyOfRange(preorder, inorderRoot + 1, preorder.length),
+                Arrays.copyOfRange(inorder, inorderRoot + 1, inorder.length));
+
+        root.left = buildTree_1(Arrays.copyOfRange(preorder, 1, inorderRoot + 1),
+                Arrays.copyOfRange(inorder, 0, inorderRoot));
+
+        return root;
     }
 }
