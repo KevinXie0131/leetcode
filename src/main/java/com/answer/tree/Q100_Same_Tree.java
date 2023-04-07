cb0b60896563ba6b61f2008f52cdebd7b88f8c29
KevinX <Xzb04720638k>
2023-04-06 21:21:06 -0400

    tree

--- src/main/java/com/answer/tree/Q100_Same_Tree.java
+++ src/main/java/com/answer/tree/Q100_Same_Tree.java
@@ -10,16 +10,17 @@ public class Q100_Same_Tree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
             return true;
-        } else if (p == null && q != null) {
+        } else if (p == null && q != null) { // ⾸先排除空节点的情况
             return false;
         } else if (p != null && q == null) {
             return false;
-        } else if (p.value != q.value) {
+        } else if (p.value != q.value) { // 排除了空节点，再排除数值不相同的情况
             return false;
         }
-        boolean left = isSameTree(p.left, q.left);
-        boolean right = isSameTree(p.right, q.right);
-        return left && right;
-
+        // 此时就是：左右节点都不为空，且数值相同的情况
+        // 此时才做递归，做下⼀层的判断
+        boolean left = isSameTree(p.left, q.left); // 左⼦树: 左、 右⼦树：左 （相对于求对称⼆叉树，只需改⼀下这⾥的顺序）
+        boolean right = isSameTree(p.right, q.right); // 左⼦树: 右、 右⼦树：右
+        return left && right; // 左⼦树: 中、 右⼦树: 中 （逻辑处理）
     }
 }
