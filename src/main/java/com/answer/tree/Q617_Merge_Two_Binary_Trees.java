e8c141a5867e99173033536498fa63384214aa2e
KevinX <Xzb04720638k>
2023-04-07 20:44:00 -0400

    tree

--- src/main/java/com/answer/tree/Q617_Merge_Two_Binary_Trees.java
+++ src/main/java/com/answer/tree/Q617_Merge_Two_Binary_Trees.java
@@ -8,16 +8,23 @@ import java.util.Deque;
 import java.util.List;
 
 public class Q617_Merge_Two_Binary_Trees {
+    /**
+     * 合并两个⼆叉树
+     * 注意: 合并必须从两个树的根节点开始。
+     *
+     * 递归 前序遍历
+     * 中序遍历也是可以的
+     */
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        if (root1 == null) {
+        if (root1 == null) { // 如果t1为空，合并之后就应该是t2
             return root2;
         }
-        if (root2 == null) {
+        if (root2 == null) { // 如果t2为空，合并之后就应该是t1
             return root1;
         }
-        TreeNode node = new TreeNode(root1.value+root2.value);
-        node.left=mergeTrees(root1.left,  root2.left);
-        node.right=mergeTrees(root1.right,  root2.right);
+        TreeNode node = new TreeNode(root1.value+root2.value); // 中 重新定义新的节点，不修改原有两个树的结构
+        node.left=mergeTrees(root1.left,  root2.left);                // 左
+        node.right=mergeTrees(root1.right,  root2.right);             // 右
         return node;
     }
     /**
@@ -39,7 +46,9 @@ public class Q617_Merge_Two_Binary_Trees {
         return node;
     }
     /**
-     *
+     * 迭代法
+     * 把两
+     * 个树的节点同时加⼊队列
      */
     public TreeNode mergeTrees2(TreeNode root1, TreeNode root2) {
         if (root1 == null) {
@@ -55,20 +64,20 @@ public class Q617_Merge_Two_Binary_Trees {
             TreeNode r1 = queue.poll();
             TreeNode r2 = queue.poll();
 
-            r1.value = r1.value + r2.value;
+            r1.value = r1.value + r2.value; // 此时两个节点⼀定不为空，val相加
 
-            if(r1.left!=null && r2.left!=null){
+            if(r1.left!=null && r2.left!=null){ // 如果两棵树左节点都不为空，加⼊队列
                 queue.offer(r1.left);
                 queue.offer(r2.left);
             }
-            if(r1.right!=null && r2.right!=null){
+            if(r1.right!=null && r2.right!=null){ // 如果两棵树右节点都不为空，加⼊队列
                 queue.offer(r1.right);
                 queue.offer(r2.right);
             }
-            if(r1.left==null && r2.left!=null) {
+            if(r1.left==null && r2.left!=null) { // 当t1的左节点 为空 t2左节点不为空，就赋值过去
                 r1.left = r2.left;
             }
-            if(r1.right==null && r2.right!=null) {
+            if(r1.right==null && r2.right!=null) { // 当t1的右节点 为空 t2右节点不为空，就赋值过去
                 r1.right = r2.right;
             }
         }
