ef6ae5dee249bd3089f5d3d7cd799a829353f0c5
KevinX <Xzb04720638k>
2023-04-07 20:49:00 -0400

    tree

--- src/main/java/com/answer/tree/Q700_Search_in_a_Binary_Search_Tree.java
+++ src/main/java/com/answer/tree/Q700_Search_in_a_Binary_Search_Tree.java
@@ -3,12 +3,19 @@ package com.answer.tree;
 import com.template.TreeNode;
 
 public class Q700_Search_in_a_Binary_Search_Tree {
+    /**
+     * ⼆叉搜索树中的搜索
+     * ⼆叉搜索树是⼀个有序树：
+     *    若它的左⼦树不空，则左⼦树上所有结点的值均⼩于它的根结点的值；
+     *    若它的右⼦树不空，则右⼦树上所有结点的值均⼤于它的根结点的值；
+     *    它的左、右⼦树也分别为⼆叉搜索树
+     */
     public TreeNode searchBST(TreeNode root, int val) {
         if (root == null)
             return null;
         if (root.value == val)
             return root;
-
+        // 如果root->val > val，搜索左⼦树，如果root->val < val，就搜索右⼦树，最后如果都没有搜索到，就返回NULL。
         TreeNode result = null;
         if (root.value > val)
             result = searchBST(root.left, val);
@@ -31,9 +38,7 @@ public class Q700_Search_in_a_Binary_Search_Tree {
         if (root.value > val) {
             return searchBST(root.left, val);
         }
-
         return null;
-
     }
     /**
      *
@@ -49,8 +54,6 @@ public class Q700_Search_in_a_Binary_Search_Tree {
             }
 
         }
-
         return null;
-
     }
 }
