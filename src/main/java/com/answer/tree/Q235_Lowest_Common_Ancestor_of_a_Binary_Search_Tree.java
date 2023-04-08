ca7b53b9047692a9e241c7e06e3adf1e00a87af6
KevinX <Xzb04720638k>
2023-04-07 23:04:34 -0400

    tree

--- src/main/java/com/answer/tree/Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
+++ src/main/java/com/answer/tree/Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -5,6 +5,12 @@ import com.template.TreeNode;
 import java.util.*;
 
 public class Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
+    /**
+     * ⼆叉搜索树的最近公共祖先 - 前序遍历
+     * 给定⼀个⼆叉搜索树, 找到该树中两个指定节点的最近公共祖先
+     *
+     * 普通⼆叉树求最近公共祖先需要使⽤回溯，从底向上来查找，⼆叉搜索树就不⽤了，因为搜索树有序（相当于⾃带⽅向），那么只要从上向下遍历就可以了
+     */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if ((root.left == p && root.right == q) ||
                 (root.left == q && root.right == p)) {
@@ -17,7 +23,39 @@ public class Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
         return root;
     }
     /**
-     *
+     * 在遍历⼆叉搜索树的时候就是寻找区间[p->val, q->val]（注意这⾥是左闭又闭）
+     * 那么如果 cur->val ⼤于 p->val，同时 cur->val ⼤于q->val，那么就应该向左遍历（说明⽬标区间在左⼦树上）。
+     * 如果 cur->val ⼩于 p->val，同时 cur->val ⼩于 q->val，那么就应该向右遍历（⽬标区间在右⼦树）。
+     */
+    public TreeNode lowestCommonAncestor_0(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+
+        if (p.value < root.value && q.value < root.value) {
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            if(left != null) return left;
+        }
+        if (p.value > root.value && q.value > root.value) {
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+            if(right != null) return right;
+        }
+        return root;
+    }
+    /**
+     * 精简后代码
+     */
+    public TreeNode lowestCommonAncestor3(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+
+        if(root.value > p.value && root.value > q.value){
+            return lowestCommonAncestor(root.left, p,q);
+        }else if(root.value < p.value && root.value < q.value){
+            return lowestCommonAncestor(root.right, p,q);
+        }else{
+            return root;
+        }
+    }
+    /**
+     * 迭代法
      */
     public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
         while (root != null) {
@@ -26,7 +64,7 @@ public class Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
             } else if (p.value > root.value && q.value > root.value) {
                 root = root.right;
             } else {
-                return root;
+                return root; // 剩下的情况，就是cur节点在区间中，那么cur就是最近公共祖先了，直接返回cur
             }
         }
         return root;
@@ -52,20 +90,6 @@ public class Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
             return root;
         } else {
             return null;
-        }
-    }
-    /**
-     *
-     */
-    public TreeNode lowestCommonAncestor3(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return null;
-
-        if(root.value > p.value && root.value > q.value){
-            return lowestCommonAncestor(root.left, p,q);
-        }else if(root.value < p.value && root.value < q.value){
-            return lowestCommonAncestor(root.right, p,q);
-        }else{
-            return root;
         }
     }
     /**
