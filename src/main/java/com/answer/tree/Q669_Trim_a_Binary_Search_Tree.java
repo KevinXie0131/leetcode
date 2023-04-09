d2ac51814a35e5707efac3f921085c91cfd84092
KevinX <Xzb04720638k>
2023-04-09 19:20:33 -0400

    tree

--- src/main/java/com/answer/tree/Q669_Trim_a_Binary_Search_Tree.java
+++ src/main/java/com/answer/tree/Q669_Trim_a_Binary_Search_Tree.java
@@ -3,21 +3,33 @@ package com.answer.tree;
 import com.template.TreeNode;
 
 public class Q669_Trim_a_Binary_Search_Tree {
-
+    /**
+     * 修剪⼆叉搜索树
+     */
     public TreeNode trimBST(TreeNode root, int low, int high) {
 
         if(root == null){
             return null;
         }
+        // 如果root（当前节点）的元素⼩于low的数值，那么应该递归右⼦树，并返回右⼦树符合条件的头结点。
         if (root.value < low) {
-            return trimBST( root.right,  low,  high);
+            return trimBST( root.right,  low,  high); // 寻找符合区间[low, high]的节点
+            /**
+             * TreeNode right = trimBST( root.right,  low,  high);
+             * return right;
+             */
         }
+        // 如果root(当前节点)的元素⼤于high的，那么应该递归左⼦树，并返回左⼦树符合条件的头结点
         if (root.value > high) {
-            return trimBST( root.left,  low,  high);
+            return trimBST( root.left,  low,  high); // 寻找符合区间[low,high]的节点
+            /**
+             * TreeNode left = trimBST( root.left,  low,  high);
+             * return left;
+             */
         }
         if (root.value >= low && root.value <= high) {
-            root.left = trimBST( root.left,  low,  high);
-            root.right = trimBST( root.right,  low,  high);
+            root.left = trimBST( root.left,  low,  high); // root->left接⼊符合条件的左孩⼦
+            root.right = trimBST( root.right,  low,  high); // root->right接⼊符合条件的右孩⼦
         }
 
         return root;
