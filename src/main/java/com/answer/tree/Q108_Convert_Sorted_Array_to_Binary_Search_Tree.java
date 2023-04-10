3b2e10ebc27a6fa773fa08f0a21ca0842e1bf06d
KevinX <Xzb04720638k>
2023-04-09 22:28:44 -0400

    tree

--- src/main/java/com/answer/tree/Q108_Convert_Sorted_Array_to_Binary_Search_Tree.java
+++ src/main/java/com/answer/tree/Q108_Convert_Sorted_Array_to_Binary_Search_Tree.java
@@ -1,6 +1,7 @@
 package com.answer.tree;
 
 import com.template.TreeNode;
+import java.util.*;
 
 public class Q108_Convert_Sorted_Array_to_Binary_Search_Tree {
     /**
@@ -26,6 +27,46 @@ public class Q108_Convert_Sorted_Array_to_Binary_Search_Tree {
         root.left = dfs(nums, left, mid -1);   // root的左孩⼦接住下⼀层左区间的构造节点
         root.right = dfs(nums, mid + 1, right); // 右孩⼦接住下⼀层右区间构造的节点
 
+        return root;
+    }
+    /**
+     * 迭代法
+     * 可以通过三个队列来模拟，⼀个队列放遍历的节点，⼀个队列放左区间下表，⼀个队列放右区间下表
+     */
+    public TreeNode sortedArrayToBST_1(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(0); // 初始根节点
+        Deque<TreeNode> nodeQue = new ArrayDeque<>(); // 放遍历的节点
+        Deque<Integer> leftQue = new ArrayDeque<>(); // 保存左区间下表
+        Deque<Integer> rightQue = new ArrayDeque<>(); // 保存右区间下表
+
+        nodeQue.offer(root); // 根节点⼊队列
+        leftQue.offer(0); // 0为左区间下表初始位置
+        rightQue.offer(nums.length - 1); // nums.size() - 1为右区间下表初始位置
+
+        while (!nodeQue.isEmpty()) {
+            TreeNode curNode = nodeQue.poll();
+            int left = leftQue.poll();
+            int right = rightQue.poll();
+
+            int mid = left + ((right - left) / 2);
+            curNode.value = nums[mid]; // 将mid对应的元素给中间节点
+
+            if (left <= mid - 1) { // 处理左区间
+                curNode.left = new TreeNode(0);
+                nodeQue.push(curNode.left);
+                leftQue.push(left);
+                rightQue.push(mid - 1);
+            }
+            if (right >= mid + 1) { // 处理右区间
+                curNode.right = new TreeNode(0);
+                nodeQue.push(curNode.right);
+                leftQue.push(mid + 1);
+                rightQue.push(right);
+            }
+        }
         return root;
     }
 }
