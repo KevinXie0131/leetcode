433901890c0dc0842d367ce1220ea2fe6737a12f
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2024-12-25 21:57:58 -0500

    update code

--- src/main/java/com/answer/two_pointers/Q27_Remove_Element.java
+++ src/main/java/com/answer/two_pointers/Q27_Remove_Element.java
@@ -2,6 +2,14 @@ package com.answer.two_pointers;
 
 public class Q27_Remove_Element {
 
+    public static void main(String[] args) {
+       int[]  nums = {0,1,2,2,3,0,4,2};
+       int val = 2;
+
+       int result =  removeElement_0(nums, val);
+       System.out.println(result);
+
+    }
     public int removeElement(int[] nums, int val) {
 
         int slowIndex = 0;
@@ -15,11 +23,27 @@ public class Q27_Remove_Element {
 
         return slowIndex;
     }
+    //相向双指针法
+    public static int removeElement_0(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(right >= 0 && nums[right] == val) right--; //将right移到从右数第一个值不为val的位置
+        while(left <= right) {
+            if(nums[left] == val) { //left位置的元素需要移除
+                //将right位置的元素移到left（覆盖），right位置移除
+                nums[left] = nums[right];
+                right--;
+            }
+            left++;
+            while(right >= 0 && nums[right] == val) right--;
+        }
+        return left;
+    }
 
     /**
-     *
+     * 相向双指针法（版本二）
      */
-    public int removeElement_1(int[] nums, int val) {
+    public static int removeElement_1(int[] nums, int val) {
 
         int left = 0;
         int right = nums.length - 1;
@@ -29,6 +53,7 @@ public class Q27_Remove_Element {
                 nums[left] = nums[right];
                 right--;
             } else {
+                // 这里兼容了right指针指向的值与val相等的情况
                 left++;
             }
         }
--- src/main/java/com/answer/two_pointers/Q977_Squares_of_a_Sorted_Array.java
+++ src/main/java/com/answer/two_pointers/Q977_Squares_of_a_Sorted_Array.java
@@ -19,4 +19,23 @@ public class Q977_Squares_of_a_Sorted_Array {
 
         return result;
     }
+
+    public int[] sortedSquares_0(int[] nums) {
+        int right = nums.length - 1;
+        int left = 0;
+        int[] result = new int[nums.length];
+        int index = result.length - 1;
+        while (left <= right) {
+            if (nums[left] * nums[left] > nums[right] * nums[right]) {
+                // 正数的相对位置是不变的， 需要调整的是负数平方后的相对位置
+                result[index] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[index] = nums[right] * nums[right];
+                right++;
+            }
+            index--;
+        }
+        return result;
+    }
 }
