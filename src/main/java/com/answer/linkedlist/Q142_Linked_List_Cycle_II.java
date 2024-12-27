ab340709de5aa7f5d664662b6a3541f8847cccc6
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2024-12-26 21:48:20 -0500

    update code

--- src/main/java/com/answer/linkedlist/Q142_Linked_List_Cycle_II.java
+++ src/main/java/com/answer/linkedlist/Q142_Linked_List_Cycle_II.java
@@ -18,13 +18,17 @@ public class Q142_Linked_List_Cycle_II {
     public static ListNode detectCycle(ListNode head) {
         ListNode fast = head, slow = head;
         while (true) {
-            if (fast == null || fast.next == null) return null;
+            if (fast == null || fast.next == null) {
+                return null; //遇到null了，说明不存在环
+            }
             fast = fast.next.next;
             slow = slow.next;
-            if (fast == slow) break;
+            if (fast == slow) {
+                break; //第一次相遇在Z点
+            }
         }
-        fast = head;
-        while(fast != slow){
+        fast = head; //从头开始走，
+        while(fast != slow){   //二者相遇在Y点，则退出
             slow = slow.next;
             fast = fast.next;
         }
@@ -44,6 +48,28 @@ public class Q142_Linked_List_Cycle_II {
                 set.add(cur);
             }
             cur = cur.next;
+        }
+        return null;
+    }
+    /**
+     *
+     */
+    public ListNode detectCycle_2(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null && slow != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {// 有环
+                ListNode index1 = fast;
+                ListNode index2 = head;
+                // 两个指针，从头结点和相遇结点，各走一步，直到相遇，相遇点即为环入口
+                while (index1 != index2) {
+                    index1 = index1.next;
+                    index2 = index2.next;
+                }
+                return index1;
+            }
         }
         return null;
     }
