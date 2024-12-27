a33e6971a4d854ec045944c9d569d0620fda85b4
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2024-12-26 20:52:43 -0500

    update code

--- src/main/java/com/answer/linkedlist/Q19_Remove_Nth_Node_From_End_of_List.java
+++ src/main/java/com/answer/linkedlist/Q19_Remove_Nth_Node_From_End_of_List.java
@@ -20,4 +20,56 @@ public class Q19_Remove_Nth_Node_From_End_of_List {
 
         return dummy.next;
     }
+    /**
+     * 虚拟头节点
+     */
+    public ListNode removeNthFromEnd_0(ListNode head, int n) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        while(n > 0){
+            fast = fast.next;
+            n--;
+        }
+        fast = fast.next; // fast再提前走一步，因为需要让slow指向删除节点的上一个节点
+
+        while(fast != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return dummy.next;
+
+    }
+    /**
+     * 虚拟头节点
+     */
+    public ListNode removeNthFromEnd_1(ListNode head, int n) {
+        //新建一个虚拟头节点指向head
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
+        //快慢指针指向虚拟头节点
+        ListNode fastIndex = dummyNode;
+        ListNode slowIndex = dummyNode;
+
+        // 只要快慢指针相差 n 个结点即可
+        for (int i = 0; i <= n; i++) {
+            fastIndex = fastIndex.next;
+        }
+        while (fastIndex != null) {
+            fastIndex = fastIndex.next;
+            slowIndex = slowIndex.next;
+        }
+
+        // 此时 slowIndex 的位置就是待删除元素的前一个位置。
+        // 具体情况可自己画一个链表长度为 3 的图来模拟代码来理解
+        // 检查 slowIndex.next 是否为 null，以避免空指针异常
+        if (slowIndex.next != null) {
+            slowIndex.next = slowIndex.next.next;
+        }
+        return dummyNode.next;
+    }
 }
