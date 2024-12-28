071f1dfe27323df033a636017f1f2bcb1d85f8cd
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2024-12-27 21:40:57 -0500

    update code

--- src/main/java/com/answer/hashmap/Q454_4Sum_II.java
+++ src/main/java/com/answer/hashmap/Q454_4Sum_II.java
@@ -3,7 +3,32 @@ package com.answer.hashmap;
 import java.util.*;
 
 public class Q454_4Sum_II {
-
+    /**
+     * 使用哈希法
+     */
+    public int fourSumCount_0(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        //统计两个数组中的元素之和，同时统计出现的次数，放入map
+        for(int i : nums1){
+            for(int j : nums2){
+                map.put(i + j, map.getOrDefault(i + j, 0) + 1);
+            }
+        }
+        //统计剩余的两个元素的和，在map中找是否存在相加为0的情况，同时记录次数
+        for(int i : nums3){
+            for(int j : nums4){
+                // count +=  map.getOrDefault(0 - (i + j), 0);
+                if(map.containsKey(0 - (i + j))){
+                    count += map.get(0 - (i + j));
+                }
+            }
+        }
+        return count;
+    }
+    /**
+     *
+     */
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 
