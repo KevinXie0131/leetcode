e20ba781b75fa94174ce7e32d48d08d0b02114fd
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2024-12-27 20:34:41 -0500

    update code

---
+++ src/main/java/com/template/UseList.java
@@ -0,0 +1,33 @@
+package com.template;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class UseList {
+    public static void main(String[] args) {
+        /**
+         * List转Array介绍
+         */
+        // List的toArray()方法可以将List类型的集合转换为数组类型
+        List<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Python");
+        list.add("JavaScript");
+        String[] result = list.toArray(new String[list.size()]);
+        System.out.println(Arrays.toString(result));
+
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(11);
+        list1.add(22);
+        list1.add(33);
+        list1.add(44);
+        Integer[] result1 = list1.toArray(new Integer[list1.size()]);
+        System.out.println(Arrays.toString(result1));
+        Integer[] result2 = list1.toArray(new Integer[0]);  //该数组的大小会根据 List 的元素数自动调整。
+        System.out.println(Arrays.toString(result2));
+        Object[] result3 = list1.toArray();  //此时数组的元素类型是 `Object`，如果需要更具体的类型，可以强制转换。
+        System.out.println(Arrays.toString(result3));
+
+    }
+}
