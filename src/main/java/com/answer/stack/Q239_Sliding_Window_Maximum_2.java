37783c8eadb7495f248e0bf5edb0230bb9b89258
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2024-12-30 23:42:55 -0500

    update code

--- src/main/java/com/answer/stack/Q239_Sliding_Window_Maximum_2.java
+++ src/main/java/com/answer/stack/Q239_Sliding_Window_Maximum_2.java
@@ -5,15 +5,15 @@ import java.util.*;
 public class Q239_Sliding_Window_Maximum_2 {
 
     public static void main(String[] args) {
-      /*  int[] nums = {1,3,-1,-3,5,3,6,7};
-        int k = 3;*/
-        int[] nums = {1,-1};
-        int k = 1;
-        int[] result = maxSlidingWindow(nums, k);
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+/*        int[] nums = {1,-1};
+        int k = 1;*/
+        int[] result = maxSlidingWindow_1(nums, k);
         System.out.println(Arrays.toString(result));
     }
     /**
-     * Use PriorityQueue
+     * Use PriorityQueue 大顶堆
      */
     public static int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
@@ -37,9 +37,9 @@ public class Q239_Sliding_Window_Maximum_2 {
         return ans;
     }
     /**
-     *
+     * 大顶堆
      */
-    public int[] maxSlidingWindow_1(int[] nums, int k) {
+    public static int[] maxSlidingWindow_1(int[] nums, int k) {
         PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b)->b[1]-a[1]);
         int n = nums.length;
         int len = nums.length - k + 1;
