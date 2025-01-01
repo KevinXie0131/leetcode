da217bc817fcb6797c9e6b33369862c28c761029
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-01-01 17:41:35 -0500

    update code

--- src/main/java/com/answer/tree/Q111_Minimum_Depth_of_Binary_Tree.java
+++ src/main/java/com/answer/tree/Q111_Minimum_Depth_of_Binary_Tree.java
@@ -7,14 +7,15 @@ import java.util.Deque;
 
 public class Q111_Minimum_Depth_of_Binary_Tree {
     /**
-     * 最⼩深度是从根节点到最近叶⼦节点的最短路径上的节点数量。，注意是叶⼦节点。
+     * 最⼩深度是从根节点到最近叶⼦节点的最短路径上的节点数量。注意是叶⼦节点。
      * 什么是叶⼦节点，左右孩⼦都为空的节点才是叶⼦节点
      *
      * 可以看出：求⼆叉树的最⼩深度和求⼆叉树的最⼤深度的差别主要在于处理左右孩⼦不为空的逻辑
      */
-    public int minDepth0(TreeNode root) {
-        if (root == null) return 0;
-
+    public int minDepth0(TreeNode root) { // 递归
+        if (root == null) {
+            return 0;
+        }
         int leftDepth = minDepth(root.left); // 左
         int rightDepth = minDepth(root.right); // 右
         // 中
@@ -31,21 +32,26 @@ public class Q111_Minimum_Depth_of_Binary_Tree {
 
         return result;
     }
-
+    /**
+     * 递归 优化
+     */
     public int minDepth(TreeNode root) {
-        if (root == null) return 0;
-
-        if (root.left == null && root.right == null) return 1;
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
 
         int left = minDepth(root.left);  // 左
         int right = minDepth(root.right);  // 右
         /**
          * 当⼀个左⼦树为空，右不为空，这时并不是最低点
-         *    if (node->left == NULL && node->right != NULL) {
+         *    if (node.left == NULL && node.right != NULL) {
          *       return 1 + rightDepth;
          *    }
          * 当⼀个右⼦树为空，左不为空，这时并不是最低点
-         *    if (node->left != NULL && node->right == NULL) {
+         *    if (node.left != NULL && node.right == NULL) {
          *       return 1 + leftDepth;
          *    }
          */
@@ -55,9 +61,8 @@ public class Q111_Minimum_Depth_of_Binary_Tree {
 
         return Math.min(left, right) + 1;
     }
-
     /**
-     *
+     * 递归 有最大初始值
      */
     public int minDepth1(TreeNode root) {
         if(root == null) {
@@ -68,14 +73,13 @@ public class Q111_Minimum_Depth_of_Binary_Tree {
         }
         int ans = Integer.MAX_VALUE;
         if(root.left != null) {
-            ans = Math.min(minDepth(root.left), ans);
+            ans = Math.min(minDepth1(root.left), ans);
         }
         if(root.right != null) {
-            ans = Math.min(minDepth(root.right), ans);
+            ans = Math.min(minDepth1(root.right), ans);
         }
         return ans + 1;
     }
-
     /**
      * 迭代法 层序遍历
      */
@@ -105,7 +109,36 @@ public class Q111_Minimum_Depth_of_Binary_Tree {
             }
         }
 
-
         return result;
+    }
+    /**
+     * 相对于 104.二叉树的最大深度 ，本题还也可以使用层序遍历的方式来解决，思路是一样的。
+     * 需要注意的是，只有当左右孩子都为空的时候，才说明遍历的最低点了。如果其中一个孩子为空则不是最低点
+     */
+    public int minDepth_3(TreeNode root) { // 迭代
+        int depth = 0;
+        if (root == null) {
+            return depth;
+        }
+
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            depth++; // 记录深度
+            while (size > 0) {
+                TreeNode cur = queue.poll();
+
+                if (cur.left != null) {queue.offer(cur.left);}
+                if (cur.right != null) {queue.offer(cur.right);}
+                if(cur.right == null && cur.left ==null){
+                    return depth; // 当左右孩子都为空的时候，说明是最低点的一层了，直接返回最小深度
+                }
+                size--;
+            }
+
+        }
+        return depth;
     }
 }
