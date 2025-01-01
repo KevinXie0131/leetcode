9d09c8709b7395696d0475a61f7e82e309cdd33f
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2024-12-31 20:25:53 -0500

    update code

--- src/main/java/com/answer/tree/Q144_Binary_Tree_Preorder_Traversal.java
+++ src/main/java/com/answer/tree/Q144_Binary_Tree_Preorder_Traversal.java
@@ -54,4 +54,35 @@ public class Q144_Binary_Tree_Preorder_Traversal {
 
         return list;
     }
+    /**
+     * 统一迭代法 前序遍历
+     *
+     * 使用栈的话，无法同时解决访问节点（遍历节点）和处理节点（将元素放进结果集）不一致的情况。
+     * 那我们就将访问的节点放入栈中，把要处理的节点也放入栈中但是要做标记。
+     * 如何标记呢，就是要处理的节点放入栈之后，紧接着放入一个空指针作为标记。 这种方法也可以叫做标记法。
+     *
+     *  (注意此时我们和中序遍历相比仅仅改变了两行代码的顺序)
+     */
+    public List<Integer> preorderTraversal_2(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        Stack<TreeNode> st = new Stack<>();
+        if (root != null) st.push(root);
+        while (!st.empty()) {
+            TreeNode node = st.peek();
+            if (node != null) {
+                st.pop(); // 将该节点弹出，避免重复操作，下面再将右中左节点添加到栈中
+                if (node.right!=null) st.push(node.right);  // 添加右节点（空节点不入栈）
+                if (node.left!=null) st.push(node.left);    // 添加左节点（空节点不入栈）
+                st.push(node);                          // 添加中节点
+                st.push(null); // 中节点访问过，但是还没有处理，加入空节点做为标记。
+
+            } else { // 只有遇到空节点的时候，才将下一个节点放进结果集
+                st.pop();           // 将空节点弹出
+                node = st.peek();    // 重新取出栈中元素
+                st.pop();
+                result.add(node.value); // 加入到结果集
+            }
+        }
+        return result;
+    }
 }
