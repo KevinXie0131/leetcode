d9fd78fc744421a343c941e772196dca569f8f66
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-01-02 23:03:13 -0500

    update code

--- src/main/java/com/answer/tree/Q404_Sum_of_Left_Leaves.java
+++ src/main/java/com/answer/tree/Q404_Sum_of_Left_Leaves.java
@@ -12,24 +12,26 @@ public class Q404_Sum_of_Left_Leaves {
      * ⾸先要注意是判断左叶⼦，不是⼆叉树左侧节点，所以不要上来想着层序遍历
      * 左叶⼦的明确定义：如果左节点不为空，且左节点没有左右孩⼦，那么这个节点就是左叶⼦
      *
+     * 判断当前节点是不是左叶子是无法判断的，必须要通过节点的父节点来判断其左孩子是不是左叶子
+     *
      * 递归法: 遍历顺序为后序遍历（左右中）, 通过递归函数的返回值来累加求取左叶⼦数值之和
      */
     public int sumOfLeftLeaves(TreeNode root) {
         if (root == null) {
-            return 0;
+            return 0; // 如果遍历到空节点，那么左叶子值一定是0
         }
+        if (root.left == null && root.right== null) return 0; //其实这个也可以不写，如果不写不影响结果，但就会让递归多进行了一层。
+
         int leftValue = sumOfLeftLeaves(root.left);    // 左
         int rightValue = sumOfLeftLeaves(root.right);  // 右
         // 中
-        if (root.left != null && root.left.left == null  // 左叶⼦节点处理逻辑
-                && root.left.right == null) {
-
+        if (root.left != null && root.left.left == null && root.left.right == null) { // 左叶⼦节点处理逻辑
             return root.left.value + leftValue + rightValue;
         }
         return  leftValue + rightValue;
     }
     /**
-     *
+     * 递归 （另一种形式的后序遍历）
      */
     public int sumOfLeftLeaves1(TreeNode root) {
         if (root == null) return 0;
@@ -42,6 +44,21 @@ public class Q404_Sum_of_Left_Leaves {
             midValue = root.left.value;
         }
         int sum = midValue + leftValue + rightValue;
+        return sum;
+    }
+    /**
+     * 递归 （另一种形式的后序遍历）也是可以的
+     */
+    public int sumOfLeftLeaves_2(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right== null) return 0;
+        int leftValue = sumOfLeftLeaves(root.left);    // 左
+
+        if (root.left != null && root.left.left == null && root.left.right == null) { // 左子树就是一个左叶子的情况
+            leftValue = root.left.value;
+        }
+        int rightValue = sumOfLeftLeaves(root.right);  // 右
+        int sum =  leftValue + rightValue;  // 中
         return sum;
     }
     /**
