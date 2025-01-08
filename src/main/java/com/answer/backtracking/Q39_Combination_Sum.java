572903f88265ae5a17881afac5af94fead757d32
KevinX <Xzb04720638k>
2025-01-08 17:21:08 -0500

    update

--- src/main/java/com/answer/backtracking/Q39_Combination_Sum.java
+++ src/main/java/com/answer/backtracking/Q39_Combination_Sum.java
@@ -3,7 +3,11 @@ package com.answer.backtracking;
 import java.util.*;
 
 public class Q39_Combination_Sum {
-
+    /**
+     * 回溯 版本一
+     * 组合没有数量要求
+     * 元素可无限重复选取
+     */
     public List<List<Integer>> combinationSum(int[] candidates, int target)    {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         Deque<Integer> path = new ArrayDeque<>();
@@ -11,10 +15,14 @@ public class Q39_Combination_Sum {
         backtracking(candidates, target, 0, result, path);
         return result;
     }
-
+    /**
+     * 本题还需要startIndex来控制for循环的起始位置，对于组合问题，什么时候需要startIndex呢？
+     * 我举过例子，如果是一个集合来求组合的话，就需要startIndex，例如：77.组合 (opens new window)，216.组合总和III (opens new window)。
+     * 如果是多个集合取组合，各个集合之间相互不影响，那么就不用startIndex，例如：17.电话号码的字母组合
+     */
     public void backtracking(int[] candidates, int target, int startIndex, List<List<Integer>> result, Deque<Integer> path ){
         if (0 == target) {
-            result.add(new ArrayList<Integer>(path));
+            result.add(new ArrayList<Integer>(path)); // 不要忘了new ArrayList
             return;
         }
         if(target < 0){
@@ -23,6 +31,7 @@ public class Q39_Combination_Sum {
         for(int i = startIndex; i < candidates.length; i++){
             path.addLast(candidates[i]);
             target -= candidates[i];
+            // 注意本题和77.组合 (opens new window)、216.组合总和III (opens new window)的一个区别是：本题元素为可重复选取的。
             backtracking(candidates, target, i, result, path); // 关键点:不⽤i+1了，表示可以重复读取当前的数
             path.removeLast(); // 回溯
             target += candidates[i]; // 回溯
--- src/main/java/com/answer/backtracking/Q39_Combination_Sum_1.java
+++ src/main/java/com/answer/backtracking/Q39_Combination_Sum_1.java
@@ -6,11 +6,14 @@ import java.util.Deque;
 import java.util.List;
 
 public class Q39_Combination_Sum_1 {
-
+    /**
+     * 剪枝优化
+     * 时间复杂度: O(n * 2^n)，注意这只是复杂度的上界，因为剪枝的存在，真实的时间复杂度远小于此
+     */
     public List<List<Integer>> combinationSum(int[] candidates, int target)    {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         Deque<Integer> path = new ArrayDeque<>();
-        Arrays.sort(candidates);
+        Arrays.sort(candidates); // 先进行排序
         backtracking(candidates, target, 0, result, path);
         return result;
     }
@@ -23,6 +26,8 @@ public class Q39_Combination_Sum_1 {
         if(target < 0){
             return;
         }
+        // 对总集合排序之后，如果下一层的sum（就是本层的 sum + candidates[i]）已经大于target，就可以结束本轮for循环的遍历。
+       /* for(int i = startIndex; i < candidates.length && target >= candidates[i] ; i++){*/
         for(int i = startIndex; i < candidates.length; i++){
             if(target < candidates[i]){  // 剪枝优化 终⽌遍历
                 break;
