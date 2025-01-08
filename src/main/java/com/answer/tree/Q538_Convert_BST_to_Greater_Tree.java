c51802114ffae61bf9bf099fcb85597d6a0fb36d
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-01-07 19:07:12 -0500

    update code

--- src/main/java/com/answer/tree/Q538_Convert_BST_to_Greater_Tree.java
+++ src/main/java/com/answer/tree/Q538_Convert_BST_to_Greater_Tree.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class Q538_Convert_BST_to_Greater_Tree {
     /**
-     * 把⼆叉搜索树转换为累加树
+     * 把⼆叉搜索树转换为累加树（Greater Sum Tree）
      * 给出⼆叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater SumTree），使每个节点 node 的新值等于原树中⼤于或等于 node.val 的值之和
      *
      * 换⼀个⾓度来看，这就是⼀个有序数组[2, 5, 13]，求从后到前的累加数组，也就是[20, 18, 13]，是不是感觉这就简单了
@@ -15,22 +15,22 @@ public class Q538_Convert_BST_to_Greater_Tree {
      */
     int pre = 0; // 记录前⼀个节点的数值
     public TreeNode convertBST(TreeNode root) {
-        dfs(root);
+        traversal(root);
         return root;
     }
     // 要右中左来遍历⼆叉树， 中节点的处理逻辑就是让cur的数值加上前⼀个节点的数值
-    public void dfs(TreeNode node){
+    public void traversal(TreeNode node){  // 右中左遍历. 不需要递归函数的返回值做什么操作了，要遍历整棵树
         if(node == null) return;
 
-        dfs(node.right); // 右
+        traversal(node.right); // 右
 
-        node.value += pre; // 中
+        node.value += pre; // 中  按右中左顺序遍历，累加即可
         pre = node.value;
 
-        dfs(node.left); // 左
+        traversal(node.left); // 左
     }
     /**
-     * 迭代法
+     * 迭代法 中序模板题(右中左)
      */
     int pre1 = 0; // 记录前⼀个节点的数值
     public TreeNode convertBST1(TreeNode root) {
@@ -46,7 +46,7 @@ public class Q538_Convert_BST_to_Greater_Tree {
             node.value += pre; // 中
             pre1= node.value;
 
-            cur = node.left; // 左
+            cur = node.left; // 左  是node.left
         }
         return root;
     }
