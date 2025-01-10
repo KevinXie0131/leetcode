3f7d17471ae9ca52f292b5f82e21a61e8447b899
KevinX <Xzb04720638k>
2025-01-10 18:42:03 -0500

    update

--- src/main/java/com/answer/binarysearch/Q34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
+++ src/main/java/com/answer/binarysearch/Q34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
@@ -8,29 +8,36 @@ public class Q34_Find_First_and_Last_Position_of_Element_in_Sorted_Array {
         int target = 2;*/
         int[] nums = {5,7,7,8,8,10};
         int target = 8;
-        System.out.println(Arrays.toString(searchRange_3(nums, target)));
-
+        System.out.println(Arrays.toString(searchRange(nums, target)));
     }
-    public int[] searchRange(int[] nums, int target) {
-        if(nums.length == 0){
-            return new int[]{-1,-1};
+    /**
+     * 解法2
+     *  1、首先，在 nums 数组中二分查找 target；
+     *  2、如果二分查找失败，则 binarySearch 返回 -1，表明 nums 中没有 target。此时，searchRange 直接返回 {-1, -1}；
+     *  3、如果二分查找成功，则 binarySearch 返回 nums 中值为 target 的一个下标。然后，通过左右滑动指针，
+     *     来找到符合题意的区间
+     */
+    static public int[] searchRange(int[] nums, int target) {
+        int index = binarySearch(nums, target); // 二分查找
+
+        if (index == -1) { // nums 中不存在 target，直接返回 {-1, -1}
+            return new int[] {-1, -1}; // 匿名数组
         }
-        int index = binarySearch(nums, target);
-        if(index == -1){
-            return new int[]{-1,-1};
-        }
+        // nums 中存在 target，则左右滑动指针，来找到符合题意的区间
         int left = index;
         int right = index;
-        while(left>=1 && nums[left] ==nums[left - 1]){
+        // 向左滑动，找左边界
+        while (left - 1 >= 0 && nums[left - 1] == nums[index]) { // 防止数组越界。逻辑短路，两个条件顺序不能换
             left--;
         }
-        while(right<nums.length - 1 && nums[right] ==nums[right + 1]){
+        // 向右滑动，找右边界
+        while (right + 1 < nums.length && nums[right + 1] == nums[index]) { // 防止数组越界。
             right++;
         }
-        return new int[]{left,right};
+        return new int[] {left, right};
     }
-
-    public int binarySearch(int[] nums, int target) {
+    // 二分查找
+    static public int binarySearch(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1; // 不变量：左闭右闭区间
 
@@ -46,7 +53,9 @@ public class Q34_Find_First_and_Last_Position_of_Element_in_Sorted_Array {
         }
         return -1; // 不存在
     }
-
+    /**
+     * 解题思路同上
+     */
     public static int[] searchRange_3(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         while (left + 1 < right) {
@@ -71,5 +80,59 @@ public class Q34_Find_First_and_Last_Position_of_Element_in_Sorted_Array {
         if (target == nums[right]) return new int[]{right, right};
         return new int[]{-1, -1};
 
+    }
+    /**
+     * 解法三
+     */
+    public int[] searchRange3(int[] nums, int target) {
+        int left = searchLeft(nums,target);
+        int right = searchRight(nums,target);
+        return new int[]{left,right};
+    }
+    public int searchLeft(int[] nums,int target){
+        // 寻找元素第一次出现的地方
+        int left = 0;
+        int right = nums.length-1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            // >= 的都要缩小 因为要找第一个元素
+            if(nums[mid]>=target){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        // right = left - 1
+        // 如果存在答案 right是首选
+        if(right>=0&&right<nums.length&&nums[right]==target){
+            return right;
+        }
+        if(left>=0&&left<nums.length&&nums[left]==target){
+            return left;
+        }
+        return -1;
+    }
+    public int searchRight(int[] nums,int target){
+        // 找最后一次出现
+        int left = 0;
+        int right = nums.length-1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            // <= 的都要更新 因为我们要找最后一个元素
+            if(nums[mid]<=target){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        // left = right + 1
+        // 要找最后一次出现 如果有答案 优先找left
+        if(left>=0&&left<nums.length&&nums[left]==target){
+            return left;
+        }
+        if(right>=0&&right<=nums.length&&nums[right]==target){
+            return right;
+        }
+        return -1;
     }
 }
--- src/main/java/com/answer/binarysearch/Q34_Find_First_and_Last_Position_of_Element_in_Sorted_Array_1.java
+++ src/main/java/com/answer/binarysearch/Q34_Find_First_and_Last_Position_of_Element_in_Sorted_Array_1.java
@@ -1,8 +1,14 @@
 package com.answer.binarysearch;
 
 public class Q34_Find_First_and_Last_Position_of_Element_in_Sorted_Array_1 {
-
-    static int[] searchRange(int[] nums, int target) {
+    /**
+     * 寻找target在数组里的左右边界，有如下三种情况：
+     *   情况一：target 在数组范围的右边或者左边，例如数组{3, 4, 5}，target为2或者数组{3, 4, 5},target为6，
+     *          此时应该返回{-1, -1}
+     *   情况二：target 在数组范围中，且数组中不存在target，例如数组{3,6,7},target为5，此时应该返回{-1, -1}
+     *   情况三：target 在数组范围中，且数组中存在target，例如数组{3,6,7},target为6，此时应该返回{1, 1}
+     */
+    int[] searchRange0(int[] nums, int target) {
         int leftBorder = getLeftBorder(nums, target);
         int rightBorder = getRightBorder(nums, target);
         // 情况一
@@ -12,15 +18,16 @@ public class Q34_Find_First_and_Last_Position_of_Element_in_Sorted_Array_1 {
         // 情况二
         return new int[]{-1, -1};
     }
-
-    static int getRightBorder(int[] nums, int target) {
+    // 二分查找，寻找target的右边界（不包括target）
+    // 如果rightBorder为没有被赋值（即target在数组范围的左边，例如数组[3,3]，target为2），为了处理情况一
+    int getRightBorder(int[] nums, int target) {
         int left = 0;
-        int right = nums.length - 1;
+        int right = nums.length - 1;  // 定义target在左闭右闭的区间里，[left, right]
         int rightBorder = -2; // 记录一下rightBorder没有被赋值的情况
-        while (left <= right) {
+        while (left <= right) { // 当left==right，区间[left, right]依然有效
             int middle = left + ((right - left) / 2);
             if (nums[middle] > target) {
-                right = middle - 1;
+                right = middle - 1;  // target 在左区间，所以[left, middle - 1]
             } else { // 寻找右边界，nums[middle] == target的时候更新left
                 left = middle + 1;
                 rightBorder = left;
@@ -28,10 +35,11 @@ public class Q34_Find_First_and_Last_Position_of_Element_in_Sorted_Array_1 {
         }
         return rightBorder;
     }
-
-    static int getLeftBorder(int[] nums, int target) {
+    // 二分查找，寻找target的左边界leftBorder（不包括target）
+    // 如果leftBorder没有被赋值（即target在数组范围的右边，例如数组[3,3],target为4），为了处理情况一
+    int getLeftBorder(int[] nums, int target) {
         int left = 0;
-        int right = nums.length - 1;
+        int right = nums.length - 1; // 定义target在左闭右闭的区间里，[left, right]
         int leftBorder = -2; // 记录一下leftBorder没有被赋值的情况
         while (left <= right) {
             int middle = left + ((right - left) / 2);
