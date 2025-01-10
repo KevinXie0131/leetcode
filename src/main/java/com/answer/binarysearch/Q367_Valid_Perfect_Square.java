4f0e1f423294160bcd2cea1e4df0c1164c3b6ee2
KevinX <Xzb04720638k>
2025-01-10 17:01:56 -0500

    update

--- src/main/java/com/answer/binarysearch/Q367_Valid_Perfect_Square.java
+++ src/main/java/com/answer/binarysearch/Q367_Valid_Perfect_Square.java
@@ -2,22 +2,24 @@ package com.answer.binarysearch;
 
 public class Q367_Valid_Perfect_Square {
     public static void main(String[] args) {
-        boolean isFound = isPerfectSquare(14);
+        boolean isFound = isPerfectSquare(2147483647);
         System.out.println(isFound);
     }
-
     /**
      * Approach 2: Binary Search - Time Limit Exceeded for 2147483647
+     * use long instead
+     *
+     * 考虑使用二分查找来优化方法二中的搜索过程。因为 num 是正整数，所以若正整数 x 满足 x*x=num，则 x 一定满足 1≤x≤num。
+     * 于是我们可以将 1 和 num 作为二分查找搜索区间的初始边界。
      */
     public static boolean isPerfectSquare(int num) {
-        int left = 1;
-        int right = num;
+        long left = 1;
+        long right = num;
         while(left <= right){
-            int mid = (left + right) >>> 1;
-            int value = mid * mid;
-            if(value == num){
+            long mid = (left + right) >>> 1;
+            if( mid * mid == num){
                 return true;
-            }else if(value < num){
+            }else if( mid * mid < num){
                 left = mid + 1;
             }else{
                 right = mid - 1;
