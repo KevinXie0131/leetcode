a1f3303f341abe1eaeba0469223dabbf1bf09504
KevinX <Xzb04720638k>
2025-01-10 16:44:06 -0500

    update

--- src/main/java/com/answer/binarysearch/Q69_Sqrt.java
+++ src/main/java/com/answer/binarysearch/Q69_Sqrt.java
@@ -1,7 +1,35 @@
 package com.answer.binarysearch;
 
 public class Q69_Sqrt {
+    /**
+     * 二分查找
+     * 由于 x 平方根的整数部分 ans 是满足 k*k ≤x 的最大 k 值，因此我们可以对 k 进行二分查找，从而得到答案。
+     *
+     * 二分查找的下界为 0，上界可以粗略地设定为 x。在二分查找的每一步中，我们只需要比较中间元素 mid 的平方与 x 的大小关系，
+     * 并通过比较的结果调整上下界的范围。由于我们所有的运算都是整数运算，不会存在误差，因此在得到最终的答案 ans 后，
+     * 也就不需要再去尝试 ans+1 了。
+     *
+     */
+    public int mySqrt0(int x) {
+        int left = 0;
+        int right = x-1;
+        int result = -1 ;
 
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            if ((mid * mid) > x) {
+                right = mid - 1;
+            } else if ((mid * mid) <= x) {
+                result = mid;
+                left = mid + 1;
+
+            }
+        }
+        return result;
+    }
+    /**
+     *
+     */
     public int mySqrt(int x) {
         int left = 0, right = x;
         while(left <= right){
@@ -15,7 +43,6 @@ public class Q69_Sqrt {
 
         return left - 1;
     }
-
     /**
      *
      */
