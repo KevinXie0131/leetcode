7fe3bdde2bb47ae2311675c4adc3f839e7bc2ca3
KevinX <Xzb04720638k>
2025-01-10 21:18:07 -0500

    update

--- src/main/java/com/answer/array/Q54_Spiral_Matrix.java
+++ src/main/java/com/answer/array/Q54_Spiral_Matrix.java
@@ -3,13 +3,22 @@ package com.answer.array;
 import java.util.*;
 
 public class Q54_Spiral_Matrix {
+    public static void main(String[] args) {
+        int [][] matrix = {{1,2,3, 4},{5,6,7, 8},{9,10,11,12}};
 
-    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> r = spiralOrder(matrix);
+        System.out.println(r);
+    }
+
+   static public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> result = new ArrayList<Integer>();
 
         int row = matrix.length;
         int col = matrix[0].length;
-        int top = 0, bottom = row -1, left = 0, right = col -1;
+        int top = 0;
+        int bottom = row -1;
+        int left = 0;
+        int right = col -1;
 
         while(top <= bottom && left <= right){
             for(int i = left; i<= right && top <= bottom; i++){
