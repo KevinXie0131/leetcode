fed868e0f4359bf665b792368c53f0ccd865628b
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-01-11 21:15:59 -0500

    update code

--- src/main/java/com/answer/priorityqueue/Q703_Kth_Largest_Element_in_a_Stream.java
+++ src/main/java/com/answer/priorityqueue/Q703_Kth_Largest_Element_in_a_Stream.java
@@ -20,6 +20,10 @@ public class Q703_Kth_Largest_Element_in_a_Stream {
      * Space: O(k)
      * 为什么使用小根堆？
      * 因为我们需要在堆中保留数据流中的前K大元素，使用小根堆能保证每次调用堆的 pop() 函数时，从堆中删除的是堆中的最小的元素（堆顶）
+     *
+     * 时间复杂度： 初始化时间复杂度为：O(nlogk) ，其中 n 为初始化时 nums 的长度；
+     *             单次插入时间复杂度为：O(logk)。
+     * 空间复杂度：O(k)。需要使用优先队列存储前 k 大的元素
      */
     PriorityQueue<Integer> heap = null;
     int k = 0;
@@ -37,6 +41,33 @@ public class Q703_Kth_Largest_Element_in_a_Stream {
         this.heap.offer(val);
         while(this.heap.size() > this.k){
             this.heap.poll();
+        }
+        return this.heap.peek();
+    }
+    /**
+     * 另一种形式 时间复杂度： 初始化时间复杂度为：O(klogk)
+     */
+    public void KthLargest1(int k, int[] nums) {
+        this.heap = new PriorityQueue<>();
+        this.k = k;
+
+        for(int n : nums){
+            if( this.heap.size() < k){
+                this.heap.offer(n);
+            }
+            else if(this.heap.peek() < n){
+                this.heap.poll();
+                this.heap.offer(n);
+            }
+        }
+    }
+    public int add1(int val) {
+        if( this.heap.size() < k){
+            this.heap.offer(val);
+        }
+        else if(this.heap.peek() < val){
+            this.heap.poll();
+            this.heap.offer(val);
         }
         return this.heap.peek();
     }
