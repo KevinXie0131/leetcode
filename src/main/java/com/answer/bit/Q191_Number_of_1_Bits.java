0dcc02f0b54d09167f169b2787dee1ee9af42b54
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-01-14 22:03:05 -0500

    update code

--- src/main/java/com/answer/bit/Q191_Number_of_1_Bits.java
+++ src/main/java/com/answer/bit/Q191_Number_of_1_Bits.java
@@ -18,14 +18,28 @@ public class Q191_Number_of_1_Bits {
         return count;
     }
     /**
+     * 另一种形式
+     */
+    public int hammingWeight_0(int n) {
+        int count = 0;
+        while(n != 0){
+            if((n % 2) == 1){
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+    /**
      * Approach 2: Bit Manipulation
      *  doing a bit-wise AND of n and n -1 flips the least-significant 1-bit in n to 0
+     * 时间复杂度：Number of 1-bit
      */
     public int hammingWeight_1(int n) {
         int count = 0;
         while(n != 0){
             count++;
-            n = n & (n-1);
+            n = n & (n-1); // For example, 11000 & 10111 = 10000, the last 1 is removed.
         }
         return count;
     }
