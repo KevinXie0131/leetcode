22a3be77d33ae0081b4b79578c661d8f0f43a084
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-01-14 19:03:53 -0500

    update code

--- src/main/java/com/answer/tree/Q116_Populating_Next_Right_Pointers_in_Each_Node.java
+++ src/main/java/com/answer/tree/Q116_Populating_Next_Right_Pointers_in_Each_Node.java
@@ -5,13 +5,35 @@ import java.util.*;
 
 public class Q116_Populating_Next_Right_Pointers_in_Each_Node {
     /**
+     * 只能使用常量级额外空间, 基本上就是要求使用递归了，迭代的方式一定会用到栈或者队列
+     * 递归: 最关键的点是可以通过上一层递归 搭出来的线，进行本次搭线。
+     */
+    public Node connect_0(Node root) {
+        trasverse(root);
+        return root;
+    }
+    public void trasverse(Node root) {
+        if(root == null) return; // 中
+        if(root.left != null){
+            root.left.next = root.right; // 操作1
+        }
+        if(root.right != null){
+            if(root.next != null){
+                root.right.next = root.next.left;  //操作2
+            }else {
+                root.right.next = null;
+            }
+        }
+        trasverse(root.left);  // 左
+        trasverse(root.right); //右
+    }
+    /**
      * 本题依然是层序遍历，只不过在单层遍历的时候记录一下本层的头部节点，然后在遍历的时候让前一个节点指向本节点就可以了
      */
     public Node connect(Node root) {
         if (root == null) {
             return root;
         }
-
         Deque<Node> queue = new ArrayDeque<>();
         queue.offer(root);
 
@@ -20,14 +42,14 @@ public class Q116_Populating_Next_Right_Pointers_in_Each_Node {
 
             for (int i = 0; i < size; i++) {
                 Node cur = queue.poll();
-
+                // 遍历每一行的时候，如果不是最后一个Node，则指向下一个Node；如果是最后一个Node，则指向null
                 if (i < size - 1) {
-                    cur.next = queue.peek();
+                    cur.next = queue.peek(); //如果不是最后一个Node 则指向下一个Node
+                } else {
+                    cur.next = null;  //如果是最后一个Node 则指向null (可省略)
                 }
-
                 if (cur.left != null) {queue.offer(cur.left);}
                 if (cur.right != null) {queue.offer(cur.right);}
-
             }
         }
         return root;
@@ -39,7 +61,6 @@ public class Q116_Populating_Next_Right_Pointers_in_Each_Node {
         if (root == null) {
             return root;
         }
-
         Deque<Node> queue = new ArrayDeque<>();
         queue.offer(root);
 
@@ -49,7 +70,7 @@ public class Q116_Populating_Next_Right_Pointers_in_Each_Node {
         while (!queue.isEmpty()) {
             int size = queue.size();
 
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) {  // 开始每一层的遍历
                 if (i == 0) {
                     nodePre = queue.pop(); // 取出一层的头结点
                     node = nodePre;
@@ -60,8 +81,8 @@ public class Q116_Populating_Next_Right_Pointers_in_Each_Node {
                 }
                 if (node.left != null) {queue.offer(node.left);}
                 if (node.right != null) {queue.offer(node.right);}
-
             }
+            nodePre.next = null;// 本层最后一个节点指向null (可省略)
         }
         return root;
     }
