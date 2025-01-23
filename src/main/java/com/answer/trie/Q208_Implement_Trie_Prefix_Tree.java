ac2b67a1c33341d3583717adf14148dba7153cb5
KevinX <Xzb04720638k>
2025-01-23 17:26:23 -0500

    trie

--- src/main/java/com/answer/trie/Q208_Implement_Trie_Prefix_Tree.java
+++ src/main/java/com/answer/trie/Q208_Implement_Trie_Prefix_Tree.java
@@ -57,51 +57,52 @@ public class Q208_Implement_Trie_Prefix_Tree {
             return node;
         }
     }
-}
-/**
- * 另一种形式
- */
-class Trie1 {
-    private Node root;
+    /**
+     * 另一种形式
+     */
+    class Trie1 {
+        private Node root;
 
-    public Trie1() {
-        root = new Node();
-    }
-
-    public void insert(String word) {
-        Node cur = root;
-        for (char ch : word.toCharArray()) {
-            int index = ch - 'a';
-            if (cur.children[index] == null) {
-                cur.children[index] = new Node();
-            }
-            cur = cur.children[index];
+        public Trie1() {
+            root = new Node();
         }
-        cur.isEnd = true;
-    }
 
-    public boolean search(String word) {
-        Node node = find(word);
-        return node != null && node.isEnd;
-    }
-
-    public boolean startsWith(String prefix) {
-        return find(prefix) != null;
-    }
-
-    private Node find(String word) {
-        Node cur = root;
-        for (char ch : word.toCharArray()) {
-            int index = ch - 'a';
-            if (cur.children[index] == null) {
-                return null;
+        public void insert(String word) {
+            Node cur = root;
+            for (char ch : word.toCharArray()) {
+                int index = ch - 'a';
+                if (cur.children[index] == null) {
+                    cur.children[index] = new Node();
+                }
+                cur = cur.children[index];
             }
-            cur = cur.children[index];
+            cur.isEnd = true;
         }
-        return cur;
+
+        public boolean search(String word) {
+            Node node = find(word);
+            return node != null && node.isEnd;
+        }
+
+        public boolean startsWith(String prefix) {
+            return find(prefix) != null;
+        }
+
+        private Node find(String word) {
+            Node cur = root;
+            for (char ch : word.toCharArray()) {
+                int index = ch - 'a';
+                if (cur.children[index] == null) {
+                    return null;
+                }
+                cur = cur.children[index];
+            }
+            return cur;
+        }
+    }
+    class Node {
+        Node[] children = new Node[26];
+        boolean isEnd;
     }
 }
-class Node {
-    Node[] children = new Node[26];
-    boolean isEnd;
-}
+
