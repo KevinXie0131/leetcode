3ae3a5809a2e4fa5f600365dab3a46b1bd57529d
KevinX <Xzb04720638k>
2025-01-24 20:25:33 -0500

    trie

--- src/main/java/com/answer/trie/Q212_Word_Search_II.java
+++ src/main/java/com/answer/trie/Q212_Word_Search_II.java
@@ -1,107 +1,98 @@
 package com.answer.trie;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 public class Q212_Word_Search_II {
-    /**
-     *
-     */
-    private TrieNode root;   // 根节点
+    Set<String> res = new HashSet<>();
 
     public List<String> findWords(char[][] board, String[] words) {
+        Trie trie = new Trie();
+        for(String str : words){
+            trie.insert(str);
+        }
         int m = board.length;
         int n = board[0].length;
-        // 构建words的字典树
-        root = new TrieNode();
-        for(String word: words){
-            if(word.length() > m * n){
-                continue;  // 字符串长度超过二维矩阵尺寸，肯定无法构成
-            }
-            insert(word);
-        }
-        // 以二维网格的每个位置(i,j)为起点，寻找以其为首字符的所有字符串
-        List<String> res = new ArrayList<>();
+        boolean[][] visited = new boolean[m][n];
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                dfs_Search(board, i, j, root, 0, res);
+                dfs(board, visited, "", i, j, trie);
             }
         }
-        return res;
+        return new ArrayList(res);
     }
-    /**
-     * 将单词word插入字段数root
-     */
-    private void insert(String word) {
-        TrieNode node = root;      // 从根节点开始构造这个word对应的路径节点
-        int n = word.length();
-        for(int i = 0; i < n; i++){
-            char ch = word.charAt(i);
-            if(!node.children.containsKey(ch)){
-                // 字符ch对应的节点不存在，新建一个
-                node.children.put(ch, new TrieNode());
-            }
-            // 更新node
-            node = node.children.get(ch);
-        }
-        node.str = word;   // 尾节点记录单词，用于后序查找的时候快速得到
-    }
-    /**
-     * 深度优先搜索的同时，判断当前路径构成的字符串是否为查找单词
-     * @param board: 二维网格
-     * @param r: 行号
-     * @param c: 列号
-     * @param node：当前字符对应的路径节点
-     * @param len: 当前路径构成的字符串长度
-     * @param res：结果集
-     */
-    private void dfs_Search(char[][] board, int r, int c, TrieNode node, int len, List<String> res){
-        if(len > 10){
-            return;     // 字符串长度超过10，返回
-        }
-        char ch = board[r][c];  // 获取当前行列对应的字符
-        if(!node.children.containsKey(ch)){
-            return;  // 当前字符对应的节点不存在，即构造的字符串不在words中
-        }
-        TrieNode last = node;  // 记录当前node
-        node = node.children.get(ch);  // 更新当前node为当前字符对应得到的节点
-        if(node.str.length() > 0){
-            res.add(node.str);    // 当前节点记录了一个单词，则得到了一个words中的单词
-            node.str = "";     // 匹配了单词，不重复匹配
-        };
-        if(node.children.size() == 0){
-            // 当前节点没有后序字符了，那么这个节点一定是某个单词最后一个字符对应的节点。
-            // 并且不是其他任何单词的前缀，因此匹配完了之后，可以将这个字符从其父节点的childran列表中删除。
-            last.children.remove(ch);
+
+    public void  dfs(char[][] board, boolean[][] visited, String str, int i, int j, Trie trie){
+        if(i< 0 || i >= board.length || j < 0 || j >= board[0].length) {
             return;
         }
-        len++;  // 更新长度
-        board[r][c] = '*';  // 用特殊符号标记当前位置已使用
-        // 四个方向转递递归
-        if(r - 1 >= 0 && board[r - 1][c] != '*'){
-            dfs_Search(board, r - 1, c, node, len, res);
+        if(visited[i][j] == true) {
+            return;
         }
-        if(r + 1 < board.length && board[r + 1][c] != '*'){
-            dfs_Search(board, r + 1, c, node, len, res);
+
+        str += board[i][j];
+
+        if(!trie.startsWith(str)) {
+            return;
         }
-        if(c - 1 >= 0 && board[r][c - 1] != '*'){
-            dfs_Search(board, r, c - 1, node, len, res);
+        if(trie.search(str)) {
+            res.add(str);
         }
-        if(c + 1 < board[0].length && board[r][c + 1] != '*'){
-            dfs_Search(board, r, c + 1, node, len, res);
-        }
-        board[r][c] = ch;   // 回溯，这个位置处理完了恢复成原来的字符
+
+        visited[i][j] = true;
+
+        dfs(board, visited, str, i - 1, j, trie);
+        dfs(board, visited, str, i + 1, j, trie);
+        dfs(board, visited, str, i, j - 1, trie);
+        dfs(board, visited, str, i, j + 1, trie);
+
+        visited[i][j] = false;
     }
+    // The same Trie structure of Q208_Implement_Trie_Prefix_Tree
+    class Trie {
+        private Trie[] children;
+        private boolean isEnd;
 
-    class TrieNode{
-        Map<Character, TrieNode> children;     // 子节点列表
-        String str;              // 如果是尾节点，存储对应的单词
+        public Trie() {
+            children = new Trie[26];
+            isEnd = false;
+        }
 
-        public TrieNode(){
-            children = new HashMap<>();
-            str = "";
+        public void insert(String word) { // 插入字符串
+            Trie node = this;
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                int index = ch - 'a';
+                if (node.children[index] == null) {
+                    node.children[index] = new Trie();
+                }
+                node = node.children[index];
+            }
+            node.isEnd = true;
+        }
+
+        public boolean search(String word) {
+            Trie node = searchPrefix(word);
+            return node != null && node.isEnd;
+        }
+
+        public boolean startsWith(String prefix) {
+            return searchPrefix(prefix) != null;
+        }
+
+        private Trie searchPrefix(String prefix) { // 查找前缀
+            Trie node = this;
+            for (int i = 0; i < prefix.length(); i++) {
+                char ch = prefix.charAt(i);
+                int index = ch - 'a';
+                if (node.children[index] == null) {
+                    return null;
+                }
+                node = node.children[index];
+            }
+            return node;
         }
     }
 }
--- src/main/java/com/answer/trie/Q212_Word_Search_II_1.java
+++ src/main/java/com/answer/trie/Q212_Word_Search_II_1.java
@@ -1,98 +1,136 @@
 package com.answer.trie;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Q212_Word_Search_II_1 {
-    Set<String> res = new HashSet<>();
+    /**
+     * 回溯算法
+     * 数据范围只有 12，且 words 中出现的单词长度不会超过 10，可以考虑使用「回溯算法」。
+     * <p>
+     * 起始先将所有 words 出现的单词放到 Set 结构中，然后以 board 中的每个点作为起点进行爆搜（由于题目规定在一个单词中每个格子只能被使用一次，
+     * 因此还需要一个 vis 数组来记录访问过的位置）：
+     * 如果当前爆搜到的字符串长度超过 10，直接剪枝；
+     * 如果当前搜索到的字符串在 Set 中，则添加到答案（同时了防止下一次再搜索到该字符串，需要将该字符串从 Set 中移除）。
+     *
+     * 时间复杂度：共有 m∗n 个起点，每次能往 4 个方向搜索（不考虑重复搜索问题），且搜索的长度不会超过 10。
+     * 整体复杂度为 O(m∗n∗4 power(10))
+     */
+    Set<String> set = new HashSet<>();
+    List<String> ans = new ArrayList<>();
+    char[][] board;
+    int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int n, m;
+    boolean[][] visited = new boolean[15][15];
 
-    public List<String> findWords(char[][] board, String[] words) {
-        Trie trie = new Trie();
-        for(String str : words){
-            trie.insert(str);
+    public List<String> findWords(char[][] _board, String[] words) {
+        board = _board;
+        m = board.length;
+        n = board[0].length;
+        for (String w : words) {
+            set.add(w);
         }
-        int m = board.length;
-        int n = board[0].length;
-        boolean[][] visited = new boolean[m][n];
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                dfs(board, visited, "", i, j, trie);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                visited[i][j] = true;
+                sb.append(board[i][j]);
+                dfs(i, j, sb);
+                visited[i][j] = false;
+                sb.deleteCharAt(sb.length() - 1);
             }
         }
-        return new ArrayList(res);
+        return ans;
     }
 
-    public void  dfs(char[][] board, boolean[][] visited, String str, int i, int j, Trie trie){
-        if(i< 0 || i >= board.length || j < 0 || j >= board[0].length) {
-            return;
+    void dfs(int i, int j, StringBuilder sb) {
+        if (sb.length() > 10) return;
+        if (set.contains(sb.toString())) {
+            ans.add(sb.toString());
+            set.remove(sb.toString());
         }
-        if(visited[i][j] == true) {
-            return;
+        for (int[] d : dirs) {
+            int dx = i + d[0], dy = j + d[1];
+            if (dx < 0 || dx >= m || dy < 0 || dy >= n) continue;
+            if (visited[dx][dy]) {
+                continue;
+            }
+            visited[dx][dy] = true;
+            sb.append(board[dx][dy]);
+            dfs(dx, dy, sb);
+            visited[dx][dy] = false;
+            sb.deleteCharAt(sb.length() - 1);
         }
-
-        str += board[i][j];
-
-        if(!trie.startsWith(str)) {
-            return;
-        }
-        if(trie.search(str)) {
-            res.add(str);
-        }
-
-        visited[i][j] = true;
-
-        dfs(board, visited, str, i - 1, j, trie);
-        dfs(board, visited, str, i + 1, j, trie);
-        dfs(board, visited, str, i, j - 1, trie);
-        dfs(board, visited, str, i, j + 1, trie);
-
-        visited[i][j] = false;
     }
-    // The same Trie structure of Q208_Implement_Trie_Prefix_Tree
-    class Trie {
-        private Trie[] children;
-        private boolean isEnd;
-
-        public Trie() {
-            children = new Trie[26];
-            isEnd = false;
-        }
-
-        public void insert(String word) { // 插入字符串
-            Trie node = this;
-            for (int i = 0; i < word.length(); i++) {
-                char ch = word.charAt(i);
-                int index = ch - 'a';
-                if (node.children[index] == null) {
-                    node.children[index] = new Trie();
-                }
-                node = node.children[index];
+    /**
+     * 在「解法一」中，对于任意一个当前位置 (i,j)，我们都不可避免的搜索了四联通的全部方向，这导致了那些无效搜索路径最终只有长度达到 10 才会被剪枝。
+     * 要进一步优化我们的搜索过程，需要考虑如何在每一步的搜索中进行剪枝。
+     *
+     * 我们可以使用 Trie 结构进行建树，对于任意一个当前位置 (i,j) 而言，
+     * 只有在 Trie 中存在往从字符 a 到 b 的边时，我们才在棋盘上搜索从 a 到 b 的相邻路径。
+     *
+     */
+    class TrieNode {
+        String s;
+        TrieNode[] tns = new TrieNode[26];
+    }
+    void insert(String s) {
+        TrieNode p = root;
+        for (int i = 0; i < s.length(); i++) {
+            int u = s.charAt(i) - 'a';
+            if (p.tns[u] == null){
+                p.tns[u] = new TrieNode();
             }
-            node.isEnd = true;
+            p = p.tns[u];
         }
+        p.s = s;
+    }
+    Set<String> set1 = new HashSet<>();
+    char[][] board1;
+    int n1, m1;
+    TrieNode root = new TrieNode();
+    int[][] dirs1 = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+    boolean[][] vis = new boolean[15][15];
 
-        public boolean search(String word) {
-            Trie node = searchPrefix(word);
-            return node != null && node.isEnd;
+    public List<String> findWords1(char[][] _board, String[] words) {
+        board1 = _board;
+        m1 = board1.length;
+        n1 = board1[0].length;
+        for (String w : words) {
+            insert(w);
         }
-
-        public boolean startsWith(String prefix) {
-            return searchPrefix(prefix) != null;
-        }
-
-        private Trie searchPrefix(String prefix) { // 查找前缀
-            Trie node = this;
-            for (int i = 0; i < prefix.length(); i++) {
-                char ch = prefix.charAt(i);
-                int index = ch - 'a';
-                if (node.children[index] == null) {
-                    return null;
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < n1; j++) {
+                int u = board1[i][j] - 'a';
+                if (root.tns[u] != null) {
+                    vis[i][j] = true;
+                    dfs(i, j, root.tns[u]);
+                    vis[i][j] = false;
                 }
-                node = node.children[index];
             }
-            return node;
+        }
+        List<String> ans = new ArrayList<>();
+        for (String s : set1) ans.add(s);
+        return ans;
+    }
+    void dfs(int i, int j, TrieNode node) {
+        if (node.s != null) {
+            set1.add(node.s);
+        }
+        for (int[] d : dirs1) {
+            int dx = i + d[0], dy = j + d[1];
+            if (dx < 0 || dx >= m1 || dy < 0 || dy >= n1) {
+                continue;
+            }
+            if (vis[dx][dy]) {
+                continue;
+            }
+            int u = board1[dx][dy] - 'a';
+            if (node.tns[u] != null) {
+                vis[dx][dy] = true;
+                dfs(dx, dy, node.tns[u]);
+                vis[dx][dy] = false;
+            }
         }
     }
 }
