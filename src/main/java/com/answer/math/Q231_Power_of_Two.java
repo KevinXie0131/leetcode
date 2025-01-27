059ea0ea96083e86990aa3ad4a5ba095b5926726
KevinX <Xzb04720638k>
2025-01-27 17:38:37 -0500

    update

--- src/main/java/com/answer/math/Q231_Power_of_Two.java
+++ src/main/java/com/answer/math/Q231_Power_of_Two.java
@@ -5,26 +5,24 @@ public class Q231_Power_of_Two {
         System.out.println( isPowerOfTwo_5(3));
         System.out.println( findSqrt(3));
     }
-
     /**
      * Binary Search
      */
     public static boolean isPowerOfTwo(int n) {
         int left = 0, right = n;
 
-        while(left <= right){
+        while (left <= right) {
             int mid = (left + right) >>> 1;
-            if(Math.pow(2, mid) == n){
+            if (Math.pow(2, mid) == n) {
                 return true;
-            }else if(Math.pow(2, mid) > n){
+            } else if (Math.pow(2, mid) > n) {
                 right = mid - 1;
-            }else{
+            } else {
                 left = mid + 1;
             }
         }
         return false;
     }
-
     /**
      * this can be accepted, but slow
      */
@@ -39,10 +37,9 @@ public class Q231_Power_of_Two {
             if(count == 1000) return false;
         }
     }
-
     /**
-     * Brute force
-     *
+     * 常用位操作
+     * 判断奇偶
      * (x & 1) == 1 ---等价---> (x % 2 == 1)
      * (x & 1) == 0 ---等价---> (x % 2 == 0)
      * x / 2 ---等价---> x >> 1
@@ -51,24 +48,32 @@ public class Q231_Power_of_Two {
      * x & ~x -----> 0
      */
     public static boolean isPowerOfTwo_4(int n) {
-        if (n < 1) return false;
-        while (n!=1){
-            if (n%2 == 1) return false;
-            n = n/2;
+        if (n < 1){
+            return false;
+        }
+        while (n != 1){
+            if (n % 2 == 1) {
+                return false;
+            }
+            n = n / 2; // 除法
         }
         return true;
     }
+    // 时间复杂度：O(logn)
     public static boolean isPowerOfTwo_4a(int n) {
-        if (n < 1) return false;
-        while (n % 2 == 0) {
-            n >>= 1;
+        if (n < 1){
+            return false;
         }
-        return n == 1;
+        while (n % 2 == 0) {
+            n >>= 1; // 通过右位操作，把n做2的幂次方的逆逻辑 (相当于除法)
+        }
+        return n == 1; // 尝试将 n 除干净，如果最后剩余数值为 1 则说明开始是 2 的幂。
     }
-
     /**
      * Bit manipulation 位运算
      * Approach 2: Bitwise operators : Turn off the Rightmost 1-bit
+     * 直接利用上面掉到的常用方法 n & (n - 1) 去除最低位的1。
+     * 如果 n 的二进行里面只有一个1，那么去掉最低位的1，就只可能是0，要么非0
      */
     public static boolean isPowerOfTwo_2(int n) {
         return n > 0 && (n & (n - 1)) == 0;  // For example: 1000 & 0111 = 0
@@ -76,6 +81,7 @@ public class Q231_Power_of_Two {
     }
     /**
      * Bit manipulation
+     * 把 n 作为除数， 2^30 作为被除数，那么，如果 n 是 2 的幂次方的话，余数肯定为0
      */
     public boolean isPowerOfTwo_3 ( int n){
         final int BIG = 1 << 30;
@@ -83,11 +89,16 @@ public class Q231_Power_of_Two {
     }
     /**
      * Bit manipulation
+     * 通过左位移操作，列举出 int 型内的全部2的幂次方 (相当于乘法)
      */
     public static boolean isPowerOfTwo_5 ( int n){
-        if (n < 1) return false;
+        if (n < 1){
+            return false;
+        }
         for (int i = 1, sub = 1; i < 32; ++i, sub <<= 1)
-            if (sub == n) return true;
+            if (sub == n) {
+                return true;
+            }
         return false;
     }
 
