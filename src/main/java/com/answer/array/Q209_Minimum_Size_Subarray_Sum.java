47ec41612d9b8b72adc145e184d5a4d540fb3a67
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-01-26 21:26:53 -0500

    update code

--- src/main/java/com/answer/array/Q209_Minimum_Size_Subarray_Sum.java
+++ src/main/java/com/answer/array/Q209_Minimum_Size_Subarray_Sum.java
@@ -1,8 +1,31 @@
 package com.answer.array;
 
 public class Q209_Minimum_Size_Subarray_Sum {
-
+    /**
+     * 方法一：暴力法 Time Limit Exceeded 时间复杂度：O(n2) 效率很差
+     */
     public int minSubArrayLen(int target, int[] nums) {
+        int result = Integer.MAX_VALUE;
+
+       for (int left = 0; left <  nums.length; left++) {
+           int sum = 0;
+            for (int right = left; right < nums.length; right++) {
+                sum += nums[right];
+                if (sum >= target) {
+                    result = Math.min(result, right - left + 1);
+                    break;
+                }
+            }
+        }
+        return result == Integer.MAX_VALUE? 0 : result;
+    }
+    /**
+     * 滑动窗口 时间复杂度：O(n)
+     * 每一轮迭代，将 nums[end] 加到 sum，如果 sum≥s，则更新子数组的最小长度（此时子数组的长度是 end−start+1），
+     * 然后将 nums[start] 从 sum 中减去并将 start 右移，直到 sum<s，在此过程中同样更新子数组的最小长度。
+     * 在每一轮迭代的最后，将 end 右移。
+     */
+    public int minSubArrayLen1(int target, int[] nums) {
         int left = 0;
         int sum = 0;
         int result = Integer.MAX_VALUE;
@@ -11,10 +34,11 @@ public class Q209_Minimum_Size_Subarray_Sum {
             sum += nums[right];
             while(sum >= target){
                 result = Math.min(result, right - left + 1);
-                sum -= nums[left++];
+                sum -= nums[left];
+                left++;
             }
         }
-
         return result == Integer.MAX_VALUE? 0 : result;
     }
+
 }
--- src/main/java/com/answer/dfs_bfs/Q200_Number_of_Islands.java
+++ src/main/java/com/answer/dfs_bfs/Q200_Number_of_Islands.java
@@ -18,8 +18,8 @@ public class Q200_Number_of_Islands {
          * 0 0 0 1 1
          */
         char[][] grid1 = {{'1', '1', '0', '0', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '1', '0', '0'}, {'0', '0', '0', '1', '1'}};
-        System.out.println(numIslands(grid));
-        System.out.println(numIslands(grid1));
+        System.out.println(numIslands_2(grid));
+        System.out.println(numIslands_2(grid1));
     }
     /**
      * Approach #1 DFS
@@ -128,25 +128,25 @@ public class Q200_Number_of_Islands {
         if (grid == null || grid.length == 0) {
             return 0;
         }
-        int nr = grid.length;
-        int nc = grid[0].length;
-        UnionFind uf = new UnionFind(grid);
+        int row = grid.length;
+        int col = grid[0].length;
+        UnionFind_Simple uf = new UnionFind_Simple(grid);
 
-        for (int r = 0; r < nr; ++r) {
-            for (int c = 0; c < nc; ++c) {
+        for (int r = 0; r < row; ++r) {
+            for (int c = 0; c < col; ++c) {
                 if (grid[r][c] == '1') {
                     grid[r][c] = '0';
-                    if (r - 1 >= 0 && grid[r-1][c] == '1') {
-                        uf.union(r * nc + c, (r-1) * nc + c);
+                    if (r - 1 >= 0 && grid[r - 1][c] == '1') {
+                        uf.union(r * col + c, (r - 1) * col + c);
                     }
-                    if (r + 1 < nr && grid[r+1][c] == '1') {
-                        uf.union(r * nc + c, (r+1) * nc + c);
+                    if (r + 1 < row && grid[r + 1][c] == '1') {
+                        uf.union(r * col + c, (r + 1) * col + c);
                     }
-                    if (c - 1 >= 0 && grid[r][c-1] == '1') {
-                        uf.union(r * nc + c, r * nc + c - 1);
+                    if (c - 1 >= 0 && grid[r][c - 1] == '1') {
+                        uf.union(r * col + c, r * col + c - 1);
                     }
-                    if (c + 1 < nc && grid[r][c+1] == '1') {
-                        uf.union(r * nc + c, r * nc + c + 1);
+                    if (c + 1 < col && grid[r][c + 1] == '1') {
+                        uf.union(r * col + c, r * col + c + 1);
                     }
                 }
             }
@@ -154,6 +154,51 @@ public class Q200_Number_of_Islands {
         return uf.getCount();
     }
 }
+/**
+ *
+ */
+class UnionFind_Simple {
+    int count;
+    int[] parent;
+    public UnionFind_Simple(char[][] grid) { // 二维数组 -> 一维数组
+        count = 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        parent = new int[row * col];
+
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (grid[i][j] == '1') {
+                    parent[i * col + j] = i * col + j;
+                    ++count;
+                }
+            }
+        }
+    }
+
+    public int find(int index) { // 找到index的祖先
+        if (parent[index] != index) {
+            parent[index] = find(parent[index]);
+        }
+        return parent[index];
+    }
+
+    public void union(int x, int y) { // 合并x, y成为公同的祖先
+        int rootx = find(x);
+        int rooty = find(y);
+        if (rootx != rooty) {
+            parent[rooty] = rootx;
+            count--;
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+/**
+ *
+ */
 class UnionFind {
     int count;
     int[] parent;
