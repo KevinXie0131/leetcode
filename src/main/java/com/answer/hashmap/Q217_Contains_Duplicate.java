1ad69224cd97d54066dbf4749f46d805549000c0
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-01-26 23:13:01 -0500

    update code

--- src/main/java/com/answer/hashmap/Q217_Contains_Duplicate.java
+++ src/main/java/com/answer/hashmap/Q217_Contains_Duplicate.java
@@ -5,6 +5,8 @@ import java.util.*;
 public class Q217_Contains_Duplicate {
     /**
      * 使用 set
+     * 时间复杂度 : O(n)
+     * 空间复杂度 : O(n)
      */
     public boolean containsDuplicate_0(int[] nums) {
         Set<Integer> set = new HashSet<>();
@@ -34,7 +36,22 @@ public class Q217_Contains_Duplicate {
         return false;
     }
     /**
+     * 使用 hashMap
+     */
+    public boolean containsDuplicate4(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n : nums){
+            map.put(n, map.getOrDefault(n, 0) + 1);
+            if(map.get(n) > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
      * Approach #2 (Sorting) 排序
+     * 时间复杂度 : O(nlogn)。即排序的时间复杂度。扫描的时间复杂度 O(n) 可忽略。
+     * 空间复杂度 : O(1)。 没有用到额外空间。如果深究 Arrays.sort(nums) 使用了栈空间，那就是 O(logn)。
      */
     public boolean containsDuplicate_1(int[] nums) {
         Arrays.sort(nums);
