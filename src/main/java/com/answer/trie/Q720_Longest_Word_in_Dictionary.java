41d4ed6ec54f531c3fe6862416f63306349bb0c2
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-01-29 20:45:18 -0500

    update code

--- src/main/java/com/answer/trie/Q720_Longest_Word_in_Dictionary.java
+++ src/main/java/com/answer/trie/Q720_Longest_Word_in_Dictionary.java
@@ -1,4 +1,44 @@
 package com.answer.trie;
 
+import java.util.HashSet;
+
 public class Q720_Longest_Word_in_Dictionary {
+    public static void main(String[] args) {
+      //  String[] words = {"w","wo","wor","worl","world"};
+        String[] words = {"a","banana","app","appl","ap","apply","apple"};
+        System.out.println(longestWord(words));
+    }
+    /**
+     * 暴力法 HashSet
+     */
+   static public String longestWord(String[] words) {
+        String result = "";
+
+        HashSet<String> set = new HashSet<>();
+        for(String s : words){
+            set.add(s);
+        }
+        for(String word : words){
+            if(word.length() > result.length()
+                    || (word.length() == result.length() && word.compareTo(result) < 0)){
+                boolean isPrefix = true;
+                for(int i = 1; i <= word.length(); i++){
+                    if(!set.contains(word.substring(0, i))){
+                        isPrefix = false;
+                        break;
+                    }
+                }
+                if(isPrefix){ // 所有前缀都匹配
+                    result = word;
+                }
+            }
+        }
+        return result;
+    }
+    /**
+     * Trie 字典树/前缀树
+     */
+    public String longestWord_1(String[] words) {
+
+    }
 }
