8027eec90b82234d0bc45cc94749a5fc0f07a541
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-02-03 21:54:18 -0500

    update code

--- src/main/java/com/answer/two_pointers/Q881_Boats_to_Save_People.java
+++ src/main/java/com/answer/two_pointers/Q881_Boats_to_Save_People.java
@@ -3,13 +3,19 @@ package com.answer.two_pointers;
 import java.util.Arrays;
 
 public class Q881_Boats_to_Save_People {
+    public static void main(String[] args) {
+        int[] people = {3, 5, 3, 4, 1, 2};
+        int limit = 5;
+        int res = numRescueBoats(people, limit);
+        System.out.println(res);
+    }
     /**
      * Approach 1: Greedy (Two Pointer)
      * Time: nlogn
      * Space: logn
      * In Java, Arrays.sort() is implemented using a variant of the Quick Sort algorithm which has a space complexity of O(logn)
      */
-    public int numRescueBoats(int[] people, int limit) {
+   static public int numRescueBoats(int[] people, int limit) {
         /**
          * Sort array at first
          */
