afb8c24f36b3ade8d8d1619d2c9c540175dc3802
KevinX <Xzb04720638k>
2025-02-04 17:24:15 -0500

    update

--- src/main/java/com/answer/binarysearch/Q74_Search_a_2D_Matrix.java
+++ src/main/java/com/answer/binarysearch/Q74_Search_a_2D_Matrix.java
@@ -46,14 +46,14 @@ public class Q74_Search_a_2D_Matrix {
      * Binary Search
      */
     public static boolean searchMatrix_1(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+        int m = matrix.length;    // row
+        int n = matrix[0].length; // column
         int left = 0;
         int right = m * n - 1;
 
         while(left <= right){
             int mid = (left + right) >>> 1;
-            int value = matrix[mid / n][mid % n];
+            int value = matrix[mid / n][mid % n]; // 二维索引 => 一维索引 (x, y) -> x * col + y
 
             if(value == target){
                 return true;
