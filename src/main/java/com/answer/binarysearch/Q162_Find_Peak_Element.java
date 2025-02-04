3a312f57ad23440d4097cfcb9a51f33a87375b5e
KevinX <Xzb04720638k>
2025-02-04 17:13:41 -0500

    update

--- src/main/java/com/answer/binarysearch/Q162_Find_Peak_Element.java
+++ src/main/java/com/answer/binarysearch/Q162_Find_Peak_Element.java
@@ -74,13 +74,13 @@ public class Q162_Find_Peak_Element {
         int r = n - 1;
         while(l < r){
             int mid = l + (r - l) / 2;
-            if(nums[mid] <= nums[n - 1]){
+            if(nums[mid] > nums[mid + 1]){
                 r = mid;
             }
             else{
                 l = mid + 1;
             }
         }
-        return nums[l];
+        return l;
     }
 }
