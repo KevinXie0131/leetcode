419baaacd7f50e215c2426bab6b81a54ffa74729
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-02-16 21:28:18 -0500

    update code

--- src/main/java/com/answer/graph/ShortestPathBellmanFordSingleSource.java
+++ src/main/java/com/answer/graph/ShortestPathBellmanFordSingleSource.java
@@ -4,7 +4,12 @@ import java.util.*;
 
 public class ShortestPathBellmanFordSingleSource {
     /**
+     * 本题为单源有限最短路问题，同样是 kama94.城市间货物运输I 延伸题目。
+     * 注意题目中描述是 最多经过 k 个城市的条件下，而不是一定经过k个城市，也可以经过的城市数量比k小，但要最短的路径。
      *
+     * 所以本题就是求：起点最多经过k + 1 条边到达终点的最短距离。bellman_ford 标准写法是松弛 n-1 次，本题就松弛 k + 1次就好。
+     * 时间复杂度： O(K * E) , K为至多经过K个节点，E为图中边的数量
+     * 空间复杂度： O(N) ，即 minDist 数组所开辟的空间
      */
     public static void main(String[] args) {
         int[][] input = {{1, 2, 1},
@@ -30,23 +35,28 @@ public class ShortestPathBellmanFordSingleSource {
             int val = input[i][2];
             graph.add(new Edge5(from, to, val));
         }
+        // 请计算在最多经过 k 个城市的条件下，从城市 src 到城市 dst 的最低运输成本。
         int src = 2;
         int dst = 6;
         int k =  1;
 
         int[] minDist = new int[n + 1];
-        int[] minDistCopy;
+        // 那么本题 为什么计算minDist 一定要基于上次 的 minDist 数值。
+        // 其关键在于本题的两个因素：
+        //   本题可以有负权回路，说明只要多做松弛，结果是会变的。
+        //   本题要求最多经过k个节点，对松弛次数是有限制的。
+        int[] minDistCopy;  // 用来记录上一次遍历的结果
 
         Arrays.fill(minDist, Integer.MAX_VALUE);
         minDist[src] = 0;
 
-        for (int i = 0; i < k + 1; i++) { // Relax all edges k + 1 times
-            minDistCopy = Arrays.copyOf(minDist, n + 1);
+        for (int i = 0; i < k + 1; i++) { // Relax all edges k + 1 times 对所有边松弛 k + 1次
+            minDistCopy = Arrays.copyOf(minDist, n + 1);  // 获取上一次计算的结果
             for (Edge5 edge : graph) {
                 int from = edge.from;
                 int to = edge.to;
                 int val = edge.val;
-                // Use minDistCopy to calculate minDist
+                // Use minDistCopy to calculate minDist 注意使用 minDist_copy 来计算 minDist
                 if (minDistCopy[from] != Integer.MAX_VALUE && minDist[to] > minDistCopy[from] + val) {
                     minDist[to] = minDistCopy[from] + val;
                 }
@@ -54,9 +64,9 @@ public class ShortestPathBellmanFordSingleSource {
         }
         // Output printing
         if (minDist[dst] == Integer.MAX_VALUE) {
-            System.out.println("unreachable");
+            System.out.println("unreachable"); // 不能到达终点
         } else {
-            System.out.println(minDist[dst]);
+            System.out.println(minDist[dst]); // 到达终点最短路径
         }
     }
 }
---
+++ src/main/java/com/answer/graph/ShortestPathBellmanFordSingleSourceSPFA.java
@@ -0,0 +1,122 @@
+package com.answer.graph;
+
+import java.util.*;
+
+public class ShortestPathBellmanFordSingleSourceSPFA {
+    /**
+     * SPFA算法（版本3）：处理含【负权回路】的有向图的最短路径问题
+     * bellman_ford（版本3） 的队列优化算法版本
+     * 限定起点、终点、至多途径k个节点
+     */
+    public static void main(String[] args) {
+        // 输入控制
+        System.out.println("1.输入N个节点、M条边（u v weight）");
+        int n = 6;
+        int m = 7;
+
+        System.out.println("2.输入M条边");
+        int[][] input = {{1, 2, 1},
+                {2, 4, -3},
+                {2, 5, 2},
+                {1, 3, 5},
+                {3, 5, 1},
+                {4, 6, 4},
+                {5, 6, -2}};
+
+        List<List<Edge6>> graph = new ArrayList<>(); // 构建邻接表
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        while (m-- > 0) {
+            int u = input[m][0];
+            int v = input[m][1];
+            int weight = input[m][2];
+            graph.get(u).add(new Edge6(u, v, weight));
+        }
+
+        System.out.println("3.输入src dst k（起点、终点、至多途径k个点）");
+        int src = 2;
+        int dst = 6;
+        int k =  1;
+
+        // 调用算法
+        int[] minDist = ShortestPathBellmanFordSingleSourceSPFA.spfa(n, graph, src, k);
+        // 校验起点->终点
+        if (minDist[dst] == Integer.MAX_VALUE) {
+            System.out.println("unreachable");
+        } else {
+            System.out.println("最短路径：" + minDist[n]);
+        }
+    }
+    /**
+     * SPFA算法
+     *
+     * @param n        节点个数[1,n]
+     * @param graph    邻接表
+     * @param startIdx 开始节点（源点）
+     */
+    public static int[] spfa(int n, List<List<Edge6>> graph, int startIdx, int k) {
+        // 定义最大范围
+        int maxVal = Integer.MAX_VALUE;
+        // minDist[i] 源点到节点i的最短距离
+        int[] minDist = new int[n + 1]; // 有效节点编号范围：[1,n]
+        Arrays.fill(minDist, maxVal); // 初始化为maxVal
+        minDist[startIdx] = 0; // 设置源点到源点的最短路径为0
+
+        // 定义queue记录每一次松弛更新的节点
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(startIdx); // 初始化：源点开始（queue和minDist的更新是同步的）
+
+        // SPFA算法核心：只对上一次松弛的时候更新过的节点关联的边进行松弛操作
+        while (k + 1 > 0 && !queue.isEmpty()) { // 限定松弛 k+1 次
+            int curSize = queue.size(); // 记录当前队列节点个数（上一次松弛更新的节点个数，用作分层统计）
+            while (curSize-- > 0) { //分层控制，限定本次松弛只针对上一次松弛更新的节点，不对新增的节点做处理
+                // 记录当前minDist状态，作为本次松弛的基础
+                int[] minDist_copy = Arrays.copyOfRange(minDist, 0, minDist.length);
+
+                // 取出节点
+                int cur = queue.poll();
+                // 获取cur节点关联的边，进行松弛操作
+                List<Edge6> relateEdges = graph.get(cur);
+                for (Edge6 edge : relateEdges) {
+                    int u = edge.u; // 与`cur`对照
+                    int v = edge.v;
+                    int weight = edge.val;
+                    if (minDist_copy[u] + weight < minDist[v]) {
+                        minDist[v] = minDist_copy[u] + weight; // 更新
+                        // 队列同步更新（此处有一个针对队列的优化:就是如果已经存在于队列的元素不需要重复添加）
+                        if (!queue.contains(v)) {
+                            queue.offer(v); // 与minDist[i]同步更新，将本次更新的节点加入队列，用做下一个松弛的参考基础
+                        }
+                    }
+                }
+            }
+            // 当次松弛结束，次数-1
+            k--;
+        }
+
+        // 返回minDist
+        return minDist;
+    }
+}
+
+class Edge6 {
+    public int u; // 边的端点1
+    public int v; // 边的端点2
+    public int val; // 边的权值
+
+    public Edge6() {
+    }
+
+    public Edge6(int u, int v) {
+        this.u = u;
+        this.v = v;
+        this.val = 0;
+    }
+
+    public Edge6(int u, int v, int val) {
+        this.u = u;
+        this.v = v;
+        this.val = val;
+    }
+}
