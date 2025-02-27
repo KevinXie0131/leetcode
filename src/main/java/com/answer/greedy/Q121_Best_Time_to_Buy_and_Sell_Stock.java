1b02a18ce7be512a6157cbf6d374d55d5049b276
KevinX <Xzb04720638k>
2025-02-27 15:37:36 -0500

    update

--- src/main/java/com/answer/greedy/Q121_Best_Time_to_Buy_and_Sell_Stock.java
+++ src/main/java/com/answer/greedy/Q121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -90,4 +90,20 @@ public class Q121_Best_Time_to_Buy_and_Sell_Stock {
        System.out.println(Arrays.deepToString(dp));
         return result;
     }
+    /**
+     * Another form
+     */
+    public int maxProfit_5(int[] prices) {
+        int[][] dp = new int[prices.length][3];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        dp[0][2] = 0;
+
+        for(int i = 1; i < prices.length; i++){
+            dp[i][0] = dp[i - 1][0];
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
+        }
+        return dp[prices.length-1][2];
+    }
 }
