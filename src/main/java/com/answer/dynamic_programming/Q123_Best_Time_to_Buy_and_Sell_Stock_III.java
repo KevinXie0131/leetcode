9d6eec82627a66486a8d842209aff1e9e9e44c67
KevinX <Xzb04720638k>
2025-02-27 13:42:21 -0500

    update

--- src/main/java/com/answer/dynamic_programming/Q123_Best_Time_to_Buy_and_Sell_Stock_III.java
+++ src/main/java/com/answer/dynamic_programming/Q123_Best_Time_to_Buy_and_Sell_Stock_III.java
@@ -23,4 +23,33 @@ public class Q123_Best_Time_to_Buy_and_Sell_Stock_III {
         }
         return dp[prices.length - 1][4];
     }
+    /**
+     * refer to Q121 template
+     */
+    public int maxProfit_4(int[] prices) {
+        int result = 0;
+
+        int[][] dp = new int[prices.length][5];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        dp[0][2] = 0;
+        dp[0][3] = -prices[0];
+        dp[0][4] = 0;
+
+        for(int i = 1; i < prices.length; i++){
+            dp[i][0] = dp[i - 1][0];
+            dp[i][1] =  Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            dp[i][2] =  Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
+            dp[i][3] =  Math.max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
+            dp[i][4] =  Math.max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
+
+            result = Math.max(result, dp[i][0]);
+            result = Math.max(result, dp[i][1]);
+            result = Math.max(result, dp[i][2]);
+            result = Math.max(result, dp[i][3]);
+            result = Math.max(result, dp[i][4]);
+        }
+
+        return result;
+    }
 }
