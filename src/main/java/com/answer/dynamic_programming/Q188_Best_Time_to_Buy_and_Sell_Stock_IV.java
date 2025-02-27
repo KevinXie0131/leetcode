3454962a27fdf489ea14b6d99f9ad3a94e205922
KevinX <Xzb04720638k>
2025-02-27 14:29:41 -0500

    update

--- src/main/java/com/answer/dynamic_programming/Q188_Best_Time_to_Buy_and_Sell_Stock_IV.java
+++ src/main/java/com/answer/dynamic_programming/Q188_Best_Time_to_Buy_and_Sell_Stock_IV.java
@@ -1,5 +1,7 @@
 package com.answer.dynamic_programming;
 
+import java.util.Arrays;
+
 public class Q188_Best_Time_to_Buy_and_Sell_Stock_IV {
     /**
      * 每天买卖k次 (k = 0: Q121 / k不限: Q122 / k = 2: Q123)
@@ -22,5 +24,43 @@ public class Q188_Best_Time_to_Buy_and_Sell_Stock_IV {
             }
         }
         return dp[prices.length - 1][2 * k];
+    }
+    /**
+     * A more general template for multiple transactions per day
+     */
+    public static void main(String[] args) {
+        int k = 2;
+        int[] prices = {3,2,6,5,0,3};
+        int res = maxProfit_5(k, prices);
+        System.out.println(res);
+    }
+    static public int maxProfit_5(int k, int[] prices) {
+        int result = 0;
+
+        int[][][] dp = new int[prices.length][k + 1][2]; // j is transaction number. k=0: not hold / k=1: hold
+        dp[0][0][0] = 0;
+        dp[0][0][1] = -prices[0];
+        for(int num = 1; num < k + 1; num++){
+            dp[0][num][0] = dp[0][num][1] = -10000; // 0 <= prices[i] <= 105
+        }
+
+        for(int i = 1; i < prices.length; i++){
+            dp[i][0][0] = dp[i - 1][0][0];
+            dp[i][0][1] = Math.max(dp[i - 1][0][1], dp[i - 1][0][0] - prices[i]);
+
+            for(int num = 1; num < k; num++){
+                dp[i][num][0] = Math.max(dp[i - 1][num][0], dp[i - 1][num - 1][1] + prices[i]);
+                dp[i][num][1] = Math.max(dp[i - 1][num][1], dp[i - 1][num][0] - prices[i]);
+            }
+
+            dp[i][k][0] = Math.max(dp[i - 1][k][0], dp[i - 1][k - 1][1] + prices[i]);
+
+        }
+        System.out.println(Arrays.deepToString(dp));
+
+        for(int num = 0; num < k + 1; num++){
+            result = Math.max(result, dp[prices.length - 1][num][0]);
+        }
+        return result;
     }
 }
