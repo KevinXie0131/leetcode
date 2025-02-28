134d1a07f012a3049fbab33f77a4046e1458b63a
KevinX <Xzb04720638k>
2025-02-28 16:19:05 -0500

    update

--- src/main/java/com/answer/dynamic_programming/Q309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
+++ src/main/java/com/answer/dynamic_programming/Q309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
@@ -10,13 +10,13 @@ public class Q309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown {
         if (n == 0) return 0;
 
         int[][] dp = new int[n][4];
-        dp[0][0] -= prices[0]; // 持股票
+        dp[0][0] -= prices[0]; // 持有股票
 
         for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], Math.max(dp[i - 1][3], dp[i - 1][1]) - prices[i]);
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][3]);
-            dp[i][2] = dp[i - 1][0] + prices[i];
-            dp[i][3] = dp[i - 1][2];
+            dp[i][0] = Math.max(dp[i - 1][0], Math.max(dp[i - 1][3] - prices[i], dp[i - 1][1] - prices[i]) ); // 持有股票
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][3]);                                     // 保持卖出状态(不持有状态之一)
+            dp[i][2] = dp[i - 1][0] + prices[i];                                                 // 卖出股票(不持有状态之一) 之后有冷冻期
+            dp[i][3] = dp[i - 1][2];                                                             // 冷冻期
         }
         return Math. max(dp[n - 1][3], Math.max(dp[n - 1][1], dp[n - 1][2]));
     }
