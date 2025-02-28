10cdf0664245db48b4a92365801fa76d9ccaa04e
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-02-27 19:30:30 -0500

    update code

--- src/main/java/com/answer/dynamic_programming/Q509_Fibonacci_Number.java
+++ src/main/java/com/answer/dynamic_programming/Q509_Fibonacci_Number.java
@@ -20,20 +20,21 @@ public class Q509_Fibonacci_Number {
     /**
      * Approach 2: Bottom-Up Approach using Tabulation 动态规划
      * dp 数组的迭代解法
+     * dp[i]是第i个数值
      */
     public int fib_1(int n) {
         if(n <= 1) {
             return n; // If n = 0, return 0. If n = 1, return 1.
         }
 
-        int[] cache = new int[n + 1]; // 0 - n
-        cache[0] = 0; // 初始状态
-        cache[1] = 1;
+        int[] dp = new int[n + 1]; // 0 - n
+        dp[0] = 0; // 初始状态
+        dp[1] = 1;
 
         for(int i = 2; i <= n; i++){
-            cache[i] = cache[i-1] + cache[i-2]; // 状态转移方程
+            dp[i] = dp[i-1] + dp[i-2]; // 状态转移方程
         }
-        return cache[n]; // 终止状态
+        return dp[n]; // 终止状态
     }
     /**
      * Approach 3: Top-Down Approach using Memoization 记忆化搜索
@@ -61,12 +62,12 @@ public class Q509_Fibonacci_Number {
         if(n <= 1) return n;
 
         int res = 0;;
-        int prepre = 0;
-        int pre = 1;
+        int first = 0;
+        int second = 1;
         for(int i = 2; i <= n; i++){
-            res = pre + prepre;
-            prepre = pre;
-            pre = res;
+            res = first + second;
+            first = second;
+            second = res;
         }
         return res;
     }
