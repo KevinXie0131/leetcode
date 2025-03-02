fdd55aa452021423e7bb1487fedb4c4c2eadb75d
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-03-01 22:24:41 -0500

    update code

--- src/main/java/com/answer/dynamic_programming/Q516_Longest_Palindromic_Subsequence.java
+++ src/main/java/com/answer/dynamic_programming/Q516_Longest_Palindromic_Subsequence.java
@@ -15,22 +15,22 @@ public class Q516_Longest_Palindromic_Subsequence{
     // 下面的代码同时也使用了滚动数组节省空间, 写法上也比较晦涩难懂, 如果您看不明白也不要担心, 看不明白就对啦，就是想让您看不明白
     /**
      * 动态规划
-     * 找出其中最长的回文子序列，并返回该序列的长度
+     * 找出其中最长的回文子序列(不要求连续)，并返回该序列的长度
      * 子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
      * A subsequence is a sequence that can be derived from another sequence by deleting some or no elements
      * without changing the order of the remaining elements.
      */
     public int longestPalindromeSubseq(String s) {
-        int[][] dp = new int[s.length()][s.length()];
+        int[][] dp = new int[s.length()][s.length()]; // dp: [i, j]回文子序列的长度
         for (int i = 0; i < s.length(); i++) {
-            dp[i][i] = 1;
+            dp[i][i] = 1; // i, j相同，指向同一个元素，一个元素也是回文，长度为1
         }
-        for (int i = s.length() - 1; i >= 0; i--) {
-            for (int j = i + 1; j <= s.length() - 1; j++) {
+        for (int i = s.length() - 1; i >= 0; i--) { // dp[i + 1][j - 1]推出[i][j]，所以i从下往上遍历
+            for (int j = i + 1; j <= s.length() - 1; j++) { // j = i 已经初始化过了
                 if (s.charAt(i) == s.charAt(j)) {
-                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                    dp[i][j] = dp[i + 1][j - 1] + 2; // [i + 1, j - 1] 是里面的回文子序列
                 } else {
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]); //不相等 考虑[i + 1, j]和[i, j - 1]
                 }
             }
         }
