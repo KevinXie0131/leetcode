523efa43d4cfbe0000ccb5ff40ac1b2c642edc2a
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-03-02 21:25:51 -0500

    update code

--- src/main/java/com/answer/dynamic_programming/Q343_Integer_Break.java
+++ src/main/java/com/answer/dynamic_programming/Q343_Integer_Break.java
@@ -13,14 +13,16 @@ public class Q343_Integer_Break {
      */
     public static int integerBreak(int n) {
         int[] dp = new int[n + 1];
+        // dp[0] dp[i] 无法拆分
         dp[2] = 1;
 
         for(int i = 3; i < n + 1; i++){ // 对于数字 i，它可以分为两份：j 和 i-j，j 的范围是 1 到 i-j
+        //  for(int j = 1; j < i; j++) // works too
+        //  for(int j = 1; j <= i/2; j++){ // works too 尽可能拆分成相似的数值
             for(int j = 1; j < i - 1; j++){ // 对于 i-j 这部分可以拆或不拆，不拆就是 i-j，拆就是 dp[i-j]
-                dp[i] = Math.max(dp[i], Math.max(dp[i-j]*j, (i-j)*j));
+                dp[i] = Math.max(dp[i], Math.max(dp[i - j] * j, (i - j) * j));
             }
         }
-
         return dp[n];
     }
 
--- src/main/java/com/answer/dynamic_programming/Q96_Unique_Binary_Search_Trees.java
+++ src/main/java/com/answer/dynamic_programming/Q96_Unique_Binary_Search_Trees.java
@@ -1,17 +1,18 @@
 package com.answer.dynamic_programming;
 
 public class Q96_Unique_Binary_Search_Trees {
-
     /**
      * Approach 1: Dynamic Programming
+     * 互不相同的 二叉搜索树 有多少种
+     * For example: dp[3] = dp[0]*dp[2] + dp[1]*dp[1] + dp[2]*dp[0]
      */
     public int numTrees(int n) {
         int[] dp = new int[n+1];
 
-        dp[0] = 1;
-        for(int i = 1; i < n+1; i++){
+        dp[0] = 1; // 0个节点只有1种
+        for(int i = 1; i < n + 1; i++){
             for(int j = 1; j <= i; j++){
-                dp[i] += dp[i-j] * dp[j-1];
+                dp[i] += dp[i - j] * dp[j - 1]; // 总共i个节点，左面有j-1个节点，右面有i-j个节点
             }
         }
 
