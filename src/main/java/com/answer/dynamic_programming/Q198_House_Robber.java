aceadec67a35f5d64a750e00fdc9a6fc4accb276
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-03-03 22:59:31 -0500

    update code

--- src/main/java/com/answer/dynamic_programming/Q198_House_Robber.java
+++ src/main/java/com/answer/dynamic_programming/Q198_House_Robber.java
@@ -33,13 +33,13 @@ public class Q198_House_Robber {
     public int rob_1(int[] nums) {
         if(nums.length == 1) return nums[0];
 
-        int[] dp = new int[nums.length]; //
+        int[] dp = new int[nums.length];
         // Base cases
         dp[0]= nums[0];
         dp[1]= Math.max(nums[0],nums[1]);
 
         for(int i = 2; i < nums.length; i++){
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);// Recurrence relation
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);// Recurrence relation 在i - 1和i - 2只能选一个
         }
         return dp[nums.length - 1];
     }
