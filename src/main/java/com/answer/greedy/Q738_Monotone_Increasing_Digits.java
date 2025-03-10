9f6ee80ebe2bc915060c4b7cbf64aa9693dfdf0e
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-03-09 22:44:59 -0400

    update code

--- src/main/java/com/answer/greedy/Q738_Monotone_Increasing_Digits.java
+++ src/main/java/com/answer/greedy/Q738_Monotone_Increasing_Digits.java
@@ -5,7 +5,26 @@ public class Q738_Monotone_Increasing_Digits {
         int n = 332;
         System.out.println(monotoneIncreasingDigits_1(n));
     }
-
+    /**
+     * 贪心算法: 题目要求小于等于N的最大单调递增的整数
+     * 一旦出现strNum[i - 1] > strNum[i]的情况（非单调递增），首先想让strNum[i - 1]--，然后strNum[i]给为9，
+     * 从后向前遍历，就可以重复利用上次比较得出的结果了，从后向前遍历332的数值变化为：332 -> 329 -> 299
+     */
+    public int monotoneIncreasingDigits_4(int N) { // java版本1中创建了String数组，多次使用Integer.parseInt了方法，这导致不管是耗时还是空间占用都非常高，用时12ms，
+        String[] strings = (N + "").split("");
+        // start 用来标记赋值9从哪里开始
+        int start = strings.length;    // 设置为这个默认值，为了防止第二个for循环在start没有被赋值的情况下执行
+        for (int i = strings.length - 1; i > 0; i--) {
+            if (Integer.parseInt(strings[i]) < Integer.parseInt(strings[i - 1])) {
+                strings[i - 1] = (Integer.parseInt(strings[i - 1]) - 1) + "";
+                start = i;
+            }
+        }
+        for (int i = start; i < strings.length; i++) {
+            strings[i] = "9";
+        }
+        return Integer.parseInt(String.join("",strings));
+    }
     /**
      * Greedy
      */
@@ -34,7 +53,7 @@ public class Q738_Monotone_Increasing_Digits {
      *
      * 从后向前遍历
      */
-    public static int monotoneIncreasingDigits_1(int n) {
+    public static int monotoneIncreasingDigits_1(int n) { // 提供一个版本在char数组上原地修改，用时1ms的版本
    //     char[] arr = new String(n + "").toCharArray();
         char[] arr = String.valueOf(n).toCharArray();
 
@@ -52,5 +71,27 @@ public class Q738_Monotone_Increasing_Digits {
         }
       //  return Integer.parseInt(new String(arr));
         return Integer.parseInt(String.valueOf(arr));
+    }
+    /**
+     * 暴力解法 Time Limit Exceeded
+     */
+    public int monotoneIncreasingDigits_2(int n) {
+        for (int i = n; i > 0; i--) { // 从大到小遍历
+            if (checkNum(i)) return i;
+        }
+        return 0;
+    }
+    public boolean checkNum(int num) {
+        int max = 10;
+        while (num >= 0) {
+            int t = num % 10;
+            if (max >= t){
+                max = t;
+            } else {
+                return false;
+            }
+            num = num / 10;
+        }
+        return true;
     }
 }
--- src/main/java/com/answer/greedy/Q968_Binary_Tree_Cameras.java
+++ src/main/java/com/answer/greedy/Q968_Binary_Tree_Cameras.java
@@ -3,7 +3,75 @@ package com.answer.greedy;
 import com.template.TreeNode;
 
 public class Q968_Binary_Tree_Cameras {
+    /**
+     * 贪心算法
+     * 因为头结点放不放摄像头也就省下一个摄像头， 叶子节点放不放摄像头省下了的摄像头数量是指数阶别的。
+     * 所以我们要从下往上看，局部最优：让叶子节点的父节点安摄像头，所用摄像头最少，整体最优：全部摄像头数量所用最少！
+     * 这道题目还有两个难点：
+     *  二叉树的遍历: 可以使用后序遍历也就是左右中的顺序，这样就可以在回溯的过程中从下到上进行推导了。
+     *  如何隔两个节点放一个摄像头
+     *
+     * 分别有三个数字来表示每个节点可能有几种状态：
+     *   0：该节点无覆盖
+ *       1：本节点有摄像头
+     *   2：本节点有覆盖
+     */
+    int res = 0;
+    public int minCameraCover_1(TreeNode root) {
+        if(minCame(root) == 0){ // 对根节点的状态做检验,防止根节点是无覆盖状态
+            res++;
+        }
+        return res;
+    }
+    // 节点的状态值： 0 表示无覆盖  1 表示有摄像头 2 表示有覆盖
+    // 后序遍历，根据左右节点的情况,来判读 自己的状态
+    public int minCame(TreeNode root){
+        if(root == null){ // 空节点默认为 有覆盖状态，避免在叶子节点上放摄像头
+            return 2;
+        }
+        int left = minCame(root.left);
+        int right = minCame(root.right);
 
+        if(left == 2 && right == 2){ // 如果左右节点都覆盖了的话, 那么本节点的状态就应该是无覆盖,没有摄像头(2,2)
+            return 0;
+        }else if(left == 0 || right == 0){ // 左右节点都是无覆盖状态,那 根节点此时应该放一个摄像头 (0,0) (0,1) (0,2) (1,0) (2,0)
+            res++; // 状态值为 1 摄像头数 ++;
+            return 1;
+        }else{
+            // 左右节点的 状态为 (1,1) (1,2) (2,1) 也就是左右节点至少存在 1个摄像头，
+            return 2;// 那么本节点就是处于被覆盖状态
+        }
+    }
+    /**
+     * 简化分支版本
+     */
+    int ans;
+    public int minCameraCover_2(TreeNode root) {
+        ans = 0; // 初始化
+        if(f(root) == 0) ans ++;
+        return ans;
+    }
+    // 定义 f 函数有三种返回值情况
+    // 0：表示 x 节点没有被相机监控，只能依靠父节点放相机
+    // 1：表示 x 节点被相机监控，但相机不是放在自身节点上
+    // 2：表示 x 节点被相机监控，但相机放在自身节点上
+    public  int f(TreeNode x) {
+        if(x == null) return 1; // 空树认为被监控，但没有相机
+        // 左右递归到最深处
+        int l = f(x.left);
+        int r = f(x.right);
+        // 有任意一个子节点为空，就需要当前节点放相机，不然以后没机会
+        if(l == 0 || r == 0) {
+            ans ++; // 放相机
+            return 2;
+        }
+        // 贪心策略，左右子树都被监控，且没有监控到当前节点，
+        // 那么最有利的情况就是将相机放置在当前节点父节点上，
+        // 因为这样能多监控可能的子树节点和父父节点
+        if(l == 1 && r == 1) return 0;
+        // 剩下情况就是左右子树有可能为 2，即当前节点被监控
+        return 1;
+    }
     /**
      * Approach 2: Greedy
      * If a node has children that are not covered by a camera, then we must place a camera here.
