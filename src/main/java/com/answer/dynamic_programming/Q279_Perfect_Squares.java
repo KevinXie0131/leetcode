4c53b8fcf9737f8df0e01dbe79d433154c2deaf5
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-03-12 23:04:35 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q279_Perfect_Squares.java
+++ src/main/java/com/answer/dynamic_programming/Q279_Perfect_Squares.java
@@ -8,7 +8,8 @@ public class Q279_Perfect_Squares {
         System.out.println(numSquares(n));
     }
     /**
-     * 类似Q322 Coin Change
+     * 完全背包 类似Q322 Coin Change
+     * 完全平方数就是物品（可以无限件使用），凑个正整数n就是背包，问凑满这个背包最少有多少物品？
      */
     public static int numSquares_0(int n) { // Memory Limit Exceeded
         int MAX = n + 1;
@@ -52,18 +53,28 @@ public class Q279_Perfect_Squares {
         return dp[n][amt] != MAX ? dp[n][amt] : -1;
     }
     /**
-     * Approach 2: Dynamic Programming
+     * Approach 2: Dynamic Programming 完全背包
+     * 所以本题外层for遍历背包，内层for遍历物品，还是外层for遍历物品，内层for遍历背包，都是可以的
      *  // 版本一，先遍历物品, 再遍历背包
      *  https://leetcode.com/problems/perfect-squares/Figures/279/279_dp.png
      */
     public static int numSquares(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
-
-        for(int i = 1; i <= n; i++) {
-            for (int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+        int[] dp = new int[n + 1]; // dp[i]：和为i的完全平方数的最少数量为dp[i]
+        Arrays.fill(dp, Integer.MAX_VALUE); // 所以非0下标的dp[j]一定要初始为最大值，这样dp[j]在递推的时候才不会被初始值覆盖。
+        dp[0] = 0; // dp[0]表示 和为0的完全平方数的最小数量，那么dp[0]一定是0
+        // dp[0] = 0
+         //dp[1] = min(dp[0] + 1) = 1
+        // dp[2] = min(dp[1] + 1) = 2
+        // dp[3] = min(dp[2] + 1) = 3
+        // dp[4] = min(dp[3] + 1, dp[0] + 1) = 1
+        // dp[5] = min(dp[4] + 1, dp[1] + 1) = 2
+        for(int i = 1; i <= n; i++) { // 遍历背包
+            for (int j = 1; j * j <= i; j++) { // 遍历物品
+                //if (dp[j - i * i] != max) {
+                    dp[i] = Math.min(dp[i], dp[i - j * j] + 1); // 选择最小的dp[i]
+                //}
+                // 不需要这个if statement，因为在完全平方数这一题不会有"凑不成"的状况发生（ 一定可以用"1"来组成任何一个n），
+                // 故comment掉这个if statement。
             }
         }
         System.out.println(Arrays.toString(dp));
