d1309216a688fefa3b90f633a7aae2f316b84897
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-03-12 21:01:41 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q1049_Last_Stone_Weight_II.java
+++ src/main/java/com/answer/dynamic_programming/Q1049_Last_Stone_Weight_II.java
@@ -4,6 +4,11 @@ public class Q1049_Last_Stone_Weight_II {
     /**
      * Dynamic Programming  0-1背包（每个元素只可以使用一次）
      * 参考Q416 Partition Equal Subset Sum
+     *
+     * 本题其实是尽量让石头分成重量相同的两堆（尽可能相同），相撞之后剩下的石头就是最小的。
+     * 问题就是有一堆石头，每个石头都有自己的重量，是否可以 装满 最大重量为 sum / 2的背包。
+     * 416. 分割等和子集 是求背包是否正好装满，而本题是求背包最多能装多少。
+     * 物品就是石头，物品的重量为stones[i]，物品的价值也为stones[i]
      */
     public int lastStoneWeightII_1(int[] stones) { // 将两堆石头尽可能分成相同重量
         int sum = 0;
@@ -13,9 +18,13 @@ public class Q1049_Last_Stone_Weight_II {
         int target = sum / 2;
 
         int[][] dp = new int[stones.length + 1][target + 1]; // 初始化为0
-
+        //dp[1][j]取决于stones[0] （可以省略）
+        for (int j = stones[0]; j <= target; j++) {
+            dp[1][j] = stones[0];
+        }
         for(int i = 1; i <= stones.length; i++){
-            for(int j = 1; j <= target; j++){
+            for(int j = 1; j <= target; j++){ //注意是等于
+                //不放:dp[i - 1][j] 放:dp[i - 1][j - stones[i]] + stones[i]
                 if (j < stones[i - 1]) {
                     dp[i][j] = dp[i - 1][j];
                 } else {
@@ -67,15 +76,17 @@ public class Q1049_Last_Stone_Weight_II {
         }
         int target = sum / 2;
 
-        int[] dp = new int[15001];
+        int[] dp = new int[15001]; // 因为提示中给出1 <= stones.length <= 30，1 <= stones[i] <= 1000，所以最大重量就是30 * 1000 。而我们要求的target其实只是最大重量的一半，所以dp数组开到15000大小就可以了。
 
         // 如果使⽤⼀维dp数组，物品遍历的for循环放在外层，遍历背包的for循环放在内层，且内层for循环倒叙遍历
         for(int i = 0; i < stones.length; i++){ // 遍历物品
             for(int j= target; j >= stones[i]; j--){ // 遍历背包
-                dp[j] = Math.max(dp[j], dp[j - stones[i]]  + stones[i]);
+                // 01背包的递推公式为：dp[j] = max(dp[j], dp[j - weight[i]] + value[i]);
+                // 本题则是：dp[j] = max(dp[j], dp[j - stones[i]] + stones[i])
+                dp[j] = Math.max(dp[j], dp[j - stones[i]]  + stones[i]);  //两种情况，要么放，要么不放
             }
         }
-
-        return sum - dp[target] - dp[target];
+        // 在计算target的时候，target = sum / 2 因为是向下取整，所以sum - dp[target] 一定是大于等于dp[target]的
+        return sum - dp[target] - dp[target]; // 相撞之后剩下的最小石头重量就是 (sum - dp[target]) - dp[target]
     }
 }
--- src/main/java/com/answer/dynamic_programming/Q416_Partition_Equal_Subset_Sum.java
+++ src/main/java/com/answer/dynamic_programming/Q416_Partition_Equal_Subset_Sum.java
@@ -5,25 +5,44 @@ public class Q416_Partition_Equal_Subset_Sum {
      * classic Knapsack problem 0-1背包（每个元素只可以使用一次）
      * Approach 3: Bottom Up Dynamic Programming
      * 判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等
+     *
+     * 这是 背包算法可以解决的经典类型题目。
+     * 本题的本质是，能否把容量为 sum / 2的背包装满。一个数字只有一个维度，即 重量等于价值。
+     * 当数字 可以装满 承载重量为 sum / 2 的背包的背包时，这个背包的价值也是 sum / 2。
+     *
+     * 01背包相对于本题，主要要理解，题目中物品是nums[i]，重量是nums[i]，价值也是nums[i]，背包体积是sum/2。
      */
     public boolean canPartition_1(int[] nums) {
         int sum = 0;
         for(int i = 0; i < nums.length; i++){
             sum += nums[i];
         }
-        if(sum % 2 == 1) return false;
+        if(sum % 2 == 1) return false; //总和为奇数，不能平分
         int target = sum / 2;
 
+        //  dp[i][j]表示从数组的 [0, i] 这个子区间内挑选一些正整数每个数只能用一次，使得这些数的和恰好等于 j。
         int[][] dp = new int[nums.length + 1][target + 1]; // 初始化为0
 
-        for(int i = 1; i <= nums.length; i++){
+        for(int i = 1; i <= nums.length; i++){ // //物品 i 的重量是 nums[i]，其价值也是 nums[i]
             for(int j = 1; j <= target; j++){
+                //如果某个物品单独的重量恰好就等于背包的重量，那么也是满足dp数组的定义的
+                if (nums[i - 1] == target) {
+                    return true;
+                }
+
                 if (j < nums[i - 1]) {
                     dp[i][j] = dp[i - 1][j];
                 } else {
+                    // 如果某个物品的重量小于j，那就可以看该物品是否放入背包
+                    // dp[i - 1][j]表示该物品不放入背包，如果在 [0, i - 1] 这个子区间内已经有一部分元素，使得它们的和为 j ，那么 dp[i][j] = true；
+                    // dp[i - 1][j - nums[i]]表示该物品放入背包。如果在 [0, i - 1] 这个子区间内就得找到一部分元素，使得它们的和为 j - nums[i]。
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i - 1]] + nums[i - 1]); //  0-1背包
                 }
             }
+
+            //剪枝一下，每一次完成內層的for-loop，立即檢查是否dp[target] == target，優化時間複雜度（26ms -> 20ms）
+            if(dp[i][target] == target)
+                return true;
         }
         return dp[nums.length][target] == target;  // 每个元素即是重量，也是价值
     }
@@ -42,11 +61,13 @@ public class Q416_Partition_Equal_Subset_Sum {
         int target = sum / 2;  // 背包体积是sum/2
 
         // 每个数组中的元素不会超过 100，数组的⼤⼩不会超过 200总和不会⼤于20000，背包最⼤只需要其中⼀半，所以10001⼤⼩就可以了
-        int[] dp = new int[20001];
+        int[] dp = new int[10001]; // dp[j] 表示: 容量（所能装的重量）为j的背包，所背的物品价值最大可以为dp[j]
         // 01背包
         for(int i = 0; i < nums.length; i++){
-            for(int j= target; j >= nums[i]; j--){
-                // 每⼀个元素⼀定是不可重复放⼊，所以从⼤到⼩遍历
+            for(int j= target; j >= nums[i]; j--){ // 每⼀个元素⼀定是不可重复放⼊，所以从⼤到⼩遍历
+                // 01背包的递推公式为：dp[j] = max(dp[j], dp[j - weight[i]] + value[i]);
+                // 本题，相当于背包里放入数值，那么物品i的重量是nums[i]，其价值也是nums[i]。
+                // 所以递推公式：dp[j] = max(dp[j], dp[j - nums[i]] + nums[i])
                 dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
             }
         }
