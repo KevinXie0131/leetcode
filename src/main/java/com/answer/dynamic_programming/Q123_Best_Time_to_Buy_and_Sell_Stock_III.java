004e6fb2727276d298dda825f59cb1a892647208
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-03-15 20:34:04 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q123_Best_Time_to_Buy_and_Sell_Stock_III.java
+++ src/main/java/com/answer/dynamic_programming/Q123_Best_Time_to_Buy_and_Sell_Stock_III.java
@@ -4,13 +4,22 @@ import java.util.Arrays;
 
 public class Q123_Best_Time_to_Buy_and_Sell_Stock_III {
     /**
+     * 这道题目相对 121.买卖股票的最佳时机 和 122.买卖股票的最佳时机II 难了不少。
+     * 关键在于至多买卖两次，这意味着可以买卖一次，可以买卖两次，也可以不买卖。
+     *
      * Dynamic Programming - Hard
      * 买卖2次: 最多可以完成两笔交易 (在再次购买前出售掉之前的股票）
+     * 一天一共就有五个状态，
+     *  没有操作 （其实我们也可以不设置这个状态）
+     *  第一次持有股票
+     *  第一次不持有股票
+     *  第二次持有股票
+     *  第二次不持有股票
      */
     public int maxProfit(int[] prices) {
         if (prices.length == 0) return 0;
-        int[][] dp = new int[prices.length][5];
-
+        int[][] dp = new int[prices.length][5]; // dp[i][j]表示第i天状态j所剩最大现金
+        // 需要注意：dp[i][1]，表示的是第i天，买入股票的状态，并不是说一定要第i天买入股票，这是容易陷入的误区。
         dp[0][0] = 0;
         dp[0][1] = -prices[0];
         dp[0][2] = 0;
@@ -19,12 +28,19 @@ public class Q123_Best_Time_to_Buy_and_Sell_Stock_III {
 
         for (int i = 1; i < prices.length; i++) {
             dp[i][0] = dp[i - 1][0];                                     // 不操作
+            // dp[i][1]状态，有两个具体操作：
+            // 操作一：第i天买入股票了，那么dp[i][1] = dp[i-1][0] - prices[i]
+            // 操作二：第i天没有操作，而是沿用前一天买入的状态，即：dp[i][1] = dp[i - 1][1]
             dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]); // 第一次持有 / 0 - prices[i] can work too
+            // dp[i][2]也有两个操作：
+            // 操作一：第i天卖出股票了，那么dp[i][2] = dp[i - 1][1] + prices[i]
+            // 操作二：第i天没有操作，沿用前一天卖出股票的状态，即：dp[i][2] = dp[i - 1][2]
             dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]); // 第一次不持有
             dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] - prices[i]); // 第二次持有
             dp[i][4] = Math.max(dp[i - 1][4], dp[i - 1][3] + prices[i]); // 第二次不持有
         }
-        return dp[prices.length - 1][4]; // dp[prices.length - 1][4] 已经包含了 dp[prices.length - 1][2]
+        // 两次卖出的状态现金最大一定是最后一次卖出
+        return dp[prices.length - 1][4]; // 如果第一次卖出已经是最大值了，那么我们可以在当天立刻买入再立刻卖出。所以dp[prices.length - 1][4] 已经包含了 dp[prices.length - 1][2], 也就是说第二次卖出手里所剩的钱一定是最多的。
       //  return Math.max(dp[prices.length - 1][2], dp[prices.length - 1][4]); // It is the same
     }
     /**
@@ -42,6 +58,45 @@ public class Q123_Best_Time_to_Buy_and_Sell_Stock_III {
             dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] + prices[i]); // 第二次不持有
         }
         return dp[prices.length - 1][3];
+    }
+    /**
+     * 版本一: 定义 5 种状态:
+     * 0: 没有操作, 1: 第一次买入, 2: 第一次卖出, 3: 第二次买入, 4: 第二次卖出
+     */
+    public int maxProfit3(int[] prices) {
+        int len = prices.length;
+        int[][] dp = new int[len][5];
+        dp[0][1] = -prices[0];
+        // 初始化第二次买入的状态是确保 最后结果是最多两次买卖的最大利润
+        dp[0][3] = -prices[0];
+
+        for (int i = 1; i < len; i++) {
+            dp[i][1] = Math.max(dp[i - 1][1], 0 - prices[i]);
+            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
+            dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
+            dp[i][4] = Math.max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
+        }
+        return dp[len - 1][4];
+    }
+    /**
+     * 版本二: 空间优化
+     */
+    public int maxProfit6(int[] prices) {
+        int[] dp = new int[4];
+        // 存储两次交易的状态就行了
+        dp[0] = -prices[0]; // dp[0]代表第一次交易的买入
+        dp[1] = 0;          // dp[1]代表第一次交易的卖出
+        dp[2] = -prices[0]; // dp[2]代表第二次交易的买入
+        dp[3] = 0;          // dp[3]代表第二次交易的卖出
+        for(int i = 1; i <= prices.length; i++){
+            // 要么保持不变，要么没有就买，有了就卖
+            dp[0] = Math.max(dp[0], 0 - prices[i-1]);
+            dp[1] = Math.max(dp[1], dp[0] + prices[i-1]);
+            // 这已经是第二次交易了，所以得加上前一次交易卖出去的收获
+            dp[2] = Math.max(dp[2], dp[1] - prices[i-1]);
+            dp[3] = Math.max(dp[3], dp[2] + prices[i-1]);
+        }
+        return dp[3];
     }
     /**
      * refer to Q121 template
--- src/main/java/com/answer/greedy/Q122_Best_Time_to_Buy_and_Sell_Stock_II.java
+++ src/main/java/com/answer/greedy/Q122_Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -2,6 +2,8 @@ package com.answer.greedy;
 
 public class Q122_Best_Time_to_Buy_and_Sell_Stock_II {
     /**
+     * 本题和121. 买卖股票的最佳时机 的唯一区别是本题股票可以买卖多次了（注意只有一只股票，所以再次购买前要出售掉之前的股票）
+     *
      * Greedy贪心算法
      * Approach 3: Simple One Pass
      * 本题首先要清楚两点：
@@ -28,8 +30,12 @@ public class Q122_Best_Time_to_Buy_and_Sell_Stock_II {
      * Dynamic Programming 动态规划
      * 每天买卖不限: 在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。
      *
-     * dp[i][0] 表⽰第i天持有股票所得现⾦。
+     * dp[i][0] 表⽰第i天持有股票所得最多现⾦。
      * dp[i][1] 表⽰第i天不持有股票所得最多现⾦
+     *
+     * 注意这里和121. 买卖股票的最佳时机 唯一不同的地方，就是推导dp[i][0]的时候，第i天买入股票的情况。
+     * 在121. 买卖股票的最佳时机 中，因为股票全程只能买卖一次，所以如果买入股票，那么第i天持有股票即dp[i][0]一定就是 -prices[i]。
+     * 而本题，因为一只股票可以买卖多次，所以当第i天买入股票的时候，所持有的现金可能有之前买卖过的利润即：dp[i - 1][1]，所以dp[i - 1][1] - prices[i]
      */
     public int maxProfit_3(int[] prices) {
         int len = prices.length;
@@ -40,10 +46,43 @@ public class Q122_Best_Time_to_Buy_and_Sell_Stock_II {
 
         for (int i = 1; i < len; i++) {
             // 第i天持股票所剩最多现金 = max(第i-1天持股票所剩现金, 第i-1天持现金-买第i天的股票)
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]); // 资金是dp[i - 1][1]
+            // 第i天买入股票，所得现金就是昨天不持有股票的所得现金减去 今天的股票价格 即：dp[i - 1][1] - prices[i]
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]); // 资金是dp[i - 1][1] 注意这⾥是和121. 买卖股票的最佳时机唯⼀不同的地⽅。
             // 第i天持有最多现金 = max(第i-1天持有的最多现金，第i-1天持有股票的最多现金+第i天卖出股票)
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]); // 注意这⾥是和121. 买卖股票的最佳时机唯⼀不同的地⽅。
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
         }
-        return dp[len - 1][1];
+        return dp[len - 1][1];  // 卖出股票收益高于持有股票收益
+    }
+    /**
+     * DP using 2*2 Array (下方還有使用一維滾動數組的更優化版本)
+     */
+    public int maxProfit_4(int[] prices) {
+        int dp[][] = new int [2][2]; // 注意这里只开辟了一个2 * 2大小的二维数组
+        //dp[i][0]: holding the stock 持有股票
+        //dp[i][1]: not holding the stock 没有股票
+        dp[0][0] = - prices[0];
+        dp[0][1] = 0;
+
+        for(int i = 1; i < prices.length; i++){
+            dp[i % 2][0] = Math.max(dp[(i - 1) % 2][0], dp[(i - 1) % 2][1] - prices[i]);
+            dp[i % 2][1] = Math.max(dp[(i - 1) % 2][1], dp[(i - 1) % 2][0] + prices[i]);
+        }
+        return dp[(prices.length - 1) % 2][1];
+    }
+    /**
+     * 优化空间
+     */
+    public int maxProfit_5(int[] prices) {
+        int[] dp = new int[2];
+        // 0表示持有，1表示卖出
+        dp[0] = -prices[0];
+        dp[1] = 0;
+        for(int i = 1; i <= prices.length; i++){
+            // 前一天持有; 既然不限制交易次数，那么再次买股票时，要加上之前的收益
+            dp[0] = Math.max(dp[0], dp[1] - prices[i-1]);
+            // 前一天卖出; 或当天卖出，当天卖出，得先持有
+            dp[1] = Math.max(dp[1], dp[0] + prices[i-1]);
+        }
+        return dp[1];
     }
 }
