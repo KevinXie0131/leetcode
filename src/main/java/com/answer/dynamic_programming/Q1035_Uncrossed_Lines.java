437d477352e2a6ffe16ca0dbbed23dcb1490272c
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-03-16 19:17:41 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q1035_Uncrossed_Lines.java
+++ src/main/java/com/answer/dynamic_programming/Q1035_Uncrossed_Lines.java
@@ -9,9 +9,13 @@ public class Q1035_Uncrossed_Lines {
         System.out.println(maxUncrossedLines(nums1, nums2));
     }
     /**
+     * 直线不能相交，这就是说明在字符串nums1中 找到一个与字符串nums2相同的子序列，且这个子序列不能改变相对顺序，
+     * 只要相对顺序不改变，连接相同数字的直线就不会相交。 公共子序列指的是相对顺序不变
+     * 本题说是求绘制的最大连线数，其实就是求两个字符串的最长公共子序列的长度！
+     *
      * Approach 3: Dynamic Programming with Space Optimization
      * Note that the above recursive relation is exactly the same as in the classical problem, Longest Common Subsequence (LCS)
-     * 寻找相同元素，并保持相同顺序。和Q1143.最长公共子序列相同，代码一致
+     * 寻找相同元素，并保持相同顺序。和Q1143.最长公共子序列 是一样的，代码一致
      */
     public static int maxUncrossedLines(int[] nums1, int[] nums2) {
         int M = nums1.length;
