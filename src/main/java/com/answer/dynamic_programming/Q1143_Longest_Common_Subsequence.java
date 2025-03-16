162fd103fa59e6418d3938dc88dd876299156636
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-03-16 18:58:13 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q1143_Longest_Common_Subsequence.java
+++ src/main/java/com/answer/dynamic_programming/Q1143_Longest_Common_Subsequence.java
@@ -2,18 +2,27 @@ package com.answer.dynamic_programming;
 
 public class Q1143_Longest_Common_Subsequence {
     /**
+     * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+     * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
+     *
      * Approach 3: Dynamic Programming
      * 2-D Dynamic Programming
      * 最长公共子序列 Subsequence：不要求元素连续
+     *
+     * 本题和动态规划：718. 最长重复子数组 区别在于这里不要求是连续的了，但要有相对顺序，即："ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+     * 时间复杂度: O(n * m)，其中 n 和 m 分别为 text1 和 text2 的长度
+     * 空间复杂度: O(n * m)
      */
     public int longestCommonSubsequence(String text1, String text2) {
         int M = text1.length();
         int N = text2.length();
         int[][] dp = new int[M + 1][N + 1]; // dp: 以[0, i-1]的num1 和 以[0, j-1]的num2 的最长公共子序列长度
-        //dp[i][0]和dp[0][j]都被初始化为0
-
+        //dp[i][0]和dp[0][j]都被初始化为0 / text1[0, i-1]和空串的最长公共子序列自然是0，所以dp[i][0] = 0;
         for(int i = 1; i <= M; i++){
             for(int j = 1; j <= N; j++){
+                // 主要就是两大情况： text1[i - 1] 与 text2[j - 1]相同，text1[i - 1] 与 text2[j - 1]不相同
+                //    如果text1[i - 1] 与 text2[j - 1]相同，那么找到了一个公共元素，所以dp[i][j] = dp[i - 1][j - 1] + 1;
+                //    如果text1[i - 1] 与 text2[j - 1]不相同，那就看看text1[0, i - 2]与text2[0, j - 1]的最长公共子序列 和 text1[0, i - 1]与text2[0, j - 2]的最长公共子序列，取最大的。
                 if(text1.charAt(i - 1) == text2.charAt(j - 1)){
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 }else{
@@ -24,8 +33,62 @@ public class Q1143_Longest_Common_Subsequence {
         return dp[M][N];
     }
     /**
+     * 另一种形式
+     * 可以在一开始的时候就先把text1, text2 转成char[]，之后就不需要有这么多为了处理字串的调整
+     */
+    public int longestCommonSubsequence_3(String text1, String text2) {
+        int M = text1.length();
+        int N = text2.length();
+        int[][] dp = new int[M + 1][N + 1]; // 先对dp数组做初始化操作
+
+        char[] arr1 = text1.toCharArray();
+        char[] arr2 = text2.toCharArray();
+        for(int i = 1; i <= M; i++){
+            char ch1 = arr1[i - 1];
+            for(int j = 1; j <= N; j++){
+                char ch2 = arr2[j - 1];
+                if(ch1 == ch2){  // 开始列出状态转移方程
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[M][N];
+    }
+    /**
+     * 一维dp数组
+     */
+    public int longestCommonSubsequence_2(String text1, String text2) {
+        int n1 = text1.length();
+        int n2 = text2.length();
+        // 多从二维dp数组过程分析
+        // 关键在于  如果记录  dp[i - 1][j - 1]
+        // 因为 dp[i - 1][j - 1]  <!=>  dp[j - 1]  <=>  dp[i][j - 1]
+        int [] dp = new int[n2 + 1];
+
+        for(int i = 1; i <= n1; i++){
+            // 这里pre相当于 dp[i - 1][j - 1]
+            int pre = dp[0];
+            for(int j = 1; j <= n2; j++){
+                //用于给pre赋值
+                int cur = dp[j];
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)){
+                    dp[j] = pre + 1; //这里pre相当于dp[i - 1][j - 1]   千万不能用dp[j - 1] !!
+                } else{
+                    // dp[j]     相当于   dp[i - 1][j]
+                    // dp[j - 1] 相当于   dp[i][j - 1]
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
+                }
+
+                pre = cur;  //更新dp[i - 1][j - 1], 为下次使用做准备
+            }
+        }
+        return dp[n2];
+    }
+    /**
      * Approach 4: Dynamic Programming with Space Optimization
-     * 1-D Dynamic Programming
+     * 1-D Dynamic Programming 一维dp数组
      */
     public int longestCommonSubsequence_1(String text1, String text2) {
 
@@ -35,11 +98,9 @@ public class Q1143_Longest_Common_Subsequence {
             text1 = text2;
             text2 = temp;
         }
-
         // The previous column starts with all 0's and like before is 1
         // more than the length of the first word.
         int[] previous = new int[text1.length() + 1];
-
         // Iterate through each column, starting from the last one.
         for (int col = text2.length() - 1; col >= 0; col--) {
             // Create a new array to represent the current column.
@@ -54,7 +115,6 @@ public class Q1143_Longest_Common_Subsequence {
             // The current column becomes the previous one.
             previous = current;
         }
-
         // The original problem's answer is in previous[0]. Return it.
         return previous[0];
     }
--- src/main/java/com/answer/dynamic_programming/Q718_Maximum_Length_of_Repeated_Subarray.java
+++ src/main/java/com/answer/dynamic_programming/Q718_Maximum_Length_of_Repeated_Subarray.java
@@ -1,46 +1,103 @@
 package com.answer.dynamic_programming;
 
+import java.util.*;
+
 public class Q718_Maximum_Length_of_Repeated_Subarray {
+    /**
+     * 注意题目中说的子数组，其实就是连续子序列。
+     *
+     * Approach #1: Brute Force with Initial Character Map
+     * 要求两个数组中最长重复子数组，如果是暴力的解法 只需要先两层for循环确定两个数组起始位置，然后再来一个循环可以是for或者while，
+     * 来从两个起始位置开始比较，取得重复子数组的长度。
+     * Time Complexity: O(M∗N∗min(M,N)), where M,N are the lengths of A, B
+     * Space Complexity: O(N), the space used by Bstarts
+     */
+    public int findLength_0(int[] A, int[] B) {
+        int ans = 0;
+        Map<Integer, ArrayList<Integer>> Bstarts = new HashMap();
+        for (int j = 0; j < B.length; j++) {
+            Bstarts.computeIfAbsent(B[j], x -> new ArrayList()).add(j);
+        }
+        for (int i = 0; i < A.length; i++) {
+            if (!Bstarts.containsKey(A[i])){
+                continue;
+            }
+            for (int j : Bstarts.get(A[i])) {
+                int k = 0;
+                while (i + k < A.length && j + k < B.length && A[i + k] == B[j + k]) {
+                    k++;
+                }
+                ans = Math.max(ans, k);
+            }
+        }
+        return ans;
+    }
     /**
      * 2-D Dynamic Programming
      * 最长重复子数组
      * Does a Subarray have to be contiguous?
      * A subarray or substring will always be contiguous, but a subsequence need not be contiguous.
+     * 时间复杂度：O(n × m)，n 为A长度，m为B长度
+     * 空间复杂度：O(n × m)
      */
     public int findLength(int[] nums1, int[] nums2) {
         int[][] dp = new int[nums1.length + 1][nums2.length + 1]; // dp: 以i-1为尾的num1 和 以j-1为尾的num2 的最长重复子数组长度
         int result = 0;
         //dp[i][0]和dp[0][j]都被初始化为0
-
-        for(int i = 1; i <= nums1.length; i++){
+        for(int i = 1; i <= nums1.length; i++){ // 其实dp[i][j]的定义也就决定着，我们在遍历dp[i][j]的时候i 和 j都要从1开始
             for(int j = 1; j <= nums2.length; j++){
-                if(nums1[i - 1] == nums2[j - 1]){
+                if(nums1[i - 1] == nums2[j - 1]){ // 即当A[i - 1] 和B[j - 1]相等的时候，dp[i][j] = dp[i - 1][j - 1] + 1;
                     dp[i][j] = dp[i - 1][j - 1] + 1; // 两个数值相同，num1和num2同时回退一步
+                    // result = Math.max(result, dp[i][j]);  // 也可以
                 }
                 if(dp[i][j] > result){
                     result = dp[i][j];
                 }
             }
         }
+        return result;
+    }
+    /**
+     * 另一种形式(实现起来麻烦一些)
+     * 如果定义 dp[i][j]为 以下标i为结尾的A，和以下标j 为结尾的B，那么 第一行和第一列毕竟要进行初始化，
+     * 如果nums1[i] 与 nums2[0] 相同的话，对应的 dp[i][0]就要初始为1， 因为此时最长重复子数组为1。 nums2[j] 与 nums1[0]相同的话，同理
+     */
+    public int findLength_1(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1]; // dp[i][j]为 以下标i为结尾的A，和以下标j 为结尾的B，最长重复子数组长度
+        int result = 0;
 
+        // 要对第一行，第一列经行初始化
+        for (int i = 0; i < nums1.length; i++) if (nums1[i] == nums2[0]) dp[i][0] = 1;
+        for (int j = 0; j < nums2.length; j++) if (nums1[0] == nums2[j]) dp[0][j] = 1;
+
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j] && i > 0 && j > 0) { // 防止 i-1 出现负数
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                if (dp[i][j] > result) result = dp[i][j];
+            }
+        }
         return result;
     }
     /**
      * 1-D Dynamic Programming
      * 空间压缩
+     * 时间复杂度：O(n × m)，n 为A长度，m为B长度
+     * 空间复杂度：O(m)
      */
     public int findLength_2(int[] nums1, int[] nums2) {
         int[] dp = new int[nums2.length + 1];
         int result = 0;
 
         for(int i = 1; i <= nums1.length; i++){
-            for(int j = nums2.length; j >= 1; j--){
-                if(nums1[i - 1] == nums2[j - 1]){
+            for(int j = nums2.length; j >= 1; j--) {
+                if(nums1[i - 1] == nums2[j - 1]) {
                     dp[j] = dp[j - 1] + 1;
-                }else{
-                    dp[j] = 0;
+                } else {
+                    dp[j] = 0; // 注意这里不相等的时候要有赋0的操作
                 }
-                if(dp[j] > result){
+                if(dp[j] > result) {
                     result = dp[j];
                 }
             }
