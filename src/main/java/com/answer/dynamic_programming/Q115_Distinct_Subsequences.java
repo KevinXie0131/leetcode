a9e42c06b8bad8ae4fe19fd22884ef5327d0cb5a
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-03-16 20:11:44 -0400

    update code

--- src/main/java/com/answer/dynamic_programming/Q115_Distinct_Subsequences.java
+++ src/main/java/com/answer/dynamic_programming/Q115_Distinct_Subsequences.java
@@ -16,20 +16,30 @@ public class Q115_Distinct_Subsequences {
         [1, 3, 4, 5]]*/
     }
     /**
+     * 字符串的一个 子序列 是指，通过删除一些（也可以不删除）字符且不干扰剩余字符相对位置所组成的新字符串。
+     * （例如，"ACE" 是 "ABCDE" 的一个子序列，而 "AEC" 不是）
      * Hard
      * Dynamic Programming
      * 字符串s中有多少像t一样的子序列 / 不要求元素连续
+     *
+     * 这道题目如果不是子序列，而是要求连续序列的，那就可以考虑用KMP。
+     * 这道题目相对于72. 编辑距离，简单了不少，因为本题相当于只有删除操作，不用考虑替换增加之类的。
+     * 但相对于刚讲过的动态规划：392.判断子序列 就有难度了，这道题目双指针法可就做不了
      */
     static public int numDistinct(String s, String t) {
-        int[][] dp = new int[s.length() + 1][t.length() + 1]; // dp: 以i-1为结尾的字符串s 和 以j-1为结尾的字符串t 的不同子序列的个数
+        int[][] dp = new int[s.length() + 1][t.length() + 1]; // dp: 以i-1为结尾的字符串s 子序列中出现以j-1为结尾的字符串t 的不同子序列的个数
         for (int i = 0; i < s.length() + 1; i++) {
             dp[i][0] = 1; // t为空字符串 有一种方法将s变为空字符串
         }
        // dp[0][j] = 0; // s为空字符串 有0种方法将s变为t
        // dp[0][0] = 1; // s和t为空字符串 有1种方法将s变为t
-
         for (int i = 1; i < s.length() + 1; i++) {
             for (int j = 1; j < t.length() + 1; j++) {
+                // 当s[i - 1] 与 t[j - 1]相等时，dp[i][j]可以有两部分组成。
+                //    一部分是用s[i - 1]来匹配，那么个数为dp[i - 1][j - 1]。即不需要考虑当前s子串和t子串的最后一位字母，所以只需要 dp[i-1][j-1]。
+                //    一部分是不用s[i - 1]来匹配，个数为dp[i - 1][j]。
+                // 当s[i - 1] 与 t[j - 1]不相等时，dp[i][j]只有一部分组成，不用s[i - 1]来匹配（就是模拟在s中删除这个元素），即：dp[i - 1][j]
+                // 我们求的是 s 中有多少个 t，而不是 求t中有多少个s，所以只考虑 s中删除元素的情况，即 不用s[i - 1]来匹配 的情况
                 if (s.charAt(i - 1) == t.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]; // For example,  bagg & bag, the last g can be used for two times
                 }else{
