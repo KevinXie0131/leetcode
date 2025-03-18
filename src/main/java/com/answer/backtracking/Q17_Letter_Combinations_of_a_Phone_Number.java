aa549fafc84ba64f22722fceed2c16fa7c26b36f
KevinX <Xzb04720638k>
2025-03-18 19:13:49 -0400

    update

--- src/main/java/com/answer/backtracking/Q17_Letter_Combinations_of_a_Phone_Number.java
+++ src/main/java/com/answer/backtracking/Q17_Letter_Combinations_of_a_Phone_Number.java
@@ -37,4 +37,29 @@ public class Q17_Letter_Combinations_of_a_Phone_Number {
             path.deleteCharAt(startIndex);
         }
     }
+    /**
+     * use array
+     */
+    public List<String> letterCombinations1(String digits) {
+        String[] map = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        List<String> result = new ArrayList<>();
+        if(digits.length() == 0){
+            return result;
+        }
+        backtracking1(digits, result, 0, new StringBuffer(), map);
+        return result;
+    }
+
+    public  void backtracking1(String digits, List<String> result, int startIndex, StringBuffer path,  String[] map ){
+        if(path.length() == digits.length()){
+            result.add(path.toString());
+            return;
+        }
+        String str = map[digits.charAt(startIndex) - '0'];
+        for(int i = 0; i < str.length(); i++){
+            path.append(str.charAt(i));
+            backtracking1(digits, result, startIndex + 1,path,map);
+            path.deleteCharAt(startIndex);
+        }
+    }
 }
