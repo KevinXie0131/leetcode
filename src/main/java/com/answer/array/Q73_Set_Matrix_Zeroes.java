5398e4aeb831a981877f80bbe9bb71a4c630bf6d
KevinX <Xzb04720638k>
2025-03-21 11:20:08 -0400

    update

--- src/main/java/com/answer/array/Q73_Set_Matrix_Zeroes.java
+++ src/main/java/com/answer/array/Q73_Set_Matrix_Zeroes.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Q73_Set_Matrix_Zeroes {
+    /**
+     * in place:  operates directly on the input data structure without requiring extra space proportional to the input size.
+     */
     public static void main(String[] args) {
         int[][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
         setZeroes(matrix);
         System.out.println(matrix);
     }
-
     /**
      * Approach 1: Additional Memory Approach
      */
@@ -40,17 +42,19 @@ public class Q73_Set_Matrix_Zeroes {
      */
     public void setZeroes_1(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
-        boolean flagCol0 = false, flagRow0 = false;
+        boolean colZero = false, rowZero = false;
+        // 预处理出两个标记变量
         for (int i = 0; i < m; i++) {
             if (matrix[i][0] == 0) {
-                flagCol0 = true;
+                colZero = true;
             }
         }
         for (int j = 0; j < n; j++) {
             if (matrix[0][j] == 0) {
-                flagRow0 = true;
+                rowZero = true;
             }
         }
+        // 使用其他行与列去处理第一行与第一列，
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
@@ -58,6 +62,7 @@ public class Q73_Set_Matrix_Zeroes {
                 }
             }
         }
+        // 然后反过来使用第一行与第一列去更新其他行与列
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
@@ -65,15 +70,49 @@ public class Q73_Set_Matrix_Zeroes {
                 }
             }
         }
-        if (flagCol0) {
+        // 使用两个标记变量更新第一行与第一列
+        if (colZero) {
             for (int i = 0; i < m; i++) {
                 matrix[i][0] = 0;
             }
         }
-        if (flagRow0) {
+        if (rowZero) {
             for (int j = 0; j < n; j++) {
                 matrix[0][j] = 0;
             }
+        }
+    }
+    /**
+     * 另一种形式
+     */
+    public void setZeroes_2(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean colZero = false, rowZero = false;
+        // 预处理出两个标记变量
+        // 使用其他行与列去处理第一行与第一列，
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                    if(j == 0) colZero = true;
+                    if(i == 0) rowZero = true;
+                }
+            }
+        }
+        // 然后反过来使用第一行与第一列去更新其他行与列
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        // 使用两个标记变量更新第一行与第一列
+        for (int i = 0; colZero && i < m; i++) {
+            matrix[i][0] = 0;
+        }
+        for (int j = 0;  rowZero && j < n; j++) {
+            matrix[0][j] = 0;
         }
     }
 }
