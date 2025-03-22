d706b5cd80f1af0e2270c3613b89aa3d8f507206
KevinX <Xzb04720638k>
2025-03-21 22:09:42 -0400

    update

--- src/main/java/com/answer/two_pointers/Q88_Merge_Sorted_Array.java
+++ src/main/java/com/answer/two_pointers/Q88_Merge_Sorted_Array.java
@@ -1,12 +1,11 @@
 package com.answer.two_pointers;
 
 public class Q88_Merge_Sorted_Array {
-
     /**
      * Approach 3: Three Pointers (Start From the End)
      */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p1 = m - 1, p2 = n - 1;
+        int p1 = m - 1, p2 = n - 1; // point to the tail to avoid override num1
         int cur;
         int tail = m + n - 1;
         while(p1 >= 0 || p2 >= 0){
@@ -23,6 +22,20 @@ public class Q88_Merge_Sorted_Array {
                 cur = nums2[p2--];
             }
             nums1[tail--] = cur;
+        }
+    }
+    /**
+     * 另一种形式
+     */
+    public void merge1(int[] nums1, int m, int[] nums2, int n) {
+        int i= m - 1, j = n - 1;
+        int tail = m + n - 1;
+        while(i >= 0 && j >= 0){
+            if(nums1[i] >= nums2[j]) nums1[tail--] = nums1[i--];
+            else  nums1[tail--] = nums2[j--];
+        }
+        while(j >= 0){
+            nums1[tail--] = nums2[j--];
         }
     }
 }
