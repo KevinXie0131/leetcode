e36df3fdc800d161c1e1e13fe867b7f24e6e4484
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-03-26 22:44:14 -0400

    update code

--- src/main/java/com/answer/array/Q189_Rotate_Array.java
+++ src/main/java/com/answer/array/Q189_Rotate_Array.java
@@ -1,4 +1,14 @@
 package com.answer.array;
 
 public class Q189_Rotate_Array {
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] newArr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            newArr[(i + k) % n] = nums[i];
+        }
+        System.arraycopy(newArr, 0, nums, 0, n);
+    }
 }
