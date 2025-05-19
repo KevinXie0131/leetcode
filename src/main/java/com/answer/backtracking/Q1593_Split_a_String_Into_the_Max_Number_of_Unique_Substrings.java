de7dd43c858226d87b404c738492ac339979503e
KevinX <Xzb04720638k>
2025-05-19 19:30:01 -0400

    additional

---
+++ src/main/java/com/answer/backtracking/Q1593_Split_a_String_Into_the_Max_Number_of_Unique_Substrings.java
@@ -0,0 +1,17 @@
+package com.answer.backtracking;
+
+public class Q1593_Split_a_String_Into_the_Max_Number_of_Unique_Substrings {
+    /**
+     * Given a string s, return the maximum number of unique substrings that the given string can be split into.
+     * You can split string s into any list of non-empty substrings, where the concatenation of the substrings forms
+     * the original string. However, you must split the substrings such that all of them are unique.
+     * A substring is a contiguous sequence of characters within a string.
+     * 拆分字符串使唯一子字符串的数目最大
+     * 一个字符串 s ，请你拆分该字符串，并返回拆分后唯一子字符串的最大数目。
+     * 字符串 s 拆分后可以得到若干 非空子字符串 ，这些子字符串连接后应当能够还原为原字符串。但是拆分出来的每个子字符串都必须是 唯一的 。
+     * 注意：子字符串 是字符串中的一个连续字符序列。
+     */
+    public int maxUniqueSplit(String s) {
+
+    }
+}
