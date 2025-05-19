2e49cb29a8ada35f1e0fe217afd690fb137f2d60
KevinX <Xzb04720638k>
2025-05-18 23:00:34 -0400

    additional

--- src/main/java/com/answer/backtracking/Q332_Reconstruct_Itinerary.java
+++ src/main/java/com/answer/backtracking/Q332_Reconstruct_Itinerary.java
@@ -3,6 +3,15 @@ package com.answer.backtracking;
 import java.util.*;
 
 public class Q332_Reconstruct_Itinerary {
+    /**
+     * a list of airline tickets where tickets[i] = [fromi, toi] represent the departure and the arrival airports of one flight.
+     * Reconstruct the itinerary in order and return it.
+     * You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
+     * 一份航线列表 tickets ，其中 tickets[i] = [fromi, toi] 表示飞机出发和降落的机场地点。请你对该行程进行重新规划排序。
+     * 所有这些机票都属于一个从 JFK（肯尼迪国际机场）出发的先生，所以该行程必须从 JFK 开始。如果存在多种有效的行程，
+     * 请你按字典排序返回最小的行程组合。
+     * 假定所有机票至少存在一种合理的行程。且所有的机票 必须都用一次 且 只能用一次。
+     */
     public static void main(String[] args) {
         List<List<String>> tickets = new ArrayList<List<String>>();
         tickets.add(Arrays.asList("MUC","LHR"));
@@ -12,6 +21,58 @@ public class Q332_Reconstruct_Itinerary {
         System.out.println(findItinerary(tickets));
     }
     /**
+     * 首先明确几点：
+     * 1.肯定能找到一条路线
+     * 2.每张机票只能使用一次
+     * 3.要找降落地字典序最小的路线结果
+     */
+    //path记录路线，res存所有路线
+    List<String> path_5 = new ArrayList<>();
+    List<List<String>> res_5 = new ArrayList<>();
+    //used数组用于标记同一树枝不能重复使用！即不能重复使用一张票
+    boolean[] used_5 = new boolean[301]; // 1 <= tickets.length <= 300
+    boolean isFound;
+
+    public List<String> findItinerary_5(List<List<String>> tickets) {
+        //先按字典序从小到大排列降落地
+        tickets.sort((o1, o2) -> o1.get(1).compareTo(o2.get(1)));
+        path_5.add("JFK");
+        backTracking_5(tickets, "JFK");
+        return res_5.get(0);
+    }
+    // Time Limit Exceeded
+    void backTracking_5(List<List<String>> tickets, String outset) {
+        //算个小剪枝吧，找到一条就行
+        if (isFound) {
+            return;
+        }
+        //因为这些航班肯定会有一条路线是正确的
+        //所以我们加入path的size如果等于tickets.size()+1说明我们找到路线了
+        if (path_5.size() == tickets.size() + 1) {
+            isFound = true;
+            res_5.add(new ArrayList<>(path_5));
+            return;
+        }
+        for (int i = 0; i < tickets.size(); i++) {
+            if(i > 0 && tickets.get(i).get(0).equals(tickets.get(i - 1).get(0)) // 再添加一个筛选条件
+                    && tickets.get(i).get(1).equals(tickets.get(i - 1).get(1))
+                    && !used_5[i - 1]){
+                continue;
+            }
+            //如果出发地和上一个的降落地相同 并且 同一条路线中没有重复使用一张票
+            if(tickets.get(i).get(0).equals(outset)  && !used_5[i]){
+                //标记该票已经使用过
+                used_5[i]= true;
+                path_5.add(tickets.get(i).get(1));
+                //把现在的降落地加入递归函数
+                backTracking_5(tickets, tickets.get(i).get(1));
+                //回溯！ 该票标记为未使用 路线中移除该票
+                used_5[i]=false;
+                path_5.remove(path_5.size()-1);
+            }
+        }
+    }
+    /**
      * 这道题目还是很难的, 实际上确实是图论中的深度优先搜索，但这是深搜中使用了回溯的例子
      * Backtracking - Hard
      *
@@ -19,7 +80,6 @@ public class Q332_Reconstruct_Itinerary {
      * in graph and flight between airports as an edge in graph.
      *
      * The input graph is NOT what we call a DAG (Directed Acyclic Graph), since we could find at least a cycle in the graph.
-     *
      * Typically, backtracking is used to enumerate all possible solutions for a problem, in a trial-fail-and-fallback strategy.
      */
     static private Deque<String> res;
