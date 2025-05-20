0527cc8742c0f35c2dec0d9b66d03a24c58331e2
KevinX <Xzb04720638k>
2025-05-20 15:41:19 -0400

    update

--- src/main/java/com/answer/bit/Q136_Single_Number.java
+++ src/main/java/com/answer/bit/Q136_Single_Number.java
@@ -1,15 +1,23 @@
 package com.answer.bit;
 
 public class Q136_Single_Number {
-
+    /**
+     * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+     * You must implement a solution with a linear runtime complexity and use only constant extra space.
+     * 只出现一次的数字
+     * 给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+     * 你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。
+     */
     public static void main(String[] args) {
         System.out.println(5 ^ 0);
         System.out.println(5 ^ 5);
-
         System.out.println(2 ^ 2 ^ 1);
     }
     /**
      * 位运算 异或
+     * 一个数和 0 做 XOR 运算等于本身：a⊕0 = a
+     * 一个数和其本身做 XOR 运算等于 0：a⊕a = 0
+     * XOR 运算满足交换律和结合律：a⊕b⊕a = (a⊕a)⊕b = 0⊕b = b
      */
     public int singleNumber(int[] nums) {
         int single = 0;
--- src/main/java/com/answer/bit/Q137_Single_Number_II.java
+++ src/main/java/com/answer/bit/Q137_Single_Number_II.java
@@ -1,6 +1,15 @@
 package com.answer.bit;
 
+import java.util.*;
+
 public class Q137_Single_Number_II {
+    /**
+     * Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+     * You must implement a solution with a linear runtime complexity and use only constant extra space.
+     * 只出现一次的数字 II
+     * 给你一个整数数组 nums ，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 。请你找出并返回那个只出现了一次的元素。
+     * 你必须设计并实现线性时间复杂度的算法且使用常数级空间来解决此问题。
+     */
     /**
      * 二进制
      * 统计所有数字每一位中 1 的数量，如果可以整除 3，说明结果这一位为 0，否则为 1。
@@ -44,5 +53,25 @@ public class Q137_Single_Number_II {
         }
 
         return seenOnce;
+    }
+    /**
+     * 方法一：哈希表
+     * 时间复杂度：O(n)，其中 n 是数组的长度。
+     * 空间复杂度：O(n)
+     */
+    public int singleNumber_2(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        int ans = 0;
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            int num = entry.getKey(), occ = entry.getValue();
+            if (occ == 1) {
+                ans = num;
+                break;
+            }
+        }
+        return ans;
     }
 }
