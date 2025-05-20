1b9b2bf19090f92f295b0794a58d8912316dd637
KevinX <Xzb04720638k>
2025-05-20 16:28:52 -0400

    update

--- src/main/java/com/answer/bit/Q338_Counting_Bits.java
+++ src/main/java/com/answer/bit/Q338_Counting_Bits.java
@@ -2,6 +2,11 @@ package com.answer.bit;
 
 public class Q338_Counting_Bits {
     /**
+     * Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n),
+     * ans[i] is the number of 1's in the binary representation of i
+     * 给你一个整数 n ，对于 0 <= i <= n 中的每个 i ，计算其二进制表示中 1 的个数 ，返回一个长度为 n + 1 的数组 ans 作为答案。
+     */
+    /**
      * Approach 1: Pop Count (Brian Kernighan's Algorithm: https://www.educative.io/answers/what-is-kernighans-algorithm)
      * Time complexity: O(nlogn)
      * Space complexity: O(1)
