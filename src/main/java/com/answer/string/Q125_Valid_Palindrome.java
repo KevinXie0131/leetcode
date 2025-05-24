7bd172fc3de2ff7b4b19d9e2933fee2f667cc844
KevinX <Xzb04720638k>
2025-05-24 19:22:40 -0400

    update

--- src/main/java/com/answer/string/Q125_Valid_Palindrome.java
+++ src/main/java/com/answer/string/Q125_Valid_Palindrome.java
@@ -3,10 +3,19 @@ package com.answer.string;
 import java.util.Arrays;
 
 public class Q125_Valid_Palindrome {
+    /**
+     * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing
+     * all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include
+     * letters and numbers.
+     * Given a string s, return true if it is a palindrome, or false otherwise.
+     * 如果在将所有大写字符转换为小写字符、并移除所有非字母数字字符之后，短语正着读和反着读都一样。则可以认为该短语是一个 回文串 。
+     * 字母和数字都属于字母数字字符。
+     * 给你一个字符串 s，如果它是 回文串 ，返回 true ；否则，返回 false 。
+     */
     public static void main(String[] args) {
-        String s = "race a car";
-     //   String s = "A man, a plan, a canal: Panama";
-        System.out.println(isPalindrome_5(s));
+    //    String s = "race a car";
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome_6(s));
     }
     /**
      * Palindrome 回文串
@@ -48,7 +57,7 @@ public class Q125_Valid_Palindrome {
         return true;
     }
     /**
-     * 头尾双指针
+     * 头尾双指针, 在原字符串上直接判断, 只使用 O(1) 空间的算法
      * 使用头尾双指针不断向中间移动直到相遇，同时移动过程中对比两指针指向的字符是否相同。
      */
     public boolean isPalindrome_2(String s) {
@@ -67,6 +76,30 @@ public class Q125_Valid_Palindrome {
                 }
                 ++left;
                 --right;
+            }
+        }
+        return true;
+    }
+    /**
+     * 如果 s[i] 既不是字母也不是数字，右移左指针，也就是把 i 加一。
+     * 如果 s[j] 既不是字母也不是数字，左移右指针，也就是把 j 减一。
+     * 否则，如果 s[i] 和 s[j] 转成小写后相等，那么把 i 加一，把 j 减一，继续判断。
+     * 否则，s 不是回文串，返回 false。
+     */
+    static public boolean isPalindrome_6(String s) {
+        int i = 0 ;
+        int j = s.length() - 1;
+
+        while(i < j) {
+            if(!Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            } else  if(!Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            } else if (Character.toLowerCase(s.charAt(i)) == Character.toLowerCase(s.charAt(j))){
+                i++;
+                j--;
+            } else {
+                return false;
             }
         }
         return true;
@@ -97,7 +130,7 @@ public class Q125_Valid_Palindrome {
         // 下面两层循环就是求所有子串的固定套路
         for (int j = 0; j < n; j++) {
             for (int i = 0; i <= j; i++) {
-                if (str[j] == str[i] && (j - i < 2 || dp[i + 1][j - 1])) {
+                if (str[j] == str[i] && (j - i < 2 || dp[i + 1][j - 1])) { // (r - l <= 2 // works too
                     dp[i][j] = true;
                     // 其他处理逻辑
                     if (len < j - i + 1) {
@@ -111,4 +144,5 @@ public class Q125_Valid_Palindrome {
         System.out.println(Arrays.deepToString(dp));
         return len == str.length;
     }
+
 }
