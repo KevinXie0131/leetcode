1ac5cc61b8599f91bf8d6c1d75857deb1ee8ccaa
KevinX <Xzb04720638k>
2025-05-24 20:40:32 -0400

    update

--- src/main/java/com/answer/string/Q58_Length_of_Last_Word.java
+++ src/main/java/com/answer/string/Q58_Length_of_Last_Word.java
@@ -2,6 +2,12 @@ package com.answer.string;
 
 public class Q58_Length_of_Last_Word {
     /**
+     * Given a string s consisting of words and spaces, return the length of the last word in the string.
+     * A word is a maximal substring consisting of non-space characters only.
+     * 给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中 最后一个 单词的长度。
+     * 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+     */
+    /**
      * 双指针
      * 从后向前遍历字符串，找到第一个非空字符的位置，之后继续向前遍历，直到遇到空格位置，
      * 记录两个位置的间隔，即为最后一个单词的长度。
@@ -43,19 +49,20 @@ public class Q58_Length_of_Last_Word {
         return right - left;
     }
     /**
-     * Official answer
+     * Official answer 反向遍历
+     * 从最后一个字母开始继续反向遍历字符串，直到遇到空格或者到达字符串的起始位置。遍历到的每个字母都是最后一个单词中的字母，
+     * 因此遍历到的字母数量即为最后一个单词的长度。
      */
     public int lengthOfLastWord_1(String s) {
         // trim the trailing spaces
-        int p = s.length() - 1;
-        while (p >= 0 && s.charAt(p) == ' ') {
-            p--;
+        int index  = s.length() - 1;
+        while (index  >= 0 && s.charAt(index ) == ' ') {
+            index --;
         }
-
         // compute the length of last word
         int length = 0;
-        while (p >= 0 && s.charAt(p) != ' ') {
-            p--;
+        while (index  >= 0 && s.charAt(index ) != ' ') {
+            index --;
             length++;
         }
         return length;
