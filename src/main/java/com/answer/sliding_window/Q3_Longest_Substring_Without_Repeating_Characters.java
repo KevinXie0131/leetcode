ba0826c545b036b6f54c0b98001b1b3980053bf2
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-01 17:18:58 -0400

    update code

--- src/main/java/com/answer/sliding_window/Q3_Longest_Substring_Without_Repeating_Characters.java
+++ src/main/java/com/answer/sliding_window/Q3_Longest_Substring_Without_Repeating_Characters.java
@@ -3,28 +3,47 @@ package com.answer.sliding_window;
 import java.util.*;
 
 public class Q3_Longest_Substring_Without_Repeating_Characters {
+    /**
+     * Given a string s, find the length of the longest substring without duplicate characters.
+     * A substring is a contiguous non-empty sequence of characters within a string.
+     * 无重复字符的最长子串: 给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串 的长度。
+     * 子字符串 是字符串中连续的 非空 字符序列。
+     */
     public static void main(String[] args) {
-     //   String s = "abcabcbb";
+        String s = "abcabcbb";
      //   String s = "abba";
-        String s = "dvdf";
-        System.out.println(lengthOfLongestSubstring(s));
+     //   String s = "dvdf";
+        System.out.println(lengthOfLongestSubstring_2(s));
     }
     /**
      * Approach 3: Sliding Window Optimized 滑动窗口
      * 窗口内不含重复元素
+     * 不满足要求。所以，要移动这个队列！如何移动？只要把队列的左边的元素移出就行了，直到满足题目要求！
+     *
+     * 套路步骤：
+     * 1、定义需维护的结果变量
+     * 2、定义首尾端，开始滑动窗口
+     * 3、更新维护结果变量，有时会用到一个if语句结果变量和左指针。
+     * 4、若窗口长度固定，就用if来维护：只要右指针还没到到头，左指针+1，并减掉左指针的值；
+     * 4、若窗口长度可变，判断窗口合法性，用while去维护左指针右移，直到窗口合法，删掉左指针的值。
+     * 5、返回答案
      */
     public static int lengthOfLongestSubstring(String s) {
         int max = 0;
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>(); // 哈希集合，记录每个字符是否出现过
         char[] arr = s.toCharArray();
 
         int left = 0;
         for(int i = 0; i < arr.length; i++){
             if(map.containsKey(arr[i])){
-                // 更新窗口左边界
+                // 更新窗口左边界, 加 1 表示从字符位置后一个才开始不重复
                 left = Math.max(left, map.get(arr[i]) + 1); // 需要比较max，比如"abba"或者"dvdf"
             }
             map.put(arr[i], i);
+          /*  if(map.containsKey(arr[i])){ // works too
+                left = Math.max(left, map.get(arr[i]));
+            }
+            map.put(arr[i], i + 1);*/
             max = Math.max(max, i - left + 1);
         }
         return max;
@@ -39,7 +58,7 @@ public class Q3_Longest_Substring_Without_Repeating_Characters {
     public static int lengthOfLongestSubstring_1(String s) {
         int max = 0;
         //   Map<Character, Integer> map = new HashMap<>();
-        Integer[] map = new Integer[256];
+        Integer[] map = new Integer[256]; // new Integer[128]; // works too
         Arrays.fill(map, -1);
         char[] arr = s.toCharArray();
 
@@ -56,7 +75,7 @@ public class Q3_Longest_Substring_Without_Repeating_Characters {
     /**
      * Approach 2: Sliding Window
      */
-    public int lengthOfLongestSubstring_2(String s) {
+    static public int lengthOfLongestSubstring_2(String s) {
         Map<Character, Integer> chars = new HashMap();
 
         int left = 0;
@@ -74,6 +93,29 @@ public class Q3_Longest_Substring_Without_Repeating_Characters {
             }
             res = Math.max(res, right - left + 1);
             right++;
+        }
+        return res;
+    }
+    /**
+     * another form
+     */
+    public int lengthOfLongestSubstring_2a(String s) {
+        char[] array = s.toCharArray(); // 转换成 char[] 加快效率（忽略带来的空间消耗）
+
+        int left = 0;
+        int res = 0;
+        int[] cnt = new int[128]; // 也可以用 HashMap<Character, Integer>，这里为了效率用的数组
+
+        for (int right = 0; right < array.length; right++) {
+            char r = s.charAt(right);
+            cnt[r]++;
+
+            while (cnt[r] > 1) {
+                char l = array[left];
+                cnt[l]--;
+                left++;
+            }
+            res = Math.max(res, right - left + 1);
         }
         return res;
     }
@@ -104,5 +146,33 @@ public class Q3_Longest_Substring_Without_Repeating_Characters {
             chars.add(c);
         }
         return true;
+    }
+    /**
+     * Official answer
+     * 滑动窗口
+     * 1.我们使用两个指针表示字符串中的某个子串（或窗口）的左右边界，其中左指针代表着上文中「枚举子串的起始位置」，
+     * 而右指针即为上文中的 rk
+     * 2.在每一步的操作中，我们会将左指针向右移动一格，表示 我们开始枚举下一个字符作为起始位置，然后我们可以不断地向右移动右指针，
+     * 但需要保证这两个指针对应的子串中没有重复的字符。在移动结束后，这个子串就对应着 以左指针开始的，不包含重复字符的最长子串。
+     * 我们记录下这个子串的长度；
+     * 3.在枚举结束后，我们找到的最长的子串的长度即为答案。
+     */
+    public int lengthOfLongestSubstring5(String s) {
+        Set<Character> occ = new HashSet<Character>();// 哈希集合，记录每个字符是否出现过
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                occ.remove(s.charAt(i - 1));// 左指针向右移动一格，移除一个字符
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                occ.add(s.charAt(rk + 1)); // 不断地移动右指针
+                ++rk;
+            }
+
+            ans = Math.max(ans, rk - i + 1);  // 第 i 到 rk 个字符是一个极长的无重复字符子串
+        }
+        return ans;
     }
 }
---
+++ src/main/java/com/answer/sliding_window/Q76_Minimum_Window_Substring.java
@@ -0,0 +1,197 @@
+package com.answer.sliding_window;
+
+import java.util.*;
+
+public class Q76_Minimum_Window_Substring {
+    /**
+     * Given two strings s and t of lengths m and n respectively, return the minimum window substring of
+     * s such that every character in t (including duplicates) is included in the window.
+     * If there is no such substring, return the empty string "".
+     * 最小覆盖子串: 一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，
+     * 则返回空字符串 "" 。
+     * 注意：
+     *    对于 t 中重复字符，我们寻找的子字符串中该字符数量必须不少于 t 中该字符数量。
+     *    如果 s 中存在这样的子串，我们保证它是唯一的答案。
+     */
+    public static void main(String[] args) { // Hard 困难
+       String s = "ADOBECODEBANC", t = "ABC";
+       System.out.println(minWindow4(s, t));
+    }
+    /**
+     * 滑动窗口
+     * 枚举 s 子串的右端点 right（子串最后一个字母的下标），如果子串涵盖 t，就不断移动左端点 left 直到不涵盖为止。
+     * 在移动过程中更新最短子串的左右端点。
+     * 具体来说：
+     *  初始化 ansLeft=−1, ansRight=m，用来记录最短子串的左右端点，其中 m 是 s 的长度。
+     *  用一个哈希表（或者数组）cntT 统计 t 中每个字母的出现次数。
+     *  初始化 left=0，以及一个空哈希表（或者数组）cntS，用来统计 s 子串中每个字母的出现次数。
+     *  遍历 s，设当前枚举的子串右端点为 right，把 s[right] 的出现次数加一。
+     *  遍历 cntS 中的每个字母及其出现次数，如果出现次数都大于等于 cntT 中的字母出现次数：
+     *      如果 right−left<ansRight−ansLeft，说明我们找到了更短的子串，更新 ansLeft=left, ansRight=right。
+     *      把 s[left] 的出现次数减一。
+     *      左端点右移，即 left 加一。
+     *      重复上述三步，直到 cntS 有字母的出现次数小于 cntT 中该字母的出现次数为止。
+     *  最后，如果 ansLeft<0，说明没有找到符合要求的子串，返回空字符串，否则返回下标 ansLeft 到下标 ansRight 之间的子串。
+     * 由于本题大写字母和小写字母都有，为了方便，代码实现时可以直接创建大小为 128 的数组，保证所有 ASCII 字符都可以统计。
+     */
+    static public String minWindow(String S, String t) {
+        char[] s = S.toCharArray();
+        int m = s.length;
+        int ansLeft = -1;
+        int ansRight = m;
+        int[] cntS = new int[128]; // s 子串字母的出现次数
+        int[] cntT = new int[128]; // t 中字母的出现次数
+        for (char c : t.toCharArray()) {
+            cntT[c]++;
+        }
+
+        int left = 0;
+        for (int right = 0; right < m; right++) { // 移动子串右端点
+            cntS[s[right]]++; // 右端点字母移入子串
+            while (isCovered(cntS, cntT)) { // 涵盖
+                if (right - left < ansRight - ansLeft) { // 找到更短的子串
+                    ansLeft = left; // 记录此时的左右端点
+                    ansRight = right;
+                }
+                cntS[s[left]]--; // 左端点字母移出子串
+                left++;
+            }
+        }
+        return ansLeft < 0 ? "" : S.substring(ansLeft, ansRight + 1);
+    }
+    /**
+     * 什么是「涵盖」
+     * 看示例 1，s 的子串 BANC 中每个字母的出现次数，都大于等于 t=ABC 中每个字母的出现次数，这就叫涵盖。
+     */
+    static private boolean isCovered(int[] cntS, int[] cntT) {
+        for (int i = 'A'; i <= 'Z'; i++) {
+            if (cntS[i] < cntT[i]) {
+                return false;
+            }
+        }
+        for (int i = 'a'; i <= 'z'; i++) {
+            if (cntS[i] < cntT[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * 滑动窗口
+     */
+    Map<Character, Integer> ori = new HashMap<Character, Integer>(); // 用一个哈希表表示 t 中所有的字符以及它们的个数
+    Map<Character, Integer> cnt = new HashMap<Character, Integer>(); // 用一个哈希表动态维护窗口中所有的字符以及它们的个数
+
+    public String minWindow1(String s, String t) {
+        int tLen = t.length();
+        for (int i = 0; i < tLen; i++) {
+            char c = t.charAt(i);
+            ori.put(c, ori.getOrDefault(c, 0) + 1);
+        }
+        int l = 0, r = -1;
+        int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
+        int sLen = s.length();
+        while (r < sLen) {
+            ++r;
+            if (r < sLen && ori.containsKey(s.charAt(r))) {
+                cnt.put(s.charAt(r), cnt.getOrDefault(s.charAt(r), 0) + 1);
+            }
+            while (check() && l <= r) {
+                if (r - l + 1 < len) {
+                    len = r - l + 1;
+                    ansL = l;
+                    ansR = l + len;
+                }
+                if (ori.containsKey(s.charAt(l))) {
+                    cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0) - 1);
+                }
+                ++l;
+            }
+        }
+        return ansL == -1 ? "" : s.substring(ansL, ansR);
+    }
+    // 如果这个动态表中包含 t 的哈希表中的所有字符，并且对应的个数都不小于 t 的哈希表中各个字符的个数，那么当前的窗口是「可行」的。
+    public boolean check() {
+        Iterator iter = ori.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+            Character key = (Character) entry.getKey();
+            Integer val = (Integer) entry.getValue();
+            if (cnt.getOrDefault(key, 0) < val) {
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * 滑动窗口 + 哈希表
+     * 思路清晰
+     */
+   static public String minWindow4(String s, String t) {
+        Map<Character, Integer> cnt = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            cnt.put(c, cnt.getOrDefault(c, 0) + 1);
+        }
+        int ansL = -1;
+        int ansR = s.length();
+        int l = 0;
+        int count = cnt.size();
+
+        for (int r = 0; r < s.length(); r++) {
+            char c = s.charAt(r);
+            if (cnt.containsKey(c)) {
+                cnt.put(c, cnt.get(c) - 1);
+                if (cnt.get(c) == 0) {
+                    count--;
+                }
+            }
+            while (count == 0) {
+                if (ansR - ansL > r - l) {
+                    ansL = l;
+                    ansR = r;
+                }
+                char ch = s.charAt(l);
+                if (cnt.containsKey(ch)) {
+                    if (cnt.get(ch) == 0) {
+                        count++;
+                    }
+                    cnt.put(ch, cnt.get(ch) + 1);
+                }
+                l++;
+            }
+        }
+        return ansL == -1 ? "" : s.substring(ansL, ansR + 1);
+    }
+    /**
+     * 借助数组的标记，负数表示s子串还不能满足t某个字符的需求，正数表示需求过剩，可移动窗口
+     */
+    static public String minWindow2(String s, String t) {
+        int[] hash = new int[128]; // 统计字符需求及当前窗口频次
+        for (int i = 0; i < t.length(); i++) {
+            hash[t.charAt(i)]--; // 负值代表有 相关字符的空缺
+        }
+
+        char[] sArr = s.toCharArray();
+        int count = 0;
+        int res = Integer.MAX_VALUE;
+        int L = -1;
+        for (int i = 0, j = 0; j < sArr.length; j++) {
+            if (hash[sArr[j]] < 0) {
+                count++;  // 负值表示存在空缺未填满
+            }
+            hash[sArr[j]]++; // 更新窗口频次
+
+            // 缩小窗口：移除冗余字符（频次超过需求或非t字符）
+            while (i < j && hash[sArr[i]] > 0){
+                hash[sArr[i]]--; // 减少频次
+                i++; // 左指针右移
+            }
+            // 记录最小窗口
+            if (count == t.length() && res > j - i + 1) {
+                res = j - i + 1;
+                L = i;
+            }
+        }
+        return res == Integer.MAX_VALUE ? "" : s.substring(L, L + res);
+    }
+}
