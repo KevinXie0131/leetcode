bd70f2a37375d852872ee8e370b312f6fb0f8a11
KevinX <Xzb04720638k>
2025-06-04 18:48:16 -0400

    update

--- src/main/java/com/answer/sliding_window/Q1151_Minimum_Swaps_to_Group_All_1s_Together.java
+++ src/main/java/com/answer/sliding_window/Q1151_Minimum_Swaps_to_Group_All_1s_Together.java
@@ -25,8 +25,69 @@ public class Q1151_Minimum_Swaps_to_Group_All_1s_Together {
      *  最少交换次数来组合所有的1: 出一个二进制数组 data，你需要通过交换位置，将数组中 任何位置 上的 1 组合到一起，并返回所有可能中所需 最少的交换次数。
      */
     public static void main(String[] args) {
+        int[] data1 = {1,0,1,0,1};
+        System.out.println(minSwaps5(data1));
+        int[] data2 = {0,0,0,1,0};
+        System.out.println(minSwaps5(data2));
         int[] data = {1,0,1,0,1,0,0,1,1,0,1};
-        System.out.println(minSwaps(data));
+        System.out.println(minSwaps5(data));
+    }
+    /**
+     * 滑动窗口
+     * 先统计总共有多少个1，我们假设有m个。然后以m为滑动窗口的长度，通过滑动窗口的方式判断窗口中有多少个1（假设为n个），
+     * 那么此时我们需要移动的次数就是m-n，我们只需要找最小值即可。
+     */
+    public static int minSwaps(int[] data) {
+        int size = Arrays.stream(data).filter(i -> i == 1).sum(); // 滑动窗口的 size 是固定的
+
+        int min = Integer.MAX_VALUE, count = 0;
+        for(int index = 0; index < size; index++){
+            if(data[index] == 1){
+                count++;
+            }
+            min = Math.min(min, size - count);
+        }
+
+        for(int index = size; index < data.length; index++){
+            if(data[index] == 1){
+                count++;
+            }
+            if(data[index - size] == 1){
+                count--;
+            }
+            min = Math.min(min,  size - count);
+        }
+        return min;
+    }
+    /**
+     * another form
+     * 首先我们用一个变量 ones 记录 input 数组中一共有多少个 1。然后我们用滑动窗口的模板开始扫描 input 数组，
+     * 当 end 指针遇到 1 的时候，count++；当 end 和 start 两个指针的距离 == ones 的时候，
+     * 此时我们看一下 end 和 start 之间有几个 1，此时 ones 和 count 的差值就是需要 swap 的次数。遍历整个数组，
+     * 找到全局最小的 swap 次数即可。
+     */
+    public static int minSwaps4(int[] data) {
+        int ones = Arrays.stream(data).sum();
+        int count = 0, left = 0, right = 0, min = Integer.MAX_VALUE;
+
+       if(ones == 1 || ones == 0) return 0;
+
+        while(right < data.length){
+            if(data[right] == 1){
+                count++;
+            }
+
+            if(right - left == ones){
+                min = Math.min(min, ones - count);
+                if(data[left] == 1){
+                    count--;
+                }
+                left--;
+            }
+            right++;
+        }
+
+        return min;
     }
     /**
      * Approach 1: Sliding Window with Two Pointers
@@ -36,7 +97,7 @@ public class Q1151_Minimum_Swaps_to_Group_All_1s_Together {
      * 就得来几次交换，把它们换出去，可知求最少交换次数，就是求最少0。
      *
      */
-    public static int minSwaps(int[] data) {
+    public static int minSwaps2(int[] data) {
         int len = data.length;
         int min = Integer.MIN_VALUE;
         int totalCount = Arrays.stream(data).sum();
@@ -57,6 +118,9 @@ public class Q1151_Minimum_Swaps_to_Group_All_1s_Together {
     }
     /**
      * Approach 2: Sliding Window with Deque (Double-ended Queue)
+     * 1.Count total number of 1s as totalCount.
+     * 2.Use a sliding window of size totalCount to find the window with the most 1s (max).
+     * 3.The minimum number of swaps needed is totalCount - max
      */
     public int minSwaps_1(int[] data) {
         int len = data.length;
@@ -78,5 +142,24 @@ public class Q1151_Minimum_Swaps_to_Group_All_1s_Together {
         }
 
         return totalCount - max;
+    }
+    /**
+     * 可以通过前缀和的方式统计区间内1的个数
+     */
+    public static int minSwaps5(int[] data) {
+        int n = data.length;
+        int[] preSum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            preSum[i + 1] = preSum[i] + data[i];
+        }
+
+        int min = n;
+        int ones = preSum[n];
+
+        for(int i = ones; i < n + 1; i++){
+            min = Math.min(min, ones - (preSum[i] - preSum[i - ones]));
+        }
+
+        return min;
     }
 }
