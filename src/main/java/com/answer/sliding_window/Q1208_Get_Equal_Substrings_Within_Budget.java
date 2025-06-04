d5bdd1e2073cdcd5f90fc68d652f5b757148bf2d
KevinX <Xzb04720638k>
2025-06-03 22:29:52 -0400

    update

--- src/main/java/com/answer/sliding_window/Q1208_Get_Equal_Substrings_Within_Budget.java
+++ src/main/java/com/answer/sliding_window/Q1208_Get_Equal_Substrings_Within_Budget.java
@@ -1,6 +1,5 @@
 package com.answer.sliding_window;
 
-
 public class Q1208_Get_Equal_Substrings_Within_Budget {
     /**
      * You are given two strings s and t of the same length and an integer maxCost.
@@ -38,10 +37,35 @@ public class Q1208_Get_Equal_Substrings_Within_Budget {
         System.out.println(equalSubstring(s, t, maxCost));
     }
     /**
+     * 滑动窗口
+     * 生成一个数组 cost = abs(ord(t[i]) - ord(s[i]))
+     * 问题就转化为：在一个数组中，在连续子数组的和小于等于 maxCost 的情况下，找到最长的连续子数组长度。
+     */
+    public int equalSubstring3(String s, String t, int maxCost) {
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        int len = sArr.length;
+        int[] cost = new int[len];
+
+        for(int i = 0; i < len; i++){
+            cost[i] = Math.abs(sArr[i] - tArr[i]);
+        }
+
+        int maxLen = 0, sum = 0, left = 0;
+        for (int right = 0; right < len; right++) {
+            sum += cost[right];
+
+            while (sum > maxCost) {
+                sum -= cost[left];
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+    /**
      * Similar with Q487 Max Consecutive Ones II
-     *
-     * Sliding window
-     *
+     * Sliding window 滑动窗口
      * 「滑动窗口」是固定大小的，「双指针」是不固定大小的；
      * 「滑动窗口」一定是同向移动的，「双指针」可以相向移动。
      */
@@ -51,15 +75,15 @@ public class Q1208_Get_Equal_Substrings_Within_Budget {
 
         while(right < s.length()){
             int diff = Math.abs(s.charAt(right) - t.charAt(right));
-            maxCost -= diff;
+            maxCost -= diff; // 当前字符的转换开销
 
-            if(maxCost < 0){
+            if(maxCost < 0){ // 如果超预算，就收缩窗口
                 int diff1 = Math.abs(s.charAt(left) - t.charAt(left));
                 maxCost += diff1;
                 left++;
             }
 
-            max = Math.max(max, right - left + 1);
+            max = Math.max(max, right - left + 1); // 窗口合理，更新最大窗口长度
             right++;
         }
 
@@ -87,5 +111,70 @@ public class Q1208_Get_Equal_Substrings_Within_Budget {
             end++;
         }
         return maxLength;
+    }
+    /**
+     * 前缀和 + 二分查找
+     * 由于 diff 的的每个元素都是非负的，因此 accDiff 是递增的，对于每个下标 j，可以通过在 accDiff 内
+     * 进行二分查找的方法找到符合要求的最小的下标 k。
+     */
+    public int equalSubstring4(String s, String t, int maxCost) {
+        int n = s.length();
+        int[] accDiff = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            accDiff[i + 1] = accDiff[i] + Math.abs(s.charAt(i) - t.charAt(i));
+        }
+        /**
+         * 对于下标范围 [1,n] 内的每个 i，通过二分查找的方式，在下标范围 [0,i] 内找到最小的下标 start，
+         * 使得 accDiff[start]≥accDiff[i]−maxCost，此时对应的 diff 的子数组的下标范围是从 start 到 i−1，
+         * 子数组的长度是 i−start。
+         */
+        int maxLength = 0;
+        for (int i = 1; i <= n; i++) {
+            int start = binarySearch(accDiff, i, accDiff[i] - maxCost);
+            maxLength = Math.max(maxLength, i - start);
+        }
+        return maxLength;
+    }
+
+    public int binarySearch(int[] accDiff, int endIndex, int target) {
+        int low = 0, high = endIndex;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (accDiff[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+    /**
+     * 前缀和 + 二分法
+     * 构造 前缀和 数组 prefixSum
+     * prefixSum[i] 代表 s[0..i-1] 转换到 t[0..i-1] 的总开销。也就是：
+     * prefixSum[i] = prefixSum[i-1] + abs(s[i-1] - t[i-1])
+     *
+     * 有了前缀和，我们可以利用前缀和的减法，快速得到一个区间值。这样就可以使用 二分查找法
+     * 确定区间值是否符合题意：每个 r，找到 最左的 l，使得 prefixSum[r+1] - prefixSum[l] <= maxCost
+     */
+    public int equalSubstring5(String s, String t, int maxCost) {
+        int n = s.length(), res = 0;
+        int[] prefixSum = new int[n + 1]; // 前缀和数组
+        for (int i = 0; i < n; ++i) //计算前缀和
+            prefixSum[i + 1] = prefixSum[i] + Math.abs(s.charAt(i) - t.charAt(i));
+
+        for (int r = 0; r < n; r++) { // 遍历前缀和数组
+            // 二分查找 `l`，满足 `prefixSum[r+1] - prefixSum[l] <= maxCost`
+            int left = 0, right = r + 1;  // `right = r+1` 因为 `prefixSum` 有 n+1 长度
+
+            while (left < right) { // 二分查找
+                int mid = left + (right - left) / 2; // 二分
+                if (prefixSum[r + 1] - prefixSum[mid] > maxCost) // 对比
+                    left = mid + 1;  // 右移
+                else right = mid;  // 左移
+            }
+            res = Math.max(res, r - left + 1); // 更新窗口大小
+        }
+        return res;
     }
 }
