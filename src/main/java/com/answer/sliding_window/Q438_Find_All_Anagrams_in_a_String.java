d2349d85b567f7b7303edcdf46167b2ee0a21270
KevinX <Xzb04720638k>
2025-06-04 21:32:42 -0400

    update

--- src/main/java/com/answer/sliding_window/Q438_Find_All_Anagrams_in_a_String.java
+++ src/main/java/com/answer/sliding_window/Q438_Find_All_Anagrams_in_a_String.java
@@ -12,8 +12,8 @@ public class Q438_Find_All_Anagrams_in_a_String {
      * 字母异位词是通过重新排列不同单词或短语的字母而形成的单词或短语，并使用所有原字母一次。
      */
     public static void main(String[] args) {
-        String s1 = "cbaebabacd";
-        String s2 = "abc";
+     //   String s1 = "cbaebabacd";
+     //   String s2 = "abc";
         /**
          * 输出: [0,6]
          * 解释: 起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
@@ -22,10 +22,69 @@ public class Q438_Find_All_Anagrams_in_a_String {
         String s2 ="c";*/
       /*  String s1 = "aa";
         String s2 ="bb";*/
-
-        System.out.println(findAnagrams_2(s1, s2));
+        String s1 = "abab";
+        String s2 = "ab";
+        System.out.println(findAnagrams_5(s1, s2));
     }
+    /**
+     * 定长滑窗
+     */
+    static public List<Integer> findAnagrams_5(String s, String p) {
+        List<Integer> res = new ArrayList<Integer>();
+        int m = s.length(), n = p.length();
+        if(m < n) return res;
 
+        int[] counter1 = new int[26];
+        int[] counter2 = new int[26];
+        for(int i = 0; i < n; i++){
+            counter1[p.charAt(i) - 'a']++;
+            counter2[s.charAt(i) - 'a']++;
+        }
+
+        int left = 0, right = n;
+        while(right < m){
+            if(Arrays.equals(counter1, counter2)){
+                res.add(left);
+            }
+
+            counter2[s.charAt(right) - 'a']++;
+            counter2[s.charAt(right - n) - 'a']--;
+            left++;
+            right++;
+        }
+        if(Arrays.equals(counter1, counter2)){
+            res.add(left);
+        }
+        return res;
+    }
+    /**
+     * 不定长滑窗
+     * 代码实现时，可以把 cntS 和 cntP 合并成一个 cnt：
+     *  对于 p 的字母 c，把 cnt[p] 加一。
+     *  对于 s′的字母 c，把 cnt[c] 减一。
+     *  如果 cnt[c]<0，说明窗口中的字母 c 的个数比 p 的多，右移左端点。
+     */
+    public static List<Integer> findAnagrams4(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+        int[] counter = new int[26]; // 统计 p 的每种字母的出现次数
+        for (char c : p.toCharArray()) {
+            counter[c - 'a']++;
+        }
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            int ch = s.charAt(right) - 'a';
+            counter[ch]--; // 右端点字母进入窗口
+
+            while (counter[ch] < 0) { // 字母 c 太多了
+                counter[s.charAt(left) - 'a']++; // 左端点字母离开窗口
+                left++;
+            }
+            if (right - left + 1 == p.length()) { // s' 和 p 的每种字母的出现次数都相同
+                ans.add(left); // s' 左端点下标加入答案
+            }
+        }
+        return ans;
+    }
     /**
      * Brute force
      */
@@ -58,6 +117,7 @@ public class Q438_Find_All_Anagrams_in_a_String {
     }
     /**
      * Approach 2: Sliding Window with Array
+     * 使用数组来存储字符串 p 和滑动窗口中每种字母的数量。
      */
     public static List<Integer> findAnagrams_1(String s, String p) {
         List<Integer> res = new ArrayList<>();
