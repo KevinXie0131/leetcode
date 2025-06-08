e6121385a40ab7768a872544a2515423088fa7f5
KevinX <Xzb04720638k>
2025-06-08 16:42:28 -0400

    additional

---
+++ src/main/java/com/answer/sorting/Q1200_Minimum_Absolute_Difference.java
@@ -0,0 +1,22 @@
+package com.answer.sorting;
+
+public class Q1200_Minimum_Absolute_Difference {
+    /**
+     * Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements.
+     * Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
+     *  a, b are from arr
+     *  a < b
+     *  b - a equals to the minimum absolute difference of any two elements in arr
+     * 最小绝对差
+     * 整数数组 arr，其中每个元素都 不相同。请你找到所有具有最小绝对差的元素对，并且按升序的顺序返回。
+     * 每对元素对 [a,b] 如下：
+     *  a , b 均为数组 arr 中的元素
+     *  a < b
+     *  b - a 等于 arr 中任意两个元素的最小绝对差
+     *
+     * Example 1:
+     * Input: arr = [4,2,1,3]
+     * Output: [[1,2],[2,3],[3,4]]
+     * Explanation: The minimum absolute difference is 1. List all pairs with difference equal to 1 in ascending order.
+     */
+}
