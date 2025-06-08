70382662fbd01655f844f12767cc2968e5368777
KevinX <Xzb04720638k>
2025-06-08 16:34:14 -0400

    additional

--- src/main/java/com/answer/sorting/Q912_Sort_an_Array.java
+++ src/main/java/com/answer/sorting/Q912_Sort_an_Array.java
@@ -3,7 +3,21 @@ package com.answer.sorting;
 import java.util.*;
 
 public class Q912_Sort_an_Array {
-
+    /**
+     * Given an array of integers nums, sort the array in ascending order and return it.
+     * You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+     * 排序数组
+     * 一个整数数组 nums，请你将该数组升序排列。
+     * 你必须在 不使用任何内置函数 的情况下解决问题，时间复杂度为 O(nlog(n))，并且空间复杂度尽可能小。
+     * Example 1:
+     *  Input: nums = [5,2,3,1]
+     *  Output: [1,2,3,5]
+     *  Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+     * Example 2:
+     *  Input: nums = [5,1,1,2,0,0]
+     *  Output: [0,0,1,1,2,5]
+     *  Explanation: Note that the values of nums are not necessairly unique.
+     */
     public static void main(String[] args) {
         int[] nums = {5, 2, 3, 1, 9, 4, 8, 10, 7};
         Q912_Sort_an_Array solution = new Q912_Sort_an_Array();
