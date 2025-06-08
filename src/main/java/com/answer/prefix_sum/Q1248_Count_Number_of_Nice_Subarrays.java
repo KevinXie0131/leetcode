7aae3d7928b022212bdb537d03900e89af0afcf0
KevinX <Xzb04720638k>
2025-06-07 20:38:13 -0400

    update

--- src/main/java/com/answer/prefix_sum/Q1248_Count_Number_of_Nice_Subarrays.java
+++ src/main/java/com/answer/prefix_sum/Q1248_Count_Number_of_Nice_Subarrays.java
@@ -127,4 +127,41 @@ public class Q1248_Count_Number_of_Nice_Subarrays {
         }
         return count;
     }
+    /**
+     * 双指针 数组可以分成三个部分：
+     *  包含 k 个奇数的区间（左右边界都是奇数）
+     *  k 个奇数前面的偶数
+     *  k 个奇数后面的偶数
+     * 有种思路是分别计算前后的偶数个数，相乘加到结果里面。
+     * 我的做法是先计算前面的偶数个数，后面遍历时遇到偶数直接加进来就可以了
+     * 判断奇偶的操作可以继续用位运算来优化
+     */
+    public int numberOfSubarrays7(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || nums.length < k) return 0;
+        // 双指针
+        int left = 0, right = 0;
+        int count = 0; // 连续子数组中奇数的个数
+        int res = 0;
+        int preEven = 0; // 记录第一个奇数前面的偶数个数
+        while (right < nums.length){
+            // 连续子数组中奇数个数不够
+            if (count < k){
+                if (nums[right] % 2 != 0) count++;
+                right++; // 移动右侧指针
+            }
+            // 连续子数组中奇数个数够了，看第一个奇数前面有多少个偶数
+            if (count == k) {
+                preEven = 0;
+                while (count == k){
+                    res++;
+                    if (nums[left] % 2 != 0) count--;
+                    left++;
+                    preEven++;
+                }
+            } else {
+                res += preEven; // 每次遇到 right 为偶数的时候就进行累加 相当于区间前面偶数个数 * 后面偶数个数
+            }
+        }
+        return res;
+    }
 }
