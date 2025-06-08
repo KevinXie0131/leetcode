1f9d1ba33f8fecf203e5344ae2a36d7012b1f191
KevinX <Xzb04720638k>
2025-06-07 22:15:19 -0400

    additional

--- src/main/java/com/answer/union_find/Q721_Accounts_Merge.java
+++ src/main/java/com/answer/union_find/Q721_Accounts_Merge.java
@@ -1,16 +1,31 @@
 package com.answer.union_find;
 
-import java.awt.*;
 import java.util.*;
-import java.util.List;
 
 public class Q721_Accounts_Merge {
+    /**
+     * 账户合并
+     * 给定一个列表 accounts，每个元素 accounts[i] 是一个字符串列表，其中第一个元素 accounts[i][0] 是 名称 (name)，其余元素是 emails 表示该账户的邮箱地址。
+     * 现在，我们想合并这些账户。如果两个账户都有一些共同的邮箱地址，则两个账户必定属于同一个人。请注意，即使两个账户具有相同的名称，
+     * 它们也可能属于不同的人，因为人们可能具有相同的名称。一个人最初可以拥有任意数量的账户，但其所有账户都具有相同的名称。
+     * 合并账户后，按以下格式返回账户：每个账户的第一个元素是名称，其余元素是 按字符 ASCII 顺序排列 (in sorted order) 的邮箱地址。
+     * 账户本身可以以 任意顺序( any order) 返回。
+     */
     public static void main(String[] args) {
         List<List<String>> accounts = new ArrayList<>();
         accounts.add(Arrays.asList("John","johnsmith@mail.com","john_newyork@mail.com"));
         accounts.add(Arrays.asList("John","johnsmith@mail.com","john00@mail.com"));
         accounts.add(Arrays.asList("Mary","mary@mail.com"));
         accounts.add(Arrays.asList("John","johnnybravo@mail.com"));
+        /**
+         * 输出：[["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],
+         * ["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]]
+         * 解释：
+         * 第一个和第三个 John 是同一个人，因为他们有共同的邮箱地址 "johnsmith@mail.com"。
+         * 第二个 John 和 Mary 是不同的人，因为他们的邮箱地址没有被其他帐户使用。
+         * 可以以任何顺序返回这些列表，例如答案 [['Mary'，'mary@mail.com']，['John'，'johnnybravo@mail.com']，
+         * ['John'，'john00@mail.com'，'john_newyork@mail.com'，'johnsmith@mail.com']] 也是正确的。
+         */
         System.out.println(accountsMerge_1(accounts));
     }
     /**
@@ -124,6 +139,7 @@ public class Q721_Accounts_Merge {
         }
         return mergedAccounts;
     }
+
     static private void DFS(List<String> mergedAccount, String email) {
         visited.add(email);
         // Add the email vector that contains the current component's emails
