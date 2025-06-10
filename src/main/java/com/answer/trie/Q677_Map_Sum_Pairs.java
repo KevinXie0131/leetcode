c4997d8bbf8754cb264559bf4614fa598714034c
KevinX <Xzb04720638k>
2025-06-10 16:44:27 -0400

    update

--- src/main/java/com/answer/trie/Q677_Map_Sum_Pairs.java
+++ src/main/java/com/answer/trie/Q677_Map_Sum_Pairs.java
@@ -23,15 +23,77 @@ public class Q677_Map_Sum_Pairs {
      *  mapSum.sum("ap");           // 返回 5 (apple + app = 3 + 2 = 5)
      * key and prefix consist of only lowercase English letters.
      */
+    public static void main(String[] args) {
+        Q677_Map_Sum_Pairs mapSum = new Q677_Map_Sum_Pairs();
+        mapSum.insert("apple", 3);
+        System.out.println(mapSum.sum("ap"));// 返回 3 (apple = 3)
+        mapSum.insert("app", 2);
+        System.out.println( mapSum.sum("ap"));  // 返回 5 (apple + app = 3 + 2 = 5)
+    }
+    /**
+     * Trie + DFS
+     */
+    Trie4 trie4;
+
     public Q677_Map_Sum_Pairs() {
-
+        trie4 = new Trie4();
     }
-
+    // 不能简单记录单词的结束位置，还要存储 key 对应的 val 是多少
     public void insert(String key, int val) {
-
+        trie4.insert(key, val);
     }
-
+    // 先对入参 prefix 进行字典树搜索，到达尾部后再使用 DFS 搜索后面的所有方案，并累加结果。
     public int sum(String prefix) {
-        return 0;
+        return trie4.searchPrefix(prefix);
     }
+}
+
+class Trie4 {
+    private Trie4[] children; // 字典树的子节点
+    private int value;
+
+    public Trie4() {
+        children = new Trie4[26]; //  //初始时每个都是26个小写字母
+        value = 0;
+    }
+
+    public void insert(String word, int val) { // 插入字符串
+        Trie4 node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie4();
+            }
+            node = node.children[index];  //指针下移
+        }
+        node.value = val;
+    }
+
+    public int searchPrefix(String word) {
+        Trie4 node = this;
+        // 找到前缀 prefix 的最后一个节点
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] != null) {
+                node = node.children[index];
+            } else {
+                return 0; // 不存在这个前缀，返回0
+            }
+        }
+        return getAllSum(node); // 从这个节点往下DFS
+    }
+
+    // 辅助函数，求以 node 为根节点的子树的节点和
+    private int getAllSum(Trie4 node) {
+        if (node == null) return 0;
+        int sum = 0;
+
+        for (int i = 0; i < 26; i++) {
+            sum += getAllSum(node.children[i]);
+        }
+        return sum + node.value;
+    }
+
 }
---
+++ src/main/java/com/answer/trie/Q677_Map_Sum_Pairs_1.java
@@ -0,0 +1,105 @@
+package com.answer.trie;
+
+import java.util.*;
+
+public class Q677_Map_Sum_Pairs_1 {
+    /**
+     * 暴力扫描
+     * 我们将所有的 key-val 键值进行存储，每次需要搜索给定的前缀 prefix 时，我们依次搜索所有的键值。
+     * 如果键值包含给定的前缀，则我们将其 val 进行相加，返回所有符合要求的 val 的和。
+     */
+    Map<String, Integer> map;
+
+    public void MapSum() {
+        map = new HashMap<>();
+    }
+
+    public void insert(String key, int val) {
+        map.put(key,val);
+    }
+
+    public int sum(String prefix) {
+        int res = 0;
+        for (String s : map.keySet()) {
+            if (s.startsWith(prefix)) {
+                res += map.get(s);
+            }
+        }
+        return res;
+    }
+    /**
+     * 前缀哈希映射
+     * 我们可以用哈希表存储所有可能前缀的值。当我们得到一个新的 key-val 键值，我们将 key 的每个前缀 prefix[i]
+     * 都在哈希表中进行存储，我们需要更新每个前缀 prefix[i] 对应的值。我们计算出它对应的值的增加为 delta，计算方式如下：
+     *
+     *  如果键 key 不存在，则此时 delta 等于 val。
+     *  如果键 key 存在，则此时键 key 对应得前缀的值都增加 val−map[key]，其中 map[key] 表示键 key 当前对应的值。
+     *  我们在完成前缀的值改写后，同时要更新键 key 对应的值为 val。
+     * 求 sum 时,我们直接利用哈希表查找给定的前缀对应的值即可。
+     */
+    Map<String, Integer> map1;
+    Map<String, Integer> prefixmap;
+
+    public void MapSum1() {
+        map1 = new HashMap<>();
+        prefixmap = new HashMap<>();
+    }
+
+    public void insert1(String key, int val) {
+        int delta = val - map1.getOrDefault(key, 0);
+        map1.put(key, val);
+        for (int i = 1; i <= key.length(); ++i) {
+            String curPrefix = key.substring(0, i);
+            prefixmap.put(curPrefix, prefixmap.getOrDefault(curPrefix, 0) + delta);
+        }
+    }
+
+    public int sum1(String prefix) {
+        return prefixmap.getOrDefault(prefix, 0);
+    }
+    /**
+     * 字典树
+     * 由于我们要处理前缀，自然而然联想到可以用 Trie（前缀树）来处理此问题。处理方法跟方法二的原理一样，
+     * 我们直接在前缀对应的 Trie 的每个节点存储该前缀对应的值。
+     *
+     * insert 操作：原理与方法二一样，我们首先求出前缀对应的值的改变 delta，我们直接在 Trie 节点上更新键 key 的每个前缀对应的值。
+     * sum 操作: 我们直接在前缀树上搜索该给定的前缀对应的值即可，如果给定的前缀不在前缀树中，则返回 0。
+     * 当然在实际中我们也可以在 Trie 的节点只存储键 key 对应的 val, 每次求 sum 时利用 DFS 或者 BFS 遍历前缀树的子树即可。
+     */
+    class TrieNode {
+        int val = 0;
+        TrieNode[] next = new TrieNode[26];
+    }
+
+    TrieNode root;
+    Map<String, Integer> map2;
+
+    public void MapSum2() {
+        root = new TrieNode();
+        map2 = new HashMap<>();
+    }
+
+    public void insert2(String key, int val) {
+        int delta = val - map2.getOrDefault(key, 0);
+        map2.put(key, val);
+        TrieNode node = root;
+        for (char c : key.toCharArray()) {
+            if (node.next[c - 'a'] == null) {
+                node.next[c - 'a'] = new TrieNode();
+            }
+            node = node.next[c - 'a'];
+            node.val += delta;
+        }
+    }
+
+    public int sum2(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if (node.next[c - 'a'] == null) {
+                return 0;
+            }
+            node = node.next[c - 'a'];
+        }
+        return node.val;
+    }
+}
