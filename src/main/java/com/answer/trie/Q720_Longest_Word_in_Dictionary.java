23134617eec7ec7e6b4be8cf3832600e5157d917
KevinX <Xzb04720638k>
2025-06-10 21:19:31 -0400

    update

--- src/main/java/com/answer/trie/Q720_Longest_Word_in_Dictionary.java
+++ src/main/java/com/answer/trie/Q720_Longest_Word_in_Dictionary.java
@@ -1,8 +1,6 @@
 package com.answer.trie;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 
 public class Q720_Longest_Word_in_Dictionary {
     /**
@@ -24,10 +22,11 @@ public class Q720_Longest_Word_in_Dictionary {
     public static void main(String[] args) {
       //  String[] words = {"w","wo","wor","worl","world"};
         String[] words = {"a","banana","app","appl","ap","apply","apple"};
-        System.out.println(longestWord_1(words));
+        System.out.println(longestWord(words));
     }
     /**
      * 暴力法 HashSet
+     * 用集合统计全部字符，遍历统计所有前缀都在集合中的字符串，返回最长且字典序最小的那个。
      */
    static public String longestWord(String[] words) {
         String result = "";
@@ -37,8 +36,8 @@ public class Q720_Longest_Word_in_Dictionary {
             set.add(s);
         }
         for(String word : words){
-            if(word.length() > result.length()
-                    || (word.length() == result.length() && word.compareTo(result) < 0)){
+            if(word.length() > result.length() // 利用当前的最长单词来做剪枝
+                    || (word.length() == result.length() && word.compareTo(result) < 0)){ // 若其中有多个可行的答案，则返回答案中字典序最小的单词
                 boolean isPrefix = true;
                 for(int i = 1; i <= word.length(); i++){
                     if(!set.contains(word.substring(0, i))){
@@ -52,6 +51,73 @@ public class Q720_Longest_Word_in_Dictionary {
             }
         }
         return result;
+    }
+    /**
+     * another form
+     */
+    public String longestWord3(String[] words) {
+        Set<String> set = new HashSet<>();
+        for(String word: words) {
+            set.add(word);
+        }
+        String longestWord = "";
+
+        for(String word: words) {
+            boolean isValid = true;
+            for (int i = 1; i < word.length(); i++) {
+                if (!set.contains(word.substring(0, i))) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if(isValid){
+                if(word.length() > longestWord.length()) {
+                    longestWord = word;
+                } else if(word.length() == longestWord.length()) {
+                    if(word.compareTo(longestWord) < 0) {
+                        longestWord = word;
+                    }
+                }
+            }
+
+        }
+        return longestWord;
+    }
+    /**
+     * 哈希集合 + 排序
+     * 定义「符合要求的单词」如下：
+     *  空字符串是符合要求的单词；
+     *  在符合要求的单词的末尾添加一个字母，得到的新单词是符合要求的单词。
+     * 这道题要求返回数组 words 中的最长的符合要求的单词，如果有多个最长的符合要求的单词则返回其中字典序最小的单词。
+     *
+     * 为了方便处理，需要将数组 words 排序，排序的规则是首先按照单词的长度升序排序，如果单词的长度相同则按照字典序降序排序。
+     * 排序之后，可以确保当遍历到每个单词时，比该单词短的全部单词都已经遍历过，且每次遇到符合要求的单词一定是最长且字典序
+     * 最小的单词，可以直接更新答案。
+     */
+    public String longestWord2(String[] words) {
+      /*  Arrays.sort(words, (a, b) ->  {
+            if (a.length() != b.length()) {
+                return a.length() - b.length();
+            } else {
+                return b.compareTo(a);
+            }
+        });*/
+        Arrays.sort(words, (a, b) -> a.length() == b.length() ? b.compareTo(a) : a.length() - b.length());// 相同长度下把字典序较大的排在前面
+
+        String longest = "";
+        Set<String> candidates = new HashSet<String>(); // 使用哈希集合存储所有符合要求的单词
+        candidates.add("");
+        int n = words.length;
+        for (int i = 0; i < n; i++) {
+            String word = words[i];
+            // 判断当前单词去掉最后一个字母之后的前缀是否在哈希集合中，如果该前缀在哈希集合中则当前单词是符合要求的单词，
+            // 将当前单词加入哈希集合，并将答案更新为当前单词
+            if (candidates.contains(word.substring(0, word.length() - 1))) {
+                candidates.add(word);  // 注意是逐步多一，所以，这个添加要放在if里面
+                longest = word;
+            }
+        }
+        return longest;
     }
     /**
      * Trie 字典树/前缀树
---
+++ src/main/java/com/answer/trie/Q720_Longest_Word_in_Dictionary_1.java
@@ -0,0 +1,64 @@
+package com.answer.trie;
+
+import java.util.*;
+
+public class Q720_Longest_Word_in_Dictionary_1 {
+    /**
+     * 字典树
+     * 由于符合要求的单词的每个前缀都是符合要求的单词，因此可以使用字典树存储所有符合要求的单词。
+     *
+     * 如果一个单词是符合要求的单词，则比较当前单词与答案，如果当前单词的长度大于答案的长度，
+     * 或者当前单词的长度等于答案的长度且当前单词的字典序小于答案的字典序，则将答案更新为当前单词。
+     */
+    public String longestWord(String[] words) {
+        Trie6 trie = new Trie6();
+        for (String word : words) {
+            trie.insert(word);
+        }
+        String longest = "";
+        for (String word : words) {
+            if (trie.search(word)) {
+                if (word.length() > longest.length() || (word.length() == longest.length() && word.compareTo(longest) < 0)) {
+                    longest = word;
+                }
+            }
+        }
+        return longest;
+    }
+}
+
+class Trie6 {
+    Trie6[] children;
+    boolean isEnd;
+
+    public Trie6() {
+        children = new Trie6[26];
+        isEnd = false;
+    }
+
+    public void insert(String word) {
+        Trie6 node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie6();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Trie6 node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null || !node.children[index].isEnd) { // 判断每个 words[i] 是否为「合法单词」
+                return false;
+            }
+            node = node.children[index];
+        }
+        return node != null && node.isEnd;
+    }
+}
