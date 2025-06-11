66d01fc6c8f5b833eb7899ecb447012cbff0e5be
KevinX <Xzb04720638k>
2025-06-11 18:31:59 -0400

    update

--- src/main/java/com/answer/trie/Q421_Maximum_XOR_of_Two_Numbers_in_an_Array.java
+++ src/main/java/com/answer/trie/Q421_Maximum_XOR_of_Two_Numbers_in_an_Array.java
@@ -1,5 +1,7 @@
 package com.answer.trie;
 
+import java.util.*;
+
 public class Q421_Maximum_XOR_of_Two_Numbers_in_an_Array {
     /**
      * Given an integer array nums, return the maximum result of nums[i] XOR nums[j], where 0 <= i <= j < n.
@@ -13,7 +15,130 @@ public class Q421_Maximum_XOR_of_Two_Numbers_in_an_Array {
      *  输入：nums = [14,70,53,83,49,91,36,80,92,51,66,70]
      *  输出：127
      */
-    public int findMaximumXOR(int[] nums) {
-        return 0;
+    /**
+     * 暴力 Time Limit Exceeded
+     */
+    public int findMaximumXOR0(int[] nums) {
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                ans = Math.max(ans, nums[i] ^ nums[j]);
+            }
+        }
+        return ans;
     }
+    /**
+     * 方法一：哈希表
+     * 从高位到低位依次确定 x 二进制表示的每一位
+     */
+    static final int HIGH_BIT = 30; //  31 个二进制位  最高位的二进制位编号为 30
+
+    public int findMaximumXOR(int[] nums) {
+        int x = 0;
+        for (int k = HIGH_BIT; k >= 0; --k) {
+            Set<Integer> seen = new HashSet<Integer>();
+            // 将所有的 pre^k(a_j) 放入哈希表中
+            for (int num : nums) {
+                seen.add(num >> k); // 如果只想保留从最高位开始到第 k 个二进制位为止的部分 只需将其右移 k 位
+            }
+            // 目前 x 包含从最高位开始到第 k+1 个二进制位为止的部分
+            // 我们将 x 的第 k 个二进制位置为 1，即为 x = x*2+1
+            int xNext = x * 2 + 1;
+            boolean found = false;
+            // 枚举 i
+            for (int num : nums) {
+                if (seen.contains(xNext ^ (num >> k))) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) {
+                x = xNext;
+            } else {
+                x = xNext - 1;// 如果没有找到满足等式的 a_i 和 a_j，那么 x 的第 k 个二进制位只能为 0 即为 x = x*2
+            }
+        }
+        return x;
+    }
+    /**
+     * 用 ⊕ 表示异或，如果 a⊕b=newAns，那么两边同时异或 b，由于 b⊕b=0，所以得到 a=newAns⊕b（相当于把两数之和代码中的减法改成异或）。
+     * 这样就可以一边枚举 b，一边在哈希表中查找 newAns⊕b 了
+     *
+     */
+    public int findMaximumXOR1(int[] nums) {
+        int max = 0;
+        for (int x : nums) {
+            max = Math.max(max, x);
+        }
+        int highBit = 31 - Integer.numberOfLeadingZeros(max);
+
+        int ans = 0, mask = 0;
+        Set<Integer> seen = new HashSet<>();
+        for (int i = highBit; i >= 0; i--) { // 从最高位开始枚举
+            seen.clear();
+            mask |= 1 << i;
+            int newAns = ans | (1 << i); // 这个比特位可以是 1 吗？
+            for (int x : nums) {
+                x &= mask; // 低于 i 的比特位置为 0
+                if (seen.contains(newAns ^ x)) {
+                    ans = newAns; // 这个比特位可以是 1
+                    break;
+                }
+                seen.add(x);
+            }
+        }
+        return ans;
+    }
+    /**
+     * 求最大值，高位的值能是 1 就应当尽量是 1。
+     * 任意两个数「异或」得到第三个数
+     */
+    public int findMaximumXOR3(int[] nums) {
+        int res = 0;
+        int mask = 0;
+
+        // 每一位的确定都要使用新的「哈希表」，为此在循环外初始化，每一轮循环结束清空
+        Set<Integer> hashSet = new HashSet<>();
+        for (int i = 31; i >= 0; i--) {
+            // mask 每一轮的样子是这样的：
+            // 1000 0000 0000 0000 0000 0000 0000 0000
+            // 1100 0000 0000 0000 0000 0000 0000 0000
+            // 1110 0000 0000 0000 0000 0000 0000 0000
+            // 1111 0000 0000 0000 0000 0000 0000 0000
+            mask = mask | (1 << i);
+            for (int num : nums) {
+                // 用 & 依次提取最高位
+                hashSet.add(num & mask);
+            }
+
+            // 这里先假定该位是 1，所以用变量 temp
+            int temp = res | (1 << i);
+            for (Integer prefix : hashSet) {
+                if (hashSet.contains(prefix ^ temp)) {
+                    // 如果 temp 与某个前缀的异或结果也在「哈希表」中，说明 temp 是结果的前缀
+                    res = temp;
+                    break;
+                }
+            }
+            hashSet.clear();
+        }
+        return res;
+    }
+    /**
+     * 如果不好理解的话，先可以思考这么一个问题：
+     * 给定1个数组nums，再给定1个数值x。请判断nums数组中是否存在2个数，且这2个数的异或结果值为x。
+     * 暴力法：
+     *
+     * for(int i = 1; i < nums.length; ++i)
+     *     for(int j = 0; j < i; ++j)
+     *         if(nums[i] ^ nums[j] == x)
+     *             return true;
+     * 如何不使用暴力法解决该问题呢？假设数组中存在a和b，满足a ^ b == x，则a ^ x == b与b ^ x == a也必然满足。
+     * 反过来讲，你可以遍历数组nums，将当前遍历的数记为a，计算a ^ x，若数组中存在1个数(记为b)，满足b == a ^ x，则说明数组中存在2个数a和b，满足a ^ b == x。
+     * 理解了这一点的话，就可利用HashSet，将nums数组中所有数值都存入HashSet，再如下列代码一样操作：
+     *
+     * for(int num : hashSet)
+     *     if(hashSet.contains(num ^ x))
+     *         return true;
+     */
 }
--- src/main/java/com/answer/trie/Q421_Maximum_XOR_of_Two_Numbers_in_an_Array_1.java
+++ src/main/java/com/answer/trie/Q421_Maximum_XOR_of_Two_Numbers_in_an_Array_1.java
@@ -4,16 +4,40 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Q421_Maximum_XOR_of_Two_Numbers_in_an_Array_1 {
+    public static void main(String[] args) {
+       int[] nums = {3,10,5,25,2,8};
+       int res = findMaximumXOR(nums);
+       System.out.println(res);
+    }
     /**
+     * 构建一个前缀树, 每个树有俩个节点,一个代表0,一个代表1, 即前缀树有一个长度为2的前缀树数组
+     * Trie[0]代表为0的节点,Trie[1]代表为1的节点, 树的深度为31
+     *
+     * 贪心策略：
+     *  总之每一位的数一定更希望遇到和它不同的那个数
+     *
+     * 如果有期望的数
+     *  直接从最后结果里添加这位的十进制值
+     * 如果没有期望的数
+     *  没有就只能走另一个分支,走另一个分支说明俩数相等, 相等异或为0,这一位一定是0,不需要加，直接下一层
      *
      */
     public static int findMaximumXOR(int[] nums) {
         Trie trie = new Trie(); //构建前缀树
         int max = 0;  //最大的异或结果
 
-        for (int i = 0; i < nums.length; i++) {
+/*        for (int i = 0; i < nums.length; i++) {
+            //向树中添加数
+            trie.add(nums[i]);
+            //从树中查找与当前数的 异或和最大的数
+            //如果只要一个数,则他只能异或自己,一定为0
+            max=Math.max(max,trie.findMax(nums[i]));
+        }*/
+       for (int i = 0; i < nums.length; i++) { // works too, but slow
             trie.add(nums[i]); //向树中添加数
-            max = Math.max(max, trie.findMax(nums[i]));   //从树中查找与当前数的 异或和最大的数. 如果只要一个数,则他只能异或自己,一定为0
+           }
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(max, trie.findMax(nums[i]));
         }
         return max;
     }
@@ -60,4 +84,17 @@ public class Q421_Maximum_XOR_of_Two_Numbers_in_an_Array_1 {
             return res;
         }
     }
+     /*
+    前缀树+贪心:
+    nums[i] XOR nums[j]实质上是要使得nums[i]与nums[j]的每一位尽量不同,那么异或出来的结果就是1居多
+    设想一下,只要从高位开始寻找,就可以利用贪心的思想来优先取不同的数位
+    那么问题来了,用什么数据结构存储前面的nums[i]每一个二进制位信息比较合理?-->前缀树
+    前缀树是一种特殊的数据结构,他适用于将组成元素种类有限的串(英文字符串、二进制串)等保存在一棵高度有限的树中
+    其可以快速查找出当前字符串是否存在;以及查找出是否有公共前缀
+    这里都是int数据类型,本质上就是32位的二进制串,并且都是非负数,因此符号位可以忽略
+    可以存储在一颗高度为32(含root盲节点)的Tire树中,在遍历到nums[j]时,将nums[j]加入前缀树
+    从最高位开始寻找nums[0,j]构成的Tire中尽量多的与nums[j]不同的数位,异或的结果就是[0,j]中异或的最大值
+    当nums中所有元素都遍历完的时候,得到的就是最大的异或值
+    时间复杂度O(n),空间复杂度O(n)
+    */
 }
