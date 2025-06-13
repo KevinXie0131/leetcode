e8800c745646c1c2ea90db381b01ffd36db1a631
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-12 22:37:32 -0400

    update code

--- src/main/java/com/answer/greedy/Q1005_Maximize_Sum_Of_Array_After_K_Negations.java
+++ src/main/java/com/answer/greedy/Q1005_Maximize_Sum_Of_Array_After_K_Negations.java
@@ -5,6 +5,16 @@ import java.util.Comparator;
 import java.util.stream.IntStream;
 
 public class Q1005_Maximize_Sum_Of_Array_After_K_Negations {
+    /**
+     * K 次取反后最大化的数组和
+     * 一个整数数组 nums 和一个整数 k ，按以下方法修改该数组：
+     *  选择某个下标 i 并将 nums[i] 替换为 -nums[i] 。
+     * 重复这个过程恰好 k 次。可以多次选择同一个下标 i 。
+     * 以这种方式修改数组后，返回数组 可能的最大和 。
+     * 输入：nums = [4,2,3], k = 1
+     * 输出：5
+     * 解释：选择下标 1 ，nums 变为 [4,-2,3] 。
+     */
     public static void main(String[] args) {
         int[] nums = {3,-1,0,2};
         int k = 3;
