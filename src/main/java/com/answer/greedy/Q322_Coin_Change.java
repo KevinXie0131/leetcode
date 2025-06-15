bf92a5c6c9927c4d40e0b8d87b2351079d2c33aa
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-15 11:58:55 -0400

    update code

--- src/main/java/com/answer/greedy/Q322_Coin_Change.java
+++ src/main/java/com/answer/greedy/Q322_Coin_Change.java
@@ -189,7 +189,7 @@ public class Q322_Coin_Change {
             for (int coin : coins) {
                 // 子问题无解，跳过
                 if (i - coin < 0) continue;
-                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1); //  dp[i] = min(dp[i - coin] + 1), if i - coin >= 0
             }
         }
         return (dp[amount] == amount + 1) ? -1 : dp[amount];
