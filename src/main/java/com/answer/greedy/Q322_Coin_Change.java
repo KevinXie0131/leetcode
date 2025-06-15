64ab4b17f086a1dc013dcbca3994353dd4593132
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-15 12:10:51 -0400

    update code

--- src/main/java/com/answer/greedy/Q322_Coin_Change.java
+++ src/main/java/com/answer/greedy/Q322_Coin_Change.java
@@ -95,28 +95,28 @@ public class Q322_Coin_Change {
     int[] memo;
     public int coinChange5(int[] coins, int amount) {
         if(coins.length == 0) return -1;
-        memo = new int[amount];
-        return findWay(coins,amount);
+        memo = new int[amount + 1];
+        return findWay(coins, amount);
     }
     // memo[n] 表示钱币n可以被换取的最少的硬币数，不能换取就为-1
     // findWay函数的目的是为了找到 amount数量的零钱可以兑换的最少硬币数量，返回其值int
-    public int findWay(int[] coins,int amount){
-        if(amount < 0) return -1;
-        if(amount == 0) return 0;
+    public int findWay(int[] coins, int remain){
+        if(remain < 0) return -1;   // 金额小于0，无法兑换
+        if(remain == 0) return 0;  // 目标金额为0，硬币数为0
         // 记忆化的处理，memo[n]用赋予了值，就不用继续下面的循环
         // 直接的返回memo[n] 的最优值
-        if(memo[amount - 1] != 0){
-            return memo[amount - 1];
+        if(memo[remain] != 0){
+            return memo[remain]; // 已经计算过，直接返回
         }
         int min = Integer.MAX_VALUE;
-        for(int i = 0; i < coins.length; i++){
-            int res = findWay(coins,amount - coins[i]);
+        for(int coin : coins){
+            int res = findWay(coins,remain - coin); // 递归计算剩余金额所需硬币数
             if(res >= 0 && res < min){
-                min = res + 1; // 加1，是为了加上得到res结果的那个步骤中，兑换的一个硬币
+                min = res + 1; // 加1，是为了加上得到res结果的那个步骤中，兑换的一个硬币 // 当前硬币加上子问题的结果
             }
         }
-        memo[amount - 1] = (min == Integer.MAX_VALUE ? -1 : min); // 如果没有任何一种硬币组合能组成总金额，返回 -1
-        return memo[amount - 1];
+        memo[remain] = (min == Integer.MAX_VALUE ? -1 : min); // 如果没有任何一种硬币组合能组成总金额，返回 -1  // 记忆化
+        return memo[remain];
     }
     /**
      * 动态规划 完全背包问题
