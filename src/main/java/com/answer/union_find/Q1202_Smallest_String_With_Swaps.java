b3c8685708043ad265b023076ae935b88eed8dcc
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-16 16:55:27 -0400

    update code

--- src/main/java/com/answer/union_find/Q1202_Smallest_String_With_Swaps.java
+++ src/main/java/com/answer/union_find/Q1202_Smallest_String_With_Swaps.java
@@ -19,7 +19,60 @@ public class Q1202_Smallest_String_With_Swaps {
      *  解释：交换 s[0] 和 s[3], s = "bcad"
      *        交换 s[1] 和 s[2], s = "bacd"
      */
-    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
-        return null;
+    public static void main(String[] args) {
+        String s = "dcab";
+        int[][] pairs = {{0,3},{1,2}}; // [[0,3],[1,2],[0,2]]
+        List<List<Integer>> pairs1 = new ArrayList<>();
+        pairs1.add(Arrays.asList(0, 3));
+        pairs1.add(Arrays.asList(1, 2));
+        pairs1.add(Arrays.asList(0, 2));
+        System.out.println(smallestStringWithSwaps(s, pairs1));
+    }
+    /**
+     * 并查集
+     */
+    static int[] connected;
+
+    static public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+        int length = s.length();
+        int n = pairs.size();
+        connected = new int[length];
+        for(int i = 0; i < length; i++) {
+            connected[i] = i;
+        }
+
+        for(List<Integer> pair : pairs) {
+            if (find(connected, pair.get(0)) != find(connected, pair.get(1))  ) {
+                union(connected, pair.get(0), pair.get(1) );    //合并两个连通分量
+            }
+        }
+
+        Map<Integer, PriorityQueue<Character>> map = new HashMap<>();
+
+        for(int i = 0; i < length; i++){
+            int parent = find(connected, i);
+            PriorityQueue queue = map.getOrDefault(parent, new PriorityQueue<>());
+            queue.offer(s.charAt(i));
+            map.put(parent, queue);
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < length; i++){
+            int parent = find(connected, i);
+            PriorityQueue queue = map.get(parent);
+            sb.append(queue.poll());
+        }
+        return sb.toString();
+    }
+
+    public static void union(int[] parent, int index1, int index2) {
+        parent[find(parent, index2)] = find(parent, index1);
+    }
+
+    public static int find(int[] parent, int index) {
+        if (parent[index] != index) {
+            parent[index] = find(parent, parent[index]);
+        }
+        return parent[index];
     }
 }
