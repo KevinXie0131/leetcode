7c4a4eaf5180a314e748c95152d775a3bb6ed469
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-17 16:06:54 -0400

    update code

--- src/main/java/com/answer/dfs_bfs/Q994_Rotting_Oranges.java
+++ src/main/java/com/answer/dfs_bfs/Q994_Rotting_Oranges.java
@@ -3,6 +3,29 @@ package com.answer.dfs_bfs;
 import java.util.*;
 
 public class Q994_Rotting_Oranges {
+    /**
+     * 腐烂的橘子
+     * You are given an m x n grid where each cell can have one of three values:
+     *  0 representing an empty cell,
+     *  1 representing a fresh orange, or
+     *  2 representing a rotten orange.
+     * Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+     * Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+     * 在给定的 m x n 网格 grid 中，每个单元格可以有以下三个值之一：
+     *  值 0 代表空单元格；
+     *  值 1 代表新鲜橘子；
+     *  值 2 代表腐烂的橘子。
+     * 每分钟，腐烂的橘子 周围 4 个方向上相邻 的新鲜橘子都会腐烂。
+     * 返回 直到单元格中没有新鲜橘子为止所必须经过的最小分钟数。如果不可能，返回 -1 。
+     *
+     * 示例 1：
+     *  输入：grid = [[2,1,1],[1,1,0],[0,1,1]]
+     *  输出：4
+     * 示例 2：
+     *  输入：grid = [[2,1,1],[0,1,1],[1,0,1]]
+     *  输出：-1
+     *  解释：左下角的橘子（第 2 行， 第 0 列）永远不会腐烂，因为腐烂只会发生在 4 个方向上。
+     */
     public static void main(String[] args) {
         int[][] grid = {{2,1,1},{1,1,0},{0,1,1}};
       //  int[][] grid = {{0, 2, 2}};
