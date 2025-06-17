0f9de4e739a3091638eaf6753999a4ae6b91be81
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-17 15:58:19 -0400

    update code

--- src/main/java/com/answer/dfs_bfs/Q785_Is_Graph_Bipartite.java
+++ src/main/java/com/answer/dfs_bfs/Q785_Is_Graph_Bipartite.java
@@ -6,7 +6,26 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class Q785_Is_Graph_Bipartite {
-
+    /**
+     * 判断二分图
+     * 存在一个 无向undirected图 ，图中有 n 个节点。其中每个节点都有一个介于 0 到 n - 1 之间的唯一编号。给你一个二维数组 graph ，其中 graph[u] 是一个节点数组，由节点 u 的邻接adjacent节点组成。
+     * 形式上，对于 graph[u] 中的每个 v ，都存在一条位于节点 u 和节点 v 之间的无向边(undirected edge)。该无向图同时具有以下属性：
+     *  不存在自环(no self-edges)（graph[u] 不包含 u）。
+     *  不存在平行边(no parallel edges)（graph[u] 不包含重复duplicate值）。
+     *  如果 v 在 graph[u] 内，那么 u 也应该在 graph[v] 内（该图是无向undirected图）
+     *  这个图可能不是连通图，也就是说两个节点 u 和 v 之间可能不存在一条连通彼此的路径。
+     * 二分图bipartite 定义：如果能将一个图的节点集合分割成两个独立的子集(partitioned into two independent sets) A 和 B ，并使图中的每一条边的两个节点一个来自 A 集合，一个来自 B 集合，就将这个图称为 二分图 。
+     * 如果图是二分图，返回 true ；否则，返回 false 。
+     *
+     * 输入：graph = [[1,2,3],[0,2],[0,1,3],[0,2]]
+     * 输出：false
+     * 解释：不能将节点分割成两个独立的子集，以使每条边都连通一个子集中的一个节点与另一个子集中的一个节点。
+     *      There is no way to partition the nodes into two independent sets such that every edge connects a node in one and a node in the other.
+     *
+     * 输入：graph = [[1,3],[0,2],[1,3],[0,2]]
+     * 输出：true
+     * 解释：可以将节点分成两组: {0, 2} 和 {1, 3} 。
+     */
     /**
      * Approach #1: Coloring by Depth-First Search - Recursion
      *
--- src/main/java/com/answer/dfs_bfs/Q797_All_Paths_From_Source_to_Target.java
+++ src/main/java/com/answer/dfs_bfs/Q797_All_Paths_From_Source_to_Target.java
@@ -4,6 +4,17 @@ import java.util.*;
 
 public class Q797_All_Paths_From_Source_to_Target {
     /**
+     * Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1 and return them in any order.
+     * The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
+     * 所有可能的路径
+     * 一个有 n 个节点的 有向无环图（DAG），请你找出从节点 0 到节点 n-1 的所有路径并输出（不要求按特定顺序）
+     * graph[i] 是一个从节点 i 可以访问的所有节点的列表（即从节点 i 到节点 graph[i][j]存在一条有向边）。
+     *
+     * 输入：graph = [[1,2],[3],[3],[]]
+     * 输出：[[0,1,3],[0,2,3]]
+     * 解释：有两条路径 0 -> 1 -> 3 和 0 -> 2 -> 3
+     */
+    /**
      * 深搜三部曲
      * dfs写法一：处理当前访问的节点
      *     void dfs1(ArrayList<ArrayList<Integer>> graph, int key, boolean[] visited) {
