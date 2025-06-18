821af2d5cad6a682326f890adebf9d2a636e6b7f
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-17 22:26:30 -0400

    update

--- src/main/java/com/answer/union_find/Q959_Regions_Cut_By_Slashes.java
+++ src/main/java/com/answer/union_find/Q959_Regions_Cut_By_Slashes.java
@@ -15,7 +15,64 @@ public class Q959_Regions_Cut_By_Slashes {
      * 输出：5
      * 解释：回想一下，因为 \ 字符是转义的，所以 "/\\" 表示 /\，而 "\\/" 表示 \/。
      */
-    public int regionsBySlashes(String[] grid) {
-        return 0;
+    public static void main(String[] args) {
+       String[] grid = {"/\\","\\/"};
+       System.out.println(regionsBySlashes(grid));
+    }
+    /**
+     * 并查集
+     */
+    static int[] connected;
+
+    static public int regionsBySlashes(String[] grid) {
+        int n = grid.length;
+        int size = 4 * n * n;
+        connected = new int[size];
+        for(int i = 0; i < size; i++) {
+            connected[i] = i;
+        }
+
+        for(int row = 0 ; row < n; row++){
+            char[] ch = grid[row].toCharArray();
+
+            for(int col = 0 ; col < n; col++){
+                int index = 4 * (row * n + col);
+                char c = ch[col];
+                if(c == '/'){
+                    union(index + 0, index + 3);
+                    union(index + 1, index + 2);
+                } else if((c == '\\')){
+                    union(index + 0, index + 1);
+                    union(index + 2, index + 3);
+                } else {
+                    union(index + 0, index + 1);
+                    union(index + 1, index + 2);
+                    union(index + 2, index + 3);
+                }
+
+                if(col < n - 1){
+                    union(index + 1, 4 * (row * n + col + 1) + 3);
+                }
+                if(row < n - 1){
+                    union(index + 2, 4 * ((row + 1) * n + col));
+                }
+            }
+        }
+        int count = 0;
+        for(int i = 0 ; i < connected.length; i++){
+            if(connected[i] == i) count++;
+        }
+        return count;
+    }
+
+    public static void union(int index1, int index2) {
+        connected[find(index2)] = find(index1);
+    }
+
+    public static int find(int index) {
+        if (connected[index] != index) {
+            connected[index] = find(connected[index]);
+        }
+        return connected[index];
     }
 }
