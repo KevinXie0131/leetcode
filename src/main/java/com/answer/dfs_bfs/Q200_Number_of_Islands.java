9f9b1f506955186c904a1f1fd14edee32ce29f0d
KevinX <Xzb04720638k>
2025-06-19 21:27:08 -0400

    update

--- src/main/java/com/answer/dfs_bfs/Q200_Number_of_Islands.java
+++ src/main/java/com/answer/dfs_bfs/Q200_Number_of_Islands.java
@@ -4,6 +4,9 @@ import java.util.*;
 
 public class Q200_Number_of_Islands {
     /**
+     * https://leetcode.cn/problems/number-of-islands/solutions/211211/dao-yu-lei-wen-ti-de-tong-yong-jie-fa-dfs-bian-li-/
+     */
+    /**
      * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
      * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
      * 岛屿数量
--- src/main/java/com/answer/dfs_bfs/Q827_Making_A_Large_Island.java
+++ src/main/java/com/answer/dfs_bfs/Q827_Making_A_Large_Island.java
@@ -22,22 +22,23 @@ public class Q827_Making_A_Large_Island { // Hard 困难
      * 第一步：一次遍历地图，得出各个岛屿的面积，并做编号记录。可以使用map记录，key为岛屿编号，value为岛屿面积
      * 第二步：再遍历地图，遍历0的方格（因为要将0变成1），并统计该1（由0变成的1）周边岛屿面积，将其相邻面积相加在一起，遍历所有 0 之后，就可以得出 选一个0变成1 之后的最大面积。
      * 整个解法的时间复杂度，为 n * n + n * n 也就是 n^2
+     *
+     * 这道题实际上是对网格做了两遍 DFS：第一遍 DFS 遍历陆地格子，计算每个岛屿的面积并标记岛屿；
+     * 第二遍 DFS 遍历海洋格子，观察每个海洋格子相邻的陆地格子。
      */
-    public int largestIsland(int[][] grid) {
+    public int largestIsland(int[][] grid) { // 标记岛屿 + 合并
         int m = grid.length;
         int n = grid[0].length;
         // 初始化mark变量，从2开始（区别于0水，1岛屿）
         mark = 2; // 记录每个岛屿的编号
-        // 定义二位boolean数组记录该位置是否被访问
-        boolean[][] visited = new boolean[m][n];
-        // 定义一个HashMap，记录某片岛屿的标记号和面积
-        HashMap<Integer, Integer> getSize = new HashMap<>();
-        // 定义一个HashSet，用来判断某一位置水四周是否存在不同标记编号的岛屿
-        HashSet<Integer> set = new HashSet<>();
+
+        boolean[][] visited = new boolean[m][n];// 定义二位boolean数组记录该位置是否被访问
+        HashMap<Integer, Integer> getSize = new HashMap<>(); // 定义一个HashMap，记录某片岛屿的标记号和面积
+        HashSet<Integer> set = new HashSet<>(); // 定义一个HashSet，用来判断某一位置水四周是否存在不同标记编号的岛屿
         // 定义一个boolean变量，看看DFS之后，是否全是岛屿
         boolean isAllIsland = true;  // 标记是否整个地图都是陆地
-        // 遍历二维数组进行DFS搜索，标记每片岛屿的编号，记录对应的面积
-        for (int i = 0; i < m; i++) {
+
+        for (int i = 0; i < m; i++) {  // 遍历二维数组进行DFS搜索，标记每片岛屿的编号，记录对应的面积
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 0) {
                     isAllIsland = false;
@@ -61,8 +62,7 @@ public class Q827_Making_A_Large_Island { // Hard 困难
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 0) {
                     set.clear(); // 每次使用时，清空
-                    // 当前水位置变更为岛屿，所以初始化为1
-                    int curSize = 1;
+                    int curSize = 1;// 当前水位置变更为岛屿，所以初始化为1
 
                     for (int[] dir : dirs) {
                         int curRow = i + dir[0];
@@ -74,7 +74,7 @@ public class Q827_Making_A_Large_Island { // Hard 困难
                         int curMark = grid[curRow][curCol];
                         // 如果当前相邻的岛屿已经遍历过或者HashMap中不存在这个编号，继续搜索
                         if (set.contains(curMark) || !getSize.containsKey(curMark)) { // 添加过的岛屿不要重复添加
-                            continue;
+                            continue; // set 区分一个海洋格子相邻的两个 是不是来自同一个岛屿
                         }
                         set.add(curMark);// 标记该岛屿已经添加过
                         curSize += getSize.get(curMark);  // 把相邻四面的岛屿数量加起来
@@ -87,27 +87,24 @@ public class Q827_Making_A_Large_Island { // Hard 困难
     }
     // 该方法采用 DFS
     // 定义全局变量
-    // 记录每次每个岛屿的面积
-    int count;
-    // 对每个岛屿进行标记
-    int mark;
-    // 定义二维数组表示四个方位
-    int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    // DFS 进行搜索，将每个岛屿标记为不同的数字
+    int count; // 记录每次每个岛屿的面积
+    int mark;  // 对每个岛屿进行标记
+    int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}}; // 定义二维数组表示四个方位
+    /**
+     * DFS 进行搜索，将每个岛屿标记为不同的数字
+     */
     public void dfs_visited(int[][] grid, int x, int y, boolean[][] visited) {
-        // 当遇到边界，直接return
-        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) {// 当遇到边界，直接return
             return;
         }
-        // 遇到已经访问过的或者遇到海水，直接返回
-        if (visited[x][y] || grid[x][y] == 0){
+        if (visited[x][y] || grid[x][y] == 0){ // 遇到已经访问过的或者遇到海水，直接返回
             return;
         }
         visited[x][y] = true;
         count++;
         grid[x][y] = mark;
-        // 继续向下层搜索
-        dfs_visited(grid, x, y + 1, visited);
+
+        dfs_visited(grid, x, y + 1, visited);  // 继续向下层搜索
         dfs_visited(grid, x, y - 1, visited);
         dfs_visited(grid, x + 1, y, visited);
         dfs_visited(grid, x - 1, y, visited);
