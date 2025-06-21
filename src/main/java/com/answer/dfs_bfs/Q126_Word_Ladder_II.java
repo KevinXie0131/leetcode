ee279785966b5f66dba34bc379d21298c884c8f5
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-21 10:44:52 -0400

    update

--- src/main/java/com/answer/dfs_bfs/Q126_Word_Ladder_II.java
+++ src/main/java/com/answer/dfs_bfs/Q126_Word_Ladder_II.java
@@ -1,4 +1,19 @@
 package com.answer.dfs_bfs;
 
-public class Q126_Word_Ladder_II {  // Hard 困难
+public class Q126_Word_Ladder_II { // Hard 困难
+    /**
+     * 单词接龙 II
+     * 按字典 wordList 完成从单词 beginWord 到单词 endWord 转化(transformation sequence)，一个表示此过程的 转换序列 是形式上像 beginWord -> s1 -> s2 -> ... -> sk 这样的单词序列，并满足：
+     *  每对相邻(adjacent pair)的单词之间仅有单个字母不同。
+     *  转换过程中的每个单词 si（1 <= i <= k）必须是字典 wordList 中的单词。注意，beginWord 不必是字典 wordList 中的单词。
+     *  sk == endWord
+     * 给你两个单词 beginWord 和 endWord ，以及一个字典 wordList 。请你找出并返回所有从 beginWord 到 endWord 的 最短转换序列(shortest transformation sequences)，
+     * 如果不存在这样的转换序列，返回一个空列表。每个序列都应该以单词列表 [beginWord, s1, s2, ..., sk] 的形式返回。
+     *
+     * 输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+     * 输出：[["hit","hot","dot","dog","cog"],["hit","hot","lot","log","cog"]]
+     * 解释：存在 2 种最短的转换序列：
+     *      "hit" -> "hot" -> "dot" -> "dog" -> "cog"
+     *      "hit" -> "hot" -> "lot" -> "log" -> "cog"
+     */
 }
