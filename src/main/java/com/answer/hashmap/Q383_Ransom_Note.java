f4b9848ca439826e0e72fa357ac1cc531a75c557
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-23 21:57:29 -0400

    additional

--- src/main/java/com/answer/hashmap/Q383_Ransom_Note.java
+++ src/main/java/com/answer/hashmap/Q383_Ransom_Note.java
@@ -1,7 +1,24 @@
 package com.answer.hashmap;
 
 public class Q383_Ransom_Note {
+    /**
+     * 赎金信
+     * 给你两个字符串：ransomNote 和 magazine ，判断 ransomNote 能不能由 magazine 里面的字符构成。
+     * 如果可以，返回 true ；否则返回 false 。
+     * Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+     * Each letter in magazine can only be used once in ransomNote.
+     * magazine 中的每个字符只能在 ransomNote 中使用一次。
 
+     * 示例 1：
+     * 输入：ransomNote = "a", magazine = "b"
+     * 输出：false
+     * 示例 2：
+     * 输入：ransomNote = "aa", magazine = "ab"
+     * 输出：false
+     * 示例 3：
+     * 输入：ransomNote = "aa", magazine = "aab"
+     * 输出：true
+     */
     public boolean canConstruct(String ransomNote, String magazine) {
         // shortcut
         if (ransomNote.length() > magazine.length()) {
