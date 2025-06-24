b2848cb11d3735df10a6acd7258af5ba87493f46
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-23 21:12:46 -0400

    additional

--- src/main/java/com/answer/hashmap/Q16_3Sum_Closest.java
+++ src/main/java/com/answer/hashmap/Q16_3Sum_Closest.java
@@ -3,7 +3,20 @@ package com.answer.hashmap;
 import java.util.*;
 
 public class Q16_3Sum_Closest {
-
+    /**
+     * 最接近的三数之和
+     * 给你一个长度为 n 的整数数组 nums 和 一个目标值 target。请你从 nums 中选出三个整数，使它们的和与 target 最接近。
+     * 返回这三个数的和。
+     * 假定每组输入只存在恰好一个解。
+     * Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+     * Return the sum of the three integers.
+     * You may assume that each input would have exactly one solution.
+     *
+     * 示例 1：
+     *  输入：nums = [-1,2,1,-4], target = 1
+     *  输出：2
+     *  解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2)。
+     */
     public static void main(String[] args) {
         int[] nums = {0,1,2};
         int target = 3;
