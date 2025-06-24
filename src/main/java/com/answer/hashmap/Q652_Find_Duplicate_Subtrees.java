f13cfe5af08df5687c75073b0583a0cef3f50610
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-23 21:59:52 -0400

    additional

---
+++ src/main/java/com/answer/hashmap/Q652_Find_Duplicate_Subtrees.java
@@ -0,0 +1,17 @@
+package com.answer.hashmap;
+
+public class Q652_Find_Duplicate_Subtrees {
+    /**
+     * 寻找重复的子树
+     * 给你一棵二叉树的根节点 root ，返回所有 重复的子树 。
+     * 对于同一类的重复子树，你只需要返回其中任意 一棵 的根结点即可。
+     * 如果两棵树具有 相同的结构 和 相同的结点值 ，则认为二者是 重复 的。
+     * Given the root of a binary tree, return all duplicate subtrees.
+     * For each kind of duplicate subtrees, you only need to return the root node of any one of them.
+     * Two trees are duplicate if they have the same structure with the same node values.
+     *
+     * 示例 1：
+     * 输入：root = [1,2,3,4,null,2,4,null,null,4]
+     * 输出：[[2,4],[4]]
+     */
+}
