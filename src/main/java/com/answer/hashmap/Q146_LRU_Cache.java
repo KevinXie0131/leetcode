419f2e0ed19b096b310fb4a81d3915cbf5891247
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-23 21:20:02 -0400

    additional

--- src/main/java/com/answer/hashmap/Q146_LRU_Cache.java
+++ src/main/java/com/answer/hashmap/Q146_LRU_Cache.java
@@ -4,6 +4,31 @@ import java.util.*;
 
 public class Q146_LRU_Cache {
     /**
+     * LRU 缓存 Least Recently Used (LRU) cache.
+     * 请你设计并实现一个满足  LRU (最近最少使用) 缓存 约束的数据结构。
+     * 实现 LRUCache 类：
+     *  LRUCache(int capacity) 以 正整数 作为容量 capacity 初始化 LRU 缓存
+     *  int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+     *  void put(int key, int value) 如果关键字 key 已经存在，则变更其数据值 value ；如果不存在，则向缓存中插入该组 key-value 。如果插入操作导致关键字数量超过 capacity ，则应该 逐出 最久未使用的关键字。
+     * 函数 get 和 put 必须以 O(1) 的平均时间复杂度运行。
+     *
+     * 示例：
+     * 输入 ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+     *      [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+     * 输出 [null, null, null, 1, null, -1, null, -1, 3, 4]
+     * 解释
+     * LRUCache lRUCache = new LRUCache(2);
+     * lRUCache.put(1, 1); // 缓存是 {1=1}
+     * lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+     * lRUCache.get(1);    // 返回 1
+     * lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+     * lRUCache.get(2);    // 返回 -1 (未找到)
+     * lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+     * lRUCache.get(1);    // 返回 -1 (未找到)
+     * lRUCache.get(3);    // 返回 3
+     * lRUCache.get(4);    // 返回 4
+     */
+    /**
      * 方法一：哈希表 + 双向链表
      *  双向链表按照被使用的顺序存储了这些键值对，靠近头部的键值对是最近使用的，而靠近尾部的键值对是最久未使用的。
      *  哈希表即为普通的哈希映射（HashMap），通过缓存数据的键映射到其在双向链表中的位置。
--- src/main/java/com/answer/hashmap/Q170_Two_Sum_III_Data_structure_design.java
+++ src/main/java/com/answer/hashmap/Q170_Two_Sum_III_Data_structure_design.java
@@ -1,10 +1,32 @@
 package com.answer.hashmap;
 
-
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Q170_Two_Sum_III_Data_structure_design {
+    /**
+     * 两数之和 III - 数据结构设计
+     * 设计并实现一种数据结构，支持以下操作：
+     *  add(number)：向数据结构添加一个数 number。
+     *  find(value)：查找是否存在任意两数之和等于 value，如果存在返回 true，否则返回 false。
+     * 请实现 TwoSum 类：
+     *  TwoSum() 初始化 TwoSum 对象，数据结构为空。
+     *  void add(int number) 向数据结构添加一个数 number。
+     *  boolean find(int value) 如果存在任意两个数的和等于 value，返回 true，否则返回 false。
+     *
+     * Design and implement a TwoSum class. It should support the following operations: add and find.
+     *  add - Add the number to an internal data structure.
+     *  find - Find if there exists any pair of numbers which sum is equal to the value.
+     *
+     * Example 1:
+     *  add(1); add(3); add(5);
+     *  find(4) -> true
+     *  find(7) -> false
+     * Example 2:
+     *  add(3); add(1); add(2);
+     *  find(3) -> true
+     *  find(6) -> false
+     */
     /**
      * Your TwoSum object will be instantiated and called as such:
      * TwoSum obj = new TwoSum();
