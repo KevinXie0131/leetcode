5130d298d82cec9e90048ce561f861193c929145
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-23 22:03:09 -0400

    additional

---
+++ src/main/java/com/answer/hashmap/Q771_Jewels_and_Stones.java
@@ -0,0 +1,18 @@
+package com.answer.hashmap;
+
+public class Q771_Jewels_and_Stones {
+    /**
+     * 宝石与石头
+     * 给你一个字符串 jewels 代表石头中宝石的类型，另有一个字符串 stones 代表你拥有的石头。 stones 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+     * 字母区分大小写，因此 "a" 和 "A" 是不同类型的石头。
+     * You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+     * Letters are case sensitive, so "a" is considered a different type of stone from "A".
+     *
+     * 示例 1：
+     * 输入：jewels = "aA", stones = "aAAbbbb"
+     * 输出：3
+     * 示例 2：
+     * 输入：jewels = "z", stones = "ZZ"
+     * 输出：0
+     */
+}
