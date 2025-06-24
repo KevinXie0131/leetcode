f860990a32758e5238996cdce736d0aee37f43b7
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-23 21:50:41 -0400

    additional

--- src/main/java/com/answer/hashmap/Q349_Intersection_of_Two_Arrays.java
+++ src/main/java/com/answer/hashmap/Q349_Intersection_of_Two_Arrays.java
@@ -4,7 +4,18 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 public class Q349_Intersection_of_Two_Arrays {
-
+    /**
+     * 两个数组的交集
+     * 给定两个数组 nums1 和 nums2 ，返回 它们的 交集 。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
+     * Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+     * 示例 1：
+     * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+     * 输出：[2]
+     * 示例 2：
+     * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+     * 输出：[9,4]
+     * 解释：[4,9] 也是可通过的
+     */
     public static void main(String[] args) {
 /*        int[] nums1 = {4,9,5};
         int[] nums2 = {9,4,9,8,4};*/
