0df53f79592252bdf9cd124a2c03522e761e20e9
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-06-23 20:07:00 -0400

    additional

---
+++ src/main/java/com/answer/linkedlist/Q61_Rotate_List.java
@@ -0,0 +1,15 @@
+package com.answer.linkedlist;
+
+public class Q61_Rotate_List {
+    /**
+     * 旋转链表
+     * 一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。
+     * Given the head of a linked list, rotate the list to the right by k places.
+     * 示例 1：
+     *  输入：head = [1,2,3,4,5], k = 2
+     *  输出：[4,5,1,2,3]
+     * 示例 2：
+     *  输入：head = [0,1,2], k = 4
+     *  输出：[2,0,1]
+     */
+}
