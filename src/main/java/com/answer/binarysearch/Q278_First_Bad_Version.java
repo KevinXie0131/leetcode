698b5f17ee7c17a3f8b3d8520b494b81a89445d0
KevinX <Xzb04720638k>
2025-06-25 22:29:28 -0400

    update

--- src/main/java/com/answer/binarysearch/Q278_First_Bad_Version.java
+++ src/main/java/com/answer/binarysearch/Q278_First_Bad_Version.java
@@ -19,21 +19,51 @@ public class Q278_First_Bad_Version {
      *       调用 isBadVersion(4) -> true
      *       所以，4 是第一个错误的版本。
      */
+    /**
+     * 根据题目描述 “错误的版本之后的所有版本都是错的” ，说明给定的版本正确性列表是「有序的」，即以某个版本为分界点，左边（右边）都是正确（错误）版本。
+     * 因此，考虑使用「二分查找」来查找首个错误版本。
+     */
     public int firstBadVersion(int n) {
         int left = 1, right = n;
 
         while(left <= right){
             int mid = (left + right) >>> 1;
-            boolean isBad = isBadVersion(mid);
-            if(isBad){
+            boolean isBadVersion = isBadVersion(mid);
+            if(isBadVersion){
+                right = mid - 1;  // 若 mid 是错误版本，则最后一个正确版本一定在闭区间 [left, mid - 1]
+            } else {
+                left = mid + 1;   // 若 mid 是正确版本，则首个错误版本一定在闭区间 [mid + 1, right]
+            }
+        }
+        /**
+         * 出现序列false false false false true true true true true true true
+         * 退出循环的时候，是left == right + 1
+         * left 向右偏移一个单位，极端情况下，会让false发生质的变化，变为true
+         * right 向左偏移一个单位，极端情况下，会让true发生质的变化，变为false
+         * 所以说，我们这里只能return left;
+         */
+        // left 指向首个错误版本，right 指向最后一个正确版本
+        return left;
+        // return right + 1; // works too
+    }
+    /**
+     * 关于return方面，不想纠结return哪一个，我们就引入第三个变量
+     */
+    public int firstBadVersion_0(int n) {
+        int left = 1, right = n;
+        int res = 1; // 这第三个变量始终接受我们需要的值
+        while(left <= right){
+            int mid = (left + right) >>> 1;
+            boolean isBadVersion = isBadVersion(mid);
+            if(isBadVersion){
+                res = mid;
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return left;
+        return res;
     }
-
     /**
      * Another form
      */
@@ -49,10 +79,8 @@ public class Q278_First_Bad_Version {
                 left = mid + 1;
             }
         }
-
         return left;
     }
-
     /**
      * Template II
      */
