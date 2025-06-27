1a6e063e96ec91198209213388130e24216d80bf
KevinX <Xzb04720638k>
2025-06-26 21:53:01 -0400

    update

--- src/main/java/com/answer/binarysearch/Q240_Search_a_2D_Matrix_II.java
+++ src/main/java/com/answer/binarysearch/Q240_Search_a_2D_Matrix_II.java
@@ -39,7 +39,31 @@ public class Q240_Search_a_2D_Matrix_II {
         return false;
     }
     /**
+     * 矩阵的每行从左到右是升序， 每列从上到下也是升序，在矩阵中查找某个数。
+     * Z 字形查找
+     * 若当前矩阵的元素值 > target，则向上移动一行列不变，即 matrix[i][j] 变为 matrix[i - 1][j]，继续进行比较。
+     * 若当前矩阵的元素值 < target，则向右移动一列行不变，即 matrix[i][j] 变为 matrix[i][j + 1]，继续进行比较。
+     *
+     * 时间复杂度：O(m+n)
+     */
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int x = 0, y = n - 1; // 从右上角开始
+        while (x < m && y >= 0) { // 还有剩余元素
+            if (matrix[x][y] == target) { // 找到 target
+                return true;
+            }
+            if (matrix[x][y] > target) {// 这一行剩余元素全部大于 target，排除
+                --y; // 如果 matrix[x,y]>target，由于每一列的元素都是升序排列的，那么在当前的搜索矩阵中，所有位于第 y 列的元素都是严格大于 target 的，因此我们可以将它们全部忽略，即将 y 减少 1；
+            } else {// 这一列剩余元素全部小于 target，排除
+                ++x; // 如果 matrix[x,y]<target，由于每一行的元素都是升序排列的，那么在当前的搜索矩阵中，所有位于第 x 行的元素都是严格小于 target 的，因此我们可以将它们全部忽略，即将 x 增加 1。
+            }
+        }
+        return false;
+    }
+    /**
      * Binary Search
+     * 时间复杂度：O(mlogn)
      */
     public boolean searchMatrix(int[][] matrix, int target) {
         for(int i = 0; i < matrix.length; i++){
@@ -53,6 +77,7 @@ public class Q240_Search_a_2D_Matrix_II {
         }
         return false;
     }
+
     public int binarySearch(int[] num, int target) {
         int left = 0, right = num.length - 1;
 
@@ -70,4 +95,5 @@ public class Q240_Search_a_2D_Matrix_II {
         }
         return -1;
     }
+
 }
