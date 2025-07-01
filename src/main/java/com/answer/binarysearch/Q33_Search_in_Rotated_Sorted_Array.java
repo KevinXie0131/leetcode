954b8ef017debde66a150dd7c79f6151fbacad92
KevinX <Xzb04720638k>
2025-07-01 16:03:05 -0400

    update

--- src/main/java/com/answer/binarysearch/Q33_Search_in_Rotated_Sorted_Array.java
+++ src/main/java/com/answer/binarysearch/Q33_Search_in_Rotated_Sorted_Array.java
@@ -22,6 +22,83 @@ public class Q33_Search_in_Rotated_Sorted_Array {
         System.out.println(res);
     }
     /**
+     * Binary search 二分查找
+     * 对于有序数组，可以使用二分查找的方法查找元素。
+     * 但是这道题中，数组本身不是有序的，进行旋转后只保证了数组的局部是有序的，这还能进行二分查找吗？答案是可以的。
+     * 本题核心在于：对于任意一个index，其左区间和右区间至少有一个是有序的，那么就可以根据这个区间的最大值和最小值来判断Target是否在该区间内
+     */
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            //前半部分有序,注意此处用小于等于
+            if (nums[start] <= nums[mid]) {  // start 到 mid 是顺序区间
+                // target在前半部分
+                // 如果target在左区间，则在左区间二分
+                // 如果target在右区间(乱序区间)，start=mid+1，则能转移到右区间
+                if (nums[start] <= target && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {                 // mid 到 end 是顺序区间
+                if ( nums[mid] < target && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+    /**
+     * 简单的一次二分，但是没有合并相同的分支，易于理解
+     *
+     * 根据mid来切分成左右数组
+     * 分别对左右数组处理
+     *
+     * 对左数组
+     *  target在范围里 right = mid-1
+     *  不在范围里,说明在右数组 left = mid+1;
+     * 对右数组
+     *  在范围里，left = mid+1;
+     *  不在范围,说明在左数组 right = mid-1
+     */
+    public int search2(int[] nums, int target) {
+        int n = nums.length;
+        int end = nums[n - 1];
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target <= end && nums[mid] > end) {
+                // 目标在右半部分，mid在左半部分
+                left = mid + 1;
+            } else if (target > end && nums[mid] <= end) {
+                // 目标在左半部分，mid在右半部分
+                right = mid - 1;
+            } else {
+                // 目标和mid在同一部分
+                if (nums[mid] < target) {
+                    left = mid + 1;
+                } else if(nums[mid] > target){
+                    right = mid - 1;
+                } else {
+                    return mid;
+                }
+            }
+        }
+        return nums[left] != target ? -1 : left;
+    }
+    /**
      * From 睡不醒的鲤鱼
      * 对于旋转数组，我们无法直接根据 nums[mid] 与 target 的大小关系来判断 target 是在 mid 的左边还是右边，因此需要「分段讨论」。
      *  1.通过二分找到数组旋转点；
@@ -68,40 +145,5 @@ public class Q33_Search_in_Rotated_Sorted_Array {
          * }
          * return -1;
          */
-    }
-    /**
-     * Binary search
-     */
-    public int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
-            return -1;
-        }
-        int start = 0;
-        int end = nums.length - 1;
-        int mid;
-        while (start <= end) {
-            mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            }
-            //前半部分有序,注意此处用小于等于
-            if (nums[start] <= nums[mid]) {
-                //target在前半部分
-                if (target >= nums[start] && target < nums[mid]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            } else {
-                if (target <= nums[end] && target > nums[mid]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
-            }
-
-        }
-        return -1;
-
     }
 }
