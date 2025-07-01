ec4cc17f0dc331750006e5199e62a72b0d42a26f
KevinX <Xzb04720638k>
2025-07-01 15:02:19 -0400

    update

--- src/main/java/com/answer/priorityqueue/Q1046_Last_Stone_Weight.java
+++ src/main/java/com/answer/priorityqueue/Q1046_Last_Stone_Weight.java
@@ -1,5 +1,7 @@
 package com.answer.priorityqueue;
 
+import java.util.PriorityQueue;
+
 public class Q1046_Last_Stone_Weight {
     /**
      * 最后一块石头的重量
@@ -22,6 +24,37 @@ public class Q1046_Last_Stone_Weight {
      *      再选出 2 和 4，得到 2，所以数组转换为 [2,1,1,1]，
      *      接着是 2 和 1，得到 1，所以数组转换为 [1,1,1]，
      *      最后选出 1 和 1，得到 0，最终数组转换为 [1]，这就是最后剩下那块石头的重量。
-     *
      */
+    /**
+     * 最大堆
+     * 时间复杂度：O(nlogn)，其中 n 是石头数量。每次从队列中取出元素需要花费 O(logn) 的时间，最多共需要粉碎 n−1 次石头。
+     * 空间复杂度：O(n)
+     */
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
+        for(int stone : stones){
+            queue.offer(stone);
+        }
+        while(queue.size() >= 2){ // 直到剩下的石头少于 2 块
+            int x = queue.poll(); // 每次依次从队列中取出最重的两块石头
+            int y = queue.poll();
+            int z = Math.abs(x - y);
+            if(z != 0){ // 如果 a>b，则将新石头 a−b 放回到最大堆中；如果 a=b，两块石头完全被粉碎，因此不会产生新的石头
+                queue.offer(z);
+            }
+        }
+        if(queue.size() == 1){ // 最终可能剩下 1 块石头，该石头的重量即为最大堆中剩下的元素，返回该元素
+            return queue.poll();
+        } else { // 也可能没有石头剩下，此时最大堆为空，返回 0。
+            return 0;
+        }
+/*        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+            if (a > b) {
+                pq.offer(a - b);
+            }
+        }
+        return pq.isEmpty() ? 0 : pq.poll(); */
+    }
 }
