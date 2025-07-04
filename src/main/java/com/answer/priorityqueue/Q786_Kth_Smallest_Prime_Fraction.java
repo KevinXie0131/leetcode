c18e447fb967a330f09a36fd1abc8c752d8d910a
KevinX <Xzb04720638k>
2025-07-03 20:22:28 -0400

    update

--- src/main/java/com/answer/priorityqueue/Q786_Kth_Smallest_Prime_Fraction.java
+++ src/main/java/com/answer/priorityqueue/Q786_Kth_Smallest_Prime_Fraction.java
@@ -4,6 +4,10 @@ import java.util.*;
 
 public class Q786_Kth_Smallest_Prime_Fraction {
     /**
+     * https://leetcode.cn/problems/k-th-smallest-prime-fraction/solutions/1609883/by-lfool-1q1d/
+     * 多路归并技巧总结
+     */
+    /**
      * 第 K 个最小的质数分数
      * 给你一个按递增顺序排序的数组 arr 和一个整数 k 。数组 arr 由 1 和若干 质数 组成，且其中所有整数互不相同。
      * You are given a sorted integer array arr containing 1 and prime numbers, where all the integers of arr are unique. You are also given an integer k.
@@ -21,16 +25,83 @@ public class Q786_Kth_Smallest_Prime_Fraction {
      * 示例 2：
      * 输入：arr = [1,7], k = 1
      * 输出：[1,7]
-     *
      */
     public static void main(String[] args) {
         int[] arr = {1,2,3,5};
         int k = 3;
         System.out.println(Arrays.toString(kthSmallestPrimeFraction(arr, k)));
     }
-
     /**
-     * Approach #2: Heap - 多路归并
+     * 「扫描点对」+「优先队列（堆）」
+     */
+    public int[] kthSmallestPrimeFraction0(int[] arr, int k) {
+        int n = arr.length;
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> Double.compare(b[0] * 1.0 / b[1], a[0] * 1.0 / a[1])); // 大根堆
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                double t = arr[i] * 1.0 / arr[j];
+                if (q.size() < k || q.peek()[0] * 1.0 / q.peek()[1] > t) { // 如果当前计算值比堆顶元素小，那么堆顶元素不可能是第 k 小的值，使用当前计算值置换掉堆顶元素。
+                    if (q.size() == k) {
+                        q.poll();
+                    }
+                    q.add(new int[]{arr[i], arr[j]});
+                }
+            }
+        }
+        return q.poll();
+    }
+    /**
+     * 多路归并」+「优先队列（堆）
+     * 利用「数组内元素严格单调递增」的特性, 使用「优先队列（堆）」来维护多个有序序列的当前头部的最小值即可。
+     */
+    public int[] kthSmallestPrimeFraction_0(int[] arr, int k) {
+        int n = arr.length;
+        PriorityQueue<int[]> q = new PriorityQueue<>((i, j) -> { // 小顶堆
+            double i1 = arr[i[0]] * 1.0 / arr[i[1]];
+            double i2 = arr[j[0]] * 1.0 / arr[j[1]];
+            return Double.compare(i1, i2);
+        });
+        for (int i = 1; i < n; i++) {
+            q.add(new int[]{0, i});
+        }
+        // 我们首先将每个分类中最小的分数入队，优先队列底层是一个小顶堆，顶部元素最小；然后出队最小的分数，
+        // 将和该分数分母一样但分子更大的下一个分数入队，这样可以保证每次出队的元素一定是最小的；进行k-1次操作后；
+        // 优先队列首部就是最小的第k个元素了。
+        while (--k > 0) {
+            int[] poll = q.poll();
+            int i = poll[0], j = poll[1];
+            if (i + 1 < j) {  // 分子下标不能超过分母
+                q.add(new int[]{i + 1, j});
+            }
+        }
+        int[] poll = q.poll();
+        return new int[]{arr[poll[0]], arr[poll[1]]};
+    }
+    /**
+     * another form
+     */
+    public int[] kthSmallestPrimeFraction7(int[] arr, int k) {
+        // 定义比较规则
+        Queue<int[]> q = new PriorityQueue<>((a, b) -> arr[a[0]] * arr[b[1]] - arr[b[0]] * arr[a[1]]);
+        // 加入头节点
+        for (int j = 1; j < arr.length; j++) {
+            q.offer(new int[]{0, j});
+        }
+        for (int cnt = 1; cnt <= k; cnt++) {
+            // 弹出队顶元素，即最小元素
+            int[] cur = q.poll();
+            int i = cur[0], j = cur[1];
+            if (cnt == k) {
+                return new int[]{arr[i], arr[j]};
+            }
+            if (i + 1 < j) {// 分子下标不能超过分母
+                q.offer(new int[]{i + 1, j});
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    /**
+     * Approach #2: Heap - 多路归并/ 优先队列
      * Time: O(klogn)
      * Space: O(n)
      */
@@ -58,7 +129,10 @@ public class Q786_Kth_Smallest_Prime_Fraction {
         return new int[]{arr[heap.peek()[0]], arr[heap.peek()[1]]};
     }
     /**
-     * Sorting
+     * Sorting 自定义排序
+     * 比较两个分数时，我们可直接对它们的值进行比较，会引入浮点数误差. 一种可行的替代方法是用：a×d<b×c来替代
+     * 时间复杂度：O(n^2 logn)
+     * 空间复杂度：O(n^2)
      */
     public int[] kthSmallestPrimeFraction_1(int[] arr, int k) {
         int m = arr.length;
@@ -68,8 +142,37 @@ public class Q786_Kth_Smallest_Prime_Fraction {
                 frac.add(new int[]{arr[i], arr[j]});
             }
         }
-        Collections.sort(frac, (x,y)->x[0]*y[1] -x[1]*y[0]);
+        Collections.sort(frac, (x, y) -> x[0] * y[1] - x[1] * y[0]);
         return frac.get(k-1);
+    }
+    /**
+     * 二分查找 + 双指针
+     */
+    public int[] kthSmallestPrimeFraction6(int[] arr, int k) {
+        double l = 0, r = 1;
+        while(true){
+            double expectedFraction = (l + r) / 2;//在0-1.0取数，统计数组元素组合成的分数中小于expectedFraction个数
+            int cnt = 0;//记录分数中小于expectedFraction个数
+            int x = 1, y = arr[arr.length - 1];//分别记录分子和分母，当前x/y记录的就是最小的分数，因为x最小，y最大
+            for(int j = 1; j < arr.length; j++){//分母下标从1开始遍历
+                int i = 0;//分子下标从0开始
+                while ((double)arr[i] / (double)arr[j] <= expectedFraction){//分子/分母 小于expectedFraction，记录
+                    i++;//分子下标右移
+                    if((double)x / y < (double)arr[i - 1] / arr[j]){//最新的分数大于原来记录的分数，更新
+                        x = arr[i - 1];
+                        y = arr[j];
+                    }
+                }
+                cnt += i;//i对应的是符合条件的分数个数（i从0开始，i == 2结束，说明只有0，1俩下标也就是2个对应分子符合条件）
+            }
+            if(cnt == k){//小于expectionFraction的个数
+                return new int[]{x, y};//最大的分数就是对应的第k小的个数
+            }else if(cnt < k){//个数少了，将expectedFraction再扩大点，接下来的范围在(expectedFraction, r)选
+                l = expectedFraction;
+            }else {//个数多了，将expectedFraction再扩大点，接下来的范围在(l, expectedFraction)选
+                r = expectedFraction;
+            }
+        }
     }
     /**
      * Approach #1: Binary Search
