79b168b24ac7a29e385875cdc6916cc2d3a98614
KevinX <Xzb04720638k>
2025-07-03 22:09:34 -0400

    update

--- src/main/java/com/answer/priorityqueue/Q692_Top_K_Frequent_Words.java
+++ src/main/java/com/answer/priorityqueue/Q692_Top_K_Frequent_Words.java
@@ -34,7 +34,7 @@ public class Q692_Top_K_Frequent_Words {
         System.out.println(list);
     }
     /**
-     * Use Hashmap
+     * Use Hashmap 哈希表 + 排序
      */
     public List<String> topKFrequent(String[] words, int k) {
         Map<String, Integer> map = new TreeMap<String, Integer>();
@@ -47,13 +47,15 @@ public class Q692_Top_K_Frequent_Words {
         for(Map.Entry<String, Integer> entry : map.entrySet()){
             list.add(entry.getKey());
         }
+        // 排序时，如果两个字符串出现频率相同，那么我们让两字符串中字典序较小的排在前面，否则我们让出现频率较高的排在前面。
+        Collections.sort(list, (o1, o2) -> map.get(o1) == map.get(o2) ? o1.compareTo(o2) : map.get(o2) - map.get(o1));
 
-        Collections.sort(list, new Comparator<String>() {
+      /*  Collections.sort(list, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
                 return map.get(o1) == map.get(o2) ? o1.compareTo(o2) : map.get(o2) - map.get(o1);
             }
-        });
+        });*/
 /*        candidates.sort((a, b) -> {
             // 字符串频率相等按照字典序比较使得大的在堆顶,Java 可以直接使用 compareTo 方法即可。
             if (count.get(a).equals(count.get(b))) {
@@ -63,10 +65,10 @@ public class Q692_Top_K_Frequent_Words {
                 return count.get(b) - count.get(a);
             }
         });*/
-        return list.subList(0, k);
+        return list.subList(0, k); // 只需要保留序列中的前 k 个字符串即可
     }
     /**
-     * Use PriorityQueue - Min heap
+     * Use PriorityQueue 优先队列 - Min heap
      */
     public static List<String> topKFrequent_1(String[] words, int k) {
         Map<String, Integer> map = new TreeMap<String, Integer>();
@@ -75,15 +77,14 @@ public class Q692_Top_K_Frequent_Words {
         for(String s : words){
             map.put(s, map.getOrDefault(s, 0) + 1);
         }
-        /**
-         * Min Heap
-         */
-        PriorityQueue<Map.Entry<String, Integer>> queue = new PriorityQueue<>(new Comparator<Map.Entry<String, Integer>>() {
+        // 小顶堆
+        PriorityQueue<Map.Entry<String, Integer>> queue = new PriorityQueue<>((o1, o2) -> o1.getValue() == o2.getValue() ? o2.getKey().compareTo(o1.getKey()) : o1.getValue() - o2.getValue());
+        /*PriorityQueue<Map.Entry<String, Integer>> queue = new PriorityQueue<>(new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
                 return o1.getValue() == o2.getValue() ? o2.getKey().compareTo(o1.getKey()) : o1.getValue() - o2.getValue();
             }
-        });
+        });*/
         for(Map.Entry<String, Integer> entry : map.entrySet()){
             /*queue.offer(entry);
             if(queue.size() > k){
@@ -114,15 +115,14 @@ public class Q692_Top_K_Frequent_Words {
         for(String s : words){
             map.put(s, map.getOrDefault(s, 0) + 1);
         }
-        /**
-         * Max Heap
-         */
-        Queue<Map.Entry<String, Integer>> queue = new PriorityQueue<>(new Comparator<Map.Entry<String, Integer>>() {
+        // 大顶堆
+        Queue<Map.Entry<String, Integer>> queue = new PriorityQueue<>((o1, o2) -> o1.getValue() == o2.getValue() ? o1.getKey().compareTo(o2.getKey()) : o2.getValue() - o1.getValue());
+     /*   Queue<Map.Entry<String, Integer>> queue = new PriorityQueue<>(new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
                 return o1.getValue() == o2.getValue() ? o1.getKey().compareTo(o2.getKey()) : o2.getValue() - o1.getValue();
             }
-        });
+        });*/
         for(Map.Entry<String, Integer> entry : map.entrySet()){
             queue.offer(entry);
         }
