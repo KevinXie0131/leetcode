d668f4e7b0e38cc2bf73958ffe76cd2fdf930db2
KevinX <Xzb04720638k>
2025-07-09 21:29:31 -0400

    update

--- src/main/java/com/answer/hashmap/Q259_3Sum_Smaller.java
+++ src/main/java/com/answer/hashmap/Q259_3Sum_Smaller.java
@@ -13,7 +13,7 @@ public class Q259_3Sum_Smaller {
      * Explanation: Because there are two triplets which sums are less than 2:
      *              [-2,0,1]
      *              [-2,0,3]
-     * Follow up: Could you solve it in O(n2) runtime?
+     * Follow up: Could you solve it in O(n^2) runtime?
      */
     public static void main(String[] args) {
         int[] nums = {-2,0,1,3};
@@ -21,19 +21,24 @@ public class Q259_3Sum_Smaller {
         int res = threeSumSmaller(nums, target);
         System.out.println(res);
     }
-
+    /**
+     * 双指针
+     * 如果三数之和小于target，此时就找到了(k - j)个满足要求的三元组，然后j右移以指向下一个数
+     * 比如此时i, j, k分别为1, 2, 5，那么显然[(1, 2, 3), (1, 2, 4), (1, 2, 5)]都是满足要求的三元组
+     * 因此count自增3；并且j自增1，这样下次while循环将会判断三元组(1, 3, 5)是否满足要求
+     */
     public static int threeSumSmaller(int[] nums, int target) {
         int count = 0;
         Arrays.sort(nums);
         int n = nums.length;
 
-        for(int i = 0; i < n-2; i++){
+        for(int i = 0; i < n- 2; i++){
             int left = i + 1;
             int right = n -1;
             while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
                 if(sum < target){
-                    count += right - left;
+                    count += right - left; // right - left个组合都是小于目标数的
                     left++;
                 } else {
                     right--;
