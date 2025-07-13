c8a1a9a09e9210c5a86f8d03a0bf1c196fca35da
KevinX <Xzb04720638k>
2025-07-13 19:54:07 -0400

    update

--- src/main/java/com/answer/hashmap/Q706_Design_HashMap.java
+++ src/main/java/com/answer/hashmap/Q706_Design_HashMap.java
@@ -1,7 +1,6 @@
 package com.answer.hashmap;
 
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Q706_Design_HashMap {
     /**
@@ -18,18 +17,19 @@ public class Q706_Design_HashMap {
      * 输入：["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
      *       [[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
      * 输出：[null, null, null, 1, -1, null, 1, null, -1]
-     * 解释：
-     * MyHashMap myHashMap = new MyHashMap();
-     * myHashMap.put(1, 1); // myHashMap 现在为 [[1,1]]
-     * myHashMap.put(2, 2); // myHashMap 现在为 [[1,1], [2,2]]
-     * myHashMap.get(1);    // 返回 1 ，myHashMap 现在为 [[1,1], [2,2]]
-     * myHashMap.get(3);    // 返回 -1（未找到），myHashMap 现在为 [[1,1], [2,2]]
-     * myHashMap.put(2, 1); // myHashMap 现在为 [[1,1], [2,1]]（更新已有的值）
-     * myHashMap.get(2);    // 返回 1 ，myHashMap 现在为 [[1,1], [2,1]]
-     * myHashMap.remove(2); // 删除键为 2 的数据，myHashMap 现在为 [[1,1]]
-     * myHashMap.get(2);    // 返回 -1（未找到），myHashMap 现在为 [[1,1]]
+     * 解释：MyHashMap myHashMap = new MyHashMap();
+     *      myHashMap.put(1, 1); // myHashMap 现在为 [[1,1]]
+     *      myHashMap.put(2, 2); // myHashMap 现在为 [[1,1], [2,2]]
+     *      myHashMap.get(1);    // 返回 1 ，myHashMap 现在为 [[1,1], [2,2]]
+     *      myHashMap.get(3);    // 返回 -1（未找到），myHashMap 现在为 [[1,1], [2,2]]
+     *      myHashMap.put(2, 1); // myHashMap 现在为 [[1,1], [2,1]]（更新已有的值）
+     *      myHashMap.get(2);    // 返回 1 ，myHashMap 现在为 [[1,1], [2,1]]
+     *      myHashMap.remove(2); // 删除键为 2 的数据，myHashMap 现在为 [[1,1]]
+     *      myHashMap.get(2);    // 返回 -1（未找到），myHashMap 现在为 [[1,1]]
      */
     /**
+     * 链地址法
+     * refer to Q705_Design_HashSet
      * 1). hash function design and 2). collision handling
      */
     private class Pair {
@@ -69,12 +69,12 @@ public class Q706_Design_HashMap {
         Iterator<Pair> iterator = data[h].iterator();
         while(iterator.hasNext()){
             Pair pair = iterator.next();
-            if(pair.getKey() == key){
+            if(pair.getKey() == key){ // key已存在，更新value
                 pair.setValue(value);
                 return;
             }
         }
-        data[h].offerLast(new Pair(key, value));
+        data[h].offerLast(new Pair(key, value)); // key不存在，新建一个
     }
 
     public int get(int key) {
@@ -82,11 +82,11 @@ public class Q706_Design_HashMap {
         Iterator<Pair> iterator = data[h].iterator();
         while(iterator.hasNext()){
             Pair pair = iterator.next();
-            if(pair.getKey() == key){
+            if(pair.getKey() == key){ // key存在，返回value
                 return pair.value;
             }
         }
-        return -1;
+        return -1; // key不存在，返回-1
     }
 
     public void remove(int key) {
@@ -94,7 +94,7 @@ public class Q706_Design_HashMap {
         Iterator<Pair> iterator = data[h].iterator();
         while (iterator.hasNext()) {
             Pair pair = iterator.next();
-            if (pair.getKey() == key) {
+            if (pair.getKey() == key) { // key存在，移除
                 data[h].remove(pair);
                 return;
             }
@@ -103,5 +103,155 @@ public class Q706_Design_HashMap {
 
     private static int hash(int key) {
         return key % BASE;
+    }
+    /**
+     * 利用链表chaining最大化减少空间浪费
+     * 每个链表预设一个dummy head，这样head永远不会为空，省去判断为空的情况，略费空间
+     */
+    private static final int N = 1009;
+    private MyListNode[] nums;
+
+    public void MyHashMap1() {
+        nums = new MyListNode[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = new MyListNode();
+        }
+    }
+
+    private int getHashKey(int key) {
+        return key % N;
+    }
+
+    public void put1(int key, int value) {
+        int hashKey = getHashKey(key);
+        MyListNode head = nums[hashKey];
+        MyListNode p = head;
+        MyListNode tail = p;
+        while (p != null) {
+            if (p.key == key) {
+                p.val = value;
+                return;
+            }
+            tail = p;
+            p = p.next;
+        }
+        tail.next = new MyListNode(key, value);
+    }
+
+    public int get1(int key) {
+        int hashKey = getHashKey(key);
+        MyListNode head = nums[hashKey];
+        MyListNode p = head;
+        while (p != null) {
+            if (p.key == key) {
+                return p.val;
+            }
+            p = p.next;
+        }
+        return -1;
+    }
+
+    public void remove1(int key) {
+        int hashKey = getHashKey(key);
+        MyListNode head = nums[hashKey];
+        MyListNode prev = head;
+        MyListNode p = head.next;
+        while (p != null) {
+            if (p.key == key) {
+                prev.next = p.next;
+                return;
+            }
+            prev = p;
+            p = p.next;
+        }
+    }
+
+    class MyListNode {
+        int key;
+        int val;
+        MyListNode next;
+
+        public MyListNode() {
+            this.key = -1;
+            this.val = -1;
+            this.next = null;
+        }
+
+        public MyListNode(int key, int val) {
+            this.key = key;
+            this.val = val;
+            this.next = null;
+        }
+    }
+    /**
+     * 没有dummy head node，所有任何操作都要判断是否为空，代码会比较复杂，但是节省空间
+     */
+    private static final int N1 = 1009;
+    private MyListNode[] nums1;
+
+    public void MyHashMap32() {
+        nums1 = new MyListNode[N1];
+    }
+
+    private int getHashKey2(int key) {
+        return key % N1;
+    }
+
+    public void put2(int key, int value) {
+        int hashKey = getHashKey2(key);
+        MyListNode head = nums1[hashKey];
+        if (head == null) {
+            nums1[hashKey] = new MyListNode(key, value);
+            return;
+        }
+        MyListNode p = head;
+        MyListNode tail = p;
+        while (p != null) {
+            if (p.key == key) {
+                p.val = value;
+                return;
+            }
+            tail = p;
+            p = p.next;
+        }
+        tail.next = new MyListNode(key, value);
+    }
+
+    public int get2(int key) {
+        int hashKey = getHashKey2(key);
+        MyListNode head = nums1[hashKey];
+        if (head == null) {
+            return -1;
+        }
+        MyListNode p = head;
+        while (p != null) {
+            if (p.key == key) {
+                return p.val;
+            }
+            p = p.next;
+        }
+        return -1;
+    }
+
+    public void remove2(int key) {
+        int hashKey = getHashKey2(key);
+        MyListNode head = nums1[hashKey];
+        if (head == null) {
+            return;
+        }
+        MyListNode p = head;
+        MyListNode prev = null;
+        while (p != null) {
+            if (p.key == key) {
+                if (prev == null) {
+                    nums1[hashKey] = head.next;
+                } else {
+                    prev.next = p.next;
+                }
+                return;
+            }
+            prev = p;
+            p = p.next;
+        }
     }
 }
