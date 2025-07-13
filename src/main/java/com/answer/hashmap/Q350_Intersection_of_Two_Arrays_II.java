0342b524248bf89c7f1a6e6a04e5cb954b437c10
KevinX <Xzb04720638k>
2025-07-12 21:05:53 -0400

    update

--- src/main/java/com/answer/hashmap/Q350_Intersection_of_Two_Arrays_II.java
+++ src/main/java/com/answer/hashmap/Q350_Intersection_of_Two_Arrays_II.java
@@ -1,6 +1,5 @@
 package com.answer.hashmap;
 
-import java.util.Arrays;
 import java.util.*;
 
 public class Q350_Intersection_of_Two_Arrays_II {
@@ -15,6 +14,51 @@ public class Q350_Intersection_of_Two_Arrays_II {
      * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
      * 输出：[4,9]
      */
+    /**
+     * 使用Hash数组
+     * refer to Q349_Intersection_of_Two_Arrays
+     */
+    public int[] intersect_0(int[] nums1, int[] nums2) {
+        int[] hash1 = new int[1002];
+        int[] hash2 = new int[1002];
+
+        for(int i = 0; i < nums1.length; i++){
+            hash1[nums1[i]]++;
+        }
+        for(int i = 0; i < nums2.length; i++){
+            hash2[nums2[i]]++;
+        }
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0; i < 1002; i++){
+            if(hash1[i] > 0 && hash2[i] > 0){
+                int count = Math.min(hash1[i], hash2[i]);
+                while(count-- > 0){
+                    result.add(i);
+                }
+            }
+        }
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+    /**
+     * 使用HashMap
+     * refer to Q349_Intersection_of_Two_Arrays
+     */
+    public int[] intersect_3(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int n : nums1){
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for(int num: nums2){
+            if(map.containsKey(num) && map.get(num) > 0) {
+                ans.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+        return ans.stream().mapToInt(i -> i).toArray();
+    }
     /**
      * Approach 1: Hash Map
      */
@@ -43,9 +87,15 @@ public class Q350_Intersection_of_Two_Arrays_II {
         }
         return Arrays.copyOfRange(result, 0, index);
     }
-
     /**
      * Sorting & Two pointers
+     * refer to Q349_Intersection_of_Two_Arrays
+     *
+     * 1.如果两个数组是有序的，则可以使用双指针的方法得到两个数组的交集。
+     * 2.首先对两个数组进行排序，然后使用两个指针遍历两个数组。
+     * 3.初始时，两个指针分别指向两个数组的头部。每次比较两个指针指向的两个数组中的数字，如果两个数字不相等，
+     *   则将指向较小数字的指针右移一位，如果两个数字相等，将该数字添加到答案，并将两个指针都右移一位。
+     *   当至少有一个指针超出数组范围时，遍历结束。
      */
     public static int[] intersection_1(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
