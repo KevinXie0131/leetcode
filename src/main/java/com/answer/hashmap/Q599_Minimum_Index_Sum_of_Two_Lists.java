4697399c2ad1726b3c17787d9f0f62f67a2a47e2
KevinX <Xzb04720638k>
2025-07-12 22:43:02 -0400

    update

--- src/main/java/com/answer/hashmap/Q599_Minimum_Index_Sum_of_Two_Lists.java
+++ src/main/java/com/answer/hashmap/Q599_Minimum_Index_Sum_of_Two_Lists.java
@@ -1,5 +1,7 @@
 package com.answer.hashmap;
 
+import java.util.*;
+
 public class Q599_Minimum_Index_Sum_of_Two_Lists {
     /**
      * 两个列表的最小索引总和
@@ -21,4 +23,41 @@ public class Q599_Minimum_Index_Sum_of_Two_Lists {
      * 输出: ["Shogun"]
      * 解释: 具有最小索引和的公共字符串是 “Shogun”，它有最小的索引和 = (0 + 1) = 1。
      */
+    /**
+     * 哈希表
+     * 如果 list2[i] 在哈希表中：
+     *  索引之和等于 min，将 list2[i] 加入 res；
+     *  索引之和小于 min，更新 min，清空 res，将 list2[i] 加入 res；
+     *  索引之和大于 min，跳过。
+     */
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        // 只需要加这三行代码
+        // 以考虑只保存长度更小的那个到哈希表
+        if (list1.length > list2.length) {
+            return findRestaurant(list2, list1); // 优化
+        }
+
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < list1.length; i++){
+            map.put(list1[i], i);
+        }
+
+        List<String> res = new ArrayList<>();
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < list2.length; i++){
+            if (i > min) break;  // 优化
+
+            if(map.containsKey(list2[i])){
+                int index = i + map.get(list2[i]);
+                if(index < min){ // //寻找更小索引和. 如果该索引和比最小索引和小，则清空结果, 该索引和作为最小索引和
+                    min = index;
+                    res.clear(); // res = new ArrayList<>();
+                    res.add(list2[i]);
+                } else if (index == min){ // 如果该索引和等于最小索引和，则直接加入结果中。
+                    res.add(list2[i]);
+                }
+            }
+        }
+        return res.toArray(new String[0]);
+    }
 }
