237a2ca882d1a41bf62809228280686d034261b8
KevinX <Xzb04720638k>
2025-07-13 19:20:30 -0400

    update

--- src/main/java/com/answer/hashmap/Q705_Design_HashSet.java
+++ src/main/java/com/answer/hashmap/Q705_Design_HashSet.java
@@ -16,64 +16,67 @@ public class Q705_Design_HashSet {
      * 输入：["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
      *      [[], [1], [2], [1], [3], [2], [2], [2], [2]]
      * 输出:[null, null, null, true, false, null, true, null, false]
-     * 解释：
-     * MyHashSet myHashSet = new MyHashSet();
-     * myHashSet.add(1);      // set = [1]
-     * myHashSet.add(2);      // set = [1, 2]
-     * myHashSet.contains(1); // 返回 True
-     * myHashSet.contains(3); // 返回 False ，（未找到）
-     * myHashSet.add(2);      // set = [1, 2]
-     * myHashSet.contains(2); // 返回 True
-     * myHashSet.remove(2);   // set = [1]
-     * myHashSet.contains(2); // 返回 False ，（已移除）
+     * 解释：MyHashSet myHashSet = new MyHashSet();
+     *      myHashSet.add(1);      // set = [1]
+     *      myHashSet.add(2);      // set = [1, 2]
+     *      myHashSet.contains(1); // 返回 True
+     *      myHashSet.contains(3); // 返回 False ，（未找到）
+     *      myHashSet.add(2);      // set = [1, 2]
+     *      myHashSet.contains(2); // 返回 True
+     *      myHashSet.remove(2);   // set = [1]
+     *      myHashSet.contains(2); // 返回 False ，（已移除）
      */
-    private static final int BASE = 769;
-    private LinkedList[] data;
+    /**
+     * 链地址法: 为每个哈希值维护一个链表，并将具有相同哈希值的元素都放入这一链表当中
+     */
+    private static final int BASE = 769; // 应当将 base 取为一个质数。在这里，我们取 base=769
+    private LinkedList[] data; // 链表数组，用于存储元素
 
     public Q705_Design_HashSet() {
         data = new LinkedList[BASE];
         for(int i = 0; i < BASE; i++){
-            data[i] = new LinkedList<Integer>();
+            data[i] = new LinkedList<Integer>(); // 初始化每个链表
         }
     }
 
     public void add(int key) {
-        int h = hash(key);
-        Iterator<Integer> iterator = data[h].iterator();
+        int h = hash(key);  // 计算元素的哈希值
+        Iterator<Integer> iterator = data[h].iterator(); //建立迭代器
         while(iterator.hasNext()){
             Integer element = iterator.next();
-            if(element == key){
+            if(element == key){ // 如果元素已经存在，直接返回
                 return;
             }
         }
-        data[h].offerLast(key);
+        data[h].offerLast(key);  // 在链表末尾添加元素
     }
 
     public void remove(int key) {
-        int h = hash(key);
-        Iterator<Integer> iterator = data[h].iterator();
+        int h = hash(key);  // 计算元素的哈希值
+        Iterator<Integer> iterator = data[h].iterator(); // 建立迭代器
         while(iterator.hasNext()){
             Integer element = iterator.next();
             if(element == key){
-                data[h].remove(element);
+                data[h].remove(element); // 删除元素
                 return;
             }
         }
     }
 
     public boolean contains(int key) {
-        int h = hash(key);
-        Iterator<Integer> iterator = data[h].iterator();
+        int h = hash(key);  // 计算元素的哈希值
+        Iterator<Integer> iterator = data[h].iterator(); // 建立迭代器
         while(iterator.hasNext()){
             Integer element = iterator.next();
             if(element == key){
-                return true;
+                return true; // 如果找到了元素，返回true
             }
         }
-        return false;
+        return false; // 没有找到元素，返回false
     }
 
     private static int hash(int key) {
-        return key % BASE;
+        return key % BASE; // 除留余数法计算哈希值
     }
+
 }
