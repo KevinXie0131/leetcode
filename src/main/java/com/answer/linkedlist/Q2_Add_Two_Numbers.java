683b6a249fa17a4c563f9dde20f338912a67c378
KevinX <Xzb04720638k>
2025-07-16 18:20:48 -0400

    update

--- src/main/java/com/answer/linkedlist/Q2_Add_Two_Numbers.java
+++ src/main/java/com/answer/linkedlist/Q2_Add_Two_Numbers.java
@@ -196,18 +196,18 @@ public class Q2_Add_Two_Numbers {
         return head;
     }
     /**
-     * 比较易懂的递归
+     * 比较易懂的链表递归
      */
     public ListNode addTwoNumbers_7(ListNode l1, ListNode l2) {
-        return mT(l1, l2, 0);
+        return add(l1, l2, 0);
     }
-
-    public ListNode mT(ListNode l1, ListNode l2, int carry) {
-        if (l1 == null && l2 == null && carry == 0) {
+    // 返回两个链表相加的头部
+    public ListNode add(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null && carry == 0) { // 递归边界
             return null;
         }
         int sum = carry;
-        if (l1 != null) {
+        if (l1 != null) { // 累加进位与节点值
             sum += l1.val;
             l1 = l1.next;
         }
@@ -215,8 +215,8 @@ public class Q2_Add_Two_Numbers {
             sum += l2.val;
             l2 = l2.next;
         }
-        ListNode node = new ListNode(sum % 10);
-        node.next = mT(l1, l2, sum / 10);
+        ListNode node = new ListNode(sum % 10);   // sum 除以 10 的余数为当前节点值，商为进位
+        node.next = add(l1, l2, sum / 10);
         return node;
     }
     /**
@@ -251,31 +251,6 @@ public class Q2_Add_Two_Numbers {
         ListNode head = new ListNode(sum % 10);
         head.next = recursion(l1.next, (l2 != null ? l2.next : null), sum / 10);
         return head;
-    }
-    /**
-     * 链表递归
-     */
-    public ListNode addTwoNumbers_8(ListNode l1, ListNode l2) {
-        return add(l1, l2, 0);
-    }
-
-    // 返回两个链表相加的头部
-    public ListNode add(ListNode l1, ListNode l2, int carry) {
-        if (l1 == null && l2 == null && carry == 0) {  // 递归边界
-            return null;
-        }
-        int sum = carry;
-        if (l1 != null) {
-            sum += l1.val;  // 累加进位与节点值
-            l1 = l1.next;
-        }
-        if (l2 != null) {
-            sum += l2.val;
-            l2 = l2.next;
-        }
-        ListNode node = new ListNode(sum % 10);  // sum 除以 10 的余数为当前节点值，商为进位
-        node.next = add(l1, l2, sum / 10);
-        return node;
     }
     /**
      * 递归
