6dc9a621a2c70fb7c4c54ca0f91c373633079fa7
KevinX <Xzb04720638k>
2025-07-16 22:16:32 -0400

    update

--- src/main/java/com/answer/linkedlist/Q206_Reverse_Linked_List.java
+++ src/main/java/com/answer/linkedlist/Q206_Reverse_Linked_List.java
@@ -9,6 +9,8 @@ public class Q206_Reverse_Linked_List {
      * 示例 1：
      *  输入：head = [1,2,3,4,5]
      *  输出：[5,4,3,2,1]
+     *
+     *  假设链表为 1→2→3→∅，我们想要把它改成 ∅←1←2←3。
      */
     public static void main(String[] args) {
       //  ListNode node6 = new ListNode(6, null);
@@ -38,7 +40,9 @@ public class Q206_Reverse_Linked_List {
         return dummy.next;
     }
     /**
-     * Iterative
+     * Iterative 迭代
+     * 在遍历链表时，将当前节点的 next 指针改为指向前一个节点。由于节点没有引用其前一个节点，因此必须事先存储其前一个节点。
+     * 在更改引用之前，还需要存储后一个节点。最后返回新的头引用。
      */
     public static ListNode reverseList_Iterative(ListNode head) {
         ListNode pre = null;
@@ -53,6 +57,7 @@ public class Q206_Reverse_Linked_List {
     }
     /**
      * Iterative 双指针法
+     * 同上
      */
     public ListNode reverseList_Iterative1(ListNode head) {
         ListNode prev = null;
@@ -68,23 +73,57 @@ public class Q206_Reverse_Linked_List {
         return prev;
     }
     /**
+     * 以链表1->2->3->4->5举例
+     * @param head
+     * @return
+     */
+    public ListNode reverseList7(ListNode head) {
+        if (head == null || head.next == null) {
+            // 直到当前节点的下一个节点为空时返回当前节点. 由于5没有下一个节点了，所以此处返回节点5
+            return head;
+        }
+
+        ListNode newHead = reverseList7(head.next); //递归传入下一个节点，目的是为了到达最后一个节点
+        /*  第一轮出栈，head为5，head.next为空，返回5
+            第二轮出栈，head为4，head.next为5，执行head.next.next=head也就是5.next=4，
+                      把当前节点的子节点的子节点指向当前节点
+                      此时链表为1->2->3->4<->5，由于4与5互相指向，所以此处要断开4.next=null
+                      此时链表为1->2->3->4<-5
+                      返回节点5
+            第三轮出栈，head为3，head.next为4，执行head.next.next=head也就是4.next=3，
+                      此时链表为1->2->3<->4<-5，由于3与4互相指向，所以此处要断开3.next=null
+                      此时链表为1->2->3<-4<-5
+                      返回节点5
+            第四轮出栈，head为2，head.next为3，执行head.next.next=head也就是3.next=2，
+                      此时链表为1->2<->3<-4<-5，由于2与3互相指向，所以此处要断开2.next=null
+                      此时链表为1->2<-3<-4<-5
+                      返回节点5
+            第五轮出栈，head为1，head.next为2，执行head.next.next=head也就是2.next=1，
+                      此时链表为1<->2<-3<-4<-5，由于1与2互相指向，所以此处要断开1.next=null
+                      此时链表为1<-2<-3<-4<-5
+                      返回节点5
+            出栈完成，最终头节点5->4->3->2->1
+        */
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+    /**
      * Recursive - from tail to head // 从后向前递归
      */
     public static ListNode reverseList(ListNode head) {
         if(head == null || head.next == null) {  // 边缘条件判断
             return head;
         }
-        // 递归调用，翻转第二个节点开始往后的链表
-        ListNode last  = reverseList(head.next);
-        // 翻转头节点与第二个节点的指向
-        head.next.next = head;
-        // 此时的 head 节点为尾节点，next 需要指向 NULL
-        head.next = null;
+
+        ListNode last  = reverseList(head.next); // 递归调用，翻转第二个节点开始往后的链表
+        head.next.next = head;  // 翻转头节点与第二个节点的指向
+        head.next = null; // 此时的 head 节点为尾节点，next 需要指向 NULL
 
         return last; // last 始终为最末的那个节点（5），节点5不停被返回，最后作为返回值返回
     }
     /**
-     * Recursive - from head to tail
+     * Recursive - from head to tail 递归
      */
     public static ListNode reverseList_1(ListNode head) {
         return reverseList_fun(head, null );
@@ -109,7 +148,6 @@ public class Q206_Reverse_Linked_List {
                 return curNode;
             }
         }
-
         ListNode newNode = reverseList_fun(nextNext, next);
         return newNode;
     }
