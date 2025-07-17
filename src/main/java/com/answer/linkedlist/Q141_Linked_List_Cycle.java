22dd22e84d85091a1e5ac040980daee258079f5a
KevinX <Xzb04720638k>
2025-07-16 20:11:10 -0400

    update

--- src/main/java/com/answer/linkedlist/Q141_Linked_List_Cycle.java
+++ src/main/java/com/answer/linkedlist/Q141_Linked_List_Cycle.java
@@ -1,7 +1,6 @@
 package com.answer.linkedlist;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Q141_Linked_List_Cycle {
     /**
@@ -12,7 +11,6 @@ public class Q141_Linked_List_Cycle {
      * Given head, the head of a linked list, determine if the linked list has a cycle in it.
      * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
      * Return true if there is a cycle in the linked list. Otherwise, return false.
-     *
      * 示例 1：
      *  输入：head = [3,2,0,-4], pos = 1
      *  输出：true
@@ -36,7 +34,54 @@ public class Q141_Linked_List_Cycle {
         return false;  // 正常走到链表末尾，表明没有环
     }
     /**
-     * Use set
+     * another from
+     */
+    public boolean hasCycle_0(ListNode head) {
+        ListNode slow = head, fast = head;
+        do{
+            if(fast == null || fast.next == null){
+                return false;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        } while (slow != fast);
+        return true;
+    }
+    /**
+     * another from
+     */
+    public boolean hasCycle_6(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (true) {
+            if (fast == null || fast.next == null) { // 指向空节点则无环
+                return false;
+            }
+            fast = fast.next.next;    // fast 和 slow 异速前进
+            slow = slow.next;
+
+            if (slow == fast) { // 相遇
+                return true;
+            }
+        }
+    }
+    /**
+     * another from
+     */
+    public boolean hasCycle_2(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        while (head.next != null) {
+            if (head.next.val == Integer.MAX_VALUE) {
+                return true;
+            }
+            head.val = Integer.MAX_VALUE;
+            head = head.next;
+        }
+        return false;
+    }
+    /**
+     * Use set 哈希表
      */
     public boolean hasCycle_1(ListNode head) {
         Set<ListNode> set = new HashSet<>();
