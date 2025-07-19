9bb90a9f67dd72dd103f81cda5c360ded4e41379
KevinX <Xzb04720638k>
2025-07-19 18:32:03 -0400

    update

--- src/main/java/com/answer/linkedlist/Q24_Swap_Nodes_in_Pairs.java
+++ src/main/java/com/answer/linkedlist/Q24_Swap_Nodes_in_Pairs.java
@@ -5,7 +5,6 @@ public class Q24_Swap_Nodes_in_Pairs {
      * 两两交换链表中的节点
      * 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
      * Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
-     *
      * 示例 1：
      *  输入：head = [1,2,3,4]
      *  输出：[2,1,4,3]
@@ -17,7 +16,7 @@ public class Q24_Swap_Nodes_in_Pairs {
         ListNode node2 = new ListNode(2, node3);
         ListNode node1= new ListNode(1,node2);
 
-        ListNode node = swapPairs_Recursive_1(node1);
+        ListNode node = swapPairs_Recursive(node1);
         node.print();
     }
     /**
@@ -32,11 +31,10 @@ public class Q24_Swap_Nodes_in_Pairs {
 
             start.next = end.next;
             end.next = start;
-
             cur.next = end;
+
             cur = start;
         }
-
         return dummy.next;
     }
     /**
@@ -53,27 +51,27 @@ public class Q24_Swap_Nodes_in_Pairs {
 
             start.next = end.next;
             end.next = start;
-
             pre.next = end;
+
             pre = start;
             cur = start.next;
         }
         return dummy.next;
     }
     /**
-     * dummy node + pre node
+     * dummy node + pre node 同上
      */
     public static ListNode swapPairs_2(ListNode head) {
         ListNode dummy = new ListNode(-1, head);
         ListNode cur = head;
         ListNode pre = dummy;
         while(cur != null && cur.next != null){
-            ListNode temp = cur.next;
+            ListNode nxt = cur.next;
 
             cur.next = cur.next.next;
-            temp.next = cur;
+            nxt.next = cur;
+            pre.next = nxt;
 
-            pre.next = temp;
             pre = cur;
             cur = cur.next;
         }
@@ -84,22 +82,22 @@ public class Q24_Swap_Nodes_in_Pairs {
      */
     public ListNode swapPairs_3(ListNode head) {
         ListNode dummy = new ListNode(); // 设置一个虚拟头结点
-        dummy.next =head; // 将虚拟头结点指向head，这样方便后面做删除操作
+        dummy.next = head; // 将虚拟头结点指向head，这样方便后面做删除操作
         ListNode cur = dummy;
-        ListNode temp; // 临时节点，保存两个节点后面的节点
+        ListNode last; // 临时节点，保存两个节点后面的节点
         ListNode first; // 临时节点，保存两个节点之中的第一个节点
         ListNode second; // 临时节点，保存两个节点之中的第二个节点
 
         while(cur.next != null &&  cur.next.next != null){ //剩下的链表长度大于等于2
             first = cur.next;
             second = cur.next.next;
-            temp = second.next;
+            last = second.next;
 
             cur.next = second;  // 步骤一
             second.next = first; // 步骤二
-            first.next = temp;  // 步骤三
+            first.next = last;  // 步骤三
 
-            cur = first;// cur移动，准备下一轮交换
+            cur = first; // cur移动，准备下一轮交换
         }
         return dummy.next;
     }
@@ -134,7 +132,23 @@ public class Q24_Swap_Nodes_in_Pairs {
         return next;
     }
     /**
-     * Recursive - from head to tail
+     * 同上
+     */
+    public static ListNode swapPairs_Recursive_2(ListNode first) {
+        if(first == null || first.next == null) {  // base case 退出提交
+            return first;
+        }
+        ListNode second = first.next;   // 获取当前节点的下一个节点
+
+        ListNode third = swapPairs_Recursive_2(second.next);  // 进行递归
+
+        second.next = first; // 这里进行交换
+        first.next = third;
+
+        return second;
+    }
+    /**
+     * Recursive - from head to tail 递归
      */
     public static ListNode swapPairs_Recursive(ListNode head) {
         if(head == null || head.next == null){
@@ -145,13 +159,12 @@ public class Q24_Swap_Nodes_in_Pairs {
         ListNode newNode = head.next;
         newNode.next = head;
 
-        head.next = swapPairs_Recursive(nextNextNode); // 相当于head.next = swapPairs_Recursive(head.next.next);
-
+        head.next = swapPairs_Recursive(nextNextNode); // head.next = swapPairs_Recursive(head.next.next); // not work
         return newNode;
 
     }
     /**
-     * Recursive - from tail to head
+     * Recursive - from tail to head 递归
      */
     public static ListNode swapPairs_Recursive_1(ListNode head) {
         if(head == null || head.next == null){
@@ -165,21 +178,5 @@ public class Q24_Swap_Nodes_in_Pairs {
         head.next = temp;
 
         return newNode;
-    }
-
-    public static ListNode swapPairs_Recursive_2(ListNode first) {
-        // base case 退出提交
-        if(first == null || first.next == null) {
-            return first;
-        }
-        // 获取当前节点的下一个节点
-        ListNode second = first.next;
-        // 进行递归
-        ListNode third = swapPairs_Recursive_2(second.next);
-        // 这里进行交换
-        second.next = first;
-        first.next = third;
-
-        return second;
     }
 }
