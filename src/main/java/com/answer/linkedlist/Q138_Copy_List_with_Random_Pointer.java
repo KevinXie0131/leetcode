9eb7b3f349de871ee239f9127221f94cd3f43dc8
KevinX <Xzb04720638k>
2025-07-19 22:09:59 -0400

    update

--- src/main/java/com/answer/linkedlist/Q138_Copy_List_with_Random_Pointer.java
+++ src/main/java/com/answer/linkedlist/Q138_Copy_List_with_Random_Pointer.java
@@ -1,5 +1,7 @@
 package com.answer.linkedlist;
 
+import java.util.*;
+
 public class Q138_Copy_List_with_Random_Pointer {
     /**
      * 随机链表的复制
@@ -17,4 +19,61 @@ public class Q138_Copy_List_with_Random_Pointer {
      *  输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
      *  输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
      */
+    /**
+     * 哈希表
+     * 本题中因为随机指针的存在，当我们拷贝节点时，「当前节点的随机指针指向的节点」可能还没创建
+     */
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+        Node cur = head;
+        Map<Node, Node> map = new HashMap<>(); // 原节点 -> 新节点映射
+        // 3. 复制各节点，并建立 “原节点 -> 新节点” 的 Map 映射
+        while(cur != null) {
+            map.put(cur, new Node(cur.val));    // 第一次遍历：创建新节点并建立映射
+            cur = cur.next;
+        }
+        cur = head;
+        // 4. 构建新链表的 next 和 random 指向
+        while(cur != null) {
+            map.get(cur).next = map.get(cur.next);     // 第二次遍历：设置next和random指针
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        // 5. 返回新链表的头节点
+        return map.get(head);  // 返回新的链表的头节点
+    }
+    /**
+     * 回溯 + 哈希表
+     * 使用回溯的方式遍历整个链表，让每个节点的复制相互独立。对于当前节点 node，首先复制当前节点 node，
+     * 而后对当前节点的后继节点和随机指针指向的节点进行复制，最后让复制节点的 next 指针和 random 指针指向这两个复制的节点，即可完成边的复制。
+     *
+     * 为了防止多次遍历同一个节点，我们需要建立一个哈希表， 来记录源节点到克隆节点之间的映射关系。在回溯搜索过程中，
+     * 如果当前正在搜索的节点出现在了哈希表中，就说明我们已经遍历完了整个链表，此时就可以直接从哈希表中取出复制后的节点的指针并返回。
+     */
+    Map<Node, Node> cachedNode = new HashMap<Node, Node>();
+
+    public Node copyRandomList2(Node head) {
+        if (head == null) {
+            return null;
+        }
+        if (!cachedNode.containsKey(head)) {
+            Node headNew = new Node(head.val);   //复制节点
+            cachedNode.put(head, headNew); //建立源节点到复制节点的映射
+            headNew.next = copyRandomList2(head.next);  //复制边
+            headNew.random = copyRandomList2(head.random);
+        }
+        return cachedNode.get(head);  //node节点已经被访问过了,直接从哈希表hash中取出对应的复制节点返回。
+    }
+    // Definition for a Node.
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
 }
