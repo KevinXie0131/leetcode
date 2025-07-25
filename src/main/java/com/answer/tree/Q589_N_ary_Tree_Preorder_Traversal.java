85e88fa978296df0caf525165798e156d214d4a6
KevinX <Xzb04720638k>
2025-07-25 19:05:06 -0400

    update

--- src/main/java/com/answer/tree/Q589_N_ary_Tree_Preorder_Traversal.java
+++ src/main/java/com/answer/tree/Q589_N_ary_Tree_Preorder_Traversal.java
@@ -32,21 +32,22 @@ public class Q589_N_ary_Tree_Preorder_Traversal {
         }
     }
     /**
-     * 迭代
+     * 迭代 （栈实现）
+     * N叉树的前序遍历代码操作的顺序是，根节点—>子节点（从右到左进栈，出来的顺序为左到右），对应得到的遍历结果就是 根节点+子节点（从左到右）。
      */
     public List<Integer> preorder1(Node root) {
         List list = new LinkedList();
-        if(root == null) return list;
         Deque<Node> stack = new ArrayDeque<>();
 
-        stack.push(root);
+        if(root == null) return list;
+        stack.push(root);  // 首先把根节点入栈
 
         while(!stack.isEmpty()){
-            Node node = stack.pop();
-            list.add(node.val);
+            Node node = stack.pop();  // 当前栈顶节点出栈
+            list.add(node.val);  // 将值加入列表
 
             List<Node> nodes = node.children;
-            for(int i = nodes.size() - 1; i >= 0; i--){
+            for(int i = nodes.size() - 1; i >= 0; i--){ // 对该节点的每个子节点按照从右到左的顺序依次入栈
                 stack.push(nodes.get(i));
             }
         }
