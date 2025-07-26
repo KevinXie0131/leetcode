7dd01c817775f2a08f44f4766bab2dd4a7d3b76b
KevinX <Xzb04720638k>
2025-07-25 21:54:57 -0400

    update

--- src/main/java/com/answer/tree/Q226_Invert_Binary_Tree.java
+++ src/main/java/com/answer/tree/Q226_Invert_Binary_Tree.java
@@ -1,10 +1,7 @@
 package com.answer.tree;
 
 import com.template.TreeNode;
-
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Q226_Invert_Binary_Tree {
     /**
@@ -32,9 +29,8 @@ public class Q226_Invert_Binary_Tree {
         if(root.left != null){
             invertTree(root.left);
         }*/
-        return root;
+        return root; //函数返回时就表示当前这个节点，以及它的左右子树都已经交换完了
     }
-
     /**
      * 后序遍历
      */
@@ -51,13 +47,23 @@ public class Q226_Invert_Binary_Tree {
         return root;
     }
     /**
+     * another fornm
+     */
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null) return null;
+
+        TreeNode left = invertTree2(root.left);
+        TreeNode right = invertTree2(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+    /**
      * 文中我指的是递归的中序遍历是不行的，因为使用递归的中序遍历，某些节点的左右孩子会翻转两次。
      * 如果非要使用递归中序的方式写，也可以，如下代码就可以避免节点左右孩子翻转两次的情况
      */
     public TreeNode invertTree_2(TreeNode root) {
-        if(root == null ){
-            return root;
-        };
+        if(root == null ) return root;
 
         if(root.left != null){
             invertTree(root.left); // 左
@@ -73,16 +79,19 @@ public class Q226_Invert_Binary_Tree {
         return root;
     }
     /**
-     * BFS
+     * BFS 广度优先遍历
      */
     public TreeNode invertTree_3(TreeNode root) {
-        if (root == null) {return null;}
+        if (root == null) {
+            return null;
+        }
         ArrayDeque<TreeNode> deque = new ArrayDeque<>();
         deque.offer(root);
-        while (!deque.isEmpty()) {
+
+        while (!deque.isEmpty()) { // 使用队列利用层序遍历实现交换
             int size = deque.size();
             while (size-- > 0) {
-                TreeNode node = deque.poll();
+                TreeNode node = deque.poll(); //每次都从队列中拿一个节点，并交换这个节点的左右子树
                 swap(node);
                 if (node.left != null) deque.offer(node.left);
                 if (node.right != null) deque.offer(node.right);
@@ -91,7 +100,7 @@ public class Q226_Invert_Binary_Tree {
         return root;
     }
 
-    public void swap(TreeNode root) {
+    public void swap(TreeNode root) { //左右子树交换顺序
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
