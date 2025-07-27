d7c3bc17cedcf8935f86d701473696d723dd5924
KevinX <Xzb04720638k>
2025-07-27 12:23:10 -0400

    update

--- src/main/java/com/answer/tree/Q102_Binary_Tree_Level_Order_Traversal.java
+++ src/main/java/com/answer/tree/Q102_Binary_Tree_Level_Order_Traversal.java
@@ -16,31 +16,29 @@ public class Q102_Binary_Tree_Level_Order_Traversal {
      * 层序遍历方式就是图论中的广度优先遍历
      */
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> list = new ArrayList<List<Integer>>();
-        if (root == null) {
-            return list;
-        }
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null) return list;
         // BFS--迭代方式--借助队列
         // 通过一个队列来辅助操作，队列中存储的是当前层的所有节点，每次循环把队列中的当前全部节点弹出，并把弹出节点的左右孩子再加入队列，
         // 这样当一次循环结束，就完成了当前层节点的遍历以及下层节点的入队。
         Deque<TreeNode> queue = new ArrayDeque<>();
         queue.offer(root);
+
         while (!queue.isEmpty()) {
             List<Integer> sublist = new ArrayList<>();
-            int size = queue.size();  // 这里一定要使用固定大小size，不要使用que.size()，因为que.size是不断变化的
+            int size = queue.size();  // 这里一定要使用固定大小size，不要使用queue.size()，因为queue.size是不断变化的
 
             while (size > 0) {
                 TreeNode cur = queue.poll();
                 sublist.add(cur.value);
 
-                if (cur.left != null) {queue.offer(cur.left);}
-                if (cur.right != null) {queue.offer(cur.right);}
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
 
                 size--;
             }
             list.add(sublist);
         }
-
         return list;
     }
 }
--- src/main/java/com/answer/tree/Q102_Binary_Tree_Level_Order_Traversal_1.java
+++ src/main/java/com/answer/tree/Q102_Binary_Tree_Level_Order_Traversal_1.java
@@ -1,15 +1,11 @@
 package com.answer.tree;
 
 import com.template.TreeNode;
-
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 public class Q102_Binary_Tree_Level_Order_Traversal_1 {
 
-    List<List<Integer>> resListRec = new ArrayList<List<Integer>>();
+    List<List<Integer>> resListRec = new ArrayList<>();
 
     public List<List<Integer>> levelOrder(TreeNode root) {
         dfs(root, 0);
@@ -25,8 +21,7 @@ public class Q102_Binary_Tree_Level_Order_Traversal_1 {
         deep++;
 
         if (resListRec.size() < deep) {
-            //当层级增加时，list的Item也增加，利用list的索引值进行层级界定
-            List<Integer> sublist = new ArrayList<Integer>();
+            List<Integer> sublist = new ArrayList<>(); //当层级增加时，list的Item也增加，利用list的索引值进行层级界定
             resListRec.add(sublist);
         }
 
@@ -44,8 +39,7 @@ public class Q102_Binary_Tree_Level_Order_Traversal_1 {
         }
 
         if (resListRec.size() == level) {
-            //当层级增加时，list的Item也增加，利用list的索引值进行层级界定
-            List<Integer> sublist = new ArrayList<Integer>();
+            List<Integer> sublist = new ArrayList<>(); //当层级增加时，list的Item也增加，利用list的索引值进行层级界定
             resListRec.add(sublist);
         }
 
