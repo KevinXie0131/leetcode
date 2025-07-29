67bbafe88023dce471366d25dc585d7409b7141c
KevinX <Xzb04720638k>
2025-07-29 17:43:47 -0400

    update

--- src/main/java/com/answer/tree/Q129_Sum_Root_to_Leaf_Numbers.java
+++ src/main/java/com/answer/tree/Q129_Sum_Root_to_Leaf_Numbers.java
@@ -38,17 +38,52 @@ public class Q129_Sum_Root_to_Leaf_Numbers {
             path.removeLast();
             return;
         }
-
         path.addLast(root.value + "");
         dfs_0(root.left, path);
         dfs_0(root.right, path);
         path.removeLast();
     }
     /**
+     * refer to Q113_Path_Sum_II_1
+     */
+    int res2 = 0;
+
+    public int sumNumbers_0a(TreeNode root) {
+        dfs_0a(root, 0);
+        return res2;
+    }
+
+    private void dfs_0a(TreeNode root, int value){
+        if(root == null) return;
+
+        if(root.left == null && root.right == null){
+            res2 += value * 10 + root.value;
+            return;
+        }
+        dfs_0a(root.left, value * 10 + root.value);
+        dfs_0a(root.right, value * 10 + root.value);
+    }
+    /**
+     * 逻辑简单的递归
+     */
+    public int sumNumbers_0b(TreeNode root) {
+        return helper(root, 0);
+    }
+
+    private int helper(TreeNode root, int value){ // 有返回值
+        if(root == null) return 0;
+
+        if(root.left == null && root.right == null){
+            return value * 10 + root.value;
+        }
+        return helper(root.left, value * 10 + root.value) + helper(root.right, value * 10 + root.value);
+    }
+    /**
      * 本题和113.路径总和II 是类似的思路: 首先思路很明确，就是要遍历整个树把更节点到叶子节点组成的数字相加。
      */
     StringBuilder sb = new StringBuilder();
     int sum = 0;
+
     public int sumNumbers(TreeNode root) {
         dfs(root);
         return sum;
@@ -90,7 +125,7 @@ public class Q129_Sum_Root_to_Leaf_Numbers {
         if(root.left == null && root.right == null) {
             int total = 0;   // 当是叶子节点的时候，开始处理
             for(int i = 0; i < path.size(); i++){
-                total = total * 10 + path.get(i); // sum * 10 表示进位
+                total = total * 10 + path.get(i); // sum * 10 表示进位   0 <= Node.val <= 9
             }
             sum1 += total;
         }
@@ -126,5 +161,59 @@ public class Q129_Sum_Root_to_Leaf_Numbers {
         if(node.right != null) {
             dfs2(node.right, num);
         }
+    }
+    /**
+     * 广度优先搜索, 需要维护两个队列，分别存储节点和节点对应的数字
+     */
+    public int sumNumbers7(TreeNode root) {
+        if (root == null) return 0;
+
+        int sum = 0;
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        Queue<Integer> numQueue = new LinkedList<>();
+        nodeQueue.offer(root);
+        numQueue.offer(root.value);
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.poll();
+            int num = numQueue.poll();
+            TreeNode left = node.left, right = node.right;
+            if (left == null && right == null) {
+                sum += num;
+            } else {
+                if (left != null) {
+                    nodeQueue.offer(left);
+                    numQueue.offer(num * 10 + left.value);
+                }
+                if (right != null) {
+                    nodeQueue.offer(right);
+                    numQueue.offer(num * 10 + right.value);
+                }
+            }
+        }
+        return sum;
+    }
+    /**
+     * BFS直接更新子节点的值，无需另一个queue
+     */
+    public int sumNumbers8(TreeNode root) {
+        int count = 0;
+        Queue<TreeNode> nodes = new ArrayDeque<>();
+        nodes.add(root);
+
+        while (!nodes.isEmpty()){
+            TreeNode node = nodes.poll();
+            if (node.left == null && node.right == null){
+                count += node.value;
+            }
+            if (node.left != null){
+                node.left.value = node.value * 10 + node.left.value;
+                nodes.add(node.left);
+            }
+            if (node.right != null){
+                node.right.value = node.value * 10 + node.right.value;
+                nodes.add(node.right);
+            }
+        }
+        return count;
     }
 }
