e324d965d1e50629b823b41fb4c403ffd6ce43ed
KevinX <Xzb04720638k>
2025-07-29 19:30:03 -0400

    update

--- src/main/java/com/answer/tree/Q437_Path_Sum_III.java
+++ src/main/java/com/answer/tree/Q437_Path_Sum_III.java
@@ -2,8 +2,7 @@ package com.answer.tree;
 
 import com.template.TreeNode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Q437_Path_Sum_III {
     /**
@@ -35,7 +34,6 @@ public class Q437_Path_Sum_III {
         node3.right = node9;
         System.out.println(pathSum_1(node1, 8));
     }
-
     /**
      * Recursion
      * Time: N2
@@ -48,7 +46,7 @@ public class Q437_Path_Sum_III {
             return 0;
         }
         dfs(root, targetSum);
-        pathSum(root.left, targetSum);
+        pathSum(root.left, targetSum); //路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
         pathSum(root.right, targetSum);
         return res;
     }
@@ -59,33 +57,69 @@ public class Q437_Path_Sum_III {
         }
         if (targetSum == node.value) {
             res++;
+            // return; // not working. cannot add return here
         }
         if (node.left != null) dfs(node.left, targetSum - node.value);
         if (node.right != null) dfs(node.right, targetSum - node.value);
     }
-
     /**
-     * Official answer
+     * 穷举所有的可能，我们访问每一个节点 node，检测以 node 为起始节点且向下延深的路径有多少种。我们递归遍历每一个节点的所有可能的路径，
+     * 然后将这些路径数目加起来即为返回结果。
+     * 时间复杂度：O(N^2)
      */
-    public static int pathSum_1(TreeNode root, long targetSum) {
-        Map<Long, Integer> map = new HashMap<>();
-        map.put(0L, 1);
-        return dfs_1(root, map, 0, targetSum);
+    public int pathSum6(TreeNode root, long targetSum) {
+        if (root == null) return 0;
+
+        int ret = rootSum6(root, targetSum);
+        ret += pathSum6(root.left, targetSum);
+        ret += pathSum6(root.right, targetSum);
+        return ret;
     }
 
-    public static int dfs_1(TreeNode node, Map<Long, Integer> prefix, long curr, long targetSum) {
-        if (node == null) {
-            return 0;
-        }
+    public int rootSum6(TreeNode root, long targetSum) {
         int ret = 0;
-        curr += node.value;
+        if (root == null) return 0;
 
-        ret = prefix.getOrDefault(curr - targetSum, 0);
-        prefix.put(curr, prefix.getOrDefault(curr, 0) + 1);
-        ret += dfs_1(node.left, prefix, curr, targetSum);
-        ret += dfs_1(node.right, prefix, curr, targetSum);
-        prefix.put(curr, prefix.getOrDefault(curr, 0) - 1);
-
+        if (root.value == targetSum) {
+            ret++;
+        }
+        ret += rootSum6(root.left, targetSum - root.value);
+        ret += rootSum6(root.right, targetSum - root.value);
         return ret;
+    }
+    /**
+     * Official answer 前缀和
+     * 定义节点的前缀和为：由根结点到当前结点的路径上所有节点的和。我们利用先序遍历二叉树，记录下根节点 root 到当前节点 p 的路径上
+     * 除当前节点以外所有节点的前缀和，在已保存的路径前缀和中查找是否存在前缀和刚好等于当前节点到根节点的前缀和 curr 减去 targetSum
+     *
+     * 两个节点的前缀和差值时，有一个前提：一个节点必须是另一个节点的祖先节点
+     */
+    public static int pathSum_1(TreeNode root, long targetSum) {
+        Map<Long, Integer> map = new HashMap<>(); // 记录当前路径上出现的前缀和以及数量, 记录数量是因为有出现复数路径的可能。
+        map.put(0L, 1);  // 有一个默认的前缀和0
+        return dfs_1(root, map, 0, targetSum);  // 从根节点开始搜索
+    }
+    /**
+     * 深度优先搜索，返回到达当前节点及其子节点可以得到满足条件的路径
+     * @param node: 当前节点
+     * @param targetSum: 目标和
+     * @param curr: 根节点到当前节点的路径
+     * @return: 以当前节点为最后一个节点的，节点和等于目标和的路径数
+     */
+    public static int dfs_1(TreeNode node, Map<Long, Integer> prefix, long curr, long targetSum) {
+        if (node == null) return 0;  // 空节点，满足条件路径数为0
+
+        int ret = 0;
+        curr += node.value;  // 更新节点和
+        // 以当前节点为最后一个节点的，满足条件的路径数就等于根节点到当前节点路径上出现的前缀和 curr - target 的个数，通过哈希表 prefix，不存在则返回 0；
+        ret = prefix.getOrDefault(curr - targetSum, 0);    // 从哈希表中获取能和curr配对的前缀和个数
+        // 然后将当前前缀和 curr 加入哈希表中；【返回 curr 在哈希表中的值如果不存在返回0，值+1后重新存入哈希表】
+        prefix.put(curr, prefix.getOrDefault(curr, 0) + 1);  // 将当前前缀和加入哈希表
+        ret += dfs_1(node.left, prefix, curr, targetSum); // 递归处理左右子树
+        ret += dfs_1(node.right, prefix, curr, targetSum);
+        // 累加左右子节点处理的结果，同时将当前前缀和 curr 从哈希表移除【不是真的移除，而是将 preSum 的次数-1】
+        prefix.put(curr, prefix.getOrDefault(curr, 0) - 1); // 这个节点所在的路径都处理完了，这个前缀和也就没用了
+
+        return ret;   // 返回总路径数
     }
 }
--- src/main/java/com/answer/tree/Q437_Path_Sum_III_1.java
+++ src/main/java/com/answer/tree/Q437_Path_Sum_III_1.java
@@ -1,9 +1,6 @@
 package com.answer.tree;
 
 import com.template.TreeNode;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.*;
 
 public class Q437_Path_Sum_III_1 {
@@ -13,6 +10,7 @@ public class Q437_Path_Sum_III_1 {
  */
 class Solution {
     int count, num, targetSum;
+
     public int pathSum(TreeNode root, int targetSum) {
         if(root == null) return 0;
         this.targetSum = targetSum;
@@ -26,6 +24,7 @@ class Solution {
         }
         return count;
     }
+
     private void check(TreeNode node, int sum){
         if(node == null) return;
         sum = sum + node.value;
@@ -39,18 +38,21 @@ class Solution {
  */
 class Solution_1 {
     int count, num, targetSum;
+
     public int pathSum(TreeNode root, int targetSum) {
         if(root == null) return 0;
         this.targetSum = targetSum;
         dfs(root); // DFS遍历所有结点
         return count;
     }
+
     private void dfs(TreeNode node){
         if(node == null) return;
         check(node, 0); // 考察以当前结点为起始的满足要求的路径数量
         dfs(node.left);
         dfs(node.right);
     }
+
     private void check(TreeNode node, int sum){
         if(node == null) return;
         sum = sum + node.value;
@@ -68,6 +70,7 @@ class Solution_2 {
         int count = nodeSum(root, targetSum);
         return count + pathSum(root.left, targetSum) + pathSum(root.right, targetSum);
     }
+
     private int nodeSum(TreeNode node, int targetSum){
         if(node == null) return 0;
         int count = 0, val = node.value;
@@ -81,6 +84,7 @@ class Solution_2 {
 class Solution_3 {
     int targetSum, count = 0;
     Map<Integer, Integer> map;
+
     public int pathSum(TreeNode root, int targetSum) {
         if(root == null) return 0;
         this.targetSum = targetSum;
@@ -89,9 +93,18 @@ class Solution_3 {
         dfs(root, 0);
         return count;
     }
+    // 两节点间的路径和 = 两节点的前缀和之差
+    // 假如题目给定数值为5
+    //    节点1的前缀和为: 1
+    //    节点3的前缀和为: 1 + 2 + 3 = 6
+    //    prefix(3) - prefix(1) == 5
+    //    所以 节点1 到 节点3 之间有一条符合要求的路径( 2 --> 3 )
+    //
+    // 前缀和的递归回溯思路: 从当前节点反推到根节点(反推比较好理解，正向其实也只有一条)，有且仅有一条路径，因为这是一棵树
     private void dfs(TreeNode node, int preSum){
         if(node == null) return;
         preSum += node.value;
+        // preSum-targetSum相当于找路径的起点，当前点到起点的距离就是target
         count += map.getOrDefault(preSum - targetSum, 0); // #1 累计满足要求的前缀和数量
         map.put(preSum, map.getOrDefault(preSum, 0) + 1); // #2 先累计再put（先#1，再#2）
         dfs(node.left, preSum);
