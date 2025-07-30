234a86f0e0d34c82075c57856b855b80829fcb01
KevinX <Xzb04720638k>
2025-07-30 17:04:22 -0400

    update

--- src/main/java/com/answer/tree/Q99_Recover_Binary_Search_Tree.java
+++ src/main/java/com/answer/tree/Q99_Recover_Binary_Search_Tree.java
@@ -12,6 +12,7 @@ public class Q99_Recover_Binary_Search_Tree {
      */
     /**
      * 先读取所有节点，再判断顺序有问题的节点，再交换
+     * 假设有一个递增序列 a=[1,2,3,4,5,6,7]。如果我们交换两个不相邻的数字，例如 2 和 6，原序列变成了 a=[1,6,3,4,5,2,7]，
      */
     public void recoverTree(TreeNode root) {
         List<TreeNode> list = new ArrayList<>();
@@ -19,13 +20,19 @@ public class Q99_Recover_Binary_Search_Tree {
 
         TreeNode x= null;
         TreeNode y = null;
-
+        // 第一个节点，是第一个按照中序遍历时候前一个节点大于后一个节点，我们选取前一个节点
+        // 第二个节点，是在第一个节点找到之后，后面出现前一个节点大于后一个节点，我们选择后一个节点
         for(int i =0 ;i < list.size()-1; i++){
             if(list.get(i).value > list.get(i + 1).value){
-                y = list.get(i + 1);
-                if(x==null) { //有可能两个节点不相邻
+                y = list.get(i + 1);//有可能两个节点相邻
+                if(x == null) {
                     x = list.get(i);//记录第一个节点
+                } else {
+                    break;
                 }
+                /*else {
+                    y = list.get(i + 1); // not correct
+                }*/
             }
         }
         if(x!= null && y != null){
@@ -47,7 +54,7 @@ public class Q99_Recover_Binary_Search_Tree {
      */
     TreeNode firstNode = null;
     TreeNode secondNode = null;
-    TreeNode preNode = new TreeNode(Integer.MIN_VALUE);
+    TreeNode preNode = new TreeNode(Integer.MIN_VALUE); // 增加一个辅助的 pre 指针，记录 上一个 节点的值。
 
     public void recoverTree1(TreeNode root) {
         in_order(root);
@@ -69,42 +76,37 @@ public class Q99_Recover_Binary_Search_Tree {
         in_order(root.right);
     }
     /**
-     * From 睡不醒的鲤鱼
-     * Morris 遍历 + 寻找逆序对。
-     * Follow up: A solution using O(n) space is pretty straight-forward. Could you devise a constant O(1) space solution?
+     * 迭代 隐式中序遍历
      */
-    TreeNode first = null, second = null;
-    public void recoverTree3(TreeNode root) {
-        TreeNode last = null, cur = root;
-        while (cur != null) {
-            if (cur.left == null) {
-                check(last, cur);
-                last = cur;
-                cur = cur.right;
-            } else {
-                TreeNode p = cur.left;
-                while (p.right  != null && p.right != cur) p = p.right;
+    public void recoverTree2(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode cur = root;
+        TreeNode pre = null, first = null, second = null;
 
-                if (p.right == null) {p.right = cur; cur = cur.left;}
-                else {
-                    p.right = null;
-                    check(last, cur);
-                    last = cur;
-                    cur = cur.right;
+        while(cur != null || !stack.isEmpty()){
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            TreeNode node = stack.pop();
+
+            if(pre != null && pre.value > node.value){
+                second = node;
+                if(first == null){
+                    first = pre;
+                } else {
+                    break;
                 }
             }
+            pre = node; //更新pre节点
+
+            cur = node.right;
         }
-        if(first!= null && second != null){
+        //交换两个节点的值
+        if(first != null && second != null){
             int temp = first.value;
             first.value = second.value;
             second.value = temp;
-        }
-    }
-
-    void check(TreeNode last, TreeNode cur) {
-        if (last != null && last.value > cur.value) {
-            if (first == null) {first = last; second = cur;}
-            else second = cur;
         }
     }
 }
