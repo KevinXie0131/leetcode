b4203c634671cf0ede987e72f37acaf549fcd898
KevinX <Xzb04720638k>
2025-07-30 18:42:13 -0400

    update

--- src/main/java/com/answer/tree/Q114_Flatten_Binary_Tree_to_Linked_List.java
+++ src/main/java/com/answer/tree/Q114_Flatten_Binary_Tree_to_Linked_List.java
@@ -2,8 +2,7 @@ package com.answer.tree;
 
 import com.template.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Q114_Flatten_Binary_Tree_to_Linked_List {
     /**
@@ -69,12 +68,10 @@ public class Q114_Flatten_Binary_Tree_to_Linked_List {
      */
     public void flatten0(TreeNode root) {
         while (root != null) {
-            //左子树为 null，直接考虑下一个节点
-            if (root.left == null) {
+            if (root.left == null) {  //左子树为 null，直接考虑下一个节点
                 root = root.right;
             } else {
-                // 找左子树最右边的节点
-                TreeNode pre = root.left;
+                TreeNode pre = root.left; // 找左子树最右边的节点
                 while (pre.right != null) { // 首先我们需要找出左子树最右边的节点以便把右子树接过来
                     pre = pre.right;
                 }
@@ -89,13 +86,113 @@ public class Q114_Flatten_Binary_Tree_to_Linked_List {
         }
     }
     /**
+     * 先序遍历 深度优先
+     * 如果有左孩子有右孩子，就找一个结点指向左孩子的最右叶子结点，然后把这一串插入父结点和右孩子中间。
+     */
+    public void flatten5(TreeNode root) {
+        dfs1(root);
+    }
+
+    public void dfs1(TreeNode root) {
+        if (root == null) return;
+
+        if (root.left != null) {
+            TreeNode cur = root.left;
+            while (cur.right != null) {
+                cur = cur.right;
+            }
+            cur.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+        //  dfs(root.left); // can be commented
+        dfs1(root.right); //这时没有左树，只右递归
+    }
+    /**
+     * 递归
+     * 我们把 1 的右指针指向 2，那么 1 的原本的右孩子就丢失了，也就是 5 就找不到了。
+     * 解决方法的话，我们可以逆过来进行。我们依次遍历 6 5 4 3 2 1，然后每遍历一个节点就将当前节点的右指针更新为上一个节点。
+     * 遍历到 5，把 5 的右指针指向 6。6 <- 5 4 3 2 1。
+     * 遍历到 4，把 4 的右指针指向 5。6 <- 5 <- 4 3 2
+     *  6 5 4 3 2 1 的遍历顺序其实变形的后序遍历，遍历顺序是右子树->左子树->根节点。
+     */
+    private TreeNode pre = null;
+
+    public void flatten1(TreeNode root) {
+        if (root == null) return;
+
+        flatten1(root.right);
+        flatten1(root.left);
+        root.right = pre; // 利用一个全局变量 pre，更新当前根节点的右指针为 pre，左指针为 null。
+        root.left = null;
+        pre = root;
+    }
+    /**
+     * 迭代 (changed based on 后序遍历template)
+     */
+    public void flatten2(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode pre = null;
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root); // 添加根节点
+                root = root.right;// 递归添加右节点
+            }
+            TreeNode cur = stack.peek();  // 已经访问到最右的节点了
+            if (cur.left  == null || cur.left  == pre) {  // 在不存在左节点或者右节点已经访问过的情况下，访问根节点
+                stack.pop();
+                /**************修改的地方***************/
+                cur.right = pre;
+                cur.left = null;
+                /*************************************/
+                pre = cur;
+                root = null;// 当前结点下，没有要遍历的结点了
+            } else {
+                root = cur.left;  // 左节点还没有访问过就先访问左节点
+            }
+        }
+    }
+    /**
+     * 特殊的先序遍历
+     * 先序遍历的顺序是 1 2 3 4 5 6。
+     * 遍历到 2，把 1 的右指针指向 2。1 -> 2 3 4 5 6。
+     * 遍历到 3，把 2 的右指针指向 3。1 -> 2 -> 3 4 5 6。
+     * 因为我们用栈保存了右孩子，所以不需要担心右孩子丢失了。用一个 pre 变量保存上次遍历的节点。
+     */
+    public void flatten4(TreeNode root) {
+        if (root == null) return;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        TreeNode pre = null;
+
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            /***********修改的地方*************/
+            if(pre != null){
+                pre.right = temp;
+                pre.left = null;
+            }
+            /********************************/
+            if (temp.right != null){
+                stack.push(temp.right);
+            }
+            if (temp.left != null){
+                stack.push(temp.left);
+            }
+            /***********修改的地方*************/
+            pre = temp;
+            /********************************/
+        }
+    }
+    /**
      * 方法一：前序遍历
      * 将二叉树展开为单链表之后，单链表中的节点顺序即为二叉树的前序遍历访问各节点的顺序。因此，可以对二叉树进行前序遍历，获得各节点被访问到的顺序。
      * 由于将二叉树展开为链表之后会破坏二叉树的结构，因此在前序遍历结束之后更新每个节点的左右子节点的信息，将二叉树展开为单链表。
      */
     public void flatten(TreeNode root) {
         List<TreeNode> result = new ArrayList<>();
-        inorder(root, result);
+        preorderTraversal(root, result);
 
         TreeNode cur = root;
         for(int i = 1; i < result.size(); i++){
@@ -105,12 +202,12 @@ public class Q114_Flatten_Binary_Tree_to_Linked_List {
         }
     }
 
-    void inorder(TreeNode root, List<TreeNode> result) {
+    void preorderTraversal(TreeNode root, List<TreeNode> result) {
         if(root == null) {
             return;
         }
         result.add(root);      // 中
-        inorder(root.left, result);  // 左
-        inorder(root.right, result); // 右
+        preorderTraversal(root.left, result);  // 左
+        preorderTraversal(root.right, result); // 右
     }
 }
