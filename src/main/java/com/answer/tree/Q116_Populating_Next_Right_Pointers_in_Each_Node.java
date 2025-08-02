93d084aab49a8d6c5100ee0cecb625f372db75af
KevinX <Xzb04720638k>
2025-08-01 20:45:49 -0400

    uncommit

--- src/main/java/com/answer/tree/Q116_Populating_Next_Right_Pointers_in_Each_Node.java
+++ src/main/java/com/answer/tree/Q116_Populating_Next_Right_Pointers_in_Each_Node.java
@@ -1,6 +1,5 @@
 package com.answer.tree;
 
-
 import java.util.*;
 
 public class Q116_Populating_Next_Right_Pointers_in_Each_Node {
@@ -58,26 +57,77 @@ public class Q116_Populating_Next_Right_Pointers_in_Each_Node {
         trasverse(root.right); //右
     }
     /**
+     * 拉拉链解法
+     * 把完美二叉树劈成两半，先处理大的两半二叉树中间的左右连接，然后递归处理小的连接
+     */
+    public Node connect4(Node root) {
+        if (root == null) return root;
+        Node leftNode = root.left;
+        Node rightNode = root.right;
+        while(leftNode != null) {
+            leftNode.next = rightNode;
+            leftNode = leftNode.right;
+            rightNode = rightNode.left;
+        }
+        connect4(root.left);
+        connect4(root.right);
+        return root;
+    }
+    /**
      * 通过从根节点不断向左孩子遍历，来遍历每一层的头结点；而对单层内的节点 p 可以通过 next 指针遍历，然后进行以下操作
      * p->left->next = p->right;
      * p->right->next = p->next->left;
+     * 伪代码如下：
+     * leftmost = root
+     * while (leftmost.left != null) {
+     *     cur = leftmost
+     *     while (head.next != null) {
+     *         1) Establish Connection 1
+     *         2) Establish Connection 2 using next pointers
+     *         cur = cur.next
+     *     }
+     *     leftmost = leftmost.left
+     * }
      */
     public Node connect3(Node root) {
-        Node cur = root;
-        while (cur != null) {
-            Node p = cur;
-            while (p != null) {
-                if (p.left != null) {
-                    p.left.next = p.right;
+        Node leftmost  = root;
+        while (leftmost != null) {
+            Node cur = leftmost ;  // 遍历这一层节点组织成的链表，为下一层的节点更新 next 指针
+            while (cur != null) {
+                if (cur.left != null) {  // CONNECTION 1
+                    cur.left.next = cur.right;
                 }
-                if (p.right != null && p.next != null) {
-                    p.right.next = p.next.left;
+                if (cur.right != null && cur.next != null) {  // CONNECTION 2
+                    cur.right.next = cur.next.left;
                 }
-                p = p.next;
+                cur = cur.next;  // 指针向后移动
             }
-            cur = cur.left;
+            leftmost = leftmost.left;// 去下一层的最左的节点
         }
         return root;
+    }
+    /**
+     * DFS
+     */
+    private final List<Node> pre = new ArrayList<>();
+
+    public Node connect5(Node root) {
+        dfs(root, 0); // 根节点的深度为 0
+        return root;
+    }
+
+    private void dfs(Node node, int depth) {
+        if (node == null) {
+            return;
+        }
+        if (depth == pre.size()) { // node 是这一层最左边的节点
+            pre.add(node);
+        } else { // pre[depth] 是 node 左边的节点
+            pre.get(depth).next = node; // node 左边的节点指向 node
+            pre.set(depth, node);
+        }
+        dfs(node.left, depth + 1);
+        dfs(node.right, depth + 1);
     }
     /**
      * 本题依然是层序遍历，只不过在单层遍历的时候记录一下本层的头部节点，然后在遍历的时候让前一个节点指向本节点就可以了
@@ -93,15 +143,15 @@ public class Q116_Populating_Next_Right_Pointers_in_Each_Node {
             int size = queue.size();
 
             for (int i = 0; i < size; i++) {
-                Node cur = queue.poll();
+                Node cur = queue.poll(); // 从队首取出元素
                 // 遍历每一行的时候，如果不是最后一个Node，则指向下一个Node；如果是最后一个Node，则指向null
-                if (i < size - 1) {
+                if (i < size - 1) {   // 连接
                     cur.next = queue.peek(); //如果不是最后一个Node 则指向下一个Node
                 } else {
                     cur.next = null;  //如果是最后一个Node 则指向null (可省略)
                 }
-                if (cur.left != null) {queue.offer(cur.left);}
-                if (cur.right != null) {queue.offer(cur.right);}
+                if (cur.left != null) queue.offer(cur.left);    // 拓展下一层节点
+                if (cur.right != null) queue.offer(cur.right);
             }
         }
         return root;
@@ -131,8 +181,8 @@ public class Q116_Populating_Next_Right_Pointers_in_Each_Node {
                     nodePre.next = node; // 本层前一个节点next指向本节点
                     nodePre = nodePre.next;
                 }
-                if (node.left != null) {queue.offer(node.left);}
-                if (node.right != null) {queue.offer(node.right);}
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
             }
             nodePre.next = null;// 本层最后一个节点指向null (可省略)
         }
