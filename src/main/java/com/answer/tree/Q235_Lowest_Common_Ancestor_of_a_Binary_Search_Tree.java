d58129f7d84093f1c789508e42c3dbdfd9ff4637
KevinX <Xzb04720638k>
2025-08-03 15:37:42 -0400

    uncommit

--- src/main/java/com/answer/tree/Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
+++ src/main/java/com/answer/tree/Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -1,7 +1,6 @@
 package com.answer.tree;
 
 import com.template.TreeNode;
-
 import java.util.*;
 
 public class Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
@@ -20,32 +19,32 @@ public class Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
      * 普通⼆叉树求最近公共祖先需要使⽤回溯，从底向上来查找，⼆叉搜索树就不⽤了，因为搜索树有序（相当于⾃带⽅向），那么只要从上向下遍历就可以了
      */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if ((root.left == p && root.right == q) ||
-                (root.left == q && root.right == p)) {
+        if ((root.left == p && root.right == q) || (root.left == q && root.right == p)) {
             return root;
         }
 
-        while ((root.value - p.value) * (root.value - q.value) > 0)
+        while ((root.value - p.value) * (root.value - q.value) > 0) {
             root = p.value < root.value ? root.left : root.right;
-
+          //  root = q.value < root.value ? root.left : root.right; // works too
+        }
         return root;
     }
     /**
-     * 在遍历⼆叉搜索树的时候就是寻找区间[p->val, q->val]（注意这⾥是左闭又闭）
-     * 那么如果 cur->val ⼤于 p->val，同时 cur->val ⼤于q->val，那么就应该向左遍历（说明⽬标区间在左⼦树上）。
-     * 如果 cur->val ⼩于 p->val，同时 cur->val ⼩于 q->val，那么就应该向右遍历（⽬标区间在右⼦树）。
+     * 在遍历⼆叉搜索树的时候就是寻找区间[p.val, q.val]（注意这⾥是左闭又闭）
+     * 那么如果 cur.val ⼤于 p.val，同时 cur.val ⼤于q.val，那么就应该向左遍历（说明⽬标区间在左⼦树上）。
+     * 如果 cur.val ⼩于 p.val，同时 cur.val ⼩于 q.val，那么就应该向右遍历（⽬标区间在右⼦树）。
      */
     public TreeNode lowestCommonAncestor_0(TreeNode root, TreeNode p, TreeNode q) {
         if(root == null) return null; // 可省略 不存在遇到空的情况
 
         if (p.value < root.value && q.value < root.value) {
-            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode left = lowestCommonAncestor_0(root.left, p, q);
             if(left != null) {
                 return left;
             }
         }
         if (p.value > root.value && q.value > root.value) {
-            TreeNode right = lowestCommonAncestor(root.right, p, q);
+            TreeNode right = lowestCommonAncestor_0(root.right, p, q);
             if(right != null) {
                 return right;
             }
@@ -80,9 +79,9 @@ public class Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
         if (root == null) return null; // 可省略 不存在遇到空的情况
 
         if(root.value > p.value && root.value > q.value){
-            return lowestCommonAncestor(root.left, p,q);
+            return lowestCommonAncestor3(root.left, p,q);
         }else if(root.value < p.value && root.value < q.value){
-            return lowestCommonAncestor(root.right, p,q);
+            return lowestCommonAncestor3(root.right, p,q);
         }else{
             return root;
         }
@@ -110,8 +109,8 @@ public class Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
 
         if (root == p || root == q) return root;
 
-        TreeNode left = lowestCommonAncestor(root.left, p,  q);
-        TreeNode right = lowestCommonAncestor(root.right, p,  q);
+        TreeNode left = lowestCommonAncestor2(root.left, p,  q);
+        TreeNode right = lowestCommonAncestor2(root.right, p,  q);
 
         if (left == null && right == null) {
             return null;
@@ -128,37 +127,36 @@ public class Q235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
     /**
      * Q236题解 没有运用搜索树
      */
-    Map<Integer, TreeNode> parent = new HashMap<Integer, TreeNode>();
-    Set<Integer> visited = new HashSet<Integer>();
+    Map<Integer, TreeNode> parent = new HashMap<>();
+    Set<Integer> visited = new HashSet<>();
 
-    public void dfs(TreeNode root){
-        if(root == null){
-            return;
-        }
-        if(root.left!=null){
-            parent.put(root.left.value, root);
-            dfs(root.left);
-        }
-        if(root.right!=null){
-            parent.put(root.right.value, root);
-            dfs(root.right);
-        }
-
-    }
     public TreeNode lowestCommonAncestor4(TreeNode root, TreeNode p, TreeNode q) {
         dfs(root);
         while(p != null){
             visited.add(p.value);
-            p= parent.get(p.value);
+            p = parent.get(p.value);
         }
 
         while(q != null){
             if(visited.contains(q.value)){
                 return q;
             }
-            q= parent.get(q.value);
+            q = parent.get(q.value);
         }
-
         return null;
+    }
+
+    public void dfs(TreeNode root){
+        if(root == null){
+            return;
+        }
+        if(root.left != null){
+            parent.put(root.left.value, root);
+            dfs(root.left);
+        }
+        if(root.right != null){
+            parent.put(root.right.value, root);
+            dfs(root.right);
+        }
     }
 }
