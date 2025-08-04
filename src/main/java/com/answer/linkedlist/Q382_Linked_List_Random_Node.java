65bd8957d6c65169931c1cb68fc94cca1debb29b
KevinX <Xzb04720638k>
2025-08-04 19:17:38 -0400

    uncommit

--- src/main/java/com/answer/linkedlist/Q382_Linked_List_Random_Node.java
+++ src/main/java/com/answer/linkedlist/Q382_Linked_List_Random_Node.java
@@ -10,7 +10,6 @@ public class Q382_Linked_List_Random_Node {
      * 实现 Solution 类：
      *  Solution(ListNode head) 使用整数数组初始化对象。
      *  int getRandom() 从链表中随机选择一个节点并返回该节点的值。链表中所有节点被选中的概率相等。
-     *
      */
 }
 /**
