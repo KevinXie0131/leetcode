dcc29c9a829755afd19bf22e855e2ba719058679
KevinX <Xzb04720638k>
2025-08-12 15:20:42 -0400

    update

--- src/main/java/com/answer/math/Q719_Find_Kth_Smallest_Pair_Distance.java
+++ src/main/java/com/answer/math/Q719_Find_Kth_Smallest_Pair_Distance.java
@@ -23,9 +23,9 @@ public class Q719_Find_Kth_Smallest_Pair_Distance {
          * (3,1) -> 2
          * 距离第 1 小的数对是 (1,1) ，距离为 0 。
          */
-        int[] nums = {1,3,1};
-        int k = 1;
-        System.out.println(smallestDistancePair_0(nums, k));
+        int[] nums = {1,6,1};
+        int k = 3;
+        System.out.println(smallestDistancePair_5(nums, k));
     }
     /**
      * 二分查找 + 滑动窗口 + 排序
@@ -42,20 +42,19 @@ public class Q719_Find_Kth_Smallest_Pair_Distance {
         int left = 0;
         int right = nums[nums.length - 1] - nums[0];
 
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             int count = countPairs(nums, mid);
 
-            if (count < k) {
-                // 如果小于等于 mid 的个数严格小于 k 个，说明 mid 太小了
-                // 下一轮搜索区间为 [mid + 1..right]
-                left = mid + 1;
+            if (count < k) {  // 如果小于等于 mid 的个数严格小于 k 个，说明 mid 太小了
+                left = mid + 1;      // 下一轮搜索区间为 [mid + 1..right]
             } else {
-                // 下一轮搜索区间为 [left..mid]
-                right = mid;
+                // res = mid; // works too
+                right = mid - 1;// 下一轮搜索区间为 [left..mid]
             }
         }
         return left;
+        //  return right + 1; // works too
     }
     // Helper method to count the number of pairs with distance <= mid
     // 统计距离（数值之差的绝对值）小于等于 threshold 的个数
@@ -95,7 +94,32 @@ public class Q719_Find_Kth_Smallest_Pair_Distance {
         return nums[node.nei] - nums[node.root];
     }
     /**
-     * Binary Search
+     * 同上 PriorityQueue
+     * Time Limit Exceeded
+     * 19 / 20 testcases passed
+     */
+    public static int smallestDistancePair_5(int[] nums, int k) {
+        Arrays.sort(nums);
+        PriorityQueue<int[]> heap = new PriorityQueue<>((o1, o2) -> o1[0] - o2[0]);
+
+        for (int i = 0; i + 1 < nums.length; ++i) {
+            heap.offer(new int[]{nums[i + 1] - nums[i], i, i + 1});
+        }
+
+        k--;
+        int[] last = new int[]{0, 0, 0};
+        while(k >= 0){
+            last = heap.poll();
+            if (last[2] + 1 < nums.length) {
+                heap.offer(new int[]{nums[last[2] + 1] - nums[last[1]], last[1], last[2] + 1});
+            }
+            k--;
+        }
+        if(k > 0) last = heap.poll();
+        return last[0];
+    }
+    /**
+     * Binary Search 同上
      */
     public static int smallestDistancePair_1(int[] nums, int k) {
         Arrays.sort(nums);
