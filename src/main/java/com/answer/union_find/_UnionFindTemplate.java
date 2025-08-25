03b685bca02266355126600e5e62e530d32104c0
KevinX <Xzb04720638k>
2025-08-25 11:22:59 -0400

    update

--- src/main/java/com/answer/union_find/_UnionFindTemplate.java
+++ src/main/java/com/answer/union_find/_UnionFindTemplate.java
@@ -15,15 +15,21 @@ public class _UnionFindTemplate{
         return n == father[n] ? n : (father[n] = find(father[n])); // 路径压缩
 
         // 如果此时父节点指向自己，说明已经是根结点了
-  //      if (father[n] == n) return n;
-            // 否则，根据父节点继续往上找，直到找到根节点
-   //     else return find(father[x]);
+  //      if (father[n] == n){
+  //         return n;
+  //      }
+        // 否则，根据父节点继续往上找，直到找到根节点
+  //      else{
+  //          return find(father[x]);
+  //      }
     }
 
     public void join (int n, int m) {
         n = find(n);
         m = find(m);
-        if (n == m) return;
+        if (n == m) {
+            return;
+        }
         father[m] = n; // 找到根节点后，x根做y根的子树，y根做x根的子树都可以
     }
 
