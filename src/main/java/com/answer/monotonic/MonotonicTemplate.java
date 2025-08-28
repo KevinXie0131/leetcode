f20130cc0ce52cfe0bbbc6847eb77e86e2aea259
KevinX <Xzb04720638k>
2025-08-27 21:31:57 -0400

    update

--- src/main/java/com/answer/monotonic/MonotonicTemplate.java
+++ src/main/java/com/answer/monotonic/MonotonicTemplate.java
@@ -1,4 +1,25 @@
 package com.answer.monotonic;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class MonotonicTemplate {
+    /**
+     * 单调栈
+     *  如果求一个元素右边第一个更大元素，单调栈就是递增的(从栈顶到栈底的顺序，即栈顶的元素比栈底的元素小)，
+     *  如果求一个元素右边第一个更小元素，单调栈就是递减的(从栈顶到栈底的顺序，即栈顶的元素比栈底的元素大)。
+     */
+    public int[] dailyTemperatures1(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Deque<Integer> stack = new LinkedList<>();
+
+        for(int i = 0; i < temperatures.length; i++){
+            while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]){
+                result[stack.peek()] = i- stack.peek();
+                stack.pop();
+            }
+            stack.push(i); //  注意，单调栈里 加入的元素是 下标。
+        }
+        return result;
+    }
 }
