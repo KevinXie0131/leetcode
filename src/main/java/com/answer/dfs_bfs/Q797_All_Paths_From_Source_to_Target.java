285e6d4a9f8c40b9eb45eef5e840b69671f2505a
KevinX <Xzb04720638k>
2025-08-29 23:04:50 -0400

    update

--- src/main/java/com/answer/dfs_bfs/Q797_All_Paths_From_Source_to_Target.java
+++ src/main/java/com/answer/dfs_bfs/Q797_All_Paths_From_Source_to_Target.java
@@ -9,10 +9,10 @@ public class Q797_All_Paths_From_Source_to_Target {
      * 所有可能的路径
      * 一个有 n 个节点的 有向无环图（DAG），请你找出从节点 0 到节点 n-1 的所有路径并输出（不要求按特定顺序）
      * graph[i] 是一个从节点 i 可以访问的所有节点的列表（即从节点 i 到节点 graph[i][j]存在一条有向边）。
-     *
-     * 输入：graph = [[1,2],[3],[3],[]]
-     * 输出：[[0,1,3],[0,2,3]]
-     * 解释：有两条路径 0 -> 1 -> 3 和 0 -> 2 -> 3
+     * 示例：
+     *   输入：graph = [[1,2],[3],[3],[]]
+     *   输出：[[0,1,3],[0,2,3]]
+     *   解释：有两条路径 0 -> 1 -> 3 和 0 -> 2 -> 3
      */
     /**
      * 深搜三部曲
@@ -49,10 +49,10 @@ public class Q797_All_Paths_From_Source_to_Target {
     }
     /**
      * 本题中的图为有向无环图DAG, 搜索过程中不会反复遍历同一个点, 因此我们无需判断当前点是否遍历过
-     * Approach 1: Depth-First Search (DFS)  - Backtracking
+     * Approach 1: Depth-First Search (DFS) - Backtracking
      */
     List<List<Integer>> res= new ArrayList<>(); // 收集符合条件的路径
-    Deque<Integer> stack = new ArrayDeque<>(); // 1节点到终点的路径
+    Deque<Integer> stack = new ArrayDeque<>(); // 节点1到终点的路径
     // 深度优先搜索 + 栈
     // 特别地，因为本题中的图为有向无环图（DAG），搜索过程中不会反复遍历同一个点，因此我们无需判断当前点是否遍历过。
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
@@ -91,15 +91,16 @@ public class Q797_All_Paths_From_Source_to_Target {
     //深度搜索，第一个参数是要遍历的图，第二参数是当前节点编号
     public void dfs1(int[][] graph, int x, int n,  List<List<Integer>> result, List<Integer> path ) {
         if(x == n){ //如果遍历到最后一个节点，则停止遍历
-            // 使用new ArrayList<>(path)创建path的一个副本，以避免后续对path的修改影响已经添加的结果
-            result.add(new ArrayList<>(path));    //达到目标节点，保存此条路径并结束搜索
+            // 使用new LinkedList<>(path)创建path的一个副本，以避免后续对path的修改影响已经添加的结果
+            result.add(new LinkedList<>(path));    //达到目标节点，保存此条路径并结束搜索
             return;
         }
         //遍历当前节点所有关联的节点
         for(int y : graph[x]){
             path.add(y);      //将当前节点保存在本次搜索路径中
             dfs1(graph, y , graph.length - 1, result, path); //继续遍历与当前节点关联的节点
-            path.remove(path.size() - 1);  //回溯(ArrayList的remove()方法传入整数会被作为下标，使用path.size()-1可以严格控制删除最后一个元素)
+            path.remove(path.size() - 1);  //回溯(LinkedList的remove()方法传入整数会被作为下标，使用path.size()-1可以严格控制删除最后一个元素)
+         //   path.removeLast(); // for ArrayDeque
         }
     }
     /**
