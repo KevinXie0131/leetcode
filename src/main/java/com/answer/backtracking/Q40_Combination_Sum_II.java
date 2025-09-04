1c20e4d2e0b1b36374169608ec2b144f4a5bb7d7
KevinX <Xzb04720638k>
2025-09-04 19:06:02 -0400

    update

--- src/main/java/com/answer/backtracking/Q40_Combination_Sum_II.java
+++ src/main/java/com/answer/backtracking/Q40_Combination_Sum_II.java
@@ -9,8 +9,7 @@ public class Q40_Combination_Sum_II {
      * 注意：解集不能包含重复的组合。
      * 示例 1:
      *  输入: candidates = [10,1,2,7,6,1,5], target = 8,
-     *  输出:
-     *       [
+     *  输出:[
      *      [1,1,6],
      *      [1,2,5],
      *      [1,7],
@@ -47,25 +46,24 @@ public class Q40_Combination_Sum_II {
      * 本题的难点在于区别2中：集合（数组candidates）有重复元素，但还不能有重复的组合。(我把所有组合求出来，再用set或者map去重，这么做很容易超时！)
      */
     public List<List<Integer>> combinationSum2(int[] candidates, int target)    {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<List<Integer>> result = new ArrayList<>();
         Deque<Integer> path = new ArrayDeque<>();
         int[] used = new int[candidates.length]; // 加⼀个bool型数组used，⽤来记录同⼀树枝上的元素是否使⽤过
                                                  // 这个集合去重的重任就是used来完成的
-        // ⾸先把给candidates排序，让其相同的元素都挨在⼀起
-        Arrays.sort(candidates);
+        Arrays.sort(candidates); // ⾸先把给candidates排序，让其相同的元素都挨在⼀起
         backtracking(candidates, used, target, 0, result, path);
         return result;
     }
     // 使用标记数组
-    public void backtracking(int[] candidates, int[] used, int target, int startIndex, List<List<Integer>> result, Deque<Integer> path ){
+    public void backtracking(int[] candidates, int[] used, int target, int startIndex, List<List<Integer>> result, Deque<Integer> path){
         if (0 == target) {
-            result.add(new ArrayList<Integer>(path));
+            result.add(new ArrayList<>(path));
             return;
         }
         if(target < 0){
             return;
         }
-  /*      for(int i = startIndex; i < candidates.length && target >= candidates[i] ; i++){*/ //剪枝操作
+  /*    for(int i = startIndex; i < candidates.length && target >= candidates[i] ; i++){*/ //剪枝操作
         for(int i = startIndex; i < candidates.length; i++){
             if(target < candidates[i]){ // 剪枝操作
                 break;
@@ -74,15 +72,15 @@ public class Q40_Combination_Sum_II {
              * 去重
              * 要去重的是“同⼀树层上的使⽤过”，如果判断同⼀树层上元素（相同的元素）是否使⽤过了呢
              *
-             * 如果candidates[i] == candidates[i - 1] 并且 used[i - 1] == false，就说明：前
-             * ⼀个树枝，使⽤了candidates[i - 1]，也就是说同⼀树层使⽤过candidates[i - 1]
+             * 如果candidates[i] == candidates[i - 1] 并且 used[i - 1] == false，
+             * 就说明：前⼀个树枝，使⽤了candidates[i - 1]，也就是说同⼀树层使⽤过candidates[i - 1]
              *
              * 在candidates[i] == candidates[i - 1]相同的情况下：
              *      used[i - 1] == true，说明同⼀树⽀candidates[i - 1]使⽤过
              *      used[i - 1] == false，说明同⼀树层candidates[i - 1]使⽤过
              * 要对同⼀树层使⽤过的元素进⾏跳过
              */
-            if(i > 0 && candidates[i-1] == candidates[i] && used[i-1] == 0){
+            if(i > 0 && candidates[i - 1] == candidates[i] && used[i - 1] == 0){
                 continue;
             }
             path.addLast(candidates[i]);
@@ -106,15 +104,14 @@ public class Q40_Combination_Sum_II {
     static int sum = 0;
 
     static public List<List<Integer>> combinationSum2_1( int[] candidates, int target ) {
-        //为了将重复的数字都放到一起，所以先进行排序
-        Arrays.sort( candidates );
+        Arrays.sort( candidates ); //为了将重复的数字都放到一起，所以先进行排序
         backTracking1( candidates, target, 0 );
         return res;
     }
 
     static private void backTracking1( int[] candidates, int target, int start ) {
         if ( sum == target ) {
-            res.add( new ArrayList<>( path ) );
+            res.add(new ArrayList<>(path));
             return;
         }
         for ( int i = start; i < candidates.length && sum + candidates[i] <= target; i++ ) {
@@ -130,13 +127,48 @@ public class Q40_Combination_Sum_II {
             }
 
             sum += candidates[i];
-            path.add( candidates[i] );
-            // i+1 代表当前组内元素只选取一次
-            backTracking1( candidates, target, i + 1 );
+            path.add(candidates[i]);
+
+            backTracking1( candidates, target, i + 1 ); // i+1 代表当前组内元素只选取一次
 
             int temp = path.getLast();
             sum -= temp;
             path.removeLast();
         }
+    }
+    /**
+     * another form 同上
+     */
+    public List<List<Integer>> combinationSum2b(int[] candidates, int target)    {
+        List<List<Integer>> result = new ArrayList<>();
+        Deque<Integer> path = new ArrayDeque<>();
+        Arrays.sort(candidates);
+        backtracking2(candidates, target, 0, result, path);
+        return result;
+    }
+
+    public void backtracking2(int[] candidates,  int target, int startIndex, List<List<Integer>> result, Deque<Integer> path){
+        if (0 == target) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        if(target < 0){
+            return;
+        }
+    //  for(int i = startIndex; i < candidates.length && target >= candidates[i] ; i++){ //剪枝操作
+        for(int i = startIndex; i < candidates.length; i++){
+            if(target < candidates[i]){ // 剪枝操作
+                break;
+            }
+            if(i > startIndex && candidates[i - 1] == candidates[i] ){
+                continue;
+            }
+            path.addLast(candidates[i]);
+            target -= candidates[i];
+            backtracking2(candidates, target,i + 1, result, path); // 每个数组元素只能被选择一次, 设定下一轮从索引 i+1 开始向后遍历
+            path.removeLast();
+            target += candidates[i];
+        }
+        return;
     }
 }
