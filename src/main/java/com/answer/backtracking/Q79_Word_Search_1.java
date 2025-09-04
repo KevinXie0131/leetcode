d2c655a6fd6f20784f22823ccefd00ae56baa9fc
KevinX <Xzb04720638k>
2025-09-04 19:01:02 -0400

    update

--- src/main/java/com/answer/backtracking/Q79_Word_Search_1.java
+++ src/main/java/com/answer/backtracking/Q79_Word_Search_1.java
@@ -1,7 +1,10 @@
 package com.answer.backtracking;
 
 public class Q79_Word_Search_1 {
-
+    /**
+     * DFS + 回溯
+     * another form
+     */
     public boolean exist(char[][] board, String word) {
         int h = board.length, w = board[0].length;
         boolean[][] visited = new boolean[h][w];
@@ -16,20 +19,21 @@ public class Q79_Word_Search_1 {
         return false;
     }
 
-    public boolean check(char[][] board, boolean[][] visited, int i, int j, String s, int k) {
-        if (board[i][j] != s.charAt(k)) {
+    public boolean check(char[][] board, boolean[][] visited, int i, int j, String s, int index) {
+        if (board[i][j] != s.charAt(index)) {
             return false;
-        } else if (k == s.length() - 1) {
+        } else if (index == s.length() - 1) {
             return true;
         }
         visited[i][j] = true;
         int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         boolean result = false;
+
         for (int[] dir : directions) {
             int newi = i + dir[0], newj = j + dir[1];
             if (newi >= 0 && newi < board.length && newj >= 0 && newj < board[0].length) {
                 if (!visited[newi][newj]) {
-                    boolean flag = check(board, visited, newi, newj, s, k + 1);
+                    boolean flag = check(board, visited, newi, newj, s, index + 1);
                     if (flag) {
                         result = true;
                         break;
--- src/main/java/com/answer/backtracking/Q79_Word_Search_2.java
+++ src/main/java/com/answer/backtracking/Q79_Word_Search_2.java
@@ -1,28 +1,34 @@
 package com.answer.backtracking;
 
 public class Q79_Word_Search_2 {
+
     public static void main(String[] args) {
         char[][] board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
         String word = "ABCCED";
         exist(board, word);
     }
-
+    /**
+     * DFS + 回溯
+     * another form
+     */
     static int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     static public boolean exist(char[][] board, String word) {
         int h = board.length, w = board[0].length;
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
-                if (check(board, i, j, word, 0)) return true;
+                if (check(board, i, j, word, 0)) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    static public boolean check(char[][] board, int i, int j, String s, int k) {
-        if (board[i][j] != s.charAt(k)) { // 剪枝
+    static public boolean check(char[][] board, int i, int j, String s, int index) {
+        if (board[i][j] != s.charAt(index)) { // 剪枝
             return false;
-        } else if (k == s.length() - 1) { // board[i][j] == s.charAt(k) && k is equal to s.length() - 1
+        } else if (index == s.length() - 1) { // board[i][j] == s.charAt(k) && k is equal to s.length() - 1
             return true;
         }
 
@@ -33,7 +39,9 @@ public class Q79_Word_Search_2 {
             int newi = i + dir[0], newj = j + dir[1];
             if (newi >= 0 && newi < board.length && newj >= 0 && newj < board[0].length) {
                 if (board[newi][newj] != '.') {
-                    if (check(board, newi, newj, s, k + 1)) return true;
+                    if (check(board, newi, newj, s, index + 1)) {
+                        return true;
+                    }
                 }
             }
         }
@@ -57,9 +65,9 @@ public class Q79_Word_Search_2 {
         board[i][j] = '.'; // not use visited[][] to reduce space complexity 直接修改, 将其置为空
 
         boolean res = check_1(board, i - 1, j, s, k + 1)
-                || check_1(board, i, j - 1, s, k + 1)
-                || check_1(board, i + 1, j, s, k + 1)
-                || check_1(board, i,  j + 1, s, k + 1);
+                   || check_1(board, i, j - 1, s, k + 1)
+                   || check_1(board, i + 1, j, s, k + 1)
+                   || check_1(board, i,  j + 1, s, k + 1);
 
         board[i][j] = t; // backtracking 恢复成原来的值（恢复现场）
         return res;
