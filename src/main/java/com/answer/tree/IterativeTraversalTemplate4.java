3e9a0faa49e8e8576570a31ad6b2a1a3568108a2
KevinX <Xzb04720638k>
2025-09-12 20:51:02 -0400

    update

--- src/main/java/com/answer/tree/IterativeTraversalTemplate4.java
+++ src/main/java/com/answer/tree/IterativeTraversalTemplate4.java
@@ -41,4 +41,25 @@ public class IterativeTraversalTemplate4 {
         }
         return null;
     }
+    /**
+     * 相同的树
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true; // 两个节点均为空
+        if (p == null || q == null) return false; // 一个节点为空 一个节点不为空
+        return p.value == q.value && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);  // 两个节点均不为空
+    }
+    /**
+     * 二叉树的最大深度
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0; // 确定终⽌条件：如果为空节点的话，就返回0，表⽰⾼度为0
+        }
+        // 确定单层递归的逻辑：先求它的左⼦树的深度，再求的右⼦树的深度，最后取左右深度最⼤的数值 再+1
+        //                  （加1是因为算上当前中间节点）就是⽬前节点为根节点的树的深度
+        int left = maxDepth(root.left);    // 左
+        int right = maxDepth(root.right);  // 右
+        return Math.max(left, right) + 1;  // 中 用后序遍历（左右中）来计算树的高度 / 二叉树的最大深度为左右子树的最大深度加1得到。
+    }
 }
