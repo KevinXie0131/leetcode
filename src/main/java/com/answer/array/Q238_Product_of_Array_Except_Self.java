6f15a1bfed5958ed013b662ab99c083ae08a2afa
KevinX <Xzb04720638k>
2025-09-19 14:48:34 -0400

    update

---
+++ src/main/java/com/answer/array/Q238_Product_of_Array_Except_Self.java
@@ -0,0 +1,25 @@
+package com.answer.array;
+
+public class Q238_Product_of_Array_Except_Self {
+    /**
+     * 除自身以外数组的乘积
+     * 一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+     * 题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+     * 请 不要使用除法，且在 O(n) 时间复杂度内完成此题。
+     * Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+     * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+     * You must write an algorithm that runs in O(n) time and without using the division operation.
+     *
+     * 示例 1: 输入: nums = [1,2,3,4]
+     *         输出: [24,12,8,6]
+     * 示例 2: 输入: nums = [-1,1,0,-3,3]
+     *         输出: [0,0,9,0,0]
+     *
+     * 提示：2 <= nums.length <= 105
+     *      -30 <= nums[i] <= 30
+     *      输入 保证 数组 answer[i] 在  32 位 整数范围内
+     */
+    public int[] productExceptSelf(int[] nums) {
+        return null;
+    }
+}
