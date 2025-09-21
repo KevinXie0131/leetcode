3af8976ce7bcbae2860146809bf3d0d54e58c1f6
KevinX <Xzb04720638k>
2025-09-20 21:56:45 -0400

    update

--- src/main/java/com/answer/array/Q238_Product_of_Array_Except_Self.java
+++ src/main/java/com/answer/array/Q238_Product_of_Array_Except_Self.java
@@ -19,7 +19,32 @@ public class Q238_Product_of_Array_Except_Self {
      *      -30 <= nums[i] <= 30
      *      输入 保证 数组 answer[i] 在  32 位 整数范围内
      */
+    /**
+     * 左右乘积列表
+     * 不必将所有数字的乘积除以给定索引处的数字得到相应的答案，而是利用索引左侧所有数字的乘积和右侧所有数字的乘积（即前缀与后缀）相乘得到答案。
+     */
     public int[] productExceptSelf(int[] nums) {
-        return null;
+        int length = nums.length;
+        // L 和 R 分别表示左右两侧的乘积列表
+        int[] L = new int[length];
+        int[] R = new int[length];
+        int[] answer = new int[length];
+        // L[i] 为索引 i 左侧所有元素的乘积
+        // 对于索引为 '0' 的元素，因为左侧没有元素，所以 L[0] = 1
+        L[0] = 1;
+        for (int i = 1; i < length; i++) {
+            L[i] = nums[i - 1] * L[i - 1];
+        }
+        // R[i] 为索引 i 右侧所有元素的乘积
+        // 对于索引为 'length-1' 的元素，因为右侧没有元素，所以 R[length-1] = 1
+        R[length - 1] = 1;
+        for (int i = length - 2; i >= 0; i--) {
+            R[i] = nums[i + 1] * R[i + 1];
+        }
+        // 对于索引 i，除 nums[i] 之外其余各元素的乘积就是左侧所有元素的乘积乘以右侧所有元素的乘积
+        for (int i = 0; i < length; i++) {
+            answer[i] = L[i] * R[i];
+        }
+        return answer;
     }
 }
